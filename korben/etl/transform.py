75e50eff9e1306074c8d5660443fb7470429b057
bmcorser <bmcorser@gmail.com>
2016-09-07 07:34:35 +0100

    Peel some code out of etl.server module

--- korben/etl/transform.py
+++ korben/etl/transform.py
@@ -1,12 +1,12 @@
 import sqlalchemy as sqla
 from . import spec
 
-def to_leeloo(metadata, cdms_tablename, cdms_dict):
+def from_cdms_psql(metadata, cdms_tablename, cdms_dict):
     'Transform a CDMS row into a row suitable for insertion in to Leeloo'
     out_dict = {}
     mapping = spec.MAPPINGS[cdms_tablename]
     for cdms_col, leeloo_col in mapping.get('local', []):
-        if cdms_col:  # TODO: Should these appear in mappings?
+        if cdms_col:  # TODO: Should these even appear in mappings?
             out_dict[leeloo_col] = cdms_dict[cdms_col]
     for (cdms_tablename, cdms_col), leeloo_col in mapping.get('foreign', []):
         table = metadata.tables[cdms_tablename]
