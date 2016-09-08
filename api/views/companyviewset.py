6144f858ed0100bc4ff14fcc452a1dcd94d2542e
Zac Tolley <zac@thetolleys.com>
2016-09-08 14:43:17 +0100

    Moving ES-related code from leeloo to korben

--- api/views/companyviewset.py
+++ api/views/companyviewset.py
@@ -4,17 +4,8 @@ from api.models.chcompany import CHCompany
 from api.models.company import Company
 from api.serializers import CHCompanySerializer
 from api.serializers import CompanySerializer
-from api.services.searchservice import delete_for_company_number, delete_for_source_id, search_item_from_company
+# from korben.client import esclient
 
-
-def check_ch_data(request):
-    # look at the incoming data. Is there a company number?
-    # if so then lookup some CH data and add it here before turning it
-    # into a company.
-    if request.data['company_number'] and len(request.data['company_number']) > 0:
-        ch = CHCompany.objects.get(pk=request.data['company_number'])
-        request.data['registered_name'] = ch.company_name
-        request.data['business_type'] = ch.company_category
 
 
 class CompanyViewSet(viewsets.ModelViewSet):
@@ -22,19 +13,14 @@ class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanySerializer
 
     def create(self, request, **kwargs):
-        check_ch_data(request)
+        # eslient.check_ch_data(request)
 
         # Create a company, validate and save
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         new_company = serializer.save()
 
-        # Delete the existing index entry and create a new one.
-        if request.data['company_number'] and len(request.data['company_number']) > 0:
-            delete_for_company_number(new_company.company_number)
-
-        search_item = search_item_from_company(new_company)
-        search_item.save()
+        # esclient.create(new_company)
 
         # send back the newly company record and inform the user if all is well.
         response_serializer = self.get_serializer(new_company)
@@ -42,15 +28,13 @@ class CompanyViewSet(viewsets.ModelViewSet):
         return Response(response_serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
     def update(self, request, *args, **kwargs):
-        check_ch_data(request)
+        # esclient.check_ch_data(request)
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
         serializer = self.get_serializer(instance, data=request.data, partial=partial)
         serializer.is_valid(raise_exception=True)
         company = serializer.save()
-        delete_for_source_id(company.id)
-        search_item = search_item_from_company(company)
-        search_item.save()
+        # esclient.create(new_company)
 
         return Response(serializer.data)
 
