6a104ed78925f068f016f1eaa4417bed250000e7
bmcorser <bmcorser@gmail.com>
2016-09-14 07:25:38 +0000

    Move DB and ES connections out to services module

--- korben/etl/main.py
+++ korben/etl/main.py
@@ -1,13 +1,13 @@
 import functools
 from .. import config
-from .. import db
+from .. import services
 from . import spec, extract, transform, load
 
 
 def from_cdms_psql(entity_name, guids):
     cdms_tablename = entity_name + 'Set'
     mapping = spec.MAPPINGS[cdms_tablename]
-    metadata = db.poll_for_metadata(config.database_odata_url)
+    metadata = services.db.poll_for_metadata(config.database_odata_url)
     result = extract.from_cdms_psql(metadata, cdms_tablename, guids)
     transform_func = functools.partial(
         transform.from_cdms_psql, cdms_tablename
