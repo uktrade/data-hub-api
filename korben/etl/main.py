216400b9d5db5e4469e10c8593b5120631d595ad
bmcorser <bmcorser@gmail.com>
2016-09-15 07:52:45 +0000

    Django initial load, drop more search-related code

--- korben/etl/main.py
+++ korben/etl/main.py
@@ -3,10 +3,12 @@ from korben import config, services
 from . import spec, extract, transform, load
 
 
-def from_cdms_psql(table, guids):
+def from_cdms_psql(table, guids, idempotent=True):
     mapping = spec.MAPPINGS[table.name]
     result = extract.from_cdms_psql(table, guids)
     transform_func = functools.partial(transform.from_cdms_psql, table)
     django_metadata = services.db.poll_for_metadata(config.database_url)
     django_table = django_metadata.tables[mapping['to']]
+    if not idempotent:
+        return load.to_leeloo(django_table, map(transform_func, result))
     return load.to_leeloo_idempotent(django_table, map(transform_func, result))
