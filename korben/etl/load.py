cf77cd977d2bfe6ce8ca66301aea00830ddeb78e
bmcorser <bmcorser@gmail.com>
2016-09-20 20:58:15 +0100

    Add some whacky code to record integrity errors

--- korben/etl/load.py
+++ korben/etl/load.py
@@ -1,24 +1,38 @@
 import datetime
+import itertools
 import logging
+import os
+import re
 
 from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy import exc as sqla_exc
+import psycopg2
 
 from korben import config
 from .. import services
 
 LOGGER = logging.getLogger('korben.etl.load')
+INTEGRITY_DETAILS = re.compile('(\(.+\))=\((?P<pkey>.+)\).+"(?P<table>.+)"')
 
 
 def to_sqla_table(table, data):
     'Load data into an SQLA table'
-    return table.metadata.bind.execute(table.insert().values(list(data)))
+    results = []
+    for chunk in itertools.zip_longest(*[iter(data)] * 5000):
+        results.append(
+            table.metadata.bind.execute(
+                table.insert().values(list(filter(None, chunk)))
+            )
+        )
+    return results
 
 
 def to_sqla_table_idempotent(table, data):
-    'Idempotently load data into an SQLA table, ignore fkey failures'
-    primary_key = next(
-        col.name for col in table.primary_key.columns.values()
-    )
+    '''
+    Idempotently load data into an SQLA table, temporarily write out details on
+    integrity errors to a file
+    '''
+    primary_key = next(col.name for col in table.primary_key.columns.values())
     results = []
     for row in data:
         upsert = insert(table)\
@@ -26,12 +40,22 @@ def to_sqla_table_idempotent(table, data):
             .on_conflict_do_update(index_elements=[primary_key], set_=row)
         try:
             results.append(table.metadata.bind.execute(upsert))
+        except sqla_exc.IntegrityError as exc:
+            parsed = re.search(INTEGRITY_DETAILS, str(exc))
+            with open('cache/fails', 'a') as fails_fh:
+                fails_fh.write(
+                    "{0}, {1}\n".format(
+                        parsed.group('pkey'), parsed.group('table')
+                    )
+                )
+        '''
         except Exception as exc:
             LOGGER.error(exc)
             LOGGER.error("{0} {1} ({2}) failed on something".format(
                 datetime.datetime.now(), table.name, row[primary_key]
             ))
             pass
+        '''
     return results
 
 
