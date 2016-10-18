5aeb44c7af8d763da7af314c86cb98dac78ade41
bmcorser <bmcorser@gmail.com>
2016-10-18 13:35:13 +0100

    `korben sync django` fills integrity holes
     - Add "Undefined" constant to Django fixtures, Korben
     - Add to specs to use undefined const

--- korben/etl/load.py
+++ korben/etl/load.py
@@ -1,3 +1,4 @@
+import collections
 import datetime
 import itertools
 import logging
@@ -33,30 +34,25 @@ def to_sqla_table_idempotent(table, data):
     '''
     primary_key = etl.utils.primary_key(table)
     results = []
+    missing = collections.defaultdict(set)
     for row in data:
         upsert = insert(table)\
             .values(**row)\
             .on_conflict_do_update(index_elements=[primary_key], set_=row)
-        results.append(table.metadata.bind.execute(upsert))
-        '''
         try:
             results.append(table.metadata.bind.execute(upsert))
         except sqla_exc.IntegrityError as exc:
             parsed = re.search(INTEGRITY_DETAILS, str(exc))
-            with open('cache/fails', 'a') as fails_fh:
-                fails_fh.write(
-                    "{0}, {1}\n".format(
-                        parsed.group('pkey'), parsed.group('table')
-                    )
-                )
-        except Exception as exc:
+            if parsed:
+                missing[table.name].add(row[primary_key])
+                missing[parsed.group('table')].add(parsed.group('pkey'))
+                continue
             LOGGER.error(exc)
-            LOGGER.error("{0} {1} ({2}) failed on something".format(
+            LOGGER.error(
+                '%s %s (%s) failed on something',
                 datetime.datetime.now(), table.name, row[primary_key]
-            ))
-            pass
-        '''
-    return results
+            )
+    return results, missing
 
 
 def from_ch(data):
