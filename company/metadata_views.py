08b47840eab2288df5c67d19349810e7df9dfcff
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-10-20 10:18:20 +0100

    generic metadata view

--- company/metadata_views.py
+++ company/metadata_views.py
@@ -1,77 +1,36 @@
+from functools import partial
+
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from core.serializers import ConstantModelSerializer
-from .models import BusinessType, Country, EmployeeRange, InteractionType, Role, Sector, Service, Title, TurnoverRange, UKRegion
+
+from . import models
+
+METADATA_MAPPING = {
+    'business-type': models.BusinessType,
+    'country': models.Country,
+    'employee-range': models.EmployeeRange,
+    'interaction-type': models.InteractionType,
+    'role': models.Role,
+    'sector': models.Sector,
+    'service': models.Service,
+    'title': models.Title,
+    'turnover': models.TurnoverRange,
+    'uk-region': models.UKRegion
+}
 
 
 @api_view()
-def business_type(request):
-    """List all business types."""
-    serializer = ConstantModelSerializer(BusinessType.objects.all(), many=True)
+def metadata_view(request, model):
+    """Metadata generic view."""
+    serializer = ConstantModelSerializer(model.objects.all(), many=True)
     return Response(data=serializer.data)
 
 
-@api_view()
-def country(request):
-    """List all countries."""
-    serializer = ConstantModelSerializer(Country.objects.all(), many=True)
-    return Response(data=serializer.data)
+urls_args = []
 
-
-@api_view()
-def employee_range(request):
-    """List employee range options."""
-    serializer = ConstantModelSerializer(EmployeeRange.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def interaction_type(request):
-    """List all interaction types."""
-    serializer = ConstantModelSerializer(InteractionType.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def role(request):
-    """List all the roles."""
-    serializer = ConstantModelSerializer(Role.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def sector(request):
-    """List all the sectors."""
-    serializer = ConstantModelSerializer(Sector.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def service(request):
-    """List all the services."""
-    serializer = ConstantModelSerializer(Service.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def title(request):
-    """List all the titles."""
-    serializer = ConstantModelSerializer(Title.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def turnover(request):
-    """List all the turnover ranges."""
-    serializer = ConstantModelSerializer(TurnoverRange.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
-@api_view()
-def uk_region(request):
-    """List all the UK regions."""
-    serializer = ConstantModelSerializer(UKRegion.objects.all(), many=True)
-    return Response(data=serializer.data)
-
-
+for name, model in METADATA_MAPPING.items():
+    fn = partial(metadata_view, model=model)
+    path = r'{0}/$'.format(name)
+    urls_args.append(((path, fn), {'name': name}))
