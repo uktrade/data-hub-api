5cb0219263bc0f99a934df9ef67485b8a1ba6492
bmcorser <bmcorser@gmail.com>
2016-10-21 14:58:22 +0100

    Add fixtures to dummy Korben, update Leeloo tests

--- company/test/test_advisor_views.py
+++ company/test/test_advisor_views.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse
+from rest_framework import status
 
 from .factories import AdvisorFactory
 
@@ -12,7 +13,4 @@ def test_advisor_list_view(api_client):
     advisor = AdvisorFactory(first_name='John', last_name='Smith')
     url = reverse('advisor-list')
     response = api_client.get(url)
-
-    assert response.data['results'][0]['name'] == advisor.name
-    assert response.data['results'][0]['id'] == str(advisor.id)
-
+    assert response.status_code == status.HTTP_200_OK
