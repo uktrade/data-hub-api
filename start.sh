9f4ad8d2033dc17f00acd61f61d1f26f13e3a1b2
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-10-24 14:57:58 +0100

    added local docker compose for FE

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 python /app/leeloo/manage.py migrate
 python /app/leeloo/manage.py createinitialrevisions
+python /app/leeloo/manage.py loaddata metadata
 python /app/leeloo/manage.py runserver_plus 0.0.0.0:8000
