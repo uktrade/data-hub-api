86ead7a2fd2fda259d0112898b53597a3ee64a5c
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-11-01 16:17:38 +0000

    no input

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
-python /app/leeloo/manage.py migrate --noinput
-python /app/leeloo/manage.py collectstatic
+python /app/leeloo/manage.py migrate
+python /app/leeloo/manage.py collectstatic --noinput
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/metadata.yaml
 gunicorn datahubapi.wsgi --bind=0.0.0.0:8000
