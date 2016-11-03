5937721417939e5d9a20425a4c0510c4fdb67ab3
bmcorser <bmcorser@gmail.com>
2016-11-03 12:00:00 +0000

    Sentry issue /dit/data-hub-leeloo/issues/160/
     - Split off the Undefined enum and the extra business_type fixtures
     - Add to leeloo/start.sh so the above are loaded on leeloo startup
     - Function (and CLI for it) to load arbitrary Django fixtures into CDMS
     - Update README to talk about the above
     - Add `exists` method to CDMS API

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 python /app/leeloo/manage.py migrate
 python /app/leeloo/manage.py collectstatic --noinput
+python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/undefined.yaml
+python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/datahub_businesstypes.yaml
 python /app/leeloo/manage.py loaddata /app/leeloo/fixtures/metadata.yaml
 gunicorn datahubapi.wsgi --bind=0.0.0.0:8000
