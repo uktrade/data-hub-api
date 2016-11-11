9872b235a14ba68f4c0903280f14d8ed31c1758c
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-11-11 15:14:24 +0000

    inject user in the contact

--- dashboard/test/test_views.py
+++ dashboard/test/test_views.py
@@ -3,6 +3,7 @@ from django.utils.timezone import now
 from rest_framework import status
 
 from company.test.factories import CompanyFactory, ContactFactory, InteractionFactory
+from core import constants
 from core.test_utils import LeelooTestCase, get_test_user
 
 
@@ -11,7 +12,21 @@ class DashboardTestCase(LeelooTestCase):
     def test_intelligent_homepage(self):
 
         user = get_test_user()
-        contact = ContactFactory(advisor=user.advisor)
+
+        # add contact using the API to save the user from the session
+        url = reverse('contact-list')
+        api_response = self.api_client.post(url, {
+            'first_name': 'Oratio',
+            'last_name': 'Nelson',
+            'title': constants.Title.admiral_of_the_fleet.value.id,
+            'company': CompanyFactory().pk,
+            'role': constants.Role.owner.value.id,
+            'email': 'foo@bar.com',
+            'telephone_countrycode': '+44',
+            'telephone_number': '123456789',
+            'address_same_as_company': True,
+            'primary': True
+        })
         interaction = InteractionFactory(dit_advisor=user.advisor)
 
         url = reverse('dashboard:intelligent-homepage')
@@ -19,6 +34,6 @@ class DashboardTestCase(LeelooTestCase):
 
         assert response.status_code == status.HTTP_200_OK
         assert len(response.data['contacts']) == 1
-        assert response.data['contacts'][0]['id'] == str(contact.pk)
+        assert response.data['contacts'][0]['id'] == str(api_response.data['id'])
         assert len(response.data['interactions']) == 1
         assert response.data['interactions'][0]['id'] == str(interaction.pk)
