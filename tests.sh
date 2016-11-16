58fda174a269cb16055dcd8c84c3f0d068df0ee1
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-11-16 11:50:27 +0000

    codecov token

--- tests.sh
+++ tests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 pytest -s --cov-report term-missing --cov
 wget -O codecov.sh https://codecov.io/bash
-bash codecov.sh -t 3a59ba44-9d1b-45ca-a90c-3d16f6888ddc
+bash codecov.sh -t ${CODECOV_TOKEN}
