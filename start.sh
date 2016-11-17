9cf968f4a26b5e8f958ccc5c20403951dc95f91d
B M Corser <ben@steady.supply>
2016-11-17 14:00:09 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 python /app/leeloo/manage.py migrate
 python /app/leeloo/manage.py collectstatic --noinput
-gunicorn datahubapi.wsgi --bind=0.0.0.0:8000
+gunicorn config.wsgi --bind=0.0.0.0:8000
