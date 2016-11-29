9ea213d8aae781d64351411f1f33587bea596756
Dariusz Górecki <darek.krk@gmail.com>
2016-11-29 14:24:33 +0000

    Remove username from user admin

--- datahub/company/admin.py
+++ datahub/company/admin.py
@@ -15,7 +15,21 @@ for model_cls in MODELS_TO_REGISTER:
 class AdvisorAdmin(VersionAdmin, UserAdmin):
     """Advisor admin."""
 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                       'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
     list_display = ('email', 'first_name', 'last_name', 'is_staff')
+    search_fields = ('first_name', 'last_name', 'email')
     ordering = ('email', )
 
     def reversion_register(self, model, **kwargs):
