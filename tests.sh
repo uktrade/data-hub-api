27fff24d16f92ab021c56aa70cf481810096a24e
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2016-12-21 10:25:24 +0000

    add comment to test runner

--- tests.sh
+++ tests.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -xe
+# Auth tests have to be run using the pytest live_server fixture. The live server is spawned in the background in a different thread
+# For some obscure reason all the tests that run after the auth ones get stuck into using this different thread process
+# which doesn't have any access to the testing db hence there are no fixtures and the tests fail.
+# The solution is to run the auth tests first, then run all the other ones. It's a bit fiddly but it works
 pytest -k auth && pytest --cov -k "not auth" && wget -O codecov.sh https://codecov.io/bash && bash codecov.sh -t ${COV_TOKEN}
