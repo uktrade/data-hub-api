d14535b6ecfc424bacf6755ee040d950a3f65280
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-01-01 10:17:51 +0000

    add korben ping tests

--- datahub/korben/test/test_connector.py
+++ datahub/korben/test/test_connector.py
@@ -1,13 +1,32 @@
+from unittest import mock
+
+import requests
+
 from datahub.korben.connector import KorbenConnector
 
 
 def test_handle_host():
     """Test handle host."""
-    connector = KorbenConnector('foo')
-    assert connector.handle_host() == 'http://foo'
+    connector = KorbenConnector()
+    assert connector.handle_host('foo') == 'http://foo'
 
 
 def test_handle_host_with_protocol():
     """Test handle host with protocol."""
-    connector = KorbenConnector('https://foo')
-    assert connector.handle_host() == 'https://foo'
+    connector = KorbenConnector()
+    assert connector.handle_host('https://foo') == 'https://foo'
+
+
+@mock.patch('datahub.korben.connector.requests')
+def test_ping(mocked_requests):
+    """Ping Korben."""
+    mocked_requests.get.return_value = 'foo'
+    connector = KorbenConnector()
+    assert connector.ping() == 'foo'
+
+
+@mock.patch('datahub.korben.connector.requests.get', side_effect=requests.exceptions.RequestException)
+def test_ping_exception(mocked_get):
+    """Ping Korben exception."""
+    connector = KorbenConnector()
+    assert connector.ping() is False
