8008d2bf15f65c5bafcd051cf99b4308549d5e13
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-02-01 16:46:14 +0000

    no input migrate

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
-python /app/manage.py migrate
+python /app/manage.py migrate --noinput
 python /app/manage.py collectstatic --noinput
 python /app/manage.py runserver_plus 0.0.0.0:8000
