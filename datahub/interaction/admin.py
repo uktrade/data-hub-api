bd8119b752e004b05dc4ac53b19e429a29b4174c
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-02-16 13:32:39 +0000

    add admin improvements

--- datahub/interaction/admin.py
+++ datahub/interaction/admin.py
@@ -5,7 +5,15 @@ from reversion.admin import VersionAdmin
 from .models import Interaction, ServiceDelivery
 
 
-MODELS_TO_REGISTER = (Interaction, ServiceDelivery)
+@admin.register(Interaction)
+class InteractionAdmin(VersionAdmin):
+    """Interaction admin."""
 
-for model_cls in MODELS_TO_REGISTER:
-    admin.site.register(model_cls, VersionAdmin)
+    search_fields = ['id', 'company__company_number', 'company__company_name', 'contact_email']
+
+
+@admin.register(ServiceDelivery)
+class ServiceDeliveryAdmin(VersionAdmin):
+    """Service Delivery admin."""
+
+    search_fields = ['id', 'company__company_number', 'company__company_name', 'contact_email']
