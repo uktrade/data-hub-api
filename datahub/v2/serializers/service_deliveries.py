69c93e0b188a9de55219957ad178d8525f6d294f
Ed Singleton <singletoned@gmail.com>
2017-03-14 20:49:00 +0000

    Add RelationshipType

--- datahub/v2/serializers/service_deliveries.py
+++ datahub/v2/serializers/service_deliveries.py
@@ -1,58 +1,42 @@
+# -*- coding: utf-8 -*-
+
 import colander
 
-
-class RelationshipDataSchema(colander.MappingSchema):
-    type = colander.SchemaNode(colander.String())
-    id = colander.SchemaNode(colander.String())
+from colander import SchemaType, Invalid, null
 
 
-class RelationshipSchema(colander.MappingSchema):
-    data = RelationshipDataSchema()
+class RelationshipType(SchemaType):
+    def __init__(self, typename):
+        self.typename = typename
 
+    def serialize(self, node, appstruct):
+        if appstruct is null:
+            return null
+        appstruct = dict(appstruct)
+        if 'data' not in appstruct:
+            raise Invalid(node, '%r has no key data' % appstruct)
+        if 'type' not in appstruct['data']:
+            raise Invalid(node, '%r has no key type' % appstruct)
+        if appstruct['data']['type'] != self.typename:
+            raise Invalid(node, 'type %s should be %s' % (
+                appstruct['data']['type'], self.typename))
+        return appstruct and 'true' or 'false'
 
-class Status(RelationshipSchema):
-    pass
-
-
-class Company(RelationshipSchema):
-    pass
-
-
-class Contact(RelationshipSchema):
-    pass
-
-
-class Service(RelationshipSchema):
-    pass
-
-
-class DITTeam(RelationshipSchema):
-    pass
-
-
-class UKRegion(RelationshipSchema):
-    pass
-
-
-class Sector(RelationshipSchema):
-    pass
-
-
-class CountryOfInterest(RelationshipSchema):
-    pass
-
-
-class Event(RelationshipSchema):
-    pass
-
-
-def create_relationship_entity_schema(type_name):
-    data_schema = colander.MappingSchema()
-    data_schema.add(colander.String()
+    def deserialize(self, node, cstruct):
+        if cstruct is null:
+            return null
+        cstruct = dict(cstruct)
+        if 'data' not in cstruct:
+            raise Invalid(node, '%r has no key data' % cstruct)
+        if 'type' not in cstruct['data']:
+            raise Invalid(node, '%r has no key type' % cstruct)
+        if cstruct['data']['type'] != self.typename:
+            raise Invalid(node, 'type %s should be %s' % (
+                cstruct['data']['type'], self.typename))
+        return False
 
 
 class ServiceDeliveryAttributes(colander.MappingSchema):
-
     subject = colander.SchemaNode(colander.String())
     date = colander.SchemaNode(colander.DateTime())
     notes = colander.SchemaNode(colander.String())
@@ -60,16 +44,15 @@ class ServiceDeliveryAttributes(colander.MappingSchema):
 
 
 class ServiceDeliveryRelationships(colander.MappingSchema):
-
-    status = Status()
-    company = Company()
-    contact = Contact()
-    service = Service()
-    dit_team = DITTeam()
-    uk_region = UKRegion()
-    sector = Sector()
-    country_of_interest = CountryOfInterest()
-    event = Event()
+    status = colander.SchemaNode(RelationshipType(typename='Status'))
+    company = colander.SchemaNode(RelationshipType(typename='Company'))
+    contact = colander.SchemaNode(RelationshipType(typename='Contact'))
+    service = colander.SchemaNode(RelationshipType(typename='Service'))
+    dit_team = colander.SchemaNode(RelationshipType(typename='Team'))
+    uk_region = colander.SchemaNode(RelationshipType(typename='UKRegion'))
+    sector = colander.SchemaNode(RelationshipType(typename='Sector'))
+    country_of_interest = colander.SchemaNode(RelationshipType(typename='Country'))
+    event = colander.SchemaNode(RelationshipType(typename='Event'))
 
 
 class ServiceDeliverySchema(colander.Schema):
--- datahub/v2/serializers/test/test_serializers.py
+++ datahub/v2/serializers/test/test_serializers.py
@@ -1,79 +1,125 @@
+# -*- coding: utf-8 -*-
+
 import datetime
+import unittest
 
 import colander
 import pytest
 
-from datahub.v2.serializers.service_deliveries import ServiceDeliverySchema
+from datahub.v2.serializers.service_deliveries import ServiceDeliverySchema, RelationshipType
+
+
+class TestRelationshipType(unittest.TestCase):
+    def setUp(self):
+        class MySchema(colander.MappingSchema):
+            item = colander.SchemaNode(RelationshipType("flibble"))
+        self.schema = MySchema
+
+    def test_deserialize_empty(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().deserialize({'item': {}})
+        assert e.value.asdict()['item'] == '{} has no key data'
+
+    def test_deserialize_missing_type(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().deserialize({'item': {'data': {}}})
+        assert e.value.asdict()['item'] == """{'data': {}} has no key type"""
+
+    def test_deserialize_incorrect_type(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().deserialize({'item': {'data': {'type': "bamble"}}})
+        print(e.value.asdict()['item'])
+        assert e.value.asdict()['item'] == "type bamble should be flibble"
+
+    def test_serialize_empty(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().serialize({'item': {}})
+        assert e.value.asdict()['item'] == '{} has no key data'
+
+    def test_serialize_missing_type(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().serialize({'item': {'data': {}}})
+        assert e.value.asdict()['item'] == """{'data': {}} has no key type"""
+
+    def test_serialize_incorrect_type(self):
+        with pytest.raises(colander.Invalid) as e:
+            self.schema().serialize({'item': {'data': {'type': "bamble"}}})
+        assert e.value.asdict()['item'] == "type bamble should be flibble"
 
 
 def test_service_deliveries_serializer():
     data = {
-            'type': 'ServiceDelivery',
-            'attributes': {
-                'subject': 'whatever',
-                'date': datetime.datetime.now().isoformat(),
-                'notes': 'hello',
-                'feedback': 'foo'
+        'type': 'ServiceDelivery',
+        'attributes': {
+            'subject': 'whatever',
+            'date': datetime.datetime.now().isoformat(),
+            'notes': 'hello',
+            'feedback': 'foo'
+        },
+        'relationships': {
+            'status': {
+                'data': {
+                    'type': 'ServiceDeliveryStatus',
+                    'id': 'constants.ServiceDeliveryStatus.offered.value.id'
+                }
             },
-            'relationships': {
-                'status': {
-                    'data': {
-                        'type': 'ServiceDeliveryStatus',
-                        'id': 'constants.ServiceDeliveryStatus.offered.value.id'
-                    }
-                },
-                'company': {
-                    'data': {
-                        'type': 'Company',
-                        'id': 'CompanyFactory().pk'
-                    }
-                },
-                'contact': {
-                    'data': {
-                        'type': 'Contact',
-                        'id': 'ContactFactory().pk'
-                    }
-                },
-                'service': {
-                    'data': {
-                        'type': 'Service',
-                        'id': 'service_offer.service.id'
-                    }
-                },
-                'dit_team': {
-                    'data': {
-                        'type': 'Team',
-                        'id': 'service_offer.dit_team.id'
-                    }
-                },
-                'uk_region': {
-                    'data': {
-                        'type': 'UKRegion',
-                        'id': 'dsdasdsadsa'
-                    }
-                },
-                'sector': {
-                    'data': {
-                        'type': 'Sector',
-                        'id': 'dsdasdsadsa'
-                    }
-                },
-                'country_of_interest': {
-                    'data': {
-                        'type': 'flibble',
-                        'id': 'dsdasdsadsa'
-                    }
-                },
-                'event': {
-                    'data': {
-                        'type': 'event',
-                        'id': 'dsdasdsadsa'
-                    }
-                },
-            }
+            'company': {
+                'data': {
+                    'type': 'Company',
+                    'id': 'CompanyFactory().pk'
+                }
+            },
+            'contact': {
+                'data': {
+                    'type': 'Contact',
+                    'id': 'ContactFactory().pk'
+                }
+            },
+            'service': {
+                'data': {
+                    'type': 'Service',
+                    'id': 'service_offer.service.id'
+                }
+            },
+            'dit_team': {
+                'data': {
+                    'type': 'Team',
+                    'id': 'service_offer.dit_team.id'
+                }
+            },
+            'uk_region': {
+                'data': {
+                    'type': 'UKRegion',
+                    'id': 'dsdasdsadsa'
+                }
+            },
+            'sector': {
+                'data': {
+                    'type': 'Sector',
+                    'id': 'dsdasdsadsa'
+                }
+            },
+            'country_of_interest': {
+                'data': {
+                    'type': 'flibble',
+                    'id': 'dsdasdsadsa'
+                }
+            },
+            'event': {
+                'data': {
+                    'type': 'event',
+                    'id': 'dsdasdsadsa'
+                }
+            },
         }
+    }
+
+    expected = {
+        'relationships.country_of_interest': 'type flibble should be Country',
+        'relationships.event': 'type event should be Event',
+        'relationships.status': 'type ServiceDeliveryStatus should be Status'}
 
     with pytest.raises(colander.Invalid) as e:
-        result = ServiceDeliverySchema().deserialize(data)
-    import ipdb; ipdb.set_trace()
-    assert e.asdict()
+        ServiceDeliverySchema().deserialize(data)
+
+    assert e.value.asdict() == expected
