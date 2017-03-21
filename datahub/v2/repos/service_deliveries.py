e97b4023844847df9e2046a0d5e520f8b786b3a6
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-03-21 13:51:32 +0000

    handle object not there

--- datahub/v2/repos/service_deliveries.py
+++ datahub/v2/repos/service_deliveries.py
@@ -1,4 +1,5 @@
 import datetime
+from django.core.exceptions import ObjectDoesNotExist
 
 from datahub.interaction.models import ServiceDelivery
 from datahub.v2.serializers.service_deliveries import ServiceDeliverySchema
@@ -18,6 +19,38 @@ mapping = {
 }
 
 
+class ServiceDeliveryDatabaseRepo:
+    """DB repo."""
+
+    def __init__(self, config=None):
+        """Initialise the repo using the config."""
+        self.model = ServiceDelivery
+        self.schema = ServiceDeliverySchema
+
+    def get(self, object_id):
+        """Get and return a single object by its id."""
+        model_instance = self.model.objects.get(id=object_id)
+        return model_to_json_api(model_instance, schema_instance=self.schema())
+
+    def filter(self, company_id=DEFAULT, contact_id=DEFAULT, offset=0, limit=100):
+        """Filter objects."""
+        queryset = self.model.objects
+        if company_id:
+            queryset.filter(company__pk=company_id)
+        if contact_id:
+            queryset.filter(contact__pk=contact_id)
+        return self.schema(queryset.all(), many=True).data
+
+    def upsert(self, data, user):
+        """Insert or update an object."""
+        obj_id = data.pop('id')
+        obj, _ = self.model.objects.update_or_create(
+            pk=obj_id,
+            defaults=data
+        )
+        return self.schema(obj).data
+
+
 def build_relationship(model_instance, attribute):
     entity_name = mapping[attribute]
     data_dict = {'data': {'type': entity_name, 'id': str(model_instance.pk)}}
@@ -25,7 +58,7 @@ def build_relationship(model_instance, attribute):
 
 
 def build_attribute(model_instance, attribute):
-    value = getattr(model_instance,attribute, None)
+    value = getattr(model_instance, attribute, None)
     if isinstance(value, datetime.datetime):
         return value.isoformat()
     else:
@@ -45,38 +78,3 @@ def model_to_json_api(model_instance, schema_instance):
                 if relationship_instance:
                     relationships[subitem.name] = build_relationship(relationship_instance, subitem.name)
     return {'attributes': attributes, 'relationships': relationships}
-
-
-class ServiceDeliveryDatabaseRepo:
-    """DB repo."""
-
-    def __init__(self, config=None):
-        """Initialise the repo using the config."""
-        self.model = ServiceDelivery
-        self.schema = ServiceDeliverySchema
-
-    def get(self, object_id):
-        """Get and return a single object by its id."""
-        try:
-            model_instance = self.model.objects.get(id=object_id)
-            return model_to_json_api(model_instance, schema_instance=self.schema())
-        except self.model.DoesNotExist:
-            return {}
-
-    def filter(self, company_id=DEFAULT, contact_id=DEFAULT, offset=0, limit=100):
-        """Filter objects."""
-        queryset = self.model.objects
-        if company_id:
-            queryset.filter(company__pk=company_id)
-        if contact_id:
-            queryset.filter(contact__pk=contact_id)
-        return self.schema(queryset.all(), many=True).data
-
-    def upsert(self, data, user):
-        """Insert or update an object."""
-        obj_id = data.pop('id')
-        obj, _ = self.model.objects.update_or_create(
-            pk=obj_id,
-            defaults=data
-        )
-        return self.schema(obj).data
--- datahub/v2/repos/test/test_service_deliveries_repo.py
+++ datahub/v2/repos/test/test_service_deliveries_repo.py
@@ -1,4 +1,7 @@
+import uuid
+
 import pytest
+from django.core.exceptions import ObjectDoesNotExist
 from django.test import TestCase
 
 from datahub.interaction.test import factories
@@ -9,10 +12,13 @@ pytestmark = pytest.mark.django_db
 
 class ServiceDeliveriesRepoTestCase(TestCase):
 
-    def test_model_to_json_api(self):
+    def test_get(self):
         service_delivery = factories.ServiceDeliveryFactory()
         result = ServiceDeliveryDatabaseRepo().get(service_delivery.pk)
-        print(result)
         assert result['relationships']
         assert result['relationships']['company']['data']['type'] == 'Company'
         assert result['attributes']['date'] == service_delivery.date.isoformat()
+
+    def test_get_does_not_exist(self):
+        with pytest.raises(ObjectDoesNotExist) as e:
+            ServiceDeliveryDatabaseRepo().get(uuid.uuid4())
