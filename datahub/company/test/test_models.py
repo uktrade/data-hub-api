179bdc9dbed2acba5bff41e8657442c573334745
Dariusz Górecki <darek.krk@gmail.com>
2017-03-29 15:09:45 +0100

    Make flake8 happy

--- datahub/company/test/test_models.py
+++ datahub/company/test/test_models.py
@@ -1,7 +1,7 @@
 import pytest
 
 from datahub.company.models import Company
-from datahub.company.test.factories import CompanyFactory, AdvisorFactory
+from datahub.company.test.factories import AdvisorFactory, CompanyFactory
 from datahub.core import constants
 
 # mark the whole module for db use
