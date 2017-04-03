75a91186e4961c0f40c30094f550835d53c6c27f
Alessandro De Noia <alessandro.denoia@digital.bis.gov.uk>
2017-04-03 14:26:49 +0100

    add tests for parsers

--- datahub/v2/parsers.py
+++ datahub/v2/parsers.py
@@ -3,7 +3,6 @@
 from rest_framework import parsers
 from rest_framework.exceptions import ParseError
 from rest_framework_json_api.exceptions import Conflict
-from rest_framework_json_api.utils import format_keys
 
 from .renderers import JSONRenderer
 
@@ -29,51 +28,15 @@ class JSONParser(parsers.JSONParser):
     media_type = 'application/vnd.api+json'
     renderer_class = JSONRenderer
 
-    @staticmethod
-    def parse_attributes(data):
-        """Parse the attributes object."""
-        if 'attributes' in data:
-            return format_keys(data.get('attributes'), 'underscore')
-        else:
-            return dict()
-
-    @staticmethod
-    def parse_relationships(data):
-        """Parse the relationships object."""
-        if 'relationships' in data:
-            relationships = format_keys(data.get('relationships'), 'underscore')
-        else:
-            return dict()
-
-        # Parse the relationships
-        parsed_relationships = dict()
-        for field_name, field_data in relationships.items():
-            field_data = field_data.get('data')
-            if isinstance(field_data, dict) or field_data is None:
-                parsed_relationships[field_name] = field_data
-            elif isinstance(field_data, list):
-                parsed_relationships[field_name] = list(relation for relation in field_data)
-        return parsed_relationships
-
-    @staticmethod
-    def parse_metadata(result):
-        """Parse the metadata."""
-        metadata = result.get('meta')
-        if metadata:
-            return {'_meta': metadata}
-        else:
-            return {}
-
     def parse(self, stream, media_type=None, parser_context=None):
         """Parse the incoming bytestream as JSON and returns the resulting data."""
         result = super(JSONParser, self).parse(
             stream, media_type=media_type, parser_context=parser_context)
 
         if not isinstance(result, dict) or 'data' not in result:
-            raise ParseError('Received document does not contain primary data')
+            raise ParseError('Received document does not contain primary data.')
 
         data = result.get('data')
-
         request = parser_context.get('request')
 
         # Check for inconsistencies
@@ -86,6 +49,6 @@ class JSONParser(parsers.JSONParser):
                     resource_type=resource_name
                 )
             )
-        if not data.get('id') and request.method in ('PATCH', 'PUT'):
-            raise ParseError("The resource identifier object must contain an 'id' member")
+        # if not data.get('id') and request.method in ('PATCH', 'PUT'):
+        #    raise ParseError("The resource identifier object must contain an 'id' member.")
         return data
---
+++ datahub/v2/tests/views/test_parsers.py
@@ -0,0 +1,74 @@
+import json
+
+from rest_framework.reverse import reverse
+
+from datahub.core.test_utils import LeelooTestCase
+
+
+class JSONParserTestCase(LeelooTestCase):
+    """Test generic parser error through a v2 view."""
+
+    def test_data_key_not_in_post_body(self):
+        """Test data key is missing from post body."""
+        url = reverse('v2:servicedelivery-list')
+        data = {
+            'type': 'ServiceDelivery',
+            'attributes': {
+                'subject': 'whatever',
+                'date': 'foo',
+                'notes': 'hello',
+            },
+            'relationships': {
+                'status': {
+                    'data': {
+                        'type': 'foobar',
+                        'id': 'bar'
+                    }
+                },
+            }
+        }
+        response = self.api_client.post(
+            url,
+            data=json.dumps(data),
+            content_type='application/vnd.api+json'
+        )
+        content = json.loads(response.content.decode('utf-8'))
+        expected_content = {'errors': [
+            {'source': {'pointer': '/data/detail'},
+             'status': 400,
+             'detail': 'Received document does not contain primary data.'}]
+        }
+        assert content == expected_content
+
+    def test_data_contains_incorrect_entity_name(self):
+        """Test add new service delivery with incorrect format."""
+        url = reverse('v2:servicedelivery-list')
+        data = {
+            'type': 'whatever',
+            'attributes': {
+                'subject': 'whatever',
+                'date': 'foo',
+                'notes': 'hello',
+            },
+            'relationships': {
+                'status': {
+                    'data': {
+                        'type': 'foobar',
+                        'id': 'bar'
+                    }
+                },
+            }
+        }
+        response = self.api_client.post(
+            url,
+            data=json.dumps({'data': data}),
+            content_type='application/vnd.api+json'
+        )
+        content = json.loads(response.content.decode('utf-8'))
+        expected_content = {'errors': [
+            {'detail': 'The resource object\'s type (whatever) is not the type that constitute the collection '
+                       'represented by the endpoint (ServiceDelivery).',
+             'source': {'pointer': '/data/detail'},
+             'status': 409}]
+        }
+        assert content == expected_content
