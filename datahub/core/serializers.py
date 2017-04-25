d6ade408de308e4e2e71b44de90dd56a31a39291
Reupen Shah <reupen@users.noreply.github.com>
2017-04-25 11:37:08 +0100

    [DH-117] Add NestedRelatedField serialiser field

--- datahub/core/serializers.py
+++ datahub/core/serializers.py
@@ -1,4 +1,8 @@
+from uuid import UUID
+
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
+from rest_framework.fields import UUIDField
 
 
 class ConstantModelSerializer(serializers.Serializer):
@@ -10,3 +14,46 @@ class ConstantModelSerializer(serializers.Serializer):
 
     class Meta:  # noqa: D101
         fields = '__all__'
+
+
+class NestedRelatedField(serializers.RelatedField):
+    """TODO."""
+    default_error_messages = {
+        'required': 'This field is required.',
+        'missing_pk': 'pk not provided.',
+        'does_not_exist': 'Invalid pk "{pk_value}" - object does not exist.',
+        'incorrect_type': 'Incorrect type. Expected pk value, received {data_type}.',
+    }
+
+    def __init__(self, model, extra_fields=('name',), **kwargs):
+        super().__init__(**kwargs)
+        self.pk_field = UUIDField()
+        self._fields = extra_fields
+        self._model = model
+
+    def get_queryset(self):
+        return self._model.objects.all()
+
+    def to_internal_value(self, data):
+        data = self.pk_field.to_internal_value(data['id'])
+        try:
+            return self.get_queryset().get(pk=data)
+        except ObjectDoesNotExist:
+            self.fail('does_not_exist', pk_value=data)
+        except KeyError:
+            self.fail('missing_pk')
+        except (TypeError, ValueError):
+            self.fail('incorrect_type', data_type=type(data).__name__)
+
+    def to_representation(self, value):
+        extra = {field: _value_for_json(getattr(value, field)) for field in self._fields}
+        return {
+            'id': self.pk_field.to_representation(value.pk),
+            **extra
+        }
+
+
+def _value_for_json(val):
+    if isinstance(val, UUID):
+        return str(val)
+    return val
