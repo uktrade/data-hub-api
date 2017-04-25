77157d8efa61b5e71da3bf63e80fdcbabc68f64d
Reupen Shah <reupen@users.noreply.github.com>
2017-04-25 11:35:11 +0100

    [DH-117] Update investment models

--- datahub/investment/models.py
+++ datahub/investment/models.py
@@ -1,4 +1,6 @@
 """Investment project models."""
+import uuid
+
 from django.conf import settings
 from django.db import models
 
@@ -17,7 +19,8 @@ class IProjectAbstract(models.Model):
     description = models.TextField()
     nda_signed = models.BooleanField()
     estimated_land_date = models.DateField(null=True)
-    investment_type = models.ForeignKey('metadata.InvestmentType')
+    investment_type = models.ForeignKey('metadata.InvestmentType', null=True)  # FIXME
+    phase = models.ForeignKey('metadata.InvestmentProjectPhase', related_name='+', null=True)  # FIXME
 
     project_code = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
     document_link = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
@@ -25,7 +28,6 @@ class IProjectAbstract(models.Model):
     anonymous_description = models.TextField(blank=True, null=True)
     not_shareable_reason = models.TextField(blank=True, null=True)
 
-    phase = models.ForeignKey('metadata.InvestmentProjectPhase', related_name='+')
     investor_company = models.ForeignKey(
         'company.Company', related_name="investor_investment_projects", null=True
     )
@@ -45,24 +47,24 @@ class IProjectAbstract(models.Model):
         'company.Advisor', related_name='referred_investment_projects', null=True
     )
     referral_source_activity = models.ForeignKey(
-        'metadata.ReferralSourceActivity', related='investment_projects', null=True
+        'metadata.ReferralSourceActivity', related_name='investment_projects', null=True
     )
     referral_source_activity_website = models.ForeignKey(
-        'metadata.ReferralSourceActivity', related='investment_projects', null=True
+        'metadata.ReferralSourceWebsite', related_name='investment_projects', null=True
     )
     referral_source_activity_marketing = models.ForeignKey(
-        'metadata.ReferralSourceActivity', related='investment_projects', null=True
+        'metadata.ReferralSourceMarketing', related_name='investment_projects', null=True
     )
     referral_source_activity_event = models.ForeignKey(
-        'metadata.Event', related='investment_projects', null=True
+        'metadata.Event', related_name='investment_projects', null=True
     )
     fdi_type = models.ForeignKey(
-        'metadata.FDIType', related='investment_projects', null=True
+        'metadata.FDIType', related_name='investment_projects', null=True
     )
     non_fdi_type = models.ForeignKey(
-        'metadata.NonFDIType', related='investment_projects', null=True
+        'metadata.NonFDIType', related_name='investment_projects', null=True
     )
-    sector = models.ForeignKey('metadata.Sectors', related_name='+', null=True)
+    sector = models.ForeignKey('metadata.Sector', related_name='+', null=True)
     business_activity = models.ManyToManyField(
         'metadata.InvestmentBusinessActivity',
         related_name='+'
@@ -75,15 +77,15 @@ class IProjectValueAbstract(models.Model):
     class Meta:  # noqa: D101
         abstract = True
 
-    total_investment = models.DecimalField(null=True)
-    foreign_equity_investment = models.DecimalField(null=True)
-    government_assistance = models.BooleanField(null=True)
+    total_investment = models.DecimalField(null=True, max_digits=19, decimal_places=0)
+    foreign_equity_investment = models.DecimalField(null=True, max_digits=19, decimal_places=0)
+    government_assistance = models.NullBooleanField()
     number_new_jobs = models.IntegerField(null=True)
     number_safeguarded_jobs = models.IntegerField(null=True)
-    r_and_d_budget = models.BooleanField(null=True)
-    non_fdi_r_and_d_budget = models.BooleanField(null=True)
-    new_tech_to_uk = models.BooleanField(null=True)
-    export_revenue = models.BooleanField(null=True)
+    r_and_d_budget = models.NullBooleanField()
+    non_fdi_r_and_d_budget = models.NullBooleanField()
+    new_tech_to_uk = models.NullBooleanField()
+    export_revenue = models.NullBooleanField()
 
 
 class IProjectRequirementsAbstract(models.Model):
@@ -93,7 +95,7 @@ class IProjectRequirementsAbstract(models.Model):
         abstract = True
 
     client_requirements = models.TextField(blank=True, null=True)
-    site_decided = models.BooleanField(null=True)
+    site_decided = models.NullBooleanField()
     address_line_1 = models.CharField(blank=True, null=True, max_length=MAX_LENGTH)
     address_line_2 = models.CharField(blank=True, null=True, max_length=MAX_LENGTH)
     address_line_3 = models.CharField(blank=True, null=True, max_length=MAX_LENGTH)
@@ -112,10 +114,12 @@ class IProjectTeamAbstract(models.Model):
     class Meta:  # noqa: D101
         abstract = True
 
-    project_manager = models.ForeignKey('company.Advisor', null=True)
-    project_assurance_advisor = models.ForeignKey('company.Advisor', null=True)
+    project_manager = models.ForeignKey('company.Advisor', null=True, related_name='+')
+    project_assurance_advisor = models.ForeignKey('company.Advisor', null=True, related_name='+')
 
 
 class InvestmentProject(IProjectAbstract, IProjectValueAbstract, IProjectRequirementsAbstract,
-                        IProjectTeamAbstract, ArchivableModel, BaseModel):
+                        IProjectTeamAbstract, BaseModel):
     """TODO"""
+
+    id = models.UUIDField(primary_key=True, db_index=True, default=uuid.uuid4)
