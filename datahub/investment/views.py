0f28af1deeee84e56fd10256e121be4afc1ca5af
Reupen Shah <reupen@users.noreply.github.com>
2017-04-27 14:43:29 +0100

    [DH-117] Use select_related()/prefetch_related() in investment views

--- datahub/investment/views.py
+++ datahub/investment/views.py
@@ -14,7 +14,25 @@ class IProjectViewSet(CoreViewSetV3):
 
     read_serializer_class = IProjectSerializer
     write_serializer_class = IProjectSerializer
-    queryset = InvestmentProject.objects.all()
+    queryset = InvestmentProject.objects.select_related(
+        'investment_type',
+        'phase',
+        'investor_company',
+        'intermediate_company',
+        'investment_recipient_company',
+        'client_relationship_manager',
+        'referral_source_advisor',
+        'referral_source_activity',
+        'referral_source_activity_website',
+        'referral_source_activity_marketing',
+        'referral_source_activity_event',
+        'fdi_type',
+        'non_fdi_type',
+        'sector'
+    ).prefetch_related(
+        'client_contacts',
+        'business_activity'
+    )
 
 
 class IProjectValueViewSet(CoreViewSetV3):
@@ -25,7 +43,7 @@ class IProjectValueViewSet(CoreViewSetV3):
 
     read_serializer_class = IProjectValueSerializer
     write_serializer_class = IProjectValueSerializer
-    queryset = InvestmentProject.objects.all()
+    queryset = InvestmentProject.objects.select_related('average_salary')
 
 
 class IProjectRequirementsViewSet(CoreViewSetV3):
@@ -36,7 +54,11 @@ class IProjectRequirementsViewSet(CoreViewSetV3):
 
     read_serializer_class = IProjectRequirementsSerializer
     write_serializer_class = IProjectRequirementsSerializer
-    queryset = InvestmentProject.objects.all()
+    queryset = InvestmentProject.objects.prefetch_related(
+        'competitor_countries',
+        'uk_region_locations',
+        'strategic_drivers'
+    )
 
 
 class IProjectTeamViewSet(CoreViewSetV3):
@@ -47,4 +69,9 @@ class IProjectTeamViewSet(CoreViewSetV3):
 
     read_serializer_class = IProjectTeamSerializer
     write_serializer_class = IProjectTeamSerializer
-    queryset = InvestmentProject.objects.all()
+    queryset = InvestmentProject.objects.select_related(
+        'project_manager',
+        'project_manager__dit_team',
+        'project_assurance_advisor',
+        'project_assurance_advisor__dit_team'
+    )
