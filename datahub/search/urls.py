f7acc3002c8c943d9d5a960f07d2b2311accef3c
Pawel Szymanski <pawel@mikesz.com>
2017-05-22 21:53:11 +0100

    Updated urls to match develop

--- datahub/search/urls.py
+++ datahub/search/urls.py
@@ -7,7 +7,7 @@ from datahub.search.views import (
 )
 
 urlpatterns = [
-    url(r'^$', SearchBasicAPIView.as_view(), name='basic'),
-    url(r'^company/$', SearchCompanyAPIView.as_view(), name='company'),
-    url(r'^contact/$', SearchContactAPIView.as_view(), name='contact'),
+    url(r'^search$', SearchBasicAPIView.as_view(), name='basic'),
+    url(r'^search/company$', SearchCompanyAPIView.as_view(), name='company'),
+    url(r'^search/contact$', SearchContactAPIView.as_view(), name='contact'),
 ]
