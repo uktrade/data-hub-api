f389621d415105352b4b1c083d82c52f9fb0b2c5
Pawel Szymanski <pawel@mikesz.com>
2017-05-22 12:11:11 +0100

    Fixed code duplication in tests

--- datahub/search/test/test_views.py
+++ datahub/search/test/test_views.py
@@ -4,113 +4,113 @@ import pytest
 from rest_framework.reverse import reverse
 
 from datahub.core import constants
+from datahub.core.test_utils import LeelooTestCase
 
 pytestmark = pytest.mark.django_db
 
 
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_basic_search_companies(logged_in_api_client, setup_data):
-    """Tests basic aggregate companies query."""
-    term = 'abc defg'
+@pytest.mark.usefixtures('client', 'setup_data')
+class SearchTestCase(LeelooTestCase):
+    """Tests search views."""
 
-    url = reverse('api-v3:search:basic')
-    response = logged_in_api_client.get(url, {
-        'term': term,
-        'entity': 'company'
-    })
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_basic_search_companies(self):
+        """Tests basic aggregate companies query."""
+        term = 'abc defg'
 
-    assert response.data['count'] == 3
-    assert response.data['companies'][0]['name'].startswith(term)
-    assert [{'count': 3, 'entity': 'company'},
-            {'count': 1, 'entity': 'contact'}] == response.data['aggregations']
+        url = reverse('api-v3:search:basic')
+        response = self.api_client.get(url, {
+            'term': term,
+            'entity': 'company'
+        })
 
+        assert response.data['count'] == 3
+        assert response.data['companies'][0]['name'].startswith(term)
+        assert [{'count': 3, 'entity': 'company'},
+                {'count': 1, 'entity': 'contact'}] == response.data['aggregations']
 
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_basic_search_contacts(logged_in_api_client, setup_data):
-    """Tests basic aggregate contacts query."""
-    term = 'abc defg'
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_basic_search_contacts(self):
+        """Tests basic aggregate contacts query."""
+        term = 'abc defg'
 
-    url = reverse('api-v3:search:basic')
-    response = logged_in_api_client.get(url, {
-        'term': term,
-        'entity': 'contact'
-    })
+        url = reverse('api-v3:search:basic')
+        response = self.api_client.get(url, {
+            'term': term,
+            'entity': 'contact'
+        })
 
-    assert response.data['count'] == 1
-    assert response.data['contacts'][0]['first_name'] in term
-    assert response.data['contacts'][0]['last_name'] in term
-    assert [{'count': 3, 'entity': 'company'},
-            {'count': 1, 'entity': 'contact'}] == response.data['aggregations']
+        assert response.data['count'] == 1
+        assert response.data['contacts'][0]['first_name'] in term
+        assert response.data['contacts'][0]['last_name'] in term
+        assert [{'count': 3, 'entity': 'company'},
+                {'count': 1, 'entity': 'contact'}] == response.data['aggregations']
 
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_basic_search_companies_no_results(self):
+        """Tests case where there should be no results."""
+        term = 'there-should-be-no-match'
 
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_basic_search_companies_no_results(logged_in_api_client, setup_data):
-    """Tests case where there should be no results."""
-    term = 'there-should-be-no-match'
+        url = reverse('api-v3:search:basic')
+        response = self.api_client.get(url, {
+            'term': term,
+            'entity': 'company'
+        })
 
-    url = reverse('api-v3:search:basic')
-    response = logged_in_api_client.get(url, {
-        'term': term,
-        'entity': 'company'
-    })
+        assert response.data['count'] == 0
 
-    assert response.data['count'] == 0
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_basic_search_paging(self):
+        """Tests pagination of results."""
+        term = 'abc defg'
 
+        url = reverse('api-v3:search:basic')
+        response = self.api_client.get(url, {
+            'term': term,
+            'entity': 'company',
+            'offset': 1,
+            'limit': 1,
+        })
 
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_basic_search_paging(logged_in_api_client, setup_data):
-    """Tests pagination of results."""
-    term = 'abc defg'
+        assert response.data['count'] == 3
+        assert len(response.data['companies']) == 1
 
-    url = reverse('api-v3:search:basic')
-    response = logged_in_api_client.get(url, {
-        'term': term,
-        'entity': 'company',
-        'offset': 1,
-        'limit': 1,
-    })
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_search_company(self):
+        """Tests detailed company search."""
+        term = 'abc defg'
 
-    assert response.data['count'] == 3
-    assert len(response.data['companies']) == 1
+        url = '{}?offset={}&limit={}'.format(
+            reverse('api-v3:search:company'),
+            0,
+            100
+        )
 
+        response = self.api_client.post(url, {
+            'original_query': term,
+            'trading_address_country': constants.Country.united_states.value.id,
+        })
 
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_search_company(logged_in_api_client, setup_data):
-    """Tests detailed company search."""
-    term = 'abc defg'
+        assert response.data['count'] == 1
+        assert len(response.data['results']) == 1
+        assert response.data['results'][0]['trading_address_country']['id'] == constants.Country.united_states.value.id
 
-    url = '{}?offset={}&limit={}'.format(
-        reverse('api-v3:search:company'),
-        0,
-        100
-    )
+    @mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
+    def test_search_contact(self):
+        """Tests detailed contact search."""
+        term = 'abc defg'
 
-    response = logged_in_api_client.post(url, {
-        'original_query': term,
-        'trading_address_country': constants.Country.united_states.value.id,
-    })
+        url = '{}?offset={}&limit={}'.format(
+            reverse('api-v3:search:contact'),
+            0,
+            100
+        )
 
-    assert response.data['count'] == 1
-    assert len(response.data['results']) == 1
-    assert response.data['results'][0]['trading_address_country']['id'] == constants.Country.united_states.value.id
+        response = self.api_client.post(url, {
+            'original_query': term,
+            'last_name': 'defg',
+        })
 
-
-@mock.patch('datahub.search.views.elasticsearch.ES_INDEX', 'test')
-def test_search_contact(logged_in_api_client, setup_data):
-    """Tests detailed contact search."""
-    term = 'abc defg'
-
-    url = '{}?offset={}&limit={}'.format(
-        reverse('api-v3:search:contact'),
-        0,
-        100
-    )
-
-    response = logged_in_api_client.post(url, {
-        'original_query': term,
-        'last_name': 'defg',
-    })
-
-    assert response.data['count'] == 1
-    assert len(response.data['results']) == 1
-    assert response.data['results'][0]['last_name'] == 'defg'
+        assert response.data['count'] == 1
+        assert len(response.data['results']) == 1
+        assert response.data['results'][0]['last_name'] == 'defg'
