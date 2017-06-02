84b231a58dddd6fe2b9d595be19489fe8addeb1f
Reupen Shah <reupen@users.noreply.github.com>
2017-06-02 10:21:04 +0100

    Let Gunicorn config be set using environment variables

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -1,2 +1,8 @@
-accesslog = '-'
-access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({X-Forwarded-For}i)s'
+import os
+
+accesslog = os.environ.get('GUNICORN_ACCESSLOG', '-')
+access_log_format = os.environ.get(
+    'GUNICORN_ACCESS_LOG_FORMAT',
+    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" '
+    '%({X-Forwarded-For}i)s'
+)
