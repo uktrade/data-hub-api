5468768f7dc4372f21823f43639de26b0a02635a
elcct <pawel@mikesz.com>
2017-06-08 15:51:45 +0100

    Removed --ds flags from .sh files. apps.py coverage ignored.

--- tests-auth.sh
+++ tests-auth.sh
@@ -3,4 +3,4 @@
 # For some obscure reason all the tests that run after the auth ones get stuck into using this different thread process
 # which doesn't have any access to the testing db hence there are no fixtures and the tests fail.
 # The solution is to run the auth tests independently.
-pytest --ds=config.settings.test -k liveserver
+pytest -k liveserver
