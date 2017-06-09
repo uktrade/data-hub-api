1e91b6c200f7d151de1f1b32e61d6d5d60183dc3
elcct <pawel@mikesz.com>
2017-06-09 17:04:16 +0100

    Simplify removal of datahub.search initialisation for tests.

--- config/settings/test.py
+++ config/settings/test.py
@@ -5,10 +5,8 @@ env = environ.Env()
 
 from .common import *
 
-search_config_index = INSTALLED_APPS.index('datahub.search.apps.SearchConfig')
-
-INSTALLED_APPS = INSTALLED_APPS[0:search_config_index] + \
-                 ('datahub.search',) + \
-                 INSTALLED_APPS[search_config_index + 1:]
+# We need to prevent Django from initialising datahub.search for tests.
+INSTALLED_APPS.remove('datahub.search.apps.SearchConfig')
+INSTALLED_APPS.append('datahub.search')
 
 ES_INDEX = 'test'
