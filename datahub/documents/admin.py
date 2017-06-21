7c54fd35e46cbe4e55456a0797087ea4014c1d7f
Dariusz Górecki <darek.krk@gmail.com>
2017-06-21 13:29:10 +0100

    Add initiation scan TS

--- datahub/documents/admin.py
+++ datahub/documents/admin.py
@@ -8,7 +8,7 @@ class DocumentsAdmin(admin.ModelAdmin):
     """Documents admin."""
 
     list_display = (
-        'id', 'path', 'uploaded_on', 'av_clean', 'scanned_on',
+        'id', 'path', 'uploaded_on', 'av_clean', 'scan_initiated_on', 'scanned_on',
     )
     raw_id_fields = (
         'archived_by',
