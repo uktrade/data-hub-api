3b42f5f6bb821b07491962e4df59ddb2fc68c622
Reupen Shah <reupen@users.noreply.github.com>
2017-06-26 16:08:33 +0100

    [DH-360] Break up virus scanning functions and add Raven error capturing

--- datahub/documents/av_scan.py
+++ datahub/documents/av_scan.py
@@ -4,9 +4,10 @@ from logging import getLogger
 import requests
 from django.conf import settings
 from django.utils.timezone import now
+from raven.contrib.django.raven_compat.models import client
 from requests_toolbelt.multipart.encoder import MultipartEncoder
-from rest_framework import status
 
+from datahub.core.utils import get_s3_client
 from datahub.documents.models import Document
 
 logger = getLogger(__name__)
@@ -35,6 +36,14 @@ class S3StreamingBodyWrapper:
 
 def init_document_av_scan(doc_pk: str):
     """Stream file to the AV service."""
+    try:
+        _process_document(doc_pk)
+    except Exception:
+        logger.exception('Error virus scanning document')
+        client.captureException()
+
+
+def _process_document(doc_pk: str):
     if not settings.AV_SERVICE_URL:
         logger.warning(f'Ignoring AV scan of file=UUID={doc_pk}; '
                        'No AV service URL configured')
@@ -44,32 +53,40 @@ def init_document_av_scan(doc_pk: str):
     doc.scan_initiated_on = now()
     doc.save()
 
-    s3_obj = doc.get_s3_object()
-    with closing(s3_obj['Body']):
-        encoder = MultipartEncoder(
-            fields={
-                'file': (
-                    doc.filename,
-                    S3StreamingBodyWrapper(s3_obj),
-                    s3_obj['ContentType'],
-                )
-            }
+    is_file_clean = _scan_s3_object(doc.filename, doc.s3_bucket, doc.s3_key)
+
+    doc.scanned_on = now()
+    doc.av_clean = is_file_clean
+    doc.save()
+
+
+def _scan_s3_object(original_filename, bucket, key):
+    s3_client = get_s3_client()
+    response = s3_client.get_object(Bucket=bucket, Key=key)
+    with closing(response['Body']):
+        return _scan_raw_file(
+            original_filename, S3StreamingBodyWrapper(response), response['ContentType']
         )
 
-        response = requests.post(
-            # Assumes HTTP Basic auth in URL
-            # see: https://github.com/uktrade/dit-clamav-rest
-            settings.AV_SERVICE_URL,
-            data=encoder,
-            headers={'Content-Type': encoder.content_type},
-        )
 
-    if response.status_code == status.HTTP_200_OK:
-        doc.scanned_on = now()
-        doc.av_clean = response.content == 'OK'
-        doc.save()
-    else:
-        logger.error(
-            f'Unexpected response form AV Service status={response.status_code}'
-            f' content={response.content}'
+def _scan_raw_file(filename, file_object, content_type):
+    multipart_fields = {
+        'file': (
+            filename,
+            file_object,
+            content_type,
         )
+    }
+    encoder = MultipartEncoder(fields=multipart_fields)
+
+    response = requests.post(
+        # Assumes HTTP Basic auth in URL
+        # see: https://github.com/uktrade/dit-clamav-rest
+        settings.AV_SERVICE_URL,
+        data=encoder,
+        headers={'Content-Type': encoder.content_type},
+    )
+    response.raise_for_status()
+    if response.text not in ('OK', 'NOTOK'):
+        raise ValueError(f'Unexpected response from AV service: {response.content}')
+    return response.text == 'OK'
