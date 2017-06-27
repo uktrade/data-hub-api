3d995844464f6086c124369fd8d65c28e5e5922f
Reupen Shah <reupen@users.noreply.github.com>
2017-06-27 17:02:22 +0100

    [DH-360] Use pytest fixtures for stubbing in virus-scanning tests

--- datahub/documents/test/test_av_scan.py
+++ datahub/documents/test/test_av_scan.py
@@ -3,7 +3,6 @@ from io import BytesIO
 import pytest
 import requests_mock
 from botocore.stub import Stubber
-from django.test.utils import override_settings
 
 from datahub.core.utils import get_s3_client
 from datahub.documents import av_scan
@@ -12,23 +11,34 @@ from datahub.documents.test.factories import DocumentFactory
 pytestmark = pytest.mark.django_db
 
 
-def test_virus_scan_document_clean():
-    """Tests virus scanning a clean file."""
+@pytest.fixture()
+def requests_stubber():
+    """Requests stubber based on requests-mock"""
+    with requests_mock.mock() as requests_stubber:
+        yield requests_stubber
+
+
+@pytest.fixture()
+def s3_stubber():
+    """S3 stubber using the botocore Stubber class"""
     s3_client = get_s3_client()
-    stubber = Stubber(s3_client)
+    with Stubber(s3_client) as s3_stubber:
+        yield s3_stubber
+
+
+def test_virus_scan_document_clean(s3_stubber, requests_stubber):
+    """Tests virus scanning a clean file."""
     s3_response = {
         'Body': BytesIO(b'123456'),
         'ContentLength': 6,
         'ContentType': 'text/plain'
     }
     document = DocumentFactory()
-    stubber.add_response('get_object', s3_response, expected_params={
+    s3_stubber.add_response('get_object', s3_response, expected_params={
         'Bucket': document.s3_bucket, 'Key': document.s3_key
     })
+    requests_stubber.post('http://av-service/', text='OK')
 
-    with override_settings(DOCUMENT_BUCKET='test-bucket', AV_SERVICE_URL='http://av-service/'), \
-            stubber, requests_mock.mock() as requests_stubber:
-        requests_stubber.post('http://av-service/', text='OK')
-        av_scan.virus_scan_document(str(document.id))
-        document.refresh_from_db()
-        assert document.av_clean is True
+    av_scan.virus_scan_document(str(document.id))
+    document.refresh_from_db()
+    assert document.av_clean is True
