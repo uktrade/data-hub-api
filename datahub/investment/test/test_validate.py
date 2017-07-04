e41d81d866f801e0190c663de2a04d533e2b872f
Reupen Shah <reupen@users.noreply.github.com>
2017-07-04 11:51:34 +0100

    Put project stage in investment validation function names

--- datahub/investment/test/test_validate.py
+++ datahub/investment/test/test_validate.py
@@ -4,8 +4,8 @@ from datahub.company.test.factories import (AdviserFactory, ContactFactory)
 from datahub.core import constants
 from datahub.investment.test.factories import InvestmentProjectFactory
 from datahub.investment.validate import (
-    get_incomplete_project_fields, get_incomplete_reqs_fields,
-    get_incomplete_team_fields, get_incomplete_value_fields
+    get_incomplete_active_team_fields, get_incomplete_assign_pm_reqs_fields,
+    get_incomplete_assign_pm_value_fields, get_incomplete_prospect_project_fields
 )
 from datahub.metadata.models import ReferralSourceActivity
 
@@ -18,7 +18,7 @@ def test_validate_project_fail():
         investment_type_id=constants.InvestmentType.fdi.value.id,
         fdi_type_id=None
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert errors == {
         'fdi_type': 'This field is required.'
     }
@@ -29,7 +29,7 @@ def test_validate_project_instance_success():
     project = InvestmentProjectFactory(
         client_contacts=[ContactFactory().id, ContactFactory().id]
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert not errors
 
 
@@ -39,7 +39,7 @@ def test_validate_non_fdi_type():
     project = InvestmentProjectFactory(
         investment_type_id=investment_type_id
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert 'non_fdi_type' in errors
     assert 'fdi_type' not in errors
 
@@ -50,7 +50,7 @@ def test_validate_fdi_type():
     project = InvestmentProjectFactory(
         investment_type_id=investment_type_id
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert 'fdi_type' in errors
     assert 'non_fdi_type' not in errors
 
@@ -61,7 +61,7 @@ def test_validate_project_referral_website():
     project = InvestmentProjectFactory(
         referral_source_activity_id=referral_source_id
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert 'referral_source_activity_website' in errors
     assert 'referral_source_activity_event' not in errors
     assert 'referral_source_activity_marketing' not in errors
@@ -73,7 +73,7 @@ def test_validate_project_referral_event():
     project = InvestmentProjectFactory(
         referral_source_activity_id=referral_source_id
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert 'referral_source_activity_event' in errors
     assert 'referral_source_activity_website' not in errors
     assert 'referral_source_activity_marketing' not in errors
@@ -85,7 +85,7 @@ def test_validate_project_referral_marketing():
     project = InvestmentProjectFactory(
         referral_source_activity_id=referral_source_id
     )
-    errors = get_incomplete_project_fields(instance=project)
+    errors = get_incomplete_prospect_project_fields(instance=project)
     assert 'referral_source_activity_marketing' in errors
     assert 'referral_source_activity_website' not in errors
     assert 'referral_source_activity_event' not in errors
@@ -99,8 +99,8 @@ def test_validate_project_update_data():
     update_data = {
         'referral_source_activity': referral_source
     }
-    errors = get_incomplete_project_fields(instance=project,
-                                           update_data=update_data)
+    errors = get_incomplete_prospect_project_fields(instance=project,
+                                                    update_data=update_data)
     assert 'referral_source_activity_marketing' in errors
     assert 'referral_source_activity_website' not in errors
     assert 'referral_source_activity_event' not in errors
@@ -109,7 +109,7 @@ def test_validate_project_update_data():
 def test_validate_value_fail():
     """Tests validating an incomplete value section."""
     project = InvestmentProjectFactory(sector_id=None)
-    errors = get_incomplete_value_fields(instance=project)
+    errors = get_incomplete_assign_pm_value_fields(instance=project)
     assert errors == {
         'client_cannot_provide_total_investment': 'This field is required.',
         'total_investment': 'This field is required.',
@@ -124,14 +124,14 @@ def test_validate_value_instance_success():
         total_investment=100,
         number_new_jobs=0
     )
-    errors = get_incomplete_value_fields(instance=project)
+    errors = get_incomplete_assign_pm_value_fields(instance=project)
     assert not errors
 
 
 def test_validate_reqs_fail():
     """Tests validating an incomplete reqs section."""
     project = InvestmentProjectFactory(sector_id=None)
-    errors = get_incomplete_reqs_fields(instance=project)
+    errors = get_incomplete_assign_pm_reqs_fields(instance=project)
     assert errors == {
         'client_considering_other_countries': 'This field is required.',
         'client_requirements': 'This field is required.',
@@ -154,7 +154,7 @@ def test_validate_reqs_instance_success():
         strategic_drivers=strategic_drivers,
         uk_region_locations=uk_region_locations
     )
-    errors = get_incomplete_reqs_fields(instance=project)
+    errors = get_incomplete_assign_pm_reqs_fields(instance=project)
     assert not errors
 
 
@@ -163,7 +163,7 @@ def test_validate_reqs_competitor_countries_missing():
     project = InvestmentProjectFactory(
         client_considering_other_countries=True
     )
-    errors = get_incomplete_reqs_fields(instance=project)
+    errors = get_incomplete_assign_pm_reqs_fields(instance=project)
     assert 'competitor_countries' in errors
 
 
@@ -173,14 +173,14 @@ def test_validate_reqs_competitor_countries_present():
         client_considering_other_countries=True,
         competitor_countries=[constants.Country.united_states.value.id]
     )
-    errors = get_incomplete_reqs_fields(instance=project)
+    errors = get_incomplete_assign_pm_reqs_fields(instance=project)
     assert 'competitor_countries' not in errors
 
 
 def test_validate_team_fail():
     """Tests validating an incomplete team section."""
     project = InvestmentProjectFactory(sector_id=None)
-    errors = get_incomplete_team_fields(instance=project)
+    errors = get_incomplete_active_team_fields(instance=project)
     assert errors == {
         'project_assurance_adviser': 'This field is required.',
         'project_manager': 'This field is required.'
@@ -194,5 +194,5 @@ def test_validate_team_instance_success():
         project_manager=adviser,
         project_assurance_adviser=adviser
     )
-    errors = get_incomplete_team_fields(instance=project)
+    errors = get_incomplete_active_team_fields(instance=project)
     assert not errors
