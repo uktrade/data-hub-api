cd9dd6b63b5a1bcaa605bdb7b124f6288cc7421f
Dariusz Górecki <darek.krk@gmail.com>
2017-07-07 11:35:01 +0100

    Fix __all__

--- datahub/company/models/__init__.py
+++ datahub/company/models/__init__.py
@@ -2,4 +2,4 @@ from .adviser import Advisor
 from .company import CompaniesHouseCompany, Company
 from .contact import Contact
 
-__all__ = (Advisor, CompaniesHouseCompany, Company, Contact)
+__all__ = ('Advisor', 'CompaniesHouseCompany', 'Company', 'Contact')
