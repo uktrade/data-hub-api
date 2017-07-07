79f76eaef6d5197a39c6d0687fa6ca1e9952a5c3
Reupen Shah <reupen@users.noreply.github.com>
2017-07-07 11:59:01 +0100

    [DH-381] Disable related_name for archived_by

--- datahub/investment/migrations/0003_add_investment_archiving.py
+++ datahub/investment/migrations/0003_add_investment_archiving.py
@@ -23,7 +23,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='investmentproject',
             name='archived_by',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to=settings.AUTH_USER_MODEL),
         ),
         migrations.AddField(
             model_name='investmentproject',
