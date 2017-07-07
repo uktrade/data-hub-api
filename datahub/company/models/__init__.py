9b4c7ae8521b329498898ec957c64df6d42fbfa6
Dariusz Górecki <darek.krk@gmail.com>
2017-07-07 10:55:32 +0100

    Keep flake8 happy

--- datahub/company/models/__init__.py
+++ datahub/company/models/__init__.py
@@ -1,3 +1,3 @@
-from .adviser import Advisor
-from .company import Company, CompaniesHouseCompany
-from .contact import Contact
+from .adviser import Advisor  # noqa: F401
+from .company import CompaniesHouseCompany, Company  # noqa: F401
+from .contact import Contact  # noqa: F401
