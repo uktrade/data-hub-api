925be4b4aab58612876e54b1ca01409b1069ffd7
Reupen Shah <reupen@users.noreply.github.com>
2017-07-13 16:50:00 +0100

    Remove deprecated investment views

--- datahub/investment/urls.py
+++ datahub/investment/urls.py
@@ -3,20 +3,9 @@
 from django.conf.urls import url
 
 from datahub.investment.views import (
-    IProjectAuditViewSet, IProjectDocumentViewSet, IProjectRequirementsViewSet,
-    IProjectTeamMembersViewSet, IProjectTeamViewSet, IProjectUnifiedViewSet, IProjectValueViewSet,
-    IProjectViewSet
+    IProjectAuditViewSet, IProjectDocumentViewSet, IProjectTeamMembersViewSet,
+    IProjectUnifiedViewSet
 )
-
-project_collection = IProjectViewSet.as_view({
-    'get': 'list',
-    'post': 'create'
-})
-
-project_item = IProjectViewSet.as_view({
-    'get': 'retrieve',
-    'patch': 'partial_update'
-})
 
 unified_project_collection = IProjectUnifiedViewSet.as_view({
     'get': 'list',
@@ -39,30 +28,15 @@ project_team_member_item = IProjectTeamMembersViewSet.as_view({
     'delete': 'destroy'
 })
 
-value_item = IProjectValueViewSet.as_view({
-    'get': 'retrieve',
-    'patch': 'partial_update'
-})
-
-requirements_item = IProjectRequirementsViewSet.as_view({
-    'get': 'retrieve',
-    'patch': 'partial_update'
-})
-
-team_item = IProjectTeamViewSet.as_view({
-    'get': 'retrieve',
-    'patch': 'partial_update'
-})
-
 audit_item = IProjectAuditViewSet.as_view({
     'get': 'retrieve',
 })
 
-archive_item = IProjectViewSet.as_view({
+archive_item = IProjectUnifiedViewSet.as_view({
     'post': 'archive',
 })
 
-unarchive_item = IProjectViewSet.as_view({
+unarchive_item = IProjectUnifiedViewSet.as_view({
     'post': 'unarchive',
 })
 
@@ -83,7 +57,6 @@ urlpatterns = [
     url(r'^investment$', unified_project_collection, name='investment-collection'),
     url(r'^investment/(?P<pk>[0-9a-z-]{36})$', unified_project_item,
         name='investment-item'),
-    url(r'^investment/project$', project_collection, name='project'),
     url(r'^investment/(?P<pk>[0-9a-z-]{36})/archive$', archive_item,
         name='archive-item'),
     url(r'^investment/(?P<project_pk>[0-9a-z-]{36})/team-member$', project_team_member_collection,
@@ -98,14 +71,6 @@ urlpatterns = [
         project_document_callback, name='document-item-callback'),
     url(r'^investment/(?P<pk>[0-9a-z-]{36})/unarchive$', unarchive_item,
         name='unarchive-item'),
-    url(r'^investment/(?P<pk>[0-9a-z-]{36})/project$', project_item,
-        name='project-item'),
-    url(r'^investment/(?P<pk>[0-9a-z-]{36})/value$', value_item,
-        name='value-item'),
-    url(r'^investment/(?P<pk>[0-9a-z-]{36})/requirements$', requirements_item,
-        name='requirements-item'),
-    url(r'^investment/(?P<pk>[0-9a-z-]{36})/team$', team_item,
-        name='team-item'),
     url(r'^investment/(?P<pk>[0-9a-z-]{36})/audit$', audit_item,
         name='audit-item'),
 ]
