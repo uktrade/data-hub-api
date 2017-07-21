d6db8b90c40fbef7620b7160f200437fc5f09e5b
Reupen Shah <reupen@users.noreply.github.com>
2017-07-21 15:39:13 +0100

    Improve dashboard endpoint performance

---
+++ datahub/company/queryset.py
@@ -0,0 +1,12 @@
+from datahub.company.models import Contact
+
+
+def get_contact_queryset():
+    """Gets the contact query set used by views."""
+    return Contact.objects.select_related(
+        'title',
+        'company',
+        'adviser',
+        'address_country',
+        'archived_by'
+    )
