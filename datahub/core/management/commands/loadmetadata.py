68e0807c40eab6c4bbe716c7f68a1c1ab8e4a0ac
Marco Fucci <marcofucci@gmail.com>
2017-07-25 11:41:40 +0100

    Split metadata fixtures
    This splits the fixtures into separate groups so that they are
    easier to manage.
    It also adds a `loadmetadata` django command that imports all
    of them automatically.
    
    The `datahub_businesstypes.yaml` fixture has been deleted and
    incorporated into the original one.

---
+++ datahub/core/management/commands/loadmetadata.py
@@ -0,0 +1,36 @@
+from pathlib import PurePath
+
+from django.core.management import call_command
+from django.core.management.base import BaseCommand
+
+
+METADATA_FIXTURE_DIR = PurePath(__file__).parents[4] / 'fixtures/metadata'
+
+
+class Command(BaseCommand):
+    """Loads all the metadata fixtures."""
+
+    def handle(self, *args, **options):
+        """
+        It loads all metadata fixtures.
+
+        The algorithm could iterate and import all the files in the `metadata`
+        folder but some could have dependencies so it's safer to specify the
+        list manually.
+        """
+        call_command(
+            'loaddata',
+            *[
+                METADATA_FIXTURE_DIR / 'companies.yaml',
+                METADATA_FIXTURE_DIR / 'contacts.yaml',
+                METADATA_FIXTURE_DIR / 'countries.yaml',
+                METADATA_FIXTURE_DIR / 'interactions.yaml',
+                METADATA_FIXTURE_DIR / 'investment.yaml',
+                METADATA_FIXTURE_DIR / 'referrals.yaml',
+                METADATA_FIXTURE_DIR / 'sectors.yaml',
+                METADATA_FIXTURE_DIR / 'services.yaml',
+                METADATA_FIXTURE_DIR / 'teams.yaml',
+                METADATA_FIXTURE_DIR / 'titles.yaml',
+                METADATA_FIXTURE_DIR / 'uk_regions.yaml',
+            ]
+        )
