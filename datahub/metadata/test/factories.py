11414a5c1289c4be9ca579c32f4baa080d77592a
Marco Fucci <marcofucci@gmail.com>
2017-07-26 11:11:52 +0100

    Add TeamRole and extend Team
    This:
    - adds a new model called TeamRole
    - adds role, uk_region and country fields to Team
    - updates the fixtures to only load the teams we care about
    - updates the test_data fixtures to use the new values
    - updates the model admin for Team and adds a new one for TeamRole
    - adds the TeamRole GET endpoint
    - updates the Team GET endpoint response to include the new fields

--- datahub/metadata/test/factories.py
+++ datahub/metadata/test/factories.py
@@ -2,11 +2,13 @@ import uuid
 
 import factory
 
+from datahub.core import constants
+
 
 class EventFactory(factory.django.DjangoModelFactory):
     """Event factory."""
 
-    id = factory.Sequence(lambda _: str(uuid.uuid4()))
+    id = factory.LazyFunction(lambda: str(uuid.uuid4()))
     name = factory.Sequence(lambda n: f'name {n}')
 
     class Meta:  # noqa: D101
@@ -16,18 +18,31 @@ class EventFactory(factory.django.DjangoModelFactory):
 class ServiceFactory(factory.django.DjangoModelFactory):
     """Service factory."""
 
-    id = factory.Sequence(lambda _: str(uuid.uuid4()))
+    id = factory.LazyFunction(lambda: str(uuid.uuid4()))
     name = factory.Sequence(lambda n: f'name {n}')
 
     class Meta:  # noqa: D101
         model = 'metadata.Service'
 
 
+class TeamRoleFactory(factory.django.DjangoModelFactory):
+    """TeamRole factory."""
+
+    id = factory.LazyFunction(lambda: str(uuid.uuid4()))
+    name = factory.Sequence(lambda n: f'name {n}')
+
+    class Meta:  # noqa: D101
+        model = 'metadata.TeamRole'
+
+
 class TeamFactory(factory.django.DjangoModelFactory):
     """Team factory."""
 
-    id = factory.Sequence(lambda _: str(uuid.uuid4()))
-    name = factory.Sequence(lambda n: 'name {n}')
+    id = factory.LazyFunction(lambda: str(uuid.uuid4()))
+    name = factory.Sequence(lambda n: f'name {n}')
+    role = factory.SubFactory(TeamRoleFactory)
+    uk_region_id = constants.UKRegion.east_midlands.value.id
+    country_id = constants.Country.france.value.id
 
     class Meta:  # noqa: D101
         model = 'metadata.Team'
--- datahub/metadata/views.py
+++ datahub/metadata/views.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 from functools import partial
 
 from rest_framework.decorators import api_view, authentication_classes, permission_classes
@@ -5,50 +6,65 @@ from rest_framework.response import Response
 
 from datahub.core.serializers import ConstantModelSerializer
 from . import models
+from .serializers import TeamSerializer
+
+MetadataMapping = namedtuple('MetadataMapping', ['model', 'queryset', 'serializer'])
+
+
+def build_mapping(model, queryset=None, serializer=ConstantModelSerializer):
+    """Returns a MetadataMapping and defines defaults."""
+    queryset = queryset if queryset is not None else model.objects.all()
+    return MetadataMapping(model, queryset, serializer)
+
 
 METADATA_MAPPING = {
-    'business-type': models.BusinessType,
-    'country': models.Country,
-    'employee-range': models.EmployeeRange,
-    'interaction-type': models.InteractionType,
-    'role': models.Role,
-    'sector': models.Sector,
-    'service': models.Service,
-    'team': models.Team,
-    'title': models.Title,
-    'turnover': models.TurnoverRange,
-    'uk-region': models.UKRegion,
-    'service-delivery-status': models.ServiceDeliveryStatus,
-    'event': models.Event,
-    'headquarter-type': models.HeadquarterType,
-    'company-classification': models.CompanyClassification,
-    'investment-type': models.InvestmentType,
-    'fdi-type': models.FDIType,
-    'non-fdi-type': models.NonFDIType,
-    'referral-source-activity': models.ReferralSourceActivity,
-    'referral-source-website': models.ReferralSourceWebsite,
-    'referral-source-marketing': models.ReferralSourceMarketing,
-    'investment-business-activity': models.InvestmentBusinessActivity,
-    'investment-strategic-driver': models.InvestmentStrategicDriver,
-    'salary-range': models.SalaryRange,
-    'investment-project-stage': models.InvestmentProjectStage,
-    'fdi-value': models.FDIValue,
+    'business-type': build_mapping(models.BusinessType),
+    'country': build_mapping(models.Country),
+    'employee-range': build_mapping(models.EmployeeRange),
+    'interaction-type': build_mapping(models.InteractionType),
+    'role': build_mapping(models.Role),
+    'sector': build_mapping(models.Sector),
+    'service': build_mapping(models.Service),
+    'team-role': build_mapping(models.TeamRole),
+    'team': build_mapping(
+        model=models.Team,
+        queryset=models.Team.objects.select_related('role', 'uk_region', 'country'),
+        serializer=TeamSerializer
+    ),
+    'title': build_mapping(models.Title),
+    'turnover': build_mapping(models.TurnoverRange),
+    'uk-region': build_mapping(models.UKRegion),
+    'service-delivery-status': build_mapping(models.ServiceDeliveryStatus),
+    'event': build_mapping(models.Event),
+    'headquarter-type': build_mapping(models.HeadquarterType),
+    'company-classification': build_mapping(models.CompanyClassification),
+    'investment-type': build_mapping(models.InvestmentType),
+    'fdi-type': build_mapping(models.FDIType),
+    'non-fdi-type': build_mapping(models.NonFDIType),
+    'referral-source-activity': build_mapping(models.ReferralSourceActivity),
+    'referral-source-website': build_mapping(models.ReferralSourceWebsite),
+    'referral-source-marketing': build_mapping(models.ReferralSourceMarketing),
+    'investment-business-activity': build_mapping(models.InvestmentBusinessActivity),
+    'investment-strategic-driver': build_mapping(models.InvestmentStrategicDriver),
+    'salary-range': build_mapping(models.SalaryRange),
+    'investment-project-stage': build_mapping(models.InvestmentProjectStage),
+    'fdi-value': build_mapping(models.FDIValue),
 }
 
 
 @api_view()
 @authentication_classes([])
 @permission_classes([])
-def metadata_view(request, model):
+def metadata_view(request, mapping):
     """Metadata generic view."""
-    serializer = ConstantModelSerializer(model.objects.all(), many=True)
+    serializer = mapping.serializer(mapping.queryset.all(), many=True)
     return Response(data=serializer.data)
 
 
 urls_args = []
 
 # programmatically generate metadata views
-for name, model in METADATA_MAPPING.items():
-    fn = partial(metadata_view, model=model)
+for name, mapping in METADATA_MAPPING.items():
+    fn = partial(metadata_view, mapping=mapping)
     path = fr'^{name}/$'
     urls_args.append(((path, fn), {'name': name}))
