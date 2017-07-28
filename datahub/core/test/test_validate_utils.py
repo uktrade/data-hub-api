6e3418c6abe44a0662a2e1fa7ec359c09b4fb417
Marco Fucci <marcofucci@gmail.com>
2017-07-28 17:25:54 +0100

    Group Data Combiner tests

--- datahub/core/test/test_validate_utils.py
+++ datahub/core/test/test_validate_utils.py
@@ -32,53 +32,51 @@ def test_any_of_all():
     validator({'field_a': Mock(), 'field_b': Mock()})
 
 
-def test_get_value_instance():
-    """Tests getting a simple value from an instance."""
-    instance = Mock(field1=1, field2=2)
-    data = {'field2': 456}
-    data_combiner = DataCombiner(instance, data)
-    assert data_combiner.get_value('field1') == 1
+class TestDataCombiner:
+    """Data Combiner tests."""
 
+    def test_get_value_instance(self):
+        """Tests getting a simple value from an instance."""
+        instance = Mock(field1=1, field2=2)
+        data = {'field2': 456}
+        data_combiner = DataCombiner(instance, data)
+        assert data_combiner.get_value('field1') == 1
 
-def test_get_value_data():
-    """Tests getting a simple value from update data."""
-    instance = Mock(field1=1, field2=2)
-    data = {'field2': 456}
-    data_combiner = DataCombiner(instance, data)
-    assert data_combiner.get_value('field2') == 456
+    def test_get_value_data(self):
+        """Tests getting a simple value from update data."""
+        instance = Mock(field1=1, field2=2)
+        data = {'field2': 456}
+        data_combiner = DataCombiner(instance, data)
+        assert data_combiner.get_value('field2') == 456
 
+    def test_get_value_to_many_instance(self):
+        """Tests getting a to-many value from an instance."""
+        instance = Mock(field1=MagicMock())
+        instance.field1.all.return_value = [123]
+        data_combiner = DataCombiner(instance, None)
+        assert data_combiner.get_value_to_many('field1') == [123]
 
-def test_get_value_to_many_instance():
-    """Tests getting a to-many value from an instance."""
-    instance = Mock(field1=MagicMock())
-    instance.field1.all.return_value = [123]
-    data_combiner = DataCombiner(instance, None)
-    assert data_combiner.get_value_to_many('field1') == [123]
+    def test_get_value_to_many_data(self):
+        """Tests getting a to-many value from update data."""
+        instance = Mock(field1=MagicMock())
+        data = {'field1': [123]}
+        data_combiner = DataCombiner(instance, data)
+        assert data_combiner.get_value_to_many('field1') == data['field1']
 
+    def test_get_value_id_instance(self):
+        """Tests getting a foreign key from an instance."""
+        subinstance = Mock()
+        subinstance.id = 1234
+        instance = Mock(field1=subinstance)
+        data_combiner = DataCombiner(instance, None)
+        assert data_combiner.get_value_id('field1') == str(subinstance.id)
 
-def test_get_value_to_many_data():
-    """Tests getting a to-many value from update data."""
-    instance = Mock(field1=MagicMock())
-    data = {'field1': [123]}
-    data_combiner = DataCombiner(instance, data)
-    assert data_combiner.get_value_to_many('field1') == data['field1']
-
-
-def test_get_value_id_instance():
-    """Tests getting a foreign key from an instance."""
-    subinstance = Mock()
-    subinstance.id = 1234
-    instance = Mock(field1=subinstance)
-    data_combiner = DataCombiner(instance, None)
-    assert data_combiner.get_value_id('field1') == str(subinstance.id)
-
-
-def test_get_value_id_value():
-    """Tests getting a foreign key from update data."""
-    subinstance = Mock()
-    subinstance.id = 1234
-    new_subinstance = Mock()
-    new_subinstance.id = 456
-    instance = Mock(field1=subinstance)
-    data_combiner = DataCombiner(instance, {'field1': new_subinstance})
-    assert data_combiner.get_value_id('field1') == str(new_subinstance.id)
+    def test_get_value_id_value(self):
+        """Tests getting a foreign key from update data."""
+        subinstance = Mock()
+        subinstance.id = 1234
+        new_subinstance = Mock()
+        new_subinstance.id = 456
+        instance = Mock(field1=subinstance)
+        data_combiner = DataCombiner(instance, {'field1': new_subinstance})
+        assert data_combiner.get_value_id('field1') == str(new_subinstance.id)
