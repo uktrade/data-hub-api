026ec7c260bbf4c959cab46b91aa5881db86d48c
Marco Fucci <marcofucci@gmail.com>
2017-07-28 14:46:09 +0100

    Add endpoint to update order details
    This allows clients to PATCH the contact field of existing orders.
    Company and primary market cannot be changed after creation.

--- datahub/omis/order/test/factories.py
+++ datahub/omis/order/test/factories.py
@@ -13,7 +13,7 @@ class OrderFactory(factory.django.DjangoModelFactory):
 
     id = factory.LazyFunction(lambda: str(uuid.uuid4()))
     company = factory.SubFactory(CompanyFactory)
-    contact = factory.SubFactory(ContactFactory)
+    contact = factory.LazyAttribute(lambda o: ContactFactory(company=o.company))
     primary_market_id = Country.france.value.id
 
     class Meta:  # noqa: D101
