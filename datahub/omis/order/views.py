78e63706d23c93d29c5425dc298dc6c820efaeaa
Marco Fucci <marcofucci@gmail.com>
2017-08-04 16:51:24 +0100

    Add OrderAssignee and related endpoints
    This:
    - adds a new `OrderAssignee` model
    - adds 2 new endpoints:
    
    `GET /v3/omis/order/<order-id>/assignee`
    
    Returns the list of advisers assigned to an order.
    
    `PATCH /v3/omis/order/<order-id>/assignee`
    
    (Partially) updates the advisers assigned to an order.
    By default it keeps the assignees not included in the request body
    but you can pass the query string `force-delete=1` and it will
    delete them.

--- datahub/omis/order/views.py
+++ datahub/omis/order/views.py
@@ -6,7 +6,7 @@ from rest_framework.views import APIView
 from datahub.core.viewsets import CoreViewSetV3
 
 from .models import Order, OrderSubscriber
-from .serializers import OrderSerializer, SubscribedAdviserSerializer
+from .serializers import OrderAssigneeSerializer, OrderSerializer, SubscribedAdviserSerializer
 
 
 class OrderViewSet(CoreViewSetV3):
@@ -76,3 +76,57 @@ class SubscriberListView(APIView):
             )
 
         return self.get_subscriber_list_response(order)
+
+
+class AssigneeView(APIView):
+    """API View for advisers assigned to an order."""
+
+    FORCE_DELETE_PARAM = 'force-delete'
+
+    def get_order(self, order_pk):
+        """
+        Returns the related order or raises Http404 if it doesn't exist.
+        """
+        try:
+            return Order.objects.get(pk=order_pk)
+        except Order.DoesNotExist:
+            raise Http404
+
+    def get_list_response(self, order):
+        """
+        Returns a Response object with the serialised list of advisers assigned to
+        the order.
+        """
+        advisers = order.assignees.select_related('adviser').all()
+        serializer = OrderAssigneeSerializer(advisers, many=True)
+
+        return Response(serializer.data)
+
+    def get(self, request, order_pk, format=None):
+        """
+        Returns a serialised list of advisers assigned to the order.
+        """
+        order = self.get_order(order_pk)
+        return self.get_list_response(order)
+
+    def patch(self, request, order_pk, format=None):
+        """
+        Updates the list of assignees.
+        It adds/keeps/updates/deletes the advisers based on the new list passed in.
+        """
+        order = self.get_order(order_pk)
+
+        force_delete = request.query_params.get(self.FORCE_DELETE_PARAM, '0').strip() == '1'
+        serializer = OrderAssigneeSerializer(
+            many=True,
+            data=request.data,
+            context={
+                'order': order,
+                'modified_by': self.request.user,
+                'force_delete': force_delete
+            }
+        )
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return self.get_list_response(order)
