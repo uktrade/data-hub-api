f994374bfd2931d70824234c36b58e5e7a2c225c
elcct <pawel@mikesz.com>
2017-08-14 11:15:54 +0100

    Fix query tests.

--- datahub/search/test/test_elasticsearch.py
+++ datahub/search/test/test_elasticsearch.py
@@ -69,6 +69,123 @@ def test_get_basic_search_query():
     assert query.to_dict() == {
         'query': {
             'bool': {
+                'should': [
+                    {
+                        'match_phrase': {
+                            'name_keyword': {
+                                'query': 'test',
+                                'boost': 2
+                            }
+                        }
+                    }, {
+                        'match_phrase': {
+                            'id': {
+                                'query': 'test'
+                            }
+                        }
+                    }, {
+                        'match': {
+                            'name': {
+                                'query': 'test'
+                            }
+                        }
+                    }, {
+                        'match_phrase': {
+                            'name_trigram': {
+                                'query': 'test'
+                            }
+                        }
+                    }, {
+                        'match': {
+                            'address_1': 'test'
+                        }
+                    }, {
+                        'match': {
+                            'address_2': 'test'
+                        }
+                    }, {
+                        'nested': {
+                            'path': 'address_country',
+                            'query': {
+                                'bool': {
+                                    'must': [
+                                        {
+                                            'match': {
+                                                'address_country.name': 'test'
+                                            }
+                                        }
+                                    ]
+                                }
+                            }
+                        }
+                    }, {
+                        'match': {
+                            'address_county': 'test'
+                        }
+                    }, {
+                        'match': {
+                            'address_town': 'test'
+                        }
+                    }, {
+                        'match': {
+                            'email': 'test'
+                        }
+                    }, {
+                        'match': {
+                            'notes': 'test'
+                        }
+                    }
+                ]
+            }
+        },
+        'post_filter': {
+            'bool': {
+                'should': [
+                    {
+                        'term': {
+                            '_type': 'contact'
+                        }
+                    }
+                ]
+            }
+        },
+        'aggs': {
+            'count_by_type': {
+                'terms': {
+                    'field': '_type'
+                }
+            }
+        },
+        'from': 5,
+        'size': 5
+    }
+
+
+def test_search_by_entity_query():
+    """Tests search by entity."""
+    date = '2017-06-13T09:44:31.062870'
+    filters = {
+        'address_town': ['Woodside'],
+        'trading_address_country.id': ['80756b9a-5d95-e211-a939-e4115bead28a'],
+    }
+    ranges = {
+        'estimated_land_date': {
+            'gte': date,
+            'lte': date
+        }
+    }
+    query = elasticsearch.get_search_by_entity_query(
+        term='test',
+        filters=filters,
+        ranges=ranges,
+        entity=Company,
+        offset=5,
+        limit=5
+    )
+
+    assert query.to_dict() == {
+        'query': {
+            'bool': {
                 'must': [
                     {
                         'term': {
@@ -80,8 +197,7 @@ def test_get_basic_search_query():
                                 {
                                     'match_phrase': {
                                         'name_keyword': {
-                                            'query': 'test',
-                                            'boost': 2
+                                            'query': 'test', 'boost': 2
                                         }
                                     }
                                 }, {
@@ -123,9 +239,10 @@ def test_get_basic_search_query():
                                         'query': {
                                             'bool': {
                                                 'must': [
-                                                    {'match': {
-                                                        'export_to_countries.name': 'test'
-                                                    }
+                                                    {
+                                                        'match': {
+                                                            'export_to_countries.name': 'test'
+                                                        }
                                                     }
                                                 ]
                                             }
@@ -200,152 +317,6 @@ def test_get_basic_search_query():
                                 }, {
                                     'match': {
                                         'trading_address_town': 'test'
-                                    }
-                                }, {
-                                    'nested': {
-                                        'path': 'uk_region',
-                                        'query': {
-                                            'bool': {
-                                                'must': [
-                                                    {
-                                                        'match': {
-                                                            'uk_region.name': 'test'
-                                                        }
-                                                    }
-                                                ]
-                                            }
-                                        }
-                                    }
-                                }, {
-                                    'match': {
-                                        'website': 'test'
-                                    }
-                                }
-                            ]
-                        }
-                    }
-                ]
-            }
-        },
-        'post_filter': {
-            'bool': {
-                'must': [
-                    {
-                        'bool': {
-                            'should': [
-                                {
-                                    'term': {
-                                        'address_town': 'Woodside'
-                                    }
-                                }
-                            ],
-                            'minimum_should_match': 1
-                        }
-                    }, {
-                        'bool': {
-                            'should': [
-                                {
-                                    'nested': {
-                                        'path': 'trading_address_country',
-                                        'query': {
-                                            'term': {
-                                                'trading_address_country.id':
-                                                    '80756b9a-5d95-e211-a939-e4115bead28a'
-                                            }
-                                        }
-                                    }
-                                }
-                            ],
-                            'minimum_should_match': 1
-                        }
-                    }, {
-                        'range': {
-                            'estimated_land_date': {
-                                'gte': '2017-06-13T09:44:31.062870',
-                                'lte': '2017-06-13T09:44:31.062870'
-                            }
-                        }
-                    }
-                ]
-            }
-        },
-        'from': 5,
-        'size': 5
-    }
-
-
-def test_search_by_entity_query():
-    """Tests search by entity."""
-    date = '2017-06-13T09:44:31.062870'
-    filters = {
-        'address_town': ['Woodside'],
-        'trading_address_country.id': ['80756b9a-5d95-e211-a939-e4115bead28a'],
-    }
-    ranges = {
-        'estimated_land_date': {
-            'gte': date,
-            'lte': date
-        }
-    }
-    query = elasticsearch.get_search_by_entity_query(
-        term='test',
-        filters=filters,
-        ranges=ranges,
-        entity=Company,
-        offset=5,
-        limit=5
-    )
-
-    assert query.to_dict() == {
-        'query': {
-            'bool': {
-                'must': [
-                    {
-                        'term': {
-                            '_type': 'company'
-                        }
-                    }, {
-                        'bool': {
-                            'should': [
-                                {
-                                    'match_phrase': {
-                                        'name_keyword': {
-                                            'query': 'test',
-                                            'boost': 2
-                                        }
-                                    }
-                                }, {
-                                    'match_phrase': {
-                                        'id': {
-                                            'query': 'test'
-                                        }
-                                    }
-                                }, {
-                                    'match': {
-                                        'name': {
-                                            'query': 'test'
-                                        }
-                                    }
-                                }, {
-                                    'match_phrase': {
-                                        'name_trigram': {
-                                            'query': 'test'
-                                        }
-                                    }
-                                }, {
-                                    'nested': {
-                                        'path': 'trading_address_country',
-                                        'query': {
-                                            'bool': {
-                                                'must': [
-                                                    {
-                                                        'match': {
-                                                            'trading_address_country.name': 'test'
-                                                        }
-                                                    }
-                                                ]
-                                            }
-                                        }
                                     }
                                 }, {
                                     'nested': {
