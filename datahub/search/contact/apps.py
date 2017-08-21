319ff99577da20d7dc4915c3b4dfbfab9c0bf760
Reupen Shah <reupen@users.noreply.github.com>
2017-08-21 14:24:57 +0100

    Move search apps from __init__.py to separate module

---
+++ datahub/search/contact/apps.py
@@ -0,0 +1,16 @@
+from datahub.company.models import Contact as DBContact
+
+from .models import Contact
+from .views import SearchContactAPIView
+
+from ..apps import SearchApp
+
+
+class ContactSearchApp(SearchApp):
+    """SearchApp for contacts"""
+
+    name = 'contact'
+    plural_name = 'contacts'
+    ESModel = Contact
+    DBModel = DBContact
+    view = SearchContactAPIView
