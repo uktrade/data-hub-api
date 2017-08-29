1963af5daa27794309c37acc520e9755f89b0422
Reupen Shah <reupen@users.noreply.github.com>
2017-08-29 15:41:02 +0100

    Add queryset class attribute to search app
    So subclasses don't need to override get_dataset()

--- datahub/search/interaction/apps.py
+++ datahub/search/interaction/apps.py
@@ -4,7 +4,6 @@ from datahub.interaction.queryset import get_interaction_queryset_v3
 from datahub.search.apps import SearchApp
 from datahub.search.interaction.models import Interaction
 from datahub.search.interaction.views import SearchInteractionAPIView
-from datahub.search.models import DataSet
 
 
 class InteractionSearchApp(SearchApp):
@@ -15,9 +14,4 @@ class InteractionSearchApp(SearchApp):
     ESModel = Interaction
     DBModel = DBInteraction
     view = SearchInteractionAPIView
-
-    def get_dataset(self):
-        """Returns dataset that will be synchronised with Elasticsearch."""
-        queryset = get_interaction_queryset_v3().order_by('pk')
-
-        return DataSet(queryset, self.ESModel)
+    queryset = get_interaction_queryset_v3()
