55dd4e3455640d0ac6b2d21c8df90ba230d9309c
Reupen Shah <reupen@users.noreply.github.com>
2017-09-01 16:57:36 +0100

    [DH-526] Add event programme fixtures and constants

---
+++ datahub/event/constants.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+from datahub.core.constants import Constant
+
+
+class Programme(Enum):
+    """Programme constants."""
+
+    great_branded = Constant('Great Branded', '3117da1b-ac76-4b03-af7e-2487d931491c')
+    grown_in_britain = Constant('Grown in Britain', 'd352a68f-aaf4-4c43-b39d-9bca67a8322e')
