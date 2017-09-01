6f0cefea576eb73f67e404374e7fdcd24b6dc656
Reupen Shah <reupen@users.noreply.github.com>
2017-09-01 17:01:06 +0100

    [DH-529] Add event serialiser

---
+++ datahub/event/serializers.py
@@ -0,0 +1,46 @@
+from rest_framework import serializers
+
+from datahub.core.serializers import NestedRelatedField
+from datahub.event.models import Event
+
+
+class EventSerializer(serializers.ModelSerializer):
+    """Event serialiser."""
+
+    lead_team = NestedRelatedField('metadata.Team', required=False, allow_null=True)
+    additional_teams = NestedRelatedField(
+        'metadata.Team', many=True, required=False, allow_empty=True
+    )
+    address_country = NestedRelatedField('metadata.Country')
+    related_programmes = NestedRelatedField(
+        'event.Programme', many=True, required=False, allow_empty=True
+    )
+
+    class Meta:  # noqa: D101
+        model = Event
+        extra_kwargs = {
+            # As these don't have null=True, DRF defaults to required=True
+            'address_2': {'required': False},
+            'address_postcode': {'required': False},
+            'location_type': {'required': False},
+            'notes': {'required': False},
+        }
+        fields = (
+            'additional_teams',
+            'address_1',
+            'address_2',
+            'address_country',
+            'address_country',
+            'address_county',
+            'address_postcode',
+            'address_town',
+            'end_date',
+            'event_type',
+            'id',
+            'lead_team',
+            'location_type',
+            'name',
+            'notes',
+            'related_programmes',
+            'start_date',
+        )
