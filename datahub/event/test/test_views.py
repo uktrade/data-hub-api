ad1f4ef7d4689e6f551229ca6c77ceb8c8b8ecc3
Reupen Shah <reupen@users.noreply.github.com>
2017-09-05 09:45:49 +0100

    [DH-529] Change event additional_teams to teams
    The teams field includes the lead team, to make changing the lead team easier.

--- datahub/event/test/test_views.py
+++ datahub/event/test/test_views.py
@@ -44,7 +44,7 @@ class TestEventViews(APITestMixin):
                 'name': Country.united_kingdom.value.name,
             },
             'lead_team': None,
-            'additional_teams': [],
+            'teams': [],
             'related_programmes': []
         }
 
@@ -65,7 +65,7 @@ class TestEventViews(APITestMixin):
             'address_postcode': 'SW9 9AA',
             'address_country': Country.united_kingdom.value.id,
             'lead_team': Team.crm.value.id,
-            'additional_teams': [Team.healthcare_uk.value.id],
+            'teams': [Team.crm.value.id, Team.healthcare_uk.value.id],
             'related_programmes': [Programme.great_branded.value.id]
         }
         response = self.api_client.post(url, format='json', data=request_data)
@@ -99,7 +99,10 @@ class TestEventViews(APITestMixin):
                 'id': Team.crm.value.id,
                 'name': Team.crm.value.name,
             },
-            'additional_teams': [{
+            'teams': [{
+                'id': Team.crm.value.id,
+                'name': Team.crm.value.name,
+            }, {
                 'id': Team.healthcare_uk.value.id,
                 'name': Team.healthcare_uk.value.name,
             }],
@@ -107,6 +110,26 @@ class TestEventViews(APITestMixin):
                 'id': Programme.great_branded.value.id,
                 'name': Programme.great_branded.value.name,
             }]
+        }
+
+    def test_create_lead_team_not_in_teams(self):
+        """Tests specifying a lead team that isn't in the teams array."""
+        url = reverse('api-v3:event:collection')
+        request_data = {
+            'name': 'Grand exhibition',
+            'event_type': EventType.seminar.value.id,
+            'address_1': 'Grand Court Exhibition Centre',
+            'address_town': 'London',
+            'address_country': Country.united_kingdom.value.id,
+            'lead_team': Team.crm.value.id,
+            'teams': [Team.healthcare_uk.value.id],
+        }
+        response = self.api_client.post(url, format='json', data=request_data)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        response_data = response.json()
+        assert response_data == {
+            'lead_team': ['Lead team must be in teams array.']
         }
 
     def test_create_omitted_failure(self):
