fc802e36df452de4965e7c9afacc643fc9baa2d4
Reupen Shah <reupen@users.noreply.github.com>
2017-09-11 15:19:11 +0100

    Use async (gevent) gunicorn workers

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -6,3 +6,5 @@ access_log_format = os.environ.get(
     '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" '
     '%({X-Forwarded-For}i)s'
 )
+worker_class = os.environ.get('GUNICORN_WORKER_CLASS', 'gevent')
+worker_connections = os.environ.get('GUNICORN_WORKER_CONNECTIONS', '10')
