57c3caf5e9a2a93ebb9fc841afde38aa1245f46a
Marco Fucci <marcofucci@gmail.com>
2017-09-13 14:06:14 +0100

    Add VAT fields to OMIS orders
    This:
    - adds the fields:
    	- vat_status = (eu, outside_eu or uk)
    	- vat_number (only if vat_status is eu)
    	- vat_verified (only if vat_status is eu)
    - validates that
    	- if vat_status = eu, vat_verified has to be specified
    	(can be True or False, by default it's null)
    	- if vat_status = eu and vat_verified = True,
    	vat_number has to be specified as well
    The validation is triggered only when generating a quote to allow people to
    fill in an order incrementally based on the information they have at
    the time and validate just at the end when moving to a new state.

--- datahub/omis/order/constants.py
+++ datahub/omis/order/constants.py
@@ -15,3 +15,10 @@ OrderStatus = Choices(
 
 
 DEFAULT_HOURLY_RATE = '7e1ca5c3-dc5a-e511-9d3c-e4115bead28a'
+
+
+VatStatus = Choices(
+    ('eu', 'Europe'),
+    ('outside_eu', 'Outside Europe'),
+    ('uk', 'UK'),
+)
