07f4211809c199de2a59109f5d81751abc8b5088
Reupen Shah <reupen@users.noreply.github.com>
2017-09-15 15:30:49 +0100

    [DH-520] Reformat list of keys in event search model test
    Also remove unnecessary set conversion.

--- datahub/search/event/tests/test_models.py
+++ datahub/search/event/tests/test_models.py
@@ -12,13 +12,30 @@ def test_event_dbmodel_to_dict():
 
     result = ESEvent.dbmodel_to_dict(event)
 
-    keys = {'id', 'event_type', 'location_type', 'address_country',
-            'organiser', 'lead_team', 'teams', 'related_programmes',
-            'created_on', 'modified_on', 'name', 'start_date', 'end_date',
-            'address_1', 'address_2', 'address_town', 'address_county',
-            'address_postcode', 'notes', 'uk_region'}
+    keys = {
+        'id',
+        'event_type',
+        'location_type',
+        'address_country',
+        'organiser',
+        'lead_team',
+        'teams',
+        'related_programmes',
+        'created_on',
+        'modified_on',
+        'name',
+        'start_date',
+        'end_date',
+        'address_1',
+        'address_2',
+        'address_town',
+        'address_county',
+        'address_postcode',
+        'notes',
+        'uk_region',
+    }
 
-    assert set(result.keys()) == keys
+    assert result.keys() == keys
 
 
 def test_event_dbmodels_to_es_documents():
