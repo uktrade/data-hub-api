dcc23a1eb9a96ddc27e71296454695b6db0fb1e9
Marco Fucci <marcofucci@gmail.com>
2017-09-21 14:16:25 +0100

    Add OMIS order get endpoint for public facing app
    This adds `GET /v3/omis/public/order/<public-token>` which returns
    the details of a order.
    The response data is slightly different from the ordinary order
    endpoints as the public facing app needs less details.
    Also, the nested company is expanded as the app needs extra data
    and adding an extra company endpoint is overkilled.
    
    An order can only be accessed by its public token and only if
    its status is not in draft/cancelled.

--- datahub/omis/urls.py
+++ datahub/omis/urls.py
@@ -3,7 +3,11 @@ from django.conf.urls import include, url
 from .order import urls as order_urls
 from .quote import urls as quote_urls
 
-urlpatterns = [
-    url(r'^', include((order_urls, 'order'), namespace='order')),
+internal_frontend_urls = [
+    url(r'^', include((order_urls.internal_frontend_urls, 'order'), namespace='order')),
     url(r'^', include((quote_urls, 'quote'), namespace='quote')),
+]
+
+public_urls = [
+    url(r'^', include((order_urls.public_urls, 'order'), namespace='order')),
 ]
