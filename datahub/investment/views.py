0aecce54ead38a336b6f52194a20378cf58194a3
elcct <pawel@mikesz.com>
2017-09-25 17:02:20 +0100

    Fix import in investment views.

--- datahub/investment/views.py
+++ datahub/investment/views.py
@@ -4,9 +4,7 @@ from django.db.models import Prefetch
 from django.http import Http404
 from django.shortcuts import get_object_or_404
 from django_filters import IsoDateTimeFilter
-from django_filters.rest_framework import (
-    DjangoFilterBackend, FilterSet
-)
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
 from rest_framework import status
 from rest_framework.filters import OrderingFilter
 from rest_framework.pagination import BasePagination
