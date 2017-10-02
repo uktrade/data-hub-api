c5ea9aef71b2831fd81d7691395ef57936188405
Reupen Shah <reupen@users.noreply.github.com>
2017-10-02 15:44:11 +0100

    [DH-784] Add metadata fixture auto-registration mechanism
    Only used for interactions for now.

--- datahub/interaction/metadata.py
+++ datahub/interaction/metadata.py
@@ -1,5 +1,14 @@
 from datahub.interaction import models
+from datahub.metadata.fixtures import Fixture
 from datahub.metadata.registry import registry
+
+
+class InteractionFixtures(Fixture):
+    """Metadata fixtures (for the loadmetadata command)."""
+
+    files = [
+        'fixtures/communication_channels.yaml'
+    ]
 
 
 registry.register(
