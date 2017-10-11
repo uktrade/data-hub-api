0efd0a9ee3d10faaeb794481e614b38b3cf72428
Marco Fucci <marcofucci@gmail.com>
2017-10-11 14:41:41 +0100

    Add get order payments endpoints
    This adds two endpoints:
    - /v3/omis/order/<order-id>/payment
    returning a paginated list of payments for the order
    - /v3/omis/public/order/<public-token>/payment
    returning the same response body

---
+++ datahub/omis/payment/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import url
+
+from .views import PaymentViewSet, PublicPaymentViewSet
+
+
+# internal frontend API
+internal_frontend_urls = [
+    url(
+        r'^order/(?P<order_pk>[0-9a-z-]{36})/payment$',
+        PaymentViewSet.as_view({'get': 'list'}),
+        name='collection'
+    ),
+]
+
+# public facing API
+public_urls = [
+    url(
+        r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/payment$',
+        PublicPaymentViewSet.as_view({'get': 'list'}),
+        name='collection'
+    ),
+]
