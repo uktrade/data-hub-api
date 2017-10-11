feb40bfa2f02d5f375eaed7f4df3fe86760d7226
Marco Fucci <marcofucci@gmail.com>
2017-10-11 16:42:17 +0100

    Rename payment_received_on to received_on
    This is because the payment_ prefix is redundant.

--- datahub/omis/payment/tests/factories.py
+++ datahub/omis/payment/tests/factories.py
@@ -16,7 +16,7 @@ class PaymentFactory(factory.django.DjangoModelFactory):
     additional_reference = factory.Faker('pystr')
     amount = factory.Faker('random_int', max=100)
     method = constants.PaymentMethod.bacs
-    payment_received_on = factory.Faker('date_time')
+    received_on = factory.Faker('date_time')
 
     class Meta:  # noqa: D101
         model = 'omis-payment.Payment'
