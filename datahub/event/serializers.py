522d5ddbb20bbd425ac8ff0ab8123409b2007303
elcct <pawel@mikesz.com>
2017-10-30 10:51:54 +0000

    [DH-825][DH-927] Make start_date and end_date mandatory.

--- datahub/event/serializers.py
+++ datahub/event/serializers.py
@@ -19,7 +19,7 @@ class EventSerializer(serializers.ModelSerializer):
         'uk_region_non_uk_country': ugettext_lazy('Cannot specify a UK region for a non-UK '
                                                   'country.')
     }
-
+    end_date = serializers.DateField()
     event_type = NestedRelatedField('event.EventType')
     location_type = NestedRelatedField('event.LocationType', required=False, allow_null=True)
     organiser = NestedAdviserField(required=False, allow_null=True)
@@ -31,6 +31,7 @@ class EventSerializer(serializers.ModelSerializer):
         'event.Programme', many=True, required=False, allow_empty=True
     )
     service = NestedRelatedField('metadata.Service')
+    start_date = serializers.DateField()
 
     def validate(self, data):
         """Performs cross-field validation."""
@@ -66,11 +67,8 @@ class EventSerializer(serializers.ModelSerializer):
         start_date = combiner.get_value('start_date')
         end_date = combiner.get_value('end_date')
 
-        if end_date:
-            if not start_date:
-                errors['end_date'] = self.error_messages['end_date_without_start_date']
-            elif end_date < start_date:
-                errors['end_date'] = self.error_messages['end_date_before_start_date']
+        if end_date < start_date:
+            errors['end_date'] = self.error_messages['end_date_before_start_date']
 
         return errors
 
