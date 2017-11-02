5dedffb5627523eb090b875db5b740b1faab9581
Marco Fucci <marcofucci@gmail.com>
2017-11-02 15:17:44 +0000

    Update and refactor OMIS order fixtures
    This moves the metadata fixtures to the specific django app
    and updates the values in cancellation_reasons

--- datahub/omis/core/management/commands/load_omis_metadata.py
+++ datahub/omis/core/management/commands/load_omis_metadata.py
@@ -1,10 +1,5 @@
-from pathlib import PurePath
-
 from django.core.management import call_command
 from django.core.management.base import BaseCommand
-
-
-METADATA_FIXTURE_DIR = PurePath(__file__).parents[3] / 'fixtures'
 
 
 class Command(BaseCommand):
@@ -12,11 +7,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """It loads all OMIS metadata fixtures."""
-        call_command(
-            'loaddata',
-            *[
-                METADATA_FIXTURE_DIR / 'service_types.yaml',
-                METADATA_FIXTURE_DIR / 'cancellation_reasons.yaml',
-            ]
-        )
         call_command('load_initial_omis_markets')
