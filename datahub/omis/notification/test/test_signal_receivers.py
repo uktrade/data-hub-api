a3a117ed81ab7ecd2f117679a86776a182c345e9
Marco Fucci <marcofucci@gmail.com>
2017-11-08 17:37:53 +0000

    Notify advisers when added to OMIS orders
    This sends a notification to OMIS advisers when they are added
    to an order.

--- datahub/omis/notification/test/test_signal_receivers.py
+++ datahub/omis/notification/test/test_signal_receivers.py
@@ -1,12 +1,18 @@
+from unittest import mock
 import pytest
 
-from datahub.omis.order.test.factories import OrderFactory
+from datahub.core.test_utils import synchronous_executor_submit
+from datahub.omis.order.test.factories import (
+    OrderAssigneeFactory, OrderFactory, OrderSubscriberFactory
+)
 
 from ..client import notify
+from ..constants import Template
 
 pytestmark = pytest.mark.django_db
 
 
+@mock.patch('datahub.core.utils.executor.submit', synchronous_executor_submit)
 class TestNotifyPostSaveOrder:
     """Tests for notifications sent when an order is saved/updated."""
 
@@ -30,6 +36,7 @@ class TestNotifyPostSaveOrder:
         assert not notify.client.send_email_notification.called
 
 
+@mock.patch('datahub.core.utils.executor.submit', synchronous_executor_submit)
 class TestNotifyPostQuoteGenerated:
     """Tests for notifications sent when a quote is generated."""
 
@@ -42,3 +49,38 @@ class TestNotifyPostQuoteGenerated:
         order.generate_quote(by=None)
 
         assert notify.client.send_email_notification.called
+        call_args = notify.client.send_email_notification.call_args_list[0][1]
+        assert call_args['template_id'] == Template.quote_awaiting_acceptance_for_customer.value
+
+
+@mock.patch('datahub.core.utils.executor.submit', synchronous_executor_submit)
+class TestNofityPostSaveOrderAdviser:
+    """Tests for notifications sent when an adviser is added to an order."""
+
+    def test_notify_on_order_assignee_added(self):
+        """
+        Test that a notification is sent to the adviser when they get assigned to an order.
+        """
+        order = OrderFactory(assignees=[])
+
+        notify.client.reset_mock()
+
+        assignee = OrderAssigneeFactory(order=order)
+        assert notify.client.send_email_notification.called
+        call_args = notify.client.send_email_notification.call_args_list[0][1]
+        assert call_args['email_address'] == assignee.adviser.contact_email
+        assert call_args['template_id'] == Template.you_have_been_added_for_adviser.value
+
+    def test_notify_on_order_subscriber_added(self):
+        """
+        Test that a notification is sent to the adviser when they get subscribed to an order.
+        """
+        order = OrderFactory(assignees=[])
+
+        notify.client.reset_mock()
+
+        subscriber = OrderSubscriberFactory(order=order)
+        assert notify.client.send_email_notification.called
+        call_args = notify.client.send_email_notification.call_args_list[0][1]
+        assert call_args['email_address'] == subscriber.adviser.contact_email
+        assert call_args['template_id'] == Template.you_have_been_added_for_adviser.value
