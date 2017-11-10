6b37c99edd2429b6bd831c613618fdcc340e9d30
Reupen Shah <reupen@users.noreply.github.com>
2017-11-10 15:33:06 +0000

    Remove CDMS authentication

--- tests.sh
+++ tests.sh
@@ -1,6 +1,2 @@
 #!/bin/bash -xe
-# Auth tests have to be run using the pytest live_server fixture. The live server is spawned in the background in a different thread
-# For some obscure reason all the tests that run after the auth ones get stuck into using this different thread process
-# which doesn't have any access to the testing db hence there are no fixtures and the tests fail.
-# The solution is to run the auth tests independently.
-pytest --cov -k "not liveserver" -s
+pytest --cov -s
