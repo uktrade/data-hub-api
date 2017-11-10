4db3ee23dceccb76dd26b92494ce27574a2028a4
Marco Fucci <marcofucci@gmail.com>
2017-11-10 11:16:07 +0000

    Notify customer/advisers on order cancelled
    This sends a notification to the customer and all the advisers
    on the order when this gets cancelled.

--- datahub/omis/notification/constants.py
+++ datahub/omis/notification/constants.py
@@ -9,3 +9,5 @@ class Template(Enum):
     you_have_been_added_for_adviser = '1c631f72-4d33-41f5-ba9b-12862b5b273a'
     quote_sent_for_customer = '5b68a6e2-539f-4b1b-8c54-0ead23c7ca1b'
     quote_sent_for_adviser = '77157de5-43b3-4988-a4e9-c6073c18be47'
+    order_cancelled_for_customer = '3f30d3b0-78cb-4b1b-a2bc-9c19b0aeedf4'
+    order_cancelled_for_adviser = '723583be-af37-4e13-b2b0-ea496de5450e'
