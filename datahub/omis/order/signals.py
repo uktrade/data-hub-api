1bbb1ad7db37b4ed310fdd776ac14ce4b19d3233
Marco Fucci <marcofucci@gmail.com>
2017-11-14 13:45:59 +0000

    Notify customer/advisers on quote accepted
    This sends a notification to the customer and all the advisers
    on the order when the related quote gets accepted by the customer.

--- datahub/omis/order/signals.py
+++ datahub/omis/order/signals.py
@@ -3,3 +3,4 @@ import django.dispatch
 
 quote_generated = django.dispatch.Signal(providing_args=['order'])
 order_cancelled = django.dispatch.Signal(providing_args=['order'])
+quote_accepted = django.dispatch.Signal(providing_args=['order'])
