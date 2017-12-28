f79c5a36e54144f2689fc75923ed78ddfbc99482
elcct <pawel@mikesz.com>
2017-12-28 15:56:06 +0000

    Improved serializer.

--- datahub/metadata/serializers.py
+++ datahub/metadata/serializers.py
@@ -1,19 +1,11 @@
-from rest_framework import serializers
-
-from datahub.core.serializers import NestedRelatedField
+from datahub.core.serializers import ConstantModelSerializer, NestedRelatedField
 
 from .models import Country, TeamRole, UKRegion
 
 
-class TeamSerializer(serializers.Serializer):
+class TeamSerializer(ConstantModelSerializer):
     """Team serializer."""
 
-    id = serializers.ReadOnlyField()
-    name = serializers.ReadOnlyField()
     role = NestedRelatedField(TeamRole, read_only=True)
     uk_region = NestedRelatedField(UKRegion, read_only=True)
     country = NestedRelatedField(Country, read_only=True)
-    disabled_on = serializers.DateTimeField(allow_null=True, read_only=True)
-
-    class Meta:
-        fields = '__all__'
