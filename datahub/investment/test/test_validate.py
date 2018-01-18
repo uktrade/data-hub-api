9d2c703a66f85df8cd880e91331070cc5e553de1
Reupen Shah <reupen@users.noreply.github.com>
2018-01-18 14:21:55 +0000

    Add validation for investment delivery partners
    DH-1423
    To be merged once the field is in the front end.

--- datahub/investment/test/test_validate.py
+++ datahub/investment/test/test_validate.py
@@ -3,6 +3,7 @@ import pytest
 from datahub.company.test.factories import AdviserFactory, ContactFactory
 from datahub.core import constants
 from datahub.core.test_utils import random_obj_for_model
+from datahub.investment.models import InvestmentDeliveryPartner
 from datahub.investment.serializers import (
     IProjectRequirementsSerializer, IProjectSummarySerializer, IProjectTeamSerializer,
     IProjectValueSerializer
@@ -287,6 +288,7 @@ def test_validate_verify_win_instance_failure():
         'address_town': 'This field is required.',
         'address_postcode': 'This field is required.',
         'actual_uk_regions': 'This field is required.',
+        'delivery_partners': 'This field is required.',
         'average_salary': 'This field is required.',
         'client_cannot_provide_foreign_investment': 'This field is required.',
         'foreign_equity_investment': 'This field is required.',
@@ -352,7 +354,8 @@ def test_validate_verify_win_instance_with_cond_fields():
         address_town='London',
         address_postcode='SW1A 2AA',
         actual_uk_regions=[random_obj_for_model(UKRegion)],
-        average_salary_id=constants.SalaryRange.below_25000.value.id
+        delivery_partners=[random_obj_for_model(InvestmentDeliveryPartner)],
+        average_salary_id=constants.SalaryRange.below_25000.value.id,
     )
     errors = validate(instance=project)
     assert not errors
