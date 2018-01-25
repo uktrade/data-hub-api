d1ba832f1baf4ca6dcb2723f6f59da29b60933d3
Reupen Shah <reupen@users.noreply.github.com>
2018-01-25 15:39:54 +0000

    Load Companies House data without first truncating the table
    DH-1471
    
    Previously, the sync_ch command truncated the company_companieshousecompany table and then loaded the Companies House data from scratch.
    
    This was problematic as it could only be done when the system was not in use.
    
    This change makes the job update the table without truncating it first. This avoids the need for downtime (but with the cost of a slower update). Doing this kind of update is excessively slow when using the Django ORM, so native PostgreSQL INSERT ... ON CONFLICT SQL statements are used instead.

--- datahub/company/test/test_sync_ch_command.py
+++ datahub/company/test/test_sync_ch_command.py
@@ -1,12 +1,18 @@
 from datetime import date
 from os import path
 from unittest import mock
+from uuid import UUID
 
 import pytest
 from django.conf import settings
+from django.db import connection
 
 from datahub.company.management.commands import sync_ch
 from datahub.company.models import CompaniesHouseCompany
+from datahub.company.test.factories import CompaniesHouseCompanyFactory
+from datahub.core.constants import Country
+
+pytestmark = pytest.mark.django_db
 
 
 @mock.patch('datahub.company.management.commands.sync_ch.requests')
@@ -109,16 +115,82 @@ def test_process_row_company_filtering(row, num_results):
     assert len(processed) == num_results
 
 
+def test_process_rows_record():
+    """Tests that records are updated or inserted as needed."""
+    existing_ch_company = CompaniesHouseCompanyFactory(
+        company_number='00000001',
+        name='old name',
+    )
+
+    existing_record_update_data = {
+        'company_number': '00000001',
+        'company_category': 'company cat 1',
+        'company_status': 'status 1',
+        'incorporation_date': date(2017, 1, 1),
+        'name': 'name 1',
+        'registered_address_1': 'add 1-1',
+        'registered_address_2': 'add 1-2',
+        'registered_address_country_id': UUID(Country.united_kingdom.value.id),
+        'registered_address_county': 'county 1',
+        'registered_address_postcode': 'postcode 1',
+        'registered_address_town': 'town 1',
+        'sic_code_1': 'sic code 1-1',
+        'sic_code_2': 'sic code 1-2',
+        'sic_code_3': 'sic code 1-3',
+        'sic_code_4': 'sic code 1-4',
+        'uri': 'uri 1',
+    }
+    new_record_data = {
+        'company_number': '00000002',
+        'company_category': 'company cat 2',
+        'company_status': 'status 2',
+        'incorporation_date': date(2016, 12, 1),
+        'name': 'name 2',
+        'registered_address_1': 'add 2-1',
+        'registered_address_2': 'add 2-2',
+        'registered_address_country_id': UUID(Country.united_kingdom.value.id),
+        'registered_address_county': 'county 2',
+        'registered_address_postcode': 'postcode 2',
+        'registered_address_town': 'town 2',
+        'sic_code_1': 'sic code 2-1',
+        'sic_code_2': 'sic code 2-2',
+        'sic_code_3': 'sic code 2-3',
+        'sic_code_4': 'sic code 2-4',
+        'uri': 'uri 2',
+    }
+
+    with connection.cursor() as cursor:
+        syncer = sync_ch.CHSynchroniser()
+        syncer._process_batch(cursor, [existing_record_update_data, new_record_data])
+
+    existing_ch_company.refresh_from_db()
+    actual_updated_data = {
+        field: getattr(existing_ch_company, field) for field in existing_record_update_data
+    }
+
+    new_ch_company = CompaniesHouseCompany.objects.get(
+        company_number=new_record_data['company_number']
+    )
+    actual_new_data = {
+        field: getattr(new_ch_company, field) for field in new_record_data
+    }
+
+    assert CompaniesHouseCompany.objects.count() == 2
+    assert actual_updated_data == existing_record_update_data
+    assert actual_new_data == new_record_data
+
+
 @pytest.mark.django_db
 @mock.patch('datahub.company.management.commands.sync_ch.stream_to_file_pointer', mock.MagicMock())
 @mock.patch('datahub.company.management.commands.sync_ch.get_ch_latest_dump_file_list')
 def test_full_ch_sync(file_list_mock, settings):
     """Test the whole process."""
-    settings.BULK_CREATE_BATCH_SIZE = 2
+    settings.BULK_INSERT_BATCH_SIZE = 2
     file_list_mock.return_value = ['irrelevant']
     fixture_loc = path.join(path.dirname(__file__), 'fixtures', 'CH_data_test.zip')
 
-    sync_ch.sync_ch(tmp_file_creator=lambda: open(fixture_loc, 'rb'))
+    syncer = sync_ch.CHSynchroniser()
+    syncer.run(tmp_file_creator=lambda: open(fixture_loc, 'rb'))
 
     # Two companies with irrelevant types excluded
     assert CompaniesHouseCompany.objects.count() == 198
@@ -129,11 +201,12 @@ def test_full_ch_sync(file_list_mock, settings):
 @mock.patch('datahub.company.management.commands.sync_ch.get_ch_latest_dump_file_list')
 def test_simulated_ch_sync(file_list_mock, settings):
     """Test the whole process."""
-    settings.BULK_CREATE_BATCH_SIZE = 2
+    settings.BULK_INSERT_BATCH_SIZE = 2
     file_list_mock.return_value = ['irrelevant']
     fixture_loc = path.join(path.dirname(__file__), 'fixtures', 'CH_data_test.zip')
 
-    sync_ch.sync_ch(tmp_file_creator=lambda: open(fixture_loc, 'rb'), simulate=True)
+    syncer = sync_ch.CHSynchroniser(simulate=True)
+    syncer.run(tmp_file_creator=lambda: open(fixture_loc, 'rb'))
 
     # No companies house companies should have been synced
     assert CompaniesHouseCompany.objects.count() == 0
