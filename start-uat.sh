572240ef7e63121fb290f7990e5ad41fe658ea90
Graham Veal <grahamveal@gmail.com>
2018-03-01 17:33:13 +0000

    Update script after review

--- start-uat.sh
+++ start-uat.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-clear;
-docker-compose stop;
-docker-compose rm -f;
-docker-compose build \
-  && docker-compose run --publish 8000:8000 leeloo bash setup-uat.sh;
+clear
+docker-compose down
+docker-compose build
+docker-compose run --publish 8000:8000 leeloo bash setup-uat.sh
