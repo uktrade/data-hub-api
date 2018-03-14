12e5d24baa7c406900ffa5cf8541538e9a029680
Reupen Shah <reupen@users.noreply.github.com>
2018-03-14 10:42:12 +0000

    Close old and unusable connections when running thread-pool tasks

--- datahub/core/utils.py
+++ datahub/core/utils.py
@@ -5,6 +5,7 @@ from logging import getLogger
 
 import boto3
 import requests
+from django.db import close_old_connections
 from raven.contrib.django.raven_compat.models import client
 
 logger = getLogger(__name__)
@@ -67,14 +68,23 @@ def _submit_to_thread_pool(fn, *args, **kwargs):
 
 
 def _make_thread_pool_task(fn, *args, **kwargs):
+    """
+    Wraps a task with exception handling and old- and broken-connection clean-up.
+
+    close_old_connections() is called both before and after the execution of the task to mimic
+    what Django does with requests.
+    """
     def _task():
         try:
+            close_old_connections()
             fn(*args, **kwargs)
         except Exception:
             msg = f'Error running thread pool task {fn.__name__}'
             logger.exception(msg)
             client.captureException(msg)
             raise
+        finally:
+            close_old_connections()
     return _task
 
 
