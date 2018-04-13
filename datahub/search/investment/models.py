e1e56b462cba2fda88dabc4ce91aa39fdd21b2f8
Reupen Shah <reupen@users.noreply.github.com>
2018-04-13 09:57:07 +0100

    Raise error in dbmodel_to_dict() if field does not exist on model
    Previously, a default value was being passed to getattr() when getting attributes from a model object which was hiding bugs (where incorrect fields had been specified on the ES model).
    
    This stops passing a default value to getattr() and also removes the incorrect fields that had been specified.

--- datahub/search/investment/models.py
+++ datahub/search/investment/models.py
@@ -18,7 +18,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
     approved_landed = Boolean()
     approved_non_fdi = Boolean()
     actual_land_date = Date()
-    actual_land_date_documents = dsl_utils.id_uri_mapping()
     business_activities = dsl_utils.id_name_mapping()
     client_contacts = dsl_utils.contact_or_adviser_mapping('client_contacts')
     client_relationship_manager = dsl_utils.contact_or_adviser_mapping(
@@ -44,7 +43,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
     anonymous_description = dsl_utils.EnglishText()
     estimated_land_date = Date()
     fdi_type = dsl_utils.id_name_mapping()
-    fdi_type_documents = dsl_utils.id_uri_mapping()
     fdi_value = dsl_utils.id_name_mapping()
     intermediate_company = dsl_utils.id_name_mapping()
     uk_company = dsl_utils.id_name_partial_mapping('uk_company')
@@ -71,7 +69,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
     total_investment = Double()
     foreign_equity_investment = Double()
     number_new_jobs = Integer()
-    operations_commenced_documents = dsl_utils.id_uri_mapping()
     stage = dsl_utils.id_name_mapping()
     project_code = dsl_utils.SortableCaseInsensitiveKeywordText(copy_to='project_code_trigram')
     project_code_trigram = dsl_utils.TrigramText()
@@ -90,7 +87,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
 
     MAPPINGS = {
         'id': str,
-        'actual_land_date_documents': lambda col: [dict_utils.id_uri_dict(c) for c in col.all()],
         'business_activities': lambda col: [dict_utils.id_name_dict(c) for c in col.all()],
         'client_contacts': lambda col: [dict_utils.contact_or_adviser_dict(c) for c in col.all()],
         'client_relationship_manager': dict_utils.adviser_dict_with_team,
@@ -101,7 +97,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
             dict_utils.contact_or_adviser_dict(c.adviser, include_dit_team=True) for c in col.all()
         ],
         'fdi_type': dict_utils.id_name_dict,
-        'fdi_type_documents': lambda col: [dict_utils.id_uri_dict(c) for c in col.all()],
         'fdi_value': dict_utils.id_name_dict,
         'intermediate_company': dict_utils.id_name_dict,
         'investor_company': dict_utils.id_name_dict,
@@ -117,9 +112,6 @@ class InvestmentProject(DocType, MapDBModelToDict):
         'uk_company': dict_utils.id_name_dict,
         'investment_type': dict_utils.id_name_dict,
         'associated_non_fdi_r_and_d_project': dict_utils.investment_project_dict,
-        'operations_commenced_documents': lambda col: [
-            dict_utils.id_uri_dict(c) for c in col.all()
-        ],
         'stage': dict_utils.id_name_dict,
         'referral_source_activity': dict_utils.id_name_dict,
         'referral_source_activity_marketing': dict_utils.id_name_dict,
