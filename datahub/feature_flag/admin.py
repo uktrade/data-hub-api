a00fc67bdb97e5b11863dd90b8c3807ae7b1a5e1
elcct <pawel@mikesz.com>
2018-04-13 16:39:58 +0100

    Change fields in admin.

--- datahub/feature_flag/admin.py
+++ datahub/feature_flag/admin.py
@@ -7,9 +7,19 @@ from datahub.feature_flag.models import FeatureFlag
 class FeatureFlagAdmin(admin.ModelAdmin):
     """Feature flag admin."""
 
-    fields = ('code', 'description', 'is_active',)
     list_display = ('code', 'description', 'is_active', 'created_by', 'created_on',)
     search_fields = ('code',)
+    readonly_fields = (
+        'id',
+        'created_by',
+        'created_on',
+        'modified_by',
+        'modified_on',
+    )
+    raw_id_fields = (
+        'created_by',
+        'modified_by',
+    )
 
     def save_model(self, request, obj, form, change):
         """
