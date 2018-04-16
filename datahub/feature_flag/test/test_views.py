a11d3ef763adf7b86606dfde8d34fdbbe0a05351
elcct <pawel@mikesz.com>
2018-04-16 11:36:23 +0100

    Changed the format of response, improved tests.

--- datahub/feature_flag/test/test_views.py
+++ datahub/feature_flag/test/test_views.py
@@ -1,4 +1,3 @@
-import factory
 import pytest
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -13,37 +12,24 @@ pytestmark = pytest.mark.django_db
 class TestListFeatureFlags(APITestMixin):
     """Tests for listing of feature flags."""
 
-    def test_can_list_feature_flags_having_no_permissions(self, api_client):
-        """Should return 401"""
+    def test_cannot_list_feature_flags_having_no_permissions(self, api_client):
+        """Should return 401."""
         url = reverse('api-v3:feature-flag:collection')
         response = api_client.get(url)
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
-    def test_can_list_feature_flags(self):
-        """List feature flags.."""
-        feature_flags = FeatureFlagFactory.create_batch(
-            2,
-            code=factory.Iterator(('def', 'abc',))
-        )
+    def test_can_get_feature_flags(self):
+        """Get feature flags."""
+        feature_flags = FeatureFlagFactory.create_batch(10)
         url = reverse('api-v3:feature-flag:collection')
         response = self.api_client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
 
         response_data = response.json()
-        assert response_data == {
-            'count': 2,
-            'next': None,
-            'previous': None,
-            'results': [
-                {
-                    'code': feature_flags[1].code,
-                    'description': feature_flags[1].description,
-                    'is_active': feature_flags[1].is_active,
-                }, {
-                    'code': feature_flags[0].code,
-                    'description': feature_flags[0].description,
-                    'is_active': feature_flags[0].is_active,
-                }
-            ]
+
+        expected_feature_flags = {
+            feature_flag.code: feature_flag.is_active
+            for feature_flag in feature_flags
         }
+        assert response_data == expected_feature_flags
