7afb0699836b9c75aa05e9889d9746465893e75b
Marco Fucci <marcofucci@gmail.com>
2018-04-17 12:47:56 +0100

    Refactor add interaction test views
    This:
    - uses pytest.mark.parametrize to test different scenarios instead
    of having different tests
    - adds a few validation cases not covered

--- datahub/interaction/test/views/test_policy_feedback.py
+++ datahub/interaction/test/views/test_policy_feedback.py
@@ -1,5 +1,7 @@
 from datetime import date
+from functools import partial
 
+import pytest
 from freezegun import freeze_time
 from rest_framework import status
 from rest_framework.reverse import reverse
@@ -7,8 +9,12 @@ from rest_framework.reverse import reverse
 from datahub.company.test.factories import AdviserFactory, CompanyFactory, ContactFactory
 from datahub.core.constants import Service, Team
 from datahub.core.test_utils import APITestMixin, random_obj_for_model
+from datahub.event.test.factories import EventFactory
 from datahub.investment.test.factories import InvestmentProjectFactory
-from ...models import CommunicationChannel, Interaction, PolicyArea, PolicyIssueType
+from .utils import resolve_data
+from ...models import (
+    CommunicationChannel, Interaction, PolicyArea, PolicyIssueType, ServiceDeliveryStatus
+)
 
 
 class TestAddPolicyFeedback(APITestMixin):
@@ -104,64 +110,90 @@ class TestAddPolicyFeedback(APITestMixin):
             'modified_on': '2017-04-18T13:25:30.986208Z',
         }
 
-    def test_fails_with_missing_fields(self):
-        """
-        Test add new policy feedback interaction without
-        required policy_feedback-only fields.
-        """
-        adviser = AdviserFactory()
-        company = CompanyFactory()
-        contact = ContactFactory()
-        url = reverse('api-v3:interaction:collection')
-        request_data = {
-            'kind': Interaction.KINDS.policy_feedback,
-            'subject': 'whatever',
-            'date': date.today().isoformat(),
-            'dit_adviser': adviser.pk,
-            'notes': 'hello',
-            'company': company.pk,
-            'contact': contact.pk,
-            'service': Service.trade_enquiry.value.id,
-            'dit_team': Team.healthcare_uk.value.id,
-        }
-        response = self.api_client.post(url, request_data, format='json')
-        assert response.status_code == status.HTTP_400_BAD_REQUEST
-        response_data = response.json()
-        assert response_data == {
-            'policy_area': ['This field is required.'],
-            'policy_issue_type': ['This field is required.'],
-            'communication_channel': ['This field is required.'],
-        }
+    @pytest.mark.parametrize(
+        'data,errors',
+        (
+            # required fields
+            (
+                {
+                    'kind': Interaction.KINDS.policy_feedback,
+                },
+                {
+                    'date': ['This field is required.'],
+                    'subject': ['This field is required.'],
+                    'notes': ['This field is required.'],
+                    'contact': ['This field is required.'],
+                    'dit_adviser': ['This field is required.'],
+                    'service': ['This field is required.'],
+                    'dit_team': ['This field is required.'],
+                }
+            ),
 
-    @freeze_time('2017-04-18 13:25:30.986208')
-    def test_fails_with_investment_project(self):
-        """
-        Test add new policy feedback interaction for an investment project.
-        This should not be allowed.
-        """
-        project = InvestmentProjectFactory()
-        adviser = AdviserFactory()
-        contact = ContactFactory()
-        policy_area = random_obj_for_model(PolicyArea)
-        policy_issue_type = random_obj_for_model(PolicyIssueType)
+            # policy fields required
+            (
+                {
+                    'kind': Interaction.KINDS.policy_feedback,
+                    'date': date.today().isoformat(),
+                    'subject': 'whatever',
+                    'notes': 'hello',
+                    'contact': ContactFactory,
+                    'dit_adviser': AdviserFactory,
+                    'service': Service.trade_enquiry.value.id,
+                    'dit_team': Team.healthcare_uk.value.id,
+                },
+                {
+                    'company': ['This field is required.'],
+                    'policy_area': ['This field is required.'],
+                    'policy_issue_type': ['This field is required.'],
+                    'communication_channel': ['This field is required.'],
+                }
+            ),
+
+            # fields not allowed
+            (
+                {
+                    'kind': Interaction.KINDS.policy_feedback,
+                    'date': date.today().isoformat(),
+                    'subject': 'whatever',
+                    'notes': 'hello',
+                    'company': CompanyFactory,
+                    'contact': ContactFactory,
+                    'dit_adviser': AdviserFactory,
+                    'service': Service.trade_enquiry.value.id,
+                    'dit_team': Team.healthcare_uk.value.id,
+                    'communication_channel': partial(random_obj_for_model, CommunicationChannel),
+                    'policy_area': partial(random_obj_for_model, PolicyArea),
+                    'policy_issue_type': partial(random_obj_for_model, PolicyIssueType),
+
+                    # fields not allowed
+                    'investment_project': InvestmentProjectFactory,
+                    'is_event': True,
+                    'event': EventFactory,
+                    'service_delivery_status': partial(
+                        random_obj_for_model, ServiceDeliveryStatus
+                    ),
+                    'grant_amount_offered': '1111.11',
+                    'net_company_receipt': '8888.11',
+
+                },
+                {
+                    'is_event': ['This field is only valid for service deliveries.'],
+                    'event': ['This field is only valid for service deliveries.'],
+                    'service_delivery_status': [
+                        'This field is only valid for service deliveries.'
+                    ],
+                    'grant_amount_offered': ['This field is only valid for service deliveries.'],
+                    'net_company_receipt': ['This field is only valid for service deliveries.'],
+                    'investment_project': ['This field is only valid for interactions.']
+                }
+            ),
+        )
+    )
+    def test_validation(self, data, errors):
+        """Test validation errors."""
+        data = resolve_data(data)
         url = reverse('api-v3:interaction:collection')
-        response = self.api_client.post(url, {
-            'kind': Interaction.KINDS.policy_feedback,
-            'contact': contact.pk,
-            'communication_channel': random_obj_for_model(CommunicationChannel).pk,
-            'subject': 'whatever',
-            'date': date.today().isoformat(),
-            'dit_adviser': adviser.pk,
-            'notes': 'hello',
-            'investment_project': project.pk,
-            'service': Service.trade_enquiry.value.id,
-            'dit_team': Team.healthcare_uk.value.id,
-            'policy_area': policy_area.pk,
-            'policy_issue_type': policy_issue_type.pk
-        }, format='json')
+        response = self.api_client.post(url, data, format='json')
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        response_data = response.json()
-        assert response_data == {
-            'investment_project': ['This field is only valid for interactions.']
-        }
+        assert response.json() == errors
