a3ebcf1b18f21c8e5d74b32d39b0fef8f102fc28
Reupen Shah <reupen@users.noreply.github.com>
2018-04-17 15:31:24 +0100

    Remove CoreViewSetV1
    It was not being used anywhere.
    
    Also renames CoreViewSetV3 to just CoreViewSet.

--- datahub/feature_flag/views.py
+++ datahub/feature_flag/views.py
@@ -1,11 +1,11 @@
 from rest_framework.permissions import IsAuthenticated
 
-from datahub.core.viewsets import CoreViewSetV3
+from datahub.core.viewsets import CoreViewSet
 from datahub.feature_flag.models import FeatureFlag
 from datahub.feature_flag.serializers import FeatureFlagSerializer
 
 
-class FeatureFlagViewSet(CoreViewSetV3):
+class FeatureFlagViewSet(CoreViewSet):
     """Feature flag ViewSet v3."""
 
     pagination_class = None
