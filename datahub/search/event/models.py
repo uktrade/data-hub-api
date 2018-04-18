a94bb26e024e37fb384993bc6c14857a3ed6b443
Reupen Shah <reupen@users.noreply.github.com>
2018-04-18 11:16:52 +0100

    Loosely sort ES model attributes alphabetically
    As these had generally lost any meaningful order, this loosely sorts the attributes and mapping dictionaries for some of the models alphabetically

--- datahub/search/event/models.py
+++ datahub/search/event/models.py
@@ -8,13 +8,6 @@ class Event(BaseESModel):
     """Elasticsearch representation of Event model."""
 
     id = Keyword()
-    name = dsl_utils.SortableText(copy_to=['name_keyword', 'name_trigram'])
-    name_keyword = dsl_utils.SortableCaseInsensitiveKeywordText()
-    name_trigram = dsl_utils.TrigramText()
-    event_type = dsl_utils.id_name_mapping()
-    start_date = Date()
-    end_date = Date()
-    location_type = dsl_utils.id_name_mapping()
     address_1 = Text()
     address_2 = Text()
     address_town = dsl_utils.SortableCaseInsensitiveKeywordText()
@@ -22,28 +15,35 @@ class Event(BaseESModel):
     address_postcode = Text(copy_to='address_postcode_trigram')
     address_postcode_trigram = dsl_utils.TrigramText()
     address_country = dsl_utils.id_name_partial_mapping('address_country')
-    uk_region = dsl_utils.id_name_partial_mapping('uk_region')
+    created_on = Date()
+    disabled_on = Date()
+    end_date = Date()
+    event_type = dsl_utils.id_name_mapping()
+    lead_team = dsl_utils.id_name_mapping()
+    location_type = dsl_utils.id_name_mapping()
+    modified_on = Date()
+    name = dsl_utils.SortableText(copy_to=['name_keyword', 'name_trigram'])
+    name_keyword = dsl_utils.SortableCaseInsensitiveKeywordText()
+    name_trigram = dsl_utils.TrigramText()
     notes = dsl_utils.EnglishText()
     organiser = dsl_utils.contact_or_adviser_partial_mapping('organiser')
-    lead_team = dsl_utils.id_name_mapping()
-    teams = dsl_utils.id_name_partial_mapping('teams')
     related_programmes = dsl_utils.id_name_partial_mapping('related_programmes')
     service = dsl_utils.id_name_mapping()
-    created_on = Date()
-    modified_on = Date()
-    disabled_on = Date()
+    start_date = Date()
+    teams = dsl_utils.id_name_partial_mapping('teams')
+    uk_region = dsl_utils.id_name_partial_mapping('uk_region')
 
     MAPPINGS = {
         'id': str,
-        'event_type': dict_utils.id_name_dict,
-        'location_type': dict_utils.id_name_dict,
         'address_country': dict_utils.id_name_dict,
-        'uk_region': dict_utils.id_name_dict,
-        'organiser': dict_utils.contact_or_adviser_dict,
+        'event_type': dict_utils.id_name_dict,
         'lead_team': dict_utils.id_name_dict,
-        'teams': lambda col: [dict_utils.id_name_dict(c) for c in col.all()],
+        'location_type': dict_utils.id_name_dict,
+        'organiser': dict_utils.contact_or_adviser_dict,
         'related_programmes': lambda col: [dict_utils.id_name_dict(c) for c in col.all()],
         'service': dict_utils.id_name_dict,
+        'teams': lambda col: [dict_utils.id_name_dict(c) for c in col.all()],
+        'uk_region': dict_utils.id_name_dict,
     }
 
     COMPUTED_MAPPINGS = {}
