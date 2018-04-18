9617dd654064f36a3315337d26ee810c9ce4a319
Reupen Shah <reupen@users.noreply.github.com>
2018-04-18 11:16:52 +0100

    Loosely sort ES model attributes alphabetically
    As these had generally lost any meaningful order, this loosely sorts the attributes and mapping dictionaries for some of the models alphabetically

--- datahub/search/companieshousecompany/models.py
+++ datahub/search/companieshousecompany/models.py
@@ -8,6 +8,10 @@ class CompaniesHouseCompany(BaseESModel):
     """Elasticsearch representation of CompaniesHouseCompany model."""
 
     id = Keyword()
+    company_category = dsl_utils.SortableCaseInsensitiveKeywordText()
+    company_number = dsl_utils.SortableCaseInsensitiveKeywordText()
+    company_status = dsl_utils.SortableCaseInsensitiveKeywordText()
+    incorporation_date = Date()
     name = dsl_utils.SortableText(
         copy_to=[
             'name_keyword', 'name_trigram'
@@ -22,15 +26,11 @@ class CompaniesHouseCompany(BaseESModel):
     registered_address_postcode = Text(copy_to='registered_address_postcode_trigram')
     registered_address_postcode_trigram = dsl_utils.TrigramText()
     registered_address_country = dsl_utils.id_name_mapping()
-    company_number = dsl_utils.SortableCaseInsensitiveKeywordText()
-    company_category = dsl_utils.SortableCaseInsensitiveKeywordText()
-    company_status = dsl_utils.SortableCaseInsensitiveKeywordText()
     sic_code_1 = Text()
     sic_code_2 = Text()
     sic_code_3 = Text()
     sic_code_4 = Text()
     uri = Text()
-    incorporation_date = Date()
 
     MAPPINGS = {
         'id': str,
