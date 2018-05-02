49501369a6dee0e9eb2e5d542c4074fa8dbe53ae
Reupen Shah <reupen@users.noreply.github.com>
2018-05-02 10:31:27 +0100

    Use per-model default batch size when bulk syncing to Elasticsearch

--- datahub/search/apps.py
+++ datahub/search/apps.py
@@ -4,8 +4,6 @@ from importlib import import_module
 from django.apps import AppConfig
 from django.conf import settings
 
-from .models import DataSet
-
 
 SEARCH_APPS = [
     'datahub.search.companieshousecompany.CompaniesHouseCompanySearchApp',
@@ -26,6 +24,7 @@ class SearchApp:
 
     name = None
     es_model = None
+    bulk_batch_size = 2000
     view = None
     export_view = None
     queryset = None
@@ -56,10 +55,6 @@ class SearchApp:
         signals_mod = import_module(self.mod_signals)
         for receiver in signals_mod.receivers:
             receiver.disconnect()
-
-    def get_dataset(self):
-        """Returns dataset that will be synchronised with Elasticsearch."""
-        return DataSet(self.queryset, self.es_model)
 
     def get_permission_filters(self, request):
         """
