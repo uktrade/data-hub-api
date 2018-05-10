2739ac76fd2f3d742f618a6bc2efd204262e5bde
elcct <pawel@mikesz.com>
2018-05-10 16:32:15 +0100

    Improve model.

--- datahub/core/exceptions.py
+++ datahub/core/exceptions.py
@@ -13,3 +13,11 @@ class APIConflictException(APIException):
     status_code = status.HTTP_409_CONFLICT
     default_detail = _('Conflict with the current state of the resource.')
     default_code = 'conflict'
+
+
+class APIBadRequestException(APIException):
+    """DRF Exception for the 409 status code."""
+
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = _('Cannot process request due to a client error.')
+    default_code = 'bad_request'
