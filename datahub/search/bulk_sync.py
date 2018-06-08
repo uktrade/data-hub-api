fc36cd03946b4b8f6dac22f5e096556892233fdd
Reupen Shah <reupen@users.noreply.github.com>
2018-06-08 16:23:31 +0100

    Delete documents from old indexes during sync_es

--- datahub/search/bulk_sync.py
+++ datahub/search/bulk_sync.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 
+from datahub.core.exceptions import DataHubException
 from datahub.core.utils import slice_iterable_into_chunks
 from datahub.search.apps import get_search_apps
 from datahub.search.elasticsearch import bulk
@@ -7,6 +8,8 @@ from datahub.search.elasticsearch import bulk
 logger = getLogger(__name__)
 
 PROGRESS_INTERVAL = 20000
+BULK_INDEX_TIMEOUT_SECS = 300
+BULK_DELETION_TIMEOUT_SECS = 300
 
 
 def get_apps_to_sync(models=None):
@@ -27,20 +30,24 @@ def sync_app(item, batch_size=None):
     batch_size = batch_size or item.bulk_batch_size
     logger.info(f'Processing {model_name} records, using batch size {batch_size}')
 
+    read_indices, write_index = item.es_model.get_read_and_write_indices()
+    remove_indices = read_indices - {write_index}
+
     rows_processed = 0
     total_rows = item.queryset.count()
     it = item.queryset.iterator(chunk_size=batch_size)
     batches = slice_iterable_into_chunks(it, batch_size)
     for batch in batches:
-        actions = list(item.es_model.db_objects_to_es_documents(batch))
+        actions = list(item.es_model.db_objects_to_es_documents(batch, index=write_index))
         num_actions = len(actions)
         bulk(
             actions=actions,
             chunk_size=num_actions,
-            request_timeout=300,
-            raise_on_error=True,
-            raise_on_exception=True,
+            request_timeout=BULK_INDEX_TIMEOUT_SECS,
         )
+
+        for index in remove_indices:
+            _delete_documents(index, actions)
 
         emit_progress = (
             (rows_processed + num_actions) // PROGRESS_INTERVAL
@@ -55,3 +62,33 @@ def sync_app(item, batch_size=None):
                         f'{rows_processed*100//total_rows}%')
 
     logger.info(f'{model_name} rows processed: {rows_processed}/{total_rows} 100%.')
+
+
+def _delete_documents(index, complete_index_actions):
+    delete_actions = (
+        _create_delete_action(index, action['_type'], action['_id'])
+        for action in complete_index_actions
+    )
+
+    _, errors = bulk(
+        actions=delete_actions,
+        chunk_size=len(complete_index_actions),
+        request_timeout=BULK_DELETION_TIMEOUT_SECS,
+        raise_on_error=False,
+    )
+
+    non_404_errors = [error for error in errors if error['delete']['status'] != 404]
+    if non_404_errors:
+        raise DataHubException(
+            f'One or more errors during an Elasticsearch bulk deletion operation: '
+            f'{non_404_errors!r}'
+        )
+
+
+def _create_delete_action(_index, _type, _id):
+    return {
+        '_op_type': 'delete',
+        '_index': _index,
+        '_type': _type,
+        '_id': _id,
+    }
