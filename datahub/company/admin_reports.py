f7906af92032389b71d126be58d323bb67304f4f
Marco Fucci <marcofucci@gmail.com>
2018-06-15 11:16:58 +0100

    Allow export of the One List from the django admin

--- datahub/company/admin_reports.py
+++ datahub/company/admin_reports.py
@@ -1,8 +1,10 @@
+from django.conf import settings
 from django.db.models import Case, NullBooleanField, Value, When
 from django.db.models.functions import Concat
 
 from datahub.admin_report.report import QuerySetReport
-from datahub.company.models import Advisor
+from datahub.company.models import Advisor, Company
+from datahub.core import constants
 
 
 class AllAdvisersReport(QuerySetReport):
@@ -33,4 +35,41 @@ class AllAdvisersReport(QuerySetReport):
         'dit_team__name': 'Team',
         'is_team_active': 'Is team active',
         'dit_team__role__name': 'Team role',
+    }
+
+
+class OneListReport(QuerySetReport):
+    """Generates the One List."""
+
+    id = 'one-list'
+    name = 'One List'
+    model = Company
+    permissions_required = ('company.read_company',)
+    queryset = Company.objects.filter(
+        headquarter_type_id=constants.HeadquarterType.ghq.value.id,
+        classification__id__isnull=False,
+        one_list_account_owner_id__isnull=False,
+    ).annotate(
+        primary_contact_name=Concat(
+            'one_list_account_owner__first_name',
+            Value(' '),
+            'one_list_account_owner__last_name'
+        ),
+        url=Concat(
+            Value(f'{settings.DATAHUB_FRONTEND_BASE_URL}/companies/'), 'id'
+        ),
+    ).order_by(
+        'classification__order',
+        'name',
+    )
+    field_titles = {
+        'name': 'Account Name',
+        'classification__name': 'Tier',
+        'sector__segment': 'Sector',
+        'primary_contact_name': 'Primary Contact',
+        'one_list_account_owner__telephone_number': 'Contact Number',
+        'one_list_account_owner__contact_email': 'E-mail',
+        'registered_address_country__name': 'Home Market',
+        'registered_address_town': 'Town/City',
+        'url': 'Data Hub URL',
     }
