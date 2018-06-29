4933e9496785131de96efe115c0cfe0e9b8c2326
Reupen Shah <reupen@users.noreply.github.com>
2018-06-29 16:52:35 +0100

    Use a callback for deleting documents in sync_app()

--- datahub/search/bulk_sync.py
+++ datahub/search/bulk_sync.py
@@ -1,6 +1,5 @@
 from logging import getLogger
 
-from datahub.core.exceptions import DataHubException
 from datahub.core.utils import slice_iterable_into_chunks
 from datahub.search.elasticsearch import bulk
 
@@ -8,17 +7,15 @@ logger = getLogger(__name__)
 
 PROGRESS_INTERVAL = 20000
 BULK_INDEX_TIMEOUT_SECS = 300
-BULK_DELETION_TIMEOUT_SECS = 300
 
 
-def sync_app(search_app, batch_size=None):
+def sync_app(search_app, batch_size=None, post_batch_callback=None):
     """Syncs objects for an app to ElasticSearch in batches of batch_size."""
     model_name = search_app.es_model.__name__
     batch_size = batch_size or search_app.bulk_batch_size
     logger.info(f'Processing {model_name} records, using batch size {batch_size}')
 
     read_indices, write_index = search_app.es_model.get_read_and_write_indices()
-    remove_indices = read_indices - {write_index}
 
     rows_processed = 0
     total_rows = search_app.queryset.count()
@@ -33,8 +30,8 @@ def sync_app(search_app, batch_size=None):
             request_timeout=BULK_INDEX_TIMEOUT_SECS,
         )
 
-        for index in remove_indices:
-            _delete_documents(index, actions)
+        if post_batch_callback:
+            post_batch_callback(read_indices, write_index, actions)
 
         emit_progress = (
             (rows_processed + num_actions) // PROGRESS_INTERVAL
@@ -49,33 +46,3 @@ def sync_app(search_app, batch_size=None):
                         f'{rows_processed*100//total_rows}%')
 
     logger.info(f'{model_name} rows processed: {rows_processed}/{total_rows} 100%.')
-
-
-def _delete_documents(index, complete_index_actions):
-    delete_actions = (
-        _create_delete_action(index, action['_type'], action['_id'])
-        for action in complete_index_actions
-    )
-
-    _, errors = bulk(
-        actions=delete_actions,
-        chunk_size=len(complete_index_actions),
-        request_timeout=BULK_DELETION_TIMEOUT_SECS,
-        raise_on_error=False,
-    )
-
-    non_404_errors = [error for error in errors if error['delete']['status'] != 404]
-    if non_404_errors:
-        raise DataHubException(
-            f'One or more errors during an Elasticsearch bulk deletion operation: '
-            f'{non_404_errors!r}'
-        )
-
-
-def _create_delete_action(_index, _type, _id):
-    return {
-        '_op_type': 'delete',
-        '_index': _index,
-        '_type': _type,
-        '_id': _id,
-    }
--- datahub/search/test/test_bulk_sync.py
+++ datahub/search/test/test_bulk_sync.py
@@ -2,7 +2,6 @@ from unittest.mock import Mock
 
 import pytest
 
-from datahub.core.exceptions import DataHubException
 from datahub.core.test_utils import MockQuerySet
 from datahub.search.bulk_sync import sync_app
 from datahub.search.test.utils import create_mock_search_app
@@ -36,9 +35,9 @@ def test_sync_app_with_overridden_batch_size(monkeypatch):
     assert bulk_mock.call_count == 2
 
 
-def test_sync_app_with_deletions(monkeypatch):
+def test_sync_app_logic(monkeypatch):
     """Tests syncing an app to Elasticsearch during a mapping migration."""
-    bulk_mock = Mock(return_value=(True, ({'delete': {'status': 404}},)))
+    bulk_mock = Mock()
     monkeypatch.setattr('datahub.search.bulk_sync.bulk', bulk_mock)
     search_app = create_mock_search_app(
         current_mapping_hash='mapping-hash',
@@ -60,33 +59,4 @@ def test_sync_app_with_deletions(monkeypatch):
             '_type': 'test-type',
         },
     ]
-    assert list(bulk_mock.call_args_list[1][1]['actions']) == [
-        {
-            '_index': 'index2',
-            '_id': 1,
-            '_op_type': 'delete',
-            '_type': 'test-type',
-        },
-        {
-            '_index': 'index2',
-            '_id': 2,
-            '_op_type': 'delete',
-            '_type': 'test-type',
-        },
-    ]
-    assert bulk_mock.call_count == 2
-
-
-def test_sync_app_with_deletion_error(monkeypatch):
-    """Tests syncing an app to Elasticsearch during a mapping migration."""
-    bulk_mock = Mock(return_value=(True, ({'delete': {'status': 500}},)))
-    monkeypatch.setattr('datahub.search.bulk_sync.bulk', bulk_mock)
-    search_app = create_mock_search_app(
-        current_mapping_hash='mapping-hash',
-        target_mapping_hash='mapping-hash',
-        read_indices=('index1', 'index2'),
-        write_index='index1',
-        queryset=MockQuerySet([Mock(id=1), Mock(id=2)])
-    )
-    with pytest.raises(DataHubException):
-        sync_app(search_app, batch_size=1000)
+    assert bulk_mock.call_count == 1
