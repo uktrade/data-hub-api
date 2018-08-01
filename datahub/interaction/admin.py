1cf3da91ccbd38791b9c681a6359dcf6a78c3c1b
Marco Fucci <info@marcofucci.com>
2018-08-01 14:43:02 +0100

    Tidy up company admin and standardise created/modified fields
    This:
    - groups fields in fieldsets for the company admin
    - defines a new BaseModelAdminMixin for all ModelAdmin with model
      extending core.BaseModel. This updates created_by/modified_by
      automatically and can show those fields as readonly descriptions

--- datahub/interaction/admin.py
+++ datahub/interaction/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from reversion.admin import VersionAdmin
 
-from datahub.core.admin import custom_add_permission, custom_change_permission
+from datahub.core.admin import BaseModelAdminMixin, custom_add_permission, custom_change_permission
 from datahub.interaction.models import InteractionPermission, PolicyArea, PolicyIssueType
 from datahub.metadata.admin import DisableableMetadataAdmin, OrderedMetadataAdmin
 from .models import CommunicationChannel, Interaction
@@ -14,7 +14,7 @@ admin.site.register((PolicyArea, PolicyIssueType), OrderedMetadataAdmin)
 @admin.register(Interaction)
 @custom_add_permission(InteractionPermission.add_all)
 @custom_change_permission(InteractionPermission.change_all)
-class InteractionAdmin(VersionAdmin):
+class InteractionAdmin(BaseModelAdminMixin, VersionAdmin):
     """Interaction admin."""
 
     search_fields = (
@@ -39,17 +39,21 @@ class InteractionAdmin(VersionAdmin):
         'dit_adviser',
         'investment_project',
         'contact',
-        'created_by',
-        'modified_by',
     )
     readonly_fields = (
         'archived_documents_url_path',
-        'created_on',
-        'modified_on',
+        'created',
+        'modified',
     )
     list_select_related = (
         'company',
         'contact',
         'investment_project',
         'investment_project__investor_company',
+    )
+    exclude = (
+        'created_on',
+        'created_by',
+        'modified_on',
+        'modified_by',
     )
