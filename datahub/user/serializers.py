3814561442af1956e3943f2d85f53d3ea3a2c5a6
Reupen Shah <reupen@users.noreply.github.com>
2018-08-06 14:32:50 +0100

    Return read pemissions in /whoami/ endpoint for backwards compatibility

--- datahub/user/serializers.py
+++ datahub/user/serializers.py
@@ -1,7 +1,12 @@
+import re
+
 from rest_framework import serializers
 
 from datahub.company.models.adviser import Advisor
 from datahub.metadata.serializers import TeamSerializer
+
+
+VIEW_PERMISSION_REGEX = re.compile(r'^([^.]*\.)view(_.*)$')
 
 
 class WhoAmISerializer(serializers.ModelSerializer):
@@ -27,5 +32,18 @@ class WhoAmISerializer(serializers.ModelSerializer):
         depth = 2
 
     def get_permissions(self, obj):
-        """Serialize permissions."""
-        return obj.get_all_permissions()
+        """
+        Get all of the user's permissions.
+
+        To keep backwards compatibility, permissions with codenames that start with view_ are
+        replicated as permissions with codenames that start with read_. (Once the front end has
+        been updated to use the new view_ permissions, this replication can be removed.)
+        """
+        permissions = obj.get_all_permissions()
+        view_permissions_subs = (
+            VIEW_PERMISSION_REGEX.subn(r'\1read\2', permission) for permission in permissions
+        )
+        read_permissions = {
+            permission for permission, sub_count in view_permissions_subs if sub_count > 0
+        }
+        return permissions | read_permissions
