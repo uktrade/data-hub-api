8147c2d8184376dbe0cd0e62d8f9b67a50c97e71
Reupen Shah <reupen@users.noreply.github.com>
2018-08-06 16:43:10 +0100

    Move CSV response logic to separate function

--- datahub/core/csv.py
+++ datahub/core/csv.py
@@ -1,7 +1,11 @@
 from codecs import BOM_UTF8
 from csv import DictWriter
 
+from django.http import FileResponse
+
 from datahub.core.utils import EchoUTF8
+
+CSV_CONTENT_TYPE = 'text/csv'
 
 
 def csv_iterator(rows, field_titles):
@@ -12,3 +16,17 @@ def csv_iterator(rows, field_titles):
     yield writer.writerow(field_titles)
     for row in rows:
         yield writer.writerow(row)
+
+
+def create_csv_response(rows, field_titles, filename):
+    """Creates a CSV HTTP response."""
+    # TODO: Use additional FileResponse.__init__() arguments when Django 2.1 is released
+    # See https://code.djangoproject.com/ticket/16470
+    response = FileResponse(
+        csv_iterator(rows, field_titles),
+        content_type=CSV_CONTENT_TYPE
+    )
+
+    filename = filename
+    response['Content-Disposition'] = f'attachment; filename="{filename}.csv"'
+    return response
