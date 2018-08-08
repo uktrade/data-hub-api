a7c25feff32b9d11fd2e66a77a67bff5cc4a1090
Reupen Shah <reupen@users.noreply.github.com>
2018-08-08 16:36:43 +0100

    Handle errors while generating CSV responses
    As CSV responses are normally streamed, if an error occurs is the middle of the response it is too late to return a different status code.
    
    Hence, this appends an error to the CSV file to make sure it's clear that the file is incomplete.

--- datahub/core/csv.py
+++ datahub/core/csv.py
@@ -6,16 +6,28 @@ from django.http import FileResponse
 from datahub.core.utils import EchoUTF8
 
 CSV_CONTENT_TYPE = 'text/csv'
+INCOMPLETE_CSV_MESSAGE = (
+    '\r\n'
+    'An error occurred while generating the CSV file. The file is incomplete.'
+    '\r\n'
+).encode('utf-8')
 
 
 def csv_iterator(rows, field_titles):
     """Returns an iterator producing CSV formatted data from provided input."""
-    yield BOM_UTF8
-    writer = DictWriter(EchoUTF8(), fieldnames=field_titles.keys())
+    try:
+        yield BOM_UTF8
+        writer = DictWriter(EchoUTF8(), fieldnames=field_titles.keys())
 
-    yield writer.writerow(field_titles)
-    for row in rows:
-        yield writer.writerow(row)
+        yield writer.writerow(field_titles)
+        for row in rows:
+            yield writer.writerow(row)
+    except Exception:
+        # Because CSV responses are normally streamed, a 200 response will already have been
+        # returned if an error occurs at this point. Hence we append an error to the CSV
+        # contents (while re-raising the exception).
+        yield INCOMPLETE_CSV_MESSAGE
+        raise
 
 
 def create_csv_response(rows, field_titles, filename):
