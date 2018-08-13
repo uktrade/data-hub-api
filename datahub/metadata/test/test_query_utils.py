4067159514f2258ee42bab275ba9ef6f779a4ff9
Reupen Shah <reupen@users.noreply.github.com>
2018-08-13 13:16:17 +0100

    Add utility function for annotating queries with the full sector name
    This will be used e.g. in query set definitions for reports.

---
+++ datahub/metadata/test/test_query_utils.py
@@ -0,0 +1,60 @@
+import pytest
+
+from datahub.metadata.models import Sector
+from datahub.metadata.query_utils import get_sector_name_subquery
+from datahub.metadata.test.factories import SectorFactory
+
+pytestmark = pytest.mark.django_db
+
+
+def test_annotated_sector_name_root_node():
+    """Test the sector name annotation for a sector at root level."""
+    sector = SectorFactory(parent=None)
+    annotated_sector = Sector.objects.annotate(
+        name_annotation=get_sector_name_subquery()
+    ).get(
+        pk=sector.pk
+    )
+
+    assert annotated_sector.name_annotation == sector.name
+
+
+def test_annotated_sector_name_level_one():
+    """Test the sector name annotation for a sector one level deep."""
+    parent = SectorFactory()
+    sector = SectorFactory(parent=parent)
+    annotated_sector = Sector.objects.annotate(
+        name_annotation=get_sector_name_subquery()
+    ).get(
+        pk=sector.pk
+    )
+
+    assert annotated_sector.name_annotation == sector.name
+
+
+def test_annotated_sector_name_level_two():
+    """Test the sector name annotation for a sector two levels deep."""
+    grandparent = SectorFactory()
+    parent = SectorFactory(parent=grandparent)
+    sector = SectorFactory(parent=parent)
+    annotated_sector = Sector.objects.annotate(
+        name_annotation=get_sector_name_subquery()
+    ).get(
+        pk=sector.pk
+    )
+
+    assert annotated_sector.name_annotation == sector.name
+
+
+def test_annotated_sector_name_via_relation():
+    """Test the sector name annotation via a relation."""
+    grandparent = SectorFactory()
+    parent = SectorFactory(parent=grandparent)
+    sector = SectorFactory(parent=parent)
+    annotated_sector = Sector.objects.annotate(
+        parent_name_annotation=get_sector_name_subquery('parent')
+    ).get(
+        pk=sector.pk
+    )
+
+    assert annotated_sector.parent_name_annotation == parent.name
