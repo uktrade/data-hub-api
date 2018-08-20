a1d4e5f9babcd1538138b40b9f1bdb1d5af85b27
Reupen Shah <reupen@users.noreply.github.com>
2018-08-20 16:49:26 +0100

    Update interaction search export columns

--- datahub/search/interaction/views.py
+++ datahub/search/interaction/views.py
@@ -1,7 +1,10 @@
-from django.db.models import Value
-from django.db.models.functions import Concat
-
+from datahub.core.query_utils import (
+    get_choices_as_case_expression,
+    get_front_end_url_expression,
+    get_full_name_expression,
+)
 from datahub.interaction.models import Interaction as DBInteraction
+from datahub.metadata.query_utils import get_sector_name_subquery
 from datahub.oauth.scopes import Scope
 from .models import Interaction
 from .serializers import SearchInteractionSerializer
@@ -75,13 +78,29 @@ class SearchInteractionExportAPIView(SearchInteractionParams, SearchExportAPIVie
     """Filtered interaction search export view."""
 
     queryset = DBInteraction.objects.annotate(
-        dit_adviser_name=Concat('dit_adviser__first_name', Value(' '), 'dit_adviser__last_name'),
+        company_link=get_front_end_url_expression('company', 'company__pk'),
+        company_sector_name=get_sector_name_subquery('company__sector'),
+        contact_name=get_full_name_expression('contact'),
+        dit_adviser_name=get_full_name_expression('dit_adviser'),
+        link=get_front_end_url_expression('interaction', 'pk'),
+        kind_name=get_choices_as_case_expression(DBInteraction, 'kind'),
     )
     field_titles = {
         'date': 'Date',
-        'company__name': 'Company',
+        'kind_name': 'Type',
         'service__name': 'Service',
         'subject': 'Subject',
+        'link': 'Link',
+        'company__name': 'Company',
+        'company_link': 'Company link',
+        'company__registered_address_country__name': 'Company country',
+        'company__uk_region__name': 'Company UK region',
+        'company_sector_name': 'Company sector',
+        'contact_name': 'Contact',
+        'contact__job_title': 'Contact job title',
         'dit_adviser_name': 'Adviser',
         'dit_team__name': 'Service provider',
+        'event__name': 'Event',
+        'service_delivery_status__name': 'Service delivery status',
+        'net_company_receipt': 'Net company receipt',
     }
