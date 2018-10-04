56644022ab78b414623503c40f4ad40389eedd45
Marco Fucci <info@marcofucci.com>
2018-10-04 15:20:47 +0100

    Convert all imports to absolute in core app

--- datahub/core/test/support/urls.py
+++ datahub/core/test/support/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import MyDisableableModelViewset
+from datahub.core.test.support.views import MyDisableableModelViewset
 
 collection = MyDisableableModelViewset.as_view({
     'get': 'list',
