75201d7317d4cc9aa58beb6f90a33b4f62a215c1
Reupen Shah <reupen@users.noreply.github.com>
2018-11-06 11:53:11 +0000

    Simplify search signal receivers
    This removes unused arguments from the signatures of receiver functions, and changes sync_object() and sync_object_async() to take a search app instead of the search and Django models.

--- datahub/search/company/signals.py
+++ datahub/search/company/signals.py
@@ -2,15 +2,15 @@ from django.db import transaction
 from django.db.models.signals import post_save
 
 from datahub.company.models import Company as DBCompany
-from datahub.search.company.models import Company as ESCompany
+from datahub.search.company import CompanySearchApp
 from datahub.search.signals import SignalReceiver
 from datahub.search.sync_async import sync_object_async
 
 
-def company_sync_es(sender, instance, **kwargs):
+def company_sync_es(instance):
     """Sync company to the Elasticsearch."""
     transaction.on_commit(
-        lambda: sync_object_async(ESCompany, DBCompany, str(instance.pk)),
+        lambda: sync_object_async(CompanySearchApp, str(instance.pk)),
     )
 
 
--- datahub/search/sync_async.py
+++ datahub/search/sync_async.py
@@ -1,22 +1,22 @@
-from datahub.core.thread_pool import submit_to_thread_pool
 from datahub.search.bulk_sync import sync_objects
 from datahub.search.migrate_utils import delete_from_secondary_indices_callback
 
 
-def _sync_object(es_model, db_model, pk):
+def _sync_object(search_app, pk):
+    es_model = search_app.es_model
     read_indices, write_index = es_model.get_read_and_write_indices()
 
-    instance = db_model.objects.get(pk=pk)
+    obj = search_app.queryset.get(pk=pk)
     sync_objects(
         es_model,
-        [instance],
+        [obj],
         read_indices,
         write_index,
         post_batch_callback=delete_from_secondary_indices_callback,
     )
 
 
-def sync_object_async(search_model, db_model, pk):
+def sync_object_async(search_app, pk):
     """
     Syncs a single object to Elasticsearch asynchronously (using the thread pool).
 
@@ -26,4 +26,4 @@ def sync_object_async(search_model, db_model, pk):
     This function is migration-safe – if a migration is in progress, the object is added to the
     new index and then deleted from the old index.
     """
-    return submit_to_thread_pool(_sync_object, search_model, db_model, pk)
+    return _sync_object(search_app, pk)
