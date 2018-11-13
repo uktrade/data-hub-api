c458d8cb3282e41b9c98e38437f457e511149665
Marco Fucci <info@marcofucci.com>
2018-11-13 17:40:42 +0000

    Move countries.yaml to a data migration
    This gets rid of countries.yaml as initial fixture loaded via
    the loadinitialmetadata and replaces it with a data migration
    added to the first metadata migration.

--- datahub/core/management/commands/loadinitialmetadata.py
+++ datahub/core/management/commands/loadinitialmetadata.py
@@ -13,7 +13,6 @@ EVENTS_FIXTURE_DIR = SOURCE_ROOT / 'datahub' / 'event' / 'fixtures'
 
 SHARED_FIXTURES = (
     SHARED_METADATA_FIXTURE_DIR / 'companies.yaml',
-    SHARED_METADATA_FIXTURE_DIR / 'countries.yaml',
     SHARED_METADATA_FIXTURE_DIR / 'investment.yaml',
     SHARED_METADATA_FIXTURE_DIR / 'referrals.yaml',
     SHARED_METADATA_FIXTURE_DIR / 'teams.yaml',
