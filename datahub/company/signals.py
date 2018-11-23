9a2f71499e9d13e70e77bdbad7abc78901f1c30f
Marco Fucci <info@marcofucci.com>
2018-11-23 12:22:35 +0000

    Only call load_constants_to_database once
    The post_migrate signal receiver `company_business_type_post_migrate`
    is called after each individual django app is migrated.
    
    Before:
    `load_constants_to_database` was called multiple times as well.
    
    After:
    `load_constants_to_database` is only called once after migrating
    the `metadata` django app.

--- datahub/company/signals.py
+++ datahub/company/signals.py
@@ -10,7 +10,11 @@ from datahub.metadata.models import BusinessType
 logger = logging.getLogger(__name__)
 
 
-@receiver(post_migrate, dispatch_uid='company_business_type_post_migrate')
+@receiver(
+    post_migrate,
+    sender=BusinessType._meta.app_config,
+    dispatch_uid='company_business_type_post_migrate',
+)
 def company_business_type_post_migrate(sender, **kwargs):
     """
     Ensures all business types are loaded to the database.
