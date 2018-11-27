325d41cbfff08ebe8fe13f05da8be49fa7e449f2
Marco Fucci <info@marcofucci.com>
2018-11-27 13:52:05 +0000

    Add OneListTier model
    This adds the new company.OneListTier model with the intention of
    replacing the metadata.CompanyClassification one.
    
    The admins for both are made read-only so that we can migrate the
    references as well without manual updates in the meantime.

--- datahub/company/admin/metadata.py
+++ datahub/company/admin/metadata.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from datahub.company.models import ExportExperienceCategory
-from datahub.metadata.admin import DisableableMetadataAdmin
+from datahub.company.models import ExportExperienceCategory, OneListTier
+from datahub.metadata.admin import DisableableMetadataAdmin, ReadOnlyMetadataAdmin
 
 
 admin.site.register(ExportExperienceCategory, DisableableMetadataAdmin)
+admin.site.register(OneListTier, ReadOnlyMetadataAdmin)
