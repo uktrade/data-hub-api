5d9d486d79d48ed02018d0b188a0e932d9d1245d
Reupen Shah <reupen@users.noreply.github.com>
2018-12-12 10:14:39 +0000

    Translate None values to defaults for new policy feedback fields

--- datahub/interaction/serializers.py
+++ datahub/interaction/serializers.py
@@ -96,6 +96,30 @@ class InteractionSerializer(serializers.ModelSerializer):
         required=False,
     )
 
+    def to_representation(self, instance):
+        """
+        Converts an instance to a dict, ready to be serialised.
+
+        This is overridden to replace None values for some recently added fields with the
+        default value. This is until existing records have been updated and None values
+        replaced with the default value.
+
+        This overridden method can be removed once the NULL values have been removed and
+        the fields made non-nullable.
+        """
+        data = super().to_representation(instance)
+
+        replace_none_with_default_fields = (
+            'policy_feedback_notes',
+            'was_policy_feedback_provided',
+        )
+
+        for field in replace_none_with_default_fields:
+            if data[field] is None:
+                data[field] = instance._meta.get_field(field).default
+
+        return data
+
     def validate(self, data):
         """
         Removes the semi-virtual field is_event from the data.
