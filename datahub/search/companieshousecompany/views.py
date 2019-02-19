55f826789c9f483a41ebd610042caa04690771de
Marco Fucci <info@marcofucci.com>
2019-02-19 15:38:24 +0000

    Prepare companies house search API for v4

--- datahub/search/companieshousecompany/views.py
+++ datahub/search/companieshousecompany/views.py
@@ -4,8 +4,8 @@ from datahub.search.serializers import EntitySearchQuerySerializer
 from datahub.search.views import SearchAPIView
 
 
-class SearchCompaniesHouseCompanyAPIView(SearchAPIView):
-    """Filtered company search view."""
+class SearchCompaniesHouseCompanyAPIViewV3(SearchAPIView):
+    """Filtered company search view V3."""
 
     required_scopes = (Scope.internal_front_end,)
     entity = CompaniesHouseCompany
