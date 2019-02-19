1d99b09487fa1331ba4d321bbdf3f3d635195158
Marco Fucci <info@marcofucci.com>
2019-02-19 17:00:39 +0000

    Index Companies House registered address as nested object
    So that we can later migrate to that and get rid of the individual
    address fields instead.

--- datahub/search/companieshousecompany/views.py
+++ datahub/search/companieshousecompany/views.py
@@ -4,9 +4,17 @@ from datahub.search.serializers import EntitySearchQuerySerializer
 from datahub.search.views import SearchAPIView
 
 
-class SearchCompaniesHouseCompanyAPIViewV3(SearchAPIView):
-    """Filtered company search view V3."""
+class BaseSearchCompaniesHouseCompanyAPIView(SearchAPIView):
+    """Base filtered company search view V3."""
 
     required_scopes = (Scope.internal_front_end,)
     entity = CompaniesHouseCompany
     serializer_class = EntitySearchQuerySerializer
+
+
+class SearchCompaniesHouseCompanyAPIViewV3(BaseSearchCompaniesHouseCompanyAPIView):
+    """Filtered company search view V3."""
+
+    fields_to_exclude = (
+        'registered_address',
+    )
