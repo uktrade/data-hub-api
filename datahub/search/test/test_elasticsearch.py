9ff1fa8409e092d6cc788b1bc3d558926d6ea540
Reupen Shah <reupen@users.noreply.github.com>
2019-03-04 16:41:55 +0000

    Remove support for AWS authentication with Elasticsearch
    We are no longer using AWS-provided Elasticsearch with AWS request signing, so this removes support for it.

--- datahub/search/test/test_elasticsearch.py
+++ datahub/search/test/test_elasticsearch.py
@@ -39,7 +39,6 @@ def test_index_exists(mock_es_client, expected):
 def test_configure_connection(connections, settings):
     """Tests if Heroku connection is configured."""
     settings.HEROKU = True
-    settings.ES_USE_AWS_AUTH = False
     settings.ES_URL = 'https://login:password@test:1234'
     connections.configure.return_value = {}
 
