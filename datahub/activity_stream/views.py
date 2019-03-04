ed857b4a6ad9c649a8448ce724cefb296abe6f0b
Reupen Shah <reupen@users.noreply.github.com>
2019-03-04 13:56:18 +0000

    Replace Hawk response middleware with view mixin
    The previous way of signing Hawk responses relied on decorator_from_middleware which was problematic because:
    
    - decorator_from_middleware is designed for old-style middleware
    - the middleware relied on Django Request Framework request classes, so it wouldn't function as a middleware
    
    This replaces it with a view mixin (which was the simplest working alternative I could come up with).

--- datahub/activity_stream/views.py
+++ datahub/activity_stream/views.py
@@ -1,23 +1,21 @@
-from django.utils.decorators import decorator_from_middleware
 from rest_framework.response import Response
 from rest_framework.viewsets import ViewSet
 
 from config.settings.types import HawkScope
 from datahub.core.hawk_receiver import (
     HawkAuthentication,
-    HawkResponseMiddleware,
+    HawkResponseSigningMixin,
     HawkScopePermission,
 )
 
 
-class ActivityStreamViewSet(ViewSet):
+class ActivityStreamViewSet(HawkResponseSigningMixin, ViewSet):
     """List-only view set for the activity stream."""
 
     authentication_classes = (HawkAuthentication,)
     permission_classes = (HawkScopePermission,)
     required_hawk_scope = HawkScope.activity_stream
 
-    @decorator_from_middleware(HawkResponseMiddleware)
     def list(self, request):
         """A single page of activities"""
         return Response({'secret': 'content-for-pen-test'})
