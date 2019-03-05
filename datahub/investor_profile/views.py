cb689d7593dd52280063a12eb91a48bf2053fe8a
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-03-05 15:53:44 +0000

    Fix permissions

--- datahub/investor_profile/views.py
+++ datahub/investor_profile/views.py
@@ -1,5 +1,3 @@
-from oauth2_provider.contrib.rest_framework.permissions import IsAuthenticatedOrTokenHasScope
-
 from datahub.core.viewsets import CoreViewSet
 from datahub.feature_flag.utils import feature_flagged_view
 from datahub.investor_profile.constants import (
@@ -15,7 +13,6 @@ class LargeCapitalInvestorProfileViewSet(CoreViewSet):
     """Large capital investor profile view set."""
 
     required_scopes = (Scope.internal_front_end,)
-    permission_classes = (IsAuthenticatedOrTokenHasScope,)
     serializer_class = LargeCapitalInvestorProfileSerializer
     profile_type_id = ProfileTypeConstant.large.value.id
 
