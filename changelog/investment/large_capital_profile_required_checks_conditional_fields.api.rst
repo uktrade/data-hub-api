25da6647cab57a6e2378e2b0d3ff22ae0828f9f4
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-03-19 17:04:17 +0000

    Add conditional field checks for background checks

---
+++ changelog/investment/large_capital_profile_required_checks_conditional_fields.api.rst
@@ -0,0 +1,2 @@
+ The endpoint ``/v4/large-captial-profile`` now accepts and returns ``required_checks_conducted_on`` (date) and ``required_checks_conducted_by`` (adviser id).
+ Both become required fields when ``required_checks_conducted`` is set to ``Cleared`` or ``Issues identified``.
--- datahub/investment/investor_profile/constants.py
+++ datahub/investment/investor_profile/constants.py
@@ -14,3 +14,35 @@ class ProfileType(Enum):
         'Growth',
         '38a38617-84d9-41d0-bf01-d8f027cd109b',
     )
+
+
+class RequiredChecksConducted(Enum):
+    """Required checks conducted constants."""
+
+    cleared = Constant(
+        'Cleared',
+        '02d6fc9b-fbb9-4621-b247-d86f2487898e',
+    )
+    issues_identified = Constant(
+        'Issues identified',
+        '9beab8fc-1094-49b4-97d0-37bc7a9de631',
+    )
+    not_yet_checked = Constant(
+        'Not yet checked',
+        '81fafe5a-ed32-4f46-bdc5-2cafedf828e8',
+    )
+    checks_not_required = Constant(
+        'Checks not required',
+        'e6f66f9d-ed12-4bfd-9dd0-ac7e44f35034',
+    )
+
+
+REQUIRED_CHECKS_THAT_NEED_ADDITIONAL_INFORMATION = [
+    RequiredChecksConducted.cleared.value.id,
+    RequiredChecksConducted.issues_identified.value.id,
+]
+
+REQUIRED_CHECKS_THAT_DO_NOT_NEED_ADDITIONAL_INFORMATION = [
+    RequiredChecksConducted.not_yet_checked.value.id,
+    RequiredChecksConducted.checks_not_required.value.id,
+]
