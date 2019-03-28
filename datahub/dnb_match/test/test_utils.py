e2b7b065d6107bd05ba84262c9c5bd7193f9fcaf
Marco Fucci <info@marcofucci.com>
2019-03-28 13:45:48 +0000

    Add _extract_turnover dnb util function This adds the _extract_turnover util function which parses a D&B worldbase record and returns data related to turnover.

--- datahub/dnb_match/test/test_utils.py
+++ datahub/dnb_match/test/test_utils.py
@@ -1,6 +1,13 @@
+from decimal import InvalidOperation
+
 import pytest
 
-from datahub.dnb_match.utils import _extract_employees, EmployeesIndicator
+from datahub.dnb_match.utils import (
+    _extract_employees,
+    _extract_turnover,
+    EmployeesIndicator,
+    TurnoverIndicator,
+)
 
 
 class TestExtractEmployees:
@@ -198,3 +205,128 @@ class TestExtractEmployees:
         """
         with pytest.raises(expected_exception):
             _extract_employees(wb_record)
+
+
+class TestExtractTurnover:
+    """Tests for the _extract_turnover function."""
+
+    @pytest.mark.parametrize(
+        'wb_record,expected_output',
+        (
+            # Estimated
+            (
+                {
+                    'Annual Sales in US dollars': '1',
+                    'Annual Sales Indicator': TurnoverIndicator.ESTIMATED,
+                },
+                (1, True),
+            ),
+
+            # Modelled
+            (
+                {
+                    'Annual Sales in US dollars': '1',
+                    'Annual Sales Indicator': TurnoverIndicator.MODELLED,
+                },
+                (1, True),
+            ),
+
+            # Low end of range
+            (
+                {
+                    'Annual Sales in US dollars': '1',
+                    'Annual Sales Indicator': TurnoverIndicator.LOW_END_OF_RANGE,
+                },
+                (1, True),
+            ),
+
+            # Actual
+            (
+                {
+                    'Annual Sales in US dollars': '111',
+                    'Annual Sales Indicator': TurnoverIndicator.ACTUAL,
+                },
+                (111, False),
+            ),
+
+            # With scientific notation
+            (
+                {
+                    'Annual Sales in US dollars': '1.03283E+11',
+                    'Annual Sales Indicator': TurnoverIndicator.ESTIMATED,
+                },
+                (103283000000, True),
+            ),
+
+            # Float converted to int
+            (
+                {
+                    'Annual Sales in US dollars': '1.5',
+                    'Annual Sales Indicator': TurnoverIndicator.ESTIMATED,
+                },
+                (2, True),
+            ),
+
+            # Not available
+            (
+                {
+                    'Annual Sales in US dollars': '0',
+                    'Annual Sales Indicator': TurnoverIndicator.NOT_AVAILABLE,
+                },
+                (None, None),
+            ),
+        ),
+    )
+    def test_success(self, wb_record, expected_output):
+        """
+        Test successful cases related to _extract_turnover().
+        """
+        actual_output = _extract_turnover(wb_record)
+        assert actual_output == expected_output
+
+    @pytest.mark.parametrize(
+        'wb_record,exception',
+        (
+            # Annual Sales in US dollars is not a number
+            (
+                {
+                    'Annual Sales in US dollars': 'a',
+                    'Annual Sales Indicator': TurnoverIndicator.ESTIMATED,
+                },
+                InvalidOperation,
+            ),
+
+            # Annual Sales in US dollars is empty
+            (
+                {
+                    'Annual Sales in US dollars': '',
+                    'Annual Sales Indicator': TurnoverIndicator.ESTIMATED,
+                },
+                InvalidOperation,
+            ),
+
+            # Annual Sales Indicator is invalid
+            (
+                {
+                    'Annual Sales in US dollars': '1',
+                    'Annual Sales Indicator': 'a',
+                },
+                ValueError,
+            ),
+
+            # Indicator == NOT_AVAILABLE but Annual Sales in US dollars value != 0
+            (
+                {
+                    'Annual Sales in US dollars': '1',
+                    'Annual Sales Indicator': TurnoverIndicator.NOT_AVAILABLE,
+                },
+                AssertionError,
+            ),
+        ),
+    )
+    def test_bad_data(self, wb_record, exception):
+        """
+        Test cases related to bad input data when calling _extract_turnover().
+        """
+        with pytest.raises(exception):
+            _extract_turnover(wb_record)
