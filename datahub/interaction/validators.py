970bd869fe8daa3d530538b709e1e791d8743321
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2019-04-16 13:06:21 +0200

    Add status and location fields to Interaction model (#1610)
    * Add status and location fields to Interaction model
    
    - Interaction.status field records the current status of the interaction;
      either 'draft' or 'complete'.  We will eventually enforce 'complete'
      as the default and set this for all existing interactions.
    - Interaction.location field records where the interaction took place.
    - Accompanying API changes have been made; these currently enforce
      sensible defaults for the fields to ensure new rows are populated with
      data.

--- datahub/interaction/validators.py
+++ datahub/interaction/validators.py
@@ -1,6 +1,7 @@
 from rest_framework.exceptions import ValidationError
 
 from datahub.core.validate_utils import DataCombiner
+from datahub.interaction.models import Interaction
 
 
 class ContactsBelongToCompanyValidator:
@@ -35,4 +36,40 @@ class ContactsBelongToCompanyValidator:
             raise ValidationError(
                 'The interaction contacts must belong to the specified company.',
                 code='inconsistent_contacts_and_company',
+            )
+
+
+class StatusChangeValidator:
+    """
+    Validates that an interaction's status cannot change from complete back to
+    draft.  An interaction with status='complete' means that the interaction
+    occurred successfully and any extra details for it have been logged.
+    """
+
+    def __init__(self):
+        """
+        Initialises the validator.
+        """
+        self.instance = None
+
+    def set_context(self, serializer):
+        """
+        Saves a reference to the model object.
+        """
+        self.instance = serializer.instance
+
+    def __call__(self, data):
+        """
+        Performs validation.
+        """
+        if not self.instance:
+            return
+        existing_interaction_complete = self.instance.status == Interaction.STATUSES.complete
+        combiner = DataCombiner(self.instance, data)
+        new_status = combiner.get_value('status')
+        update_changes_status = new_status != self.instance.status
+        if existing_interaction_complete and update_changes_status:
+            raise ValidationError(
+                'The status of a complete interaction cannot change.',
+                code='complete_interaction_status_cannot_change',
             )
