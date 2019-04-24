e598e346ea8e78401ca4c720598dba39c9ad1934
Brendan Smith <brendan.smith@gamer-network.net>
2019-04-24 13:02:38 +0100

    Add initial email ingestion logic - Mailbox and EmailProcessor classes

---
+++ datahub/email_ingestion/mailbox.py
@@ -0,0 +1,173 @@
+import imaplib
+from contextlib import contextmanager
+from email.errors import MessageParseError
+from logging import getLogger
+
+import mailparser
+from mailparser.exceptions import MailParserError
+
+logger = getLogger(__name__)
+
+
+class Mailbox:
+    """
+    The Mailbox class acts as a gateway to an email inbox - accessed via IMAP.
+    It offers methods for getting new messages from the inbox and processing
+    them using associated processing classes.
+
+    When an email is retrieved from the inbox successfully, it is marked as
+    deleted on the upstream IMAP inbox.  This is the mechanism we use to ensure
+    that emails are only ever processed once.
+
+    The first processor to successfully consume and process the message will stop
+    the chain.  Processors will be called in order - so ordering of
+    `mail_processor_classes` is important.
+    """
+
+    def __init__(
+        self,
+        email,
+        password,
+        imap_domain,
+        mail_processor_classes,
+        imap_port=None,
+        use_ssl=True,
+    ):
+        """
+        Initialise a Mailbox object.
+
+        :param email: string - the email address of the inbox
+        :param password: string - the password for the inbox
+        :param imap_domain: string - the domain of the imap server to connect to
+        :param mail_processor_classes: iterable - EmailProcessor classes which
+            should be used to process incoming mail to this mailbox
+        :param imap_port: optional int - the port to use when connecting with imap
+        :param use_ssl: optional boolean - whether or not to use SSL with imap, defaults
+            to True
+        """
+        self.email = email
+        self.password = password
+        self.imap_domain = imap_domain
+        if imap_port:
+            self.imap_port = imap_port
+        else:
+            if use_ssl:
+                self.imap_port = 993
+            else:
+                self.imap_port = 465
+        self.use_ssl = use_ssl
+        self.processor_classes = []
+        for processor_class in mail_processor_classes:
+            self.processor_classes.append(processor_class)
+
+    @contextmanager
+    def _connect(self):
+        """
+        Connect to the email inbox over IMAP.  The connection is closed down
+        automatically when execution leaves the scope of the context.
+
+        :yields: An active imaplib server connection object.
+        """
+        transport = imaplib.IMAP4_SSL
+        if not self.use_ssl:
+            transport = imaplib.IMAP4
+        server = transport(self.imap_domain, self.imap_port)
+        server.login(self.email, self.password)
+        server.select()
+        try:
+            yield server
+        finally:
+            server.close()
+            server.logout()
+
+    def _get_all_message_ids(self, server):
+        """
+        Fetch all of the message ids that are present in the inbox.
+
+        :param server: imaplib connection object - the active connection to
+            use to retrieve message ids from
+
+        :returns: A list of message id strings.
+        """
+        # Fetch all the message uids
+        response, message_ids = server.uid('search', None, 'ALL')
+        message_id_string = message_ids[0].strip()
+        # Usually `message_id_string` will be a list of space-separated
+        # ids; we must make sure that it isn't an empty string before
+        # splitting into individual UIDs.
+        if message_id_string:
+            return message_id_string.decode().split(' ')
+        return []
+
+    def _process_email(self, message):
+        """
+        Run through the EmailProcessor objects associated with this Mailbox and
+        attempt to process the message.
+
+        The message will be processed by at most one processor; EmailProcessor
+        objects will be called in the order that they were passed in on
+        Mailbox instantiation. The method returns as soon as the message has been
+        successfully processed by a processor.
+
+        :param message: mailparser.Mailparser object - the message to process
+
+        :returns: True if the message was processed, False otherwise.
+        """
+        for processor_class in self.processor_classes:
+            processor = processor_class()
+            processed, message = processor.process_email(message)
+            if processed:
+                processor_name = processor_class.__name__
+                message = f'Email {message} was processed by processor: {processor_name}'
+                logger.info(message)
+                return True
+        return False
+
+    def _parse_message(self, message_bytes):
+        return mailparser.parse_from_bytes(message_bytes)
+
+    def get_new_mail(self):
+        """
+        Generator method which gets new messages from the email inbox. After a
+        message has been yielded, it is flagged as deleted on the inbox so that
+        it will not be ingested again later.
+
+        :yields: A mailparser.Message object for each parsed message.
+        """
+        with self._connect() as server:
+            message_ids = self._get_all_message_ids(server)
+            if not message_ids:
+                # No new messages to ingest
+                return
+
+            for uid in message_ids:
+                try:
+                    typ, msg_contents = server.uid('fetch', uid, '(RFC822)')
+                    if not msg_contents:
+                        continue
+                    try:
+                        message = self._parse_message(msg_contents[0][1])
+                    except TypeError:
+                        # This may happen if another thread/process deletes the
+                        # message between our generating the ID list and our
+                        # processing it here.
+                        raise
+                    yield message
+                except (MessageParseError, MailParserError):
+                    # If we have some problem parsing the email, it's likely
+                    # to be spam/malicious so skip it
+                    continue
+
+                # Mark the email for deletion in the inbox
+                server.uid('store', uid, '+FLAGS', '(\\Deleted)')
+            # Carry out deletion for the emails marked for deletion
+            server.expunge()
+
+    def process_new_mail(self):
+        """
+        Gets all of the new mail in the inbox and goes through the associated
+        EmailProcessor classes to process each message.
+        """
+        messages = self.get_new_mail()
+        for message in messages:
+            self._process_email(message)
---
+++ datahub/email_ingestion/test/test_mailbox.py
@@ -0,0 +1,258 @@
+import copy
+from contextlib import contextmanager
+from email.errors import MessageParseError
+from unittest import mock
+
+import pytest
+
+from datahub.email_ingestion.email_processor import EmailProcessor
+from datahub.email_ingestion.mailbox import Mailbox
+
+EXPECTED_EMAIL_MESSAGES = [
+    {
+        'uid': 'abc1',
+        'message_content': 'abc1foobar',
+    },
+    {
+        'uid': 'abc2',
+        'message_content': 'abc2foobar',
+    },
+    {
+        'uid': 'abc3',
+        'message_content': 'abc3foobar',
+    },
+]
+
+
+class TestMailbox:
+    """
+    Test the Mailbox class.
+    """
+
+    @contextmanager
+    def patch_imap(self, email_messages):
+        """
+        Helper contextmanager to patch IMAP4_SSL which is used in the Mailbox
+        class. We can specify email details returned by our mocked instance by
+        providing sample email messages.
+
+        :param email_messages: iterable of email message dictionaries with keys
+            uid (the email uid) and message_content (a string of message payload)
+        :yields: A mock.Mock object which has been patched in place of the IMAP4_SSL
+            object in the datahub.email_ingestion.mailbox module
+        """
+        with mock.patch('datahub.email_ingestion.mailbox.imaplib.IMAP4_SSL') as mocked_imap_class:
+            mocked_imap = mocked_imap_class.return_value
+            email_ids = [message['uid'] for message in email_messages]
+            email_bodies = {
+                message['uid']: message['message_content'].encode('utf-8')
+                for message in email_messages
+            }
+            encoded_ids = ' '.join(email_ids).encode('utf-8')
+
+            # Set our mocked imap server's uid method to return certain values
+            # when called with certain signatures
+            def uid_side_effect(action, *args):
+                # A search call should return our email message ids
+                if action == 'search':
+                    return (None, (encoded_ids,))
+                # Fetch calls should return the email message bodies in the expected
+                # format
+                if action == 'fetch':
+                    # The expected format is pretty naff...
+                    return (None, ((None, email_bodies[args[0]]),))
+            mocked_imap.uid.side_effect = uid_side_effect
+            yield mocked_imap
+
+    def mock_mailbox_parse_message(self, mailbox):
+        """
+        Helper to ensure that the _parse_message method of a supplied mailbox
+        object is mocked.
+        """
+        mailbox._parse_message = mock.Mock()
+
+        def parse_message_side_effect(message):
+            return message.decode()
+        mailbox._parse_message.side_effect = parse_message_side_effect
+
+    def test_get_new_mail(self):
+        """
+        Functional test for get_new_mail method. We must mock out a couple of
+        external dependencies here; namely the IMAP gateway and message parsing
+        code.
+        """
+        expected_email_messages = copy.deepcopy(EXPECTED_EMAIL_MESSAGES)
+        with self.patch_imap(expected_email_messages) as mocked_imap:
+            mailbox = Mailbox(
+                'foobar@example.net',
+                'foobarbaz1',
+                'domain.example.net',
+                mail_processor_classes=[],
+            )
+            # Ensure that we also mock the Mailbox._parse_message method - this should
+            # mean that the mailbox method gives us our messages in an expected format
+            self.mock_mailbox_parse_message(mailbox)
+
+            messages = list(mailbox.get_new_mail())
+            assert len(messages) == len(expected_email_messages)
+            # Go through all of the returned messages and ensure that the parsed
+            # message content is as expected
+            for count, message in enumerate(messages):
+                expected_email_message = expected_email_messages[count]
+                # Ensure that the messages our mailbox retrieves are those that we expect
+                assert message == expected_email_message['message_content']
+                # Ensure that a deletion call was made for each message retrieved
+                mocked_imap.uid.assert_any_call(
+                    'store',
+                    expected_email_message['uid'],
+                    '+FLAGS',
+                    '(\\Deleted)',
+                )
+            # Ensure that an expunge was called on the imap server - to clear the
+            # messages flagged for deletion
+            mocked_imap.expunge.assert_called_once()
+            # Ensure that the imap connection was cleaned up
+            mocked_imap.close.assert_called_once()
+            mocked_imap.logout.assert_called_once()
+
+    def test_get_new_mail_parsing_failure(self):
+        """
+        Functional test to ensure that the get_new_mail method can handle a
+        parsing failure as expected.
+        """
+        expected_email_messages = copy.deepcopy(EXPECTED_EMAIL_MESSAGES)
+        with self.patch_imap(expected_email_messages) as mocked_imap:
+            mailbox = Mailbox(
+                'foobar@example.net',
+                'foobarbaz1',
+                'domain.example.net',
+                mail_processor_classes=[],
+            )
+
+            # Mock the Mailbox._parse_message method - this allows us to simulate
+            # a problem when parsing a particular message
+            mailbox._parse_message = mock.Mock()
+
+            def parse_message_side_effect(message):
+                message_str = message.decode()
+                if message_str == 'abc2foobar':
+                    raise MessageParseError()
+                return message_str
+            mailbox._parse_message.side_effect = parse_message_side_effect
+
+            messages = list(mailbox.get_new_mail())
+            # We expect that the messages returned will omit the problematic message
+            expected_email_messages.pop(1)
+            assert len(messages) == len(expected_email_messages)
+            # Go through all of the returned messages and ensure that the parsed
+            # message content is as expected
+            for count, message in enumerate(messages):
+                expected_email_message = expected_email_messages[count]
+                # Ensure that the messages our mailbox retrieves are those that we expect
+                assert message == expected_email_message['message_content']
+                # Ensure that a deletion call was made for each message retrieved
+                mocked_imap.uid.assert_any_call(
+                    'store',
+                    expected_email_message['uid'],
+                    '+FLAGS',
+                    '(\\Deleted)',
+                )
+            # Ensure that an expunge was called on the imap server - to clear the
+            # messages flagged for deletion
+            mocked_imap.expunge.assert_called_once()
+            # Ensure that the imap connection was cleaned up
+            mocked_imap.close.assert_called_once()
+            mocked_imap.logout.assert_called_once()
+
+    def test_process_new_mail(self):
+        """
+        Functional test to ensure that a processor class has the opportunity to
+        process messages as expected.
+        """
+        expected_email_messages = copy.deepcopy(EXPECTED_EMAIL_MESSAGES)
+        with self.patch_imap(expected_email_messages):
+            processor_class = mock.Mock(spec=EmailProcessor)
+            processor = processor_class.return_value
+            processor.process_email.return_value = (False, 'Bad email')
+            mailbox = Mailbox(
+                'foobar@example.net',
+                'foobarbaz1',
+                'domain.example.net',
+                mail_processor_classes=[processor_class],
+            )
+            # Ensure that we also mock the Mailbox._parse_message method - this should
+            # mean that the mailbox method gives us our messages in an expected format
+            self.mock_mailbox_parse_message(mailbox)
+
+            mailbox.process_new_mail()
+            for message in expected_email_messages:
+                processor.process_email.assert_any_call(message['message_content'])
+
+    @pytest.mark.parametrize(
+        'processor_count,processor_results,email_processed',
+        (
+            # Ensure that if no processors could process the message the method
+            # reports that it was not processed
+            (
+                3,
+                (
+                    (False, 'Could not be processed'),
+                    (False, 'Could not be processed'),
+                    (False, 'Could not be processed'),
+                ),
+                False,
+            ),
+            # Ensure that the processing stops once a processor successfully
+            # processes the message
+            (
+                3,
+                (
+                    (False, 'Could not be processed'),
+                    (True, 'Processed successfully'),
+                ),
+                True,
+            ),
+            # Ensure that when there are no processors, the email is not processed
+            (
+                0,
+                tuple(),
+                False,
+            ),
+        ),
+    )
+    def test_process_email(self, processor_count, processor_results, email_processed):
+        """
+        Unit test of _process_email method to ensure that the chain of email
+        processing works as expected.
+        """
+        processor_classes = []
+        # Set up our mocked processor classes, ensuring that they return the
+        # results we expect
+        for i in range(processor_count):
+            mocked_processor_class = mock.Mock(spec=EmailProcessor)
+            mocked_processor_class.__name__ = ''
+            mocked_processor = mocked_processor_class.return_value
+            try:
+                mocked_processor.process_email.return_value = processor_results[i]
+            except IndexError:
+                continue
+            processor_classes.append(mocked_processor_class)
+        mailbox = Mailbox(
+            'foobar@example.net',
+            'foobarbaz1',
+            'domain.example.net',
+            mail_processor_classes=processor_classes,
+        )
+        message = mock.Mock()
+        processed = mailbox._process_email(message)
+        # Ensure that the processing result is as expected
+        assert processed == email_processed
+        # Ensure that the processor classes were called/not called as specified in
+        # processor_results
+        for count, processor_class in enumerate(processor_classes):
+            processor = processor_class.return_value
+            try:
+                processor_results[count]
+                assert processor.process_email.called_with(message)
+            except IndexError:
+                assert not processor.process_email.called
