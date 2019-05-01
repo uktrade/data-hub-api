5a91a5f737953e4fece426a5b6da788d770d61d0
Marcus Patino Pan <marcus.patino-pan@digital.trade.gov.uk>
2019-05-01 14:20:04 +0100

    Add RequiredChecksConductedSerializer and update validation

--- datahub/investment/validate.py
+++ datahub/investment/validate.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
 
 
 def field_incomplete(combiner, field):
@@ -5,3 +8,28 @@ def field_incomplete(combiner, field):
     if combiner.is_field_to_many(field):
         return not combiner.get_value_to_many(field)
     return combiner.get_value(field) in (None, '')
+
+
+def _is_provided_and_is_date_in_the_past(value):
+    """
+    Returns True if the date value is provided and is today's date or in the past.
+
+    Note that comparing the date to today's date could cause issues
+    with users who are in different timezones as they could be in front of UTC.
+
+    TODO: Consider a users timezone when comparing the date values.
+    """
+    if not value:
+        return False
+    return date.today() >= value
+
+
+def is_provided_and_is_date_less_than_a_year_ago(value):
+    """Returns True if the date value is provided and within the last year."""
+    if not value:
+        return False
+    return _is_provided_and_is_date_in_the_past(
+        value,
+    ) and (
+        value > date.today() - relativedelta(years=1)
+    )
