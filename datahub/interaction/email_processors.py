c8d2fbe283d85178ee583e4db335ea65a8ade4b0
Brendan Smith <brendan.smith@gamer-network.net>
2019-05-10 16:46:20 +0100

    Add CalendarInteractionEmailParser for extraction of interaction data from calendar invite emails

---
+++ datahub/interaction/email_processors.py
@@ -0,0 +1,158 @@
+import base64
+from logging import getLogger
+
+import icalendar
+import pytz
+from django.core.exceptions import ValidationError
+
+from datahub.company.models.adviser import Advisor
+from datahub.company.models.contact import Contact
+from datahub.email_ingestion.validation import was_email_sent_by_dit
+
+
+logger = getLogger(__name__)
+
+
+class CalendarInteractionEmailParser:
+    """
+    Parses and extracts calendar interaction information from a MailParser email
+    object.
+    """
+
+    def __init__(self, message):
+        """
+        Initialise the CalendarInteractionEmailParser with a MailParser email
+        object.
+        """
+        self.message = message
+
+    def _verify_email_sent_by_dit(self):
+        if not was_email_sent_by_dit(self.message):
+            raise ValidationError('Email not sent by DIT')
+
+    def _extract_sender_adviser(self):
+        sender_email = self.message.from_[0][1]
+        # TODO: replace with reupen's util
+        try:
+            sender_adviser = Advisor.objects.get(contact_email=sender_email)
+        except Advisor.DoesNotExist:
+            raise ValidationError('Email not sent by recognised DIT Adviser')
+        return sender_adviser
+
+    def _get_all_recipients(self):
+        all_recipients = set()
+        for recipients in [self.message.to, self.message.cc]:
+            all_recipients = all_recipients.union(
+                {recipient[1].strip() for recipient in recipients},
+            )
+        return all_recipients
+
+    def _extract_contacts(self, all_recipients):
+        contacts = list(Contact.objects.filter(email__in=all_recipients))
+        if not contacts:
+            raise ValidationError('No email recipients were recognised as Contacts')
+        return contacts
+
+    def _extract_secondary_advisers(self, all_recipients, sender_adviser):
+        secondary_advisers = list(
+            Advisor.objects.filter(email__in=all_recipients).exclude(id=sender_adviser.id),
+        )
+        return secondary_advisers
+
+    def _get_company_from_contacts(self, contacts):
+        """
+        Get the company from the given contacts.
+        If these contacts are related to different companies, return the company
+        with the highest number of contacts in our iterable.
+        """
+        companies = {}
+        company_counts = {}
+        for contact in contacts:
+            company = contact.company
+            if not company:
+                continue
+            try:
+                company_counts[company.id] += 1
+            except KeyError:
+                company_counts[company.id] = 1
+                companies[company.id] = company
+        sorted_company_ids = sorted(
+            list(company_counts.items()),
+            key=lambda item: item[1],
+            reverse=True,
+        )
+        top_company = companies[sorted_company_ids[0][0]]
+        return top_company
+
+    def _grab_calendar_string_from_text(self):
+        for text_part in self.message.text_plain:
+            if text_part.startswith('BEGIN:VCALENDAR'):
+                return text_part
+        return False
+
+    def _grab_calendar_string_from_attachments(self):
+        for attachment in self.message.attachments:
+            if attachment['mail_content_type'] == 'application/ics':
+                encoded_cal_text = base64.b64decode(attachment['payload'])
+                return encoded_cal_text.decode('utf-8', 'ignore')
+        return False
+
+    def _get_utc_datetime(self, localised_datetime):
+        try:
+            return localised_datetime.astimezone(pytz.utc)
+        # When the calendar event is just a date
+        except AttributeError:
+            return localised_datetime
+
+    def _get_calendar_event_metadata(self):
+        error = ValidationError('No calendar event could be extracted')
+        calendar_string = self._grab_calendar_string_from_text()
+        if not calendar_string:
+            calendar_string = self._grab_calendar_string_from_attachments()
+        if not calendar_string:
+            raise error
+        try:
+            calendar = icalendar.Calendar.from_ical(calendar_string)
+        except Exception:
+            raise error
+        # TODO: Review how naive this approach is - what if an adviser sent their
+        # whole calendar to datahub for whatever reason..?
+        for component in calendar.walk():
+            if component.name == 'VEVENT':
+                return {
+                    'subject': str(component.get('summary')),
+                    'start': self._get_utc_datetime(component.decoded('dtstart')),
+                    'end': self._get_utc_datetime(component.decoded('dtend')),
+                    'sent': self._get_utc_datetime(component.decoded('dtstamp')),
+                    'location': str(component.get('location')),
+                    'status': str(component.get('status')),
+                    'uid': str(component.get('uid')),
+                }
+        raise error
+
+    def extract_interaction_data_from_email(self):
+        """
+        Extract interaction data from the email message as a dictionary.
+
+        This raises a ValidationError if the interaction data could not be fully extracted
+        or is invalid according to business logic.
+        """
+        interaction_data = {}
+        self._verify_email_sent_by_dit()
+        interaction_data['sender'] = self._extract_sender_adviser()
+        all_recipients = self._get_all_recipients()
+        interaction_data['contacts'] = self._extract_contacts(all_recipients)
+        interaction_data['secondary_advisers'] = self._extract_secondary_advisers(
+            all_recipients,
+            interaction_data['sender'],
+        )
+        interaction_data['company'] = self._get_company_from_contacts(interaction_data['contacts'])
+        calendar_event = self._get_calendar_event_metadata()
+        interaction_data['date'] = calendar_event['start']
+        interaction_data['location'] = calendar_event['location']
+        interaction_data['meeting_details'] = calendar_event
+        interaction_data['subject'] = calendar_event['subject']
+        meeting_confirmed = calendar_event['status'] == 'CONFIRMED'
+        if not meeting_confirmed:
+            raise ValidationError('Calendar event was not status: CONFIRMED')
+        return interaction_data
---
+++ datahub/interaction/test/email_processors/test_email_processors.py
@@ -0,0 +1,273 @@
+import os
+from datetime import datetime
+
+import mailparser
+import pytest
+import pytz
+from django.core.exceptions import ValidationError
+
+from datahub.company.test.factories import AdviserFactory, CompanyFactory, ContactFactory
+from datahub.interaction.email_processors import CalendarInteractionEmailParser
+
+
+@pytest.fixture()
+def calendar_data_fixture():
+    """
+    Create advisers, contacts and companies so that our email samples can be
+    attributed to some DB entities.
+    """
+    fixture = {
+        'contacts': [],
+        'advisers': [],
+    }
+    adviser_emails = [
+        'brendan.smith@trade.gov.uk',
+        'marco.fucci@digital.trade.gov.uk',
+        'ali.zaidi@digital.trade.gov.uk',
+    ]
+    for adviser_email in adviser_emails:
+        fixture['advisers'].append(
+            AdviserFactory(contact_email=adviser_email, email=adviser_email),
+        )
+    fixture['company_1'] = CompanyFactory(name='Company 1')
+    fixture['company_2'] = CompanyFactory(name='Company 2')
+    contacts = [
+        ('Bill Adama', fixture['company_1']),
+        ('Saul Tigh', fixture['company_1']),
+        ('Laura Roslin', fixture['company_2']),
+    ]
+    for name, company in contacts:
+        first_name, last_name = name.split(' ')
+        email_prefix = name.lower().replace(' ', '.')
+        email = f'{email_prefix}@example.net'
+        fixture['contacts'].append(ContactFactory(
+            first_name=first_name,
+            last_name=last_name,
+            email=email,
+            company=company,
+        ))
+    yield fixture
+
+
+@pytest.mark.django_db
+class TestCalendarInteractionEmailParser:
+    """
+    Test the CalendarInteractionEmailProcessor class.
+    """
+
+    def _get_parser_for_email_file(self, relative_email_file_path):
+        email_file_path = os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            relative_email_file_path,
+        )
+        message = mailparser.parse_from_file(email_file_path)
+        return CalendarInteractionEmailParser(message)
+
+    @pytest.mark.parametrize(
+        'email_file,expected_event_details',
+        (
+            (
+                'email_samples/valid/outlook_online/initial.eml',
+                {
+                    'subject': 'test meet',
+                    'start': datetime(2019, 3, 29, 12, 0, tzinfo=pytz.utc),
+                    'end': datetime(2019, 3, 29, 12, 30, tzinfo=pytz.utc),
+                    'sent': datetime(2019, 3, 29, 11, 28, 24, tzinfo=pytz.utc),
+                    'location': (
+                        'SOMEWHERE Agency (10 Tunstall Studios, 34-44 '
+                        'Tunstall Road, 10/11 Tunstall Studios, London, '
+                        'England, United Kingdom)'
+                    ),
+                    'status': 'CONFIRMED',
+                    'uid': (
+                        '040000008200E00074C5B7101A82E008000000001670528522E6D4'
+                        '0100000000000000001000000079ABFE8513989A49988F0CF2BF5B0F5A'
+                    ),
+                },
+            ),
+            (
+                'email_samples/valid/outlook_online/update.eml',
+                {
+                    'subject': 'test meetz',
+                    'start': datetime(2019, 3, 29, 12, 0, tzinfo=pytz.utc),
+                    'end': datetime(2019, 3, 29, 12, 30, tzinfo=pytz.utc),
+                    'sent': datetime(2019, 3, 29, 11, 28, 53, tzinfo=pytz.utc),
+                    'location': (
+                        'SOMEWHERE Agency (10 Tunstall Studios, 34-44 '
+                        'Tunstall Road, 10/11 Tunstall Studios, London, '
+                        'England, United Kingdom)'
+                    ),
+                    'status': 'CONFIRMED',
+                    'uid': (
+                        '040000008200E00074C5B7101A82E008000000001670528522E6D4'
+                        '0100000000000000001000000079ABFE8513989A49988F0CF2BF5B0F5A'
+                    ),
+                },
+            ),
+            (
+                'email_samples/valid/gmail/initial.eml',
+                {
+                    'subject': 'initial',
+                    'start': datetime(2019, 3, 29, 16, 30, tzinfo=pytz.utc),
+                    'end': datetime(2019, 3, 29, 17, 30, tzinfo=pytz.utc),
+                    'sent': datetime(2019, 3, 29, 11, 36, 33, tzinfo=pytz.utc),
+                    'location': (
+                        'Somewhere, Unit FF - 305 - شارع المركز المالي - دبي - '
+                        'United Arab Emirates'
+                    ),
+                    'status': 'CONFIRMED',
+                    'uid': '5iggr1e2luglss6c789b0scvgr@google.com',
+                },
+            ),
+            (
+                'email_samples/valid/gmail/update.eml',
+                {
+                    'subject': 'update',
+                    'start': datetime(2019, 3, 29, 16, 30, tzinfo=pytz.utc),
+                    'end': datetime(2019, 3, 29, 18, 0, tzinfo=pytz.utc),
+                    'sent': datetime(2019, 3, 29, 11, 39, 15, tzinfo=pytz.utc),
+                    'location': (
+                        'Somewhere, Unit FF - 305 - شارع المركز المالي - دبي - '
+                        'United Arab Emirates'
+                    ),
+                    'status': 'CONFIRMED',
+                    'uid': '5iggr1e2luglss6c789b0scvgr@google.com',
+                },
+            ),
+            (
+                'email_samples/valid/gmail/no_vcalendar.eml',
+                {
+                    'subject': 'initial',
+                    'start': datetime(2019, 3, 29, 16, 30, tzinfo=pytz.utc),
+                    'end': datetime(2019, 3, 29, 17, 30, tzinfo=pytz.utc),
+                    'sent': datetime(2019, 3, 29, 11, 36, 33, tzinfo=pytz.utc),
+                    'location': (
+                        'Somewhere, Unit FF - 305 - شارع المركز المالي - دبي - '
+                        'United Arab Emirates'
+                    ),
+                    'status': 'CONFIRMED',
+                    'uid': '5iggr1e2luglss6c789b0scvgr@google.com',
+                },
+            ),
+        ),
+    )
+    def test_get_calendar_event_metadata(self, email_file, expected_event_details):
+        """
+        Verify that the get_calendar_event_metadata method extracts the expected
+        data from a given email message.
+        """
+        parser = self._get_parser_for_email_file(email_file)
+        calendar_event = parser._get_calendar_event_metadata()
+        assert(calendar_event == expected_event_details)
+
+    @pytest.mark.parametrize(
+        'email_file,expected_interaction_data',
+        (
+            # Test that interaction data can be extracted for an easy case
+            (
+                'email_samples/valid/outlook_online/initial.eml',
+                {
+                    'adviser_email': 'brendan.smith@trade.gov.uk',
+                    'contact_emails': ['bill.adama@example.net'],
+                    'secondary_adviser_emails': [],
+                    'company_name': 'Company 1',
+                    'date': datetime(2019, 3, 29, 12, 0, tzinfo=pytz.utc),
+                    'location': (
+                        'SOMEWHERE Agency (10 Tunstall Studios, 34-44 '
+                        'Tunstall Road, 10/11 Tunstall Studios, London, '
+                        'England, United Kingdom)'
+                    ),
+                    'subject': 'test meet',
+                },
+            ),
+            # Test that interaction data can be extracted for a complicated case
+            # with many advisers, contacts and some unknown contacts
+            (
+                'email_samples/valid/gmail/initial.eml',
+                {
+                    'adviser_email': 'brendan.smith@trade.gov.uk',
+                    'contact_emails': [
+                        'bill.adama@example.net',
+                        'saul.tigh@example.net',
+                        'laura.roslin@example.net',
+                    ],
+                    'secondary_adviser_emails': ['marco.fucci@digital.trade.gov.uk'],
+                    'company_name': 'Company 1',
+                    'date': datetime(2019, 3, 29, 16, 30, tzinfo=pytz.utc),
+                    'location': (
+                        'Somewhere, Unit FF - 305 - شارع المركز المالي - دبي - '
+                        'United Arab Emirates'
+                    ),
+                    'subject': 'initial',
+                },
+            ),
+        ),
+    )
+    def test_extract_interaction_data_from_email(
+        self,
+        email_file,
+        expected_interaction_data,
+        calendar_data_fixture,
+    ):
+        """
+        Functional test to ensure that interaction data is extracted as expected
+        from an email.
+        """
+        parser = self._get_parser_for_email_file(email_file)
+        interaction_data = parser.extract_interaction_data_from_email()
+        assert interaction_data['sender'].email == expected_interaction_data['adviser_email']
+        for contact in interaction_data['contacts']:
+            assert contact.email in expected_interaction_data['contact_emails']
+        for adviser in interaction_data['secondary_advisers']:
+            assert adviser.email in expected_interaction_data['secondary_adviser_emails']
+        assert interaction_data['date'] == expected_interaction_data['date']
+        assert interaction_data['location'] == expected_interaction_data['location']
+        assert interaction_data['subject'] == expected_interaction_data['subject']
+
+    @pytest.mark.parametrize(
+        'email_file,expected_error',
+        (
+            (
+                'email_samples/invalid/email_not_sent_by_dit.eml',
+                ValidationError('Email not sent by DIT'),
+            ),
+            (
+                'email_samples/invalid/email_not_sent_by_known_adviser.eml',
+                ValidationError('Email not sent by recognised DIT Adviser'),
+            ),
+            (
+                'email_samples/invalid/email_contacts_unknown.eml',
+                ValidationError('No email recipients were recognised as Contacts'),
+            ),
+            (
+                'email_samples/invalid/bad_calendar_event.eml',
+                ValidationError('No calendar event could be extracted'),
+            ),
+            (
+                'email_samples/invalid/no_calendar_in_email.eml',
+                ValidationError('No calendar event could be extracted'),
+            ),
+            (
+                'email_samples/invalid/no_calendar_event.eml',
+                ValidationError('No calendar event could be extracted'),
+            ),
+            (
+                'email_samples/invalid/calendar_event_unconfirmed.eml',
+                ValidationError('Calendar event was not status: CONFIRMED'),
+            ),
+        ),
+    )
+    def test_extract_interaction_data_from_email_raises_error(
+        self,
+        email_file,
+        expected_error,
+        calendar_data_fixture,
+    ):
+        """
+        Functional test to ensure that the extract_interaction_data_from_email method
+        raises ValidationErrors as expected in a number of situations.
+        """
+        parser = self._get_parser_for_email_file(email_file)
+        with pytest.raises(expected_error.__class__) as exc:
+            parser.extract_interaction_data_from_email()
+        assert exc.value.message == expected_error.message
