efb8fd452012b7850d963658ded95bd226e536bb
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2019-05-21 17:35:08 +0200

    Enable whitelist of exempt domains for email ingestion verification (#1725)

--- datahub/email_ingestion/validation.py
+++ datahub/email_ingestion/validation.py
@@ -2,7 +2,6 @@ from django.conf import settings
 
 
 def _verify_authentication(message, from_email):
-    # TODO: See if there's a library to make this a bit more bulletproof
     authentication_lines = [
         line.strip() for line in message.authentication_results.split('\n')
     ]
@@ -33,14 +32,22 @@ def was_email_sent_by_dit(message):
 
     :returns: True if the email was sent by DIT, False otherwise.
     """
-    from_email = message.from_[0][1]
-    # TODO: See if valid domains are recorded elsewhere in the codebase
-    # and use this to keep things DRY
+    try:
+        from_email = message.from_[0][1]
+        from_domain = from_email.rsplit('@', maxsplit=1)[1]
+    except IndexError:
+        return False
     from_domain_is_dit = any([
         domain for domain in settings.DIT_EMAIL_DOMAINS
-        if from_email.endswith(domain)
+        if from_domain == domain
     ])
     if not from_domain_is_dit:
         return False
+    from_domain_is_authentication_exempt = any([
+        domain for domain in settings.DIT_EMAIL_DOMAINS_AUTHENTICATION_EXEMPT
+        if from_domain == domain
+    ])
+    if from_domain_is_authentication_exempt:
+        return True
     authentication_pass = _verify_authentication(message, from_email)
     return authentication_pass
