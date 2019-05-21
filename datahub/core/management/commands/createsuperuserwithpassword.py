09c38ce27ec3d74d55b67ba9afb66243046cdb90
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2019-05-21 17:19:21 +0200

    Simplify docker working copy startup to one command (#1634)

---
+++ datahub/core/management/commands/createsuperuserwithpassword.py
@@ -0,0 +1,43 @@
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand, CommandError
+from django.db import IntegrityError
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    """
+    Create a superuser with a password.
+    """
+
+    help = (
+        'Create a superuser with a password - the standard createsuperuser command '
+        'is interactive, so password cannot be supplied on the CLI'
+    )
+
+    def add_arguments(self, parser):
+        """
+        Adds additional command arguments.
+        """
+        parser.add_argument(
+            'username',
+            type=str,
+            help="The superuser's username",
+        )
+        parser.add_argument(
+            'password',
+            type=str,
+            help="The superuser's password",
+        )
+
+    def handle(self, *args, **options):
+        """
+        Handle invocation of the command.
+        """
+        username = options['username']
+        password = options['password']
+        try:
+            user = User.objects.create_superuser(username, password)
+        except IntegrityError:
+            raise CommandError(f'User with username "{username}" already exists')
+        self.stdout.write(self.style.SUCCESS(f'Successfully created user #{user.id}'))
