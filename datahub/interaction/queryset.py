66bb93946c61580f084691c7626300b652d2e985
Ali Zaidi <alixedi@gmail.com>
2019-05-28 16:43:34 +0100

    Refactor base queryset from interaction queryset

--- datahub/interaction/queryset.py
+++ datahub/interaction/queryset.py
@@ -1,7 +1,40 @@
 from django.db.models import OuterRef, Prefetch, Subquery
 
+from datahub.company.models import Contact
 from datahub.core.model_helpers import get_m2m_model
 from datahub.interaction.models import Interaction, InteractionDITParticipant
+
+
+def get_base_interaction_queryset():
+    """
+    Gets the base interaction queryset with the select_related
+    and prefetch_related sorted out.
+    """
+    return Interaction.objects.select_related(
+        'company',
+        'created_by',
+        'archived_by',
+        'dit_adviser',
+        'dit_team',
+        'communication_channel',
+        'investment_project',
+        'modified_by',
+        'service',
+        'service_delivery_status',
+        'event',
+    ).prefetch_related(
+        Prefetch('contacts', queryset=Contact.objects.order_by('pk')),
+        'policy_areas',
+        'policy_issue_types',
+        Prefetch(
+            'dit_participants',
+            queryset=(
+                InteractionDITParticipant.objects
+                                         .order_by('pk')
+                                         .select_related('adviser', 'team')
+            ),
+        ),
+    )
 
 
 def get_interaction_queryset():
@@ -16,31 +49,13 @@ def get_interaction_queryset():
         'pk',
     )[:1]
 
-    return Interaction.objects.annotate(
+    queryset = get_base_interaction_queryset()
+
+    return queryset.annotate(
         first_name_of_first_contact=Subquery(
             first_contact_queryset.values('contact__first_name'),
         ),
         last_name_of_first_contact=Subquery(
             first_contact_queryset.values('contact__last_name'),
-        ),
-    ).select_related(
-        'company',
-        'created_by',
-        'archived_by',
-        'dit_adviser',
-        'dit_team',
-        'communication_channel',
-        'investment_project',
-        'modified_by',
-        'service',
-        'service_delivery_status',
-        'event',
-    ).prefetch_related(
-        'contacts',
-        'policy_areas',
-        'policy_issue_types',
-        Prefetch(
-            'dit_participants',
-            queryset=InteractionDITParticipant.objects.select_related('adviser', 'team'),
         ),
     )
