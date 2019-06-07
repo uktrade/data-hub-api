3923d39d90db9b6839e8f45adf9beb59cf2049d6
elcct <pawel@mikesz.com>
2019-06-07 10:50:30 +0100

    fixup: add more tests.

--- datahub/interaction/test/views/test_service_answers.py
+++ datahub/interaction/test/views/test_service_answers.py
@@ -1,4 +1,5 @@
 from datetime import date
+from operator import itemgetter
 
 import pytest
 from freezegun import freeze_time
@@ -18,12 +19,16 @@ from datahub.interaction.models import (
     CommunicationChannel,
     Interaction,
 )
+from datahub.interaction.test.factories import (
+    CompanyInteractionFactory,
+    InvestmentProjectInteractionFactory,
+)
 from datahub.interaction.test.permissions import (
     NON_RESTRICTED_ADD_PERMISSIONS,
+    NON_RESTRICTED_VIEW_PERMISSIONS,
 )
 from datahub.interaction.test.views.utils import resolve_data
 from datahub.metadata.models import Service
-
 
 GLOBAL_GROWTH_SERVICE_ANSWERS_ALL_OPTIONAL = {
     ServiceQuestionConstant.ggs_status.value.id: {
@@ -51,8 +56,8 @@ GLOBAL_GROWTH_SERVICE_ANSWERS_NO_OPTIONAL = {
 }
 
 
-class TestAddInteraction(APITestMixin):
-    """Tests for the add interaction view."""
+class TestServiceAnswers(APITestMixin):
+    """Tests for the interaction service answers."""
 
     @freeze_time('2017-04-18 13:25:30.986208')
     @pytest.mark.parametrize('permissions', NON_RESTRICTED_ADD_PERMISSIONS)
@@ -297,3 +302,230 @@ class TestAddInteraction(APITestMixin):
         response_data = response.json()
 
         assert response_data == expected_response
+
+    @pytest.mark.parametrize(
+        'initial_value,new_value',
+        (
+            (
+                {
+                    'service_id': ServiceConstant.global_growth_service.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_NO_OPTIONAL,
+                },
+                {
+                    'service': ServiceConstant.global_growth_service.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_ALL_OPTIONAL,
+                },
+            ),
+            (
+                {
+                    'service_id': ServiceConstant.global_growth_service.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_ALL_OPTIONAL,
+                },
+                {
+                    'service':
+                        ServiceConstant.providing_investment_advice_and_information.value.id,
+                    'service_answers': {
+                        ServiceQuestionConstant.piai_what_did_you_give_advice_about.value.id: {
+                            ServiceAnswerOptionConstant.piai_banking_and_funding.value.id: {},
+                        },
+                        ServiceQuestionConstant.piai_was_this_of_significant_assistance.value.id: {
+                            ServiceAnswerOptionConstant.piai_yes.value.id: {},
+                        },
+                    },
+                },
+            ),
+        ),
+    )
+    def test_update_service_answers(self, initial_value, new_value):
+        """Test that the service answers field can be updated."""
+        interaction = CompanyInteractionFactory(**initial_value)
+
+        url = reverse('api-v3:interaction:item', kwargs={'pk': interaction.pk})
+        response = self.api_client.patch(
+            url,
+            data=new_value,
+        )
+        assert response.status_code == status.HTTP_200_OK
+        response_data = response.json()
+        assert response_data['service']['id'] == new_value['service']
+        assert response_data['service_answers'] == new_value['service_answers']
+
+    @pytest.mark.parametrize(
+        'initial_value,new_value,expected_response',
+        (
+            (
+                {
+                    'service_id': ServiceConstant.global_growth_service.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_NO_OPTIONAL,
+                },
+                {
+                    'service':
+                        ServiceConstant.providing_investment_advice_and_information.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_ALL_OPTIONAL,
+                },
+                {
+                    ServiceQuestionConstant.ggs_status.value.id: [
+                        'This question does not relate to selected service.',
+                    ],
+                    ServiceQuestionConstant.piai_what_did_you_give_advice_about.value.id: [
+                        'This field is required.',
+                    ],
+                    ServiceQuestionConstant.piai_was_this_of_significant_assistance.value.id: [
+                        'This field is required.',
+                    ],
+                },
+            ),
+            (
+                {
+                    'service_id': ServiceConstant.global_growth_service.value.id,
+                    'service_answers': GLOBAL_GROWTH_SERVICE_ANSWERS_ALL_OPTIONAL,
+                },
+                {
+                    'service':
+                        ServiceConstant.providing_investment_advice_and_information.value.id,
+                    'service_answers': {},
+                },
+                {'service_answers': ['This field is required.']},
+            ),
+        ),
+    )
+    def test_cannot_update_service_answers(self, initial_value, new_value, expected_response):
+        """Test that the service answers field cannot be updated with incorrect data."""
+        interaction = CompanyInteractionFactory(**initial_value)
+
+        url = reverse('api-v3:interaction:item', kwargs={'pk': interaction.pk})
+        response = self.api_client.patch(
+            url,
+            data=new_value,
+        )
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == expected_response
+
+    @pytest.mark.parametrize('permissions', NON_RESTRICTED_VIEW_PERMISSIONS)
+    @freeze_time('2017-04-18 13:25:30.986208')
+    def test_non_restricted_user_can_get_interaction_with_service_answers(
+        self,
+        permissions,
+    ):
+        """Test that a non-restricted user can get interaction with service answers."""
+        requester = create_test_user(permission_codenames=permissions)
+
+        extra_data = {
+            'service_id': ServiceConstant.providing_investment_advice_and_information.value.id,
+            'service_answers': {
+                ServiceQuestionConstant.piai_what_did_you_give_advice_about.value.id: {
+                    ServiceAnswerOptionConstant.piai_banking_and_funding.value.id: {},
+                },
+                ServiceQuestionConstant.piai_was_this_of_significant_assistance.value.id: {
+                    ServiceAnswerOptionConstant.piai_yes.value.id: {},
+                },
+            },
+        }
+        interaction = InvestmentProjectInteractionFactory(**extra_data)
+        api_client = self.create_api_client(user=requester)
+        url = reverse('api-v3:interaction:item', kwargs={'pk': interaction.pk})
+        response = api_client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        response_data = response.json()
+        response_data['contacts'].sort(key=itemgetter('id'))
+        assert response_data == {
+            'id': response_data['id'],
+            'kind': Interaction.KINDS.interaction,
+            'status': Interaction.STATUSES.complete,
+            'theme': interaction.theme,
+            'is_event': None,
+            'service_delivery_status': None,
+            'grant_amount_offered': None,
+            'net_company_receipt': None,
+            'policy_areas': [
+                {
+                    'id': str(policy_area.pk),
+                    'name': policy_area.name,
+                } for policy_area in interaction.policy_areas.all()
+            ],
+            'policy_feedback_notes': interaction.policy_feedback_notes,
+            'policy_issue_types': [
+                {
+                    'id': str(policy_issue_type.pk),
+                    'name': policy_issue_type.name,
+                } for policy_issue_type in interaction.policy_issue_types.all()
+            ],
+            'was_policy_feedback_provided': interaction.was_policy_feedback_provided,
+            'communication_channel': {
+                'id': str(interaction.communication_channel.pk),
+                'name': interaction.communication_channel.name,
+            },
+            'subject': interaction.subject,
+            'date': interaction.date.date().isoformat(),
+            'dit_adviser': {
+                'id': str(interaction.dit_adviser.pk),
+                'first_name': interaction.dit_adviser.first_name,
+                'last_name': interaction.dit_adviser.last_name,
+                'name': interaction.dit_adviser.name,
+            },
+            'dit_participants': [
+                {
+                    'adviser': {
+                        'id': str(interaction.dit_adviser.pk),
+                        'first_name': interaction.dit_adviser.first_name,
+                        'last_name': interaction.dit_adviser.last_name,
+                        'name': interaction.dit_adviser.name,
+                    },
+                    'team': {
+                        'id': str(interaction.dit_team.pk),
+                        'name': interaction.dit_team.name,
+                    },
+                },
+            ],
+            'dit_team': {
+                'id': str(interaction.dit_team.pk),
+                'name': interaction.dit_team.name,
+            },
+            'notes': interaction.notes,
+            'company': {
+                'id': str(interaction.company.pk),
+                'name': interaction.company.name,
+            },
+            'contacts': [
+                {
+                    'id': str(contact.pk),
+                    'name': contact.name,
+                    'first_name': contact.first_name,
+                    'last_name': contact.last_name,
+                    'job_title': contact.job_title,
+                }
+                for contact in interaction.contacts.order_by('pk')
+            ],
+            'event': None,
+            'service': {
+                'id': str(ServiceConstant.providing_investment_advice_and_information.value.id),
+                'name': ServiceConstant.providing_investment_advice_and_information.value.name,
+            },
+            'service_answers': extra_data['service_answers'],
+            'investment_project': {
+                'id': str(interaction.investment_project.pk),
+                'name': interaction.investment_project.name,
+                'project_code': interaction.investment_project.project_code,
+            } if interaction.investment_project else None,
+            'archived_documents_url_path': interaction.archived_documents_url_path,
+            'created_by': {
+                'id': str(interaction.created_by.pk),
+                'first_name': interaction.created_by.first_name,
+                'last_name': interaction.created_by.last_name,
+                'name': interaction.created_by.name,
+            },
+            'modified_by': {
+                'id': str(interaction.modified_by.pk),
+                'first_name': interaction.modified_by.first_name,
+                'last_name': interaction.modified_by.last_name,
+                'name': interaction.modified_by.name,
+            },
+            'created_on': '2017-04-18T13:25:30.986208Z',
+            'modified_on': '2017-04-18T13:25:30.986208Z',
+            'location': '',
+            'archived': False,
+            'archived_by': None,
+            'archived_on': None,
+            'archived_reason': None,
+        }
