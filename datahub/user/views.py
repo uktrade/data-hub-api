7a222032af9716b3e8d88b167847d30df7e26dd2
Reupen Shah <reupen@users.noreply.github.com>
2019-07-04 10:15:47 +0100

    Disable atomic requests for /whoami/
    As the `/whoami/` endpoint does not directly modify the database, this opts it out of atomic requests so that it does not cause a transaction to be used for the lifetime of the request.
    
    This should also ensure that when OAuth2 token introspection causes an access token to be inserted into the database (for requests to this endpoint), it is visible to other requests as soon as possible.

--- datahub/user/views.py
+++ datahub/user/views.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -6,9 +7,20 @@ from datahub.user.serializers import WhoAmISerializer
 
 
 @api_view()
+@transaction.non_atomic_requests
 @permission_classes([IsAuthenticated])
 def who_am_i(request):
-    """Return the current user. This view is behind a login."""
+    """
+    Return the current user. This view is behind a login.
+
+    As this endpoint does not directly modify the database, it is opted out of atomic requests
+    so that it does not cause a transaction to be used for the lifetime of the request.
+
+    This should also ensure that when OAuth2 token introspection causes an access token to be
+    inserted into the database, it is visible to other requests as soon as possible.
+
+    See also: `datahub.core.reversion.NonAtomicRevisionMiddleware`.
+    """
     serializer = WhoAmISerializer(request.user)
 
     return Response(data=serializer.data)
