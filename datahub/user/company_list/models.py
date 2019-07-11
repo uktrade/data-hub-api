f12e41fe5241d53d16f57d98b2cc1befe664dcc8
Reupen Shah <reupen@users.noreply.github.com>
2019-07-11 15:40:08 +0100

    Add endpoint to add a company to a user's personal list
    This adds an endpoint at `PUT /v4/user/company-list/<company ID>` which adds a company to the authenticated user's personal list of companies.
    
    A generic view has not been used in this case as the behaviour required for these endpoints is somewhat different.
    
    At the moment, the serializer does not do much, but validation that added companies aren't archived is likely to be added.

--- datahub/user/company_list/models.py
+++ datahub/user/company_list/models.py
@@ -3,6 +3,16 @@ from uuid import uuid4
 from django.db import models
 
 from datahub.core.models import BaseModel
+from datahub.core.utils import StrEnum
+
+
+class CompanyListItemPermission(StrEnum):
+    """CompanyListItem permission codename constants."""
+
+    view_company_list_item = 'view_companylistitem'
+    add_company_list_item = 'add_companylistitem'
+    change_company_list_item = 'change_companylistitem'
+    delete_company_list_item = 'delete_companylistitem'
 
 
 class CompanyListItem(BaseModel):
