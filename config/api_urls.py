00d2bfd19960604a12de8d90f833e357ba6eba53
Brendan Smith <brendan.smith@digital.trade.gov.uk>
2019-07-18 10:37:26 +0100

    Add DNB search API endpoint (proxying dnb-service) (#1863)

--- config/api_urls.py
+++ config/api_urls.py
@@ -9,6 +9,7 @@ from datahub.company import views as company_views
 from datahub.company.urls import ch_company as ch_company_urls
 from datahub.company.urls import company as company_urls
 from datahub.company.urls import contact as contact_urls
+from datahub.dnb_api import urls as dnb_api_urls
 from datahub.dnb_match import urls as dnb_match_urls
 from datahub.event import urls as event_urls
 from datahub.feature_flag import urls as feature_flag_urls
@@ -60,6 +61,7 @@ v3_urls = [
 v4_urls = [
     path('', include((ch_company_urls.urls, 'ch-company'), namespace='ch-company')),
     path('', include((company_urls.urls, 'company'), namespace='company')),
+    path('dnb/', include((dnb_api_urls, 'dnb_api'), namespace='dnb-api')),
     path('dnb-match/', include((dnb_match_urls, 'dnb_match'), namespace='dnb-match')),
     path('', include((search_urls.urls_v4, 'search'), namespace='search')),
     path(
