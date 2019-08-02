3cdeda2e928254457c38e23676388ab4b79250a1
Reupen Shah <reupen@users.noreply.github.com>
2019-08-02 16:27:40 +0100

    Add request times to Gunicorn log format
    This updates the Gunicorn access log format to include request times in seconds (with decimal places).

--- config/gunicorn.py
+++ config/gunicorn.py
@@ -5,7 +5,7 @@ from psycogreen.gevent import patch_psycopg
 accesslog = os.environ.get('GUNICORN_ACCESSLOG', '-')
 access_log_format = os.environ.get(
     'GUNICORN_ACCESS_LOG_FORMAT',
-    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %({X-Forwarded-For}i)s',
+    '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(L)s %({X-Forwarded-For}i)s',
 )
 worker_class = os.environ.get('GUNICORN_WORKER_CLASS', 'gevent')
 worker_connections = os.environ.get('GUNICORN_WORKER_CONNECTIONS', '10')
