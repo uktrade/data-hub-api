eb961a60b56b7705c4ae850fa2d029f96c2586e2
Reupen Shah <reupen@users.noreply.github.com>
2019-08-09 15:22:40 +0100

    Add Elasticsearch TextWithTrigram field type
    This adds a TextWithTrigram field type for Elasticsearch mappings. This represents a Text field with a trigram sub-field.
    
    It has been used in places where we were making the same definition manually.

--- datahub/search/inner_docs.py
+++ datahub/search/inner_docs.py
@@ -1,6 +1,6 @@
 from elasticsearch_dsl import InnerDoc, Keyword, Text
 
-from datahub.search.fields import TrigramText
+from datahub.search.fields import TextWithTrigram
 
 
 class Person(InnerDoc):
@@ -9,19 +9,11 @@ class Person(InnerDoc):
     id = Keyword()
     first_name = Text(index=False)
     last_name = Text(index=False)
-    name = Text(
-        fields={
-            'trigram': TrigramText(),
-        },
-    )
+    name = TextWithTrigram()
 
 
 class IDNameTrigram(InnerDoc):
     """Inner doc for a named object, with a trigram sub-field on name."""
 
     id = Keyword()
-    name = Text(
-        fields={
-            'trigram': TrigramText(),
-        },
-    )
+    name = TextWithTrigram()
