2087746fcbdcff48c70bfa6af9c3243305bbb8cf
Brendan Smith <brendan.smith@gamer-network.net>
2019-08-22 15:54:46 +0100

    Adjust notification task to not retry API calls that give 400/403 responses

--- datahub/notification/tasks.py
+++ datahub/notification/tasks.py
@@ -1,16 +1,16 @@
 from celery import shared_task
+from notifications_python_client.errors import HTTPError
 
 from datahub.notification.core import notify_gateway
 
 
 @shared_task(
+    bind=True,
     acks_late=True,
     priority=9,
-    max_retries=5,
-    autoretry_for=(Exception,),
-    retry_backoff=60,
 )
 def send_email_notification(
+    self,
     recipient_email,
     template_identifier,
     context=None,
@@ -20,10 +20,16 @@ def send_email_notification(
     Celery task to call the notify API to send a templated email notification
     to an email address.
     """
-    response = notify_gateway.send_email_notification(
-        recipient_email,
-        template_identifier,
-        context,
-        notify_service_name,
-    )
+    try:
+        response = notify_gateway.send_email_notification(
+            recipient_email,
+            template_identifier,
+            context,
+            notify_service_name,
+        )
+    except HTTPError as exc:
+        # Raise 400/403 responses without retry
+        if exc.status_code in (400, 403):
+            raise exc
+        raise self.retry(exc=exc, max_retries=5, countdown=60)
     return response['id']
