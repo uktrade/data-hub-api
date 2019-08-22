846d505946b39c240cb52a75e0d1ba62db94f778
Brendan Smith <brendan.smith@gamer-network.net>
2019-08-22 16:15:11 +0100

    Add comment explaining notification client mock approach

--- datahub/notification/constants.py
+++ datahub/notification/constants.py
@@ -11,7 +11,7 @@ class NotifyServiceName(StrEnum):
 DEFAULT_SERVICE_NAME = NotifyServiceName.datahub
 
 # TODO: This module should not know or care about the specifics of our notify
-# service instances.  This should be moved up in to a setting.  To ensure easy
+# service instances. This should be moved up in to a setting. To ensure easy
 # rollback, we will turn this on once OMIS integration no longer uses a feature
 # flag.
 NOTIFY_KEYS = {
