8a20666afba47e998b296889b35266bcafa6676c
Reupen Shah <reupen@users.noreply.github.com>
2019-09-03 16:36:50 +0100

    Make sync_es management command run asynchronously by default
    This makes the `sync_es` management command run asynchronously by default.
    
    A `--foreground` argument was added to run it synchronously.

--- datahub/search/management/commands/sync_es.py
+++ datahub/search/management/commands/sync_es.py
@@ -19,6 +19,12 @@ class Command(BaseCommand):
             choices=[search_app.name for search_app in get_search_apps()],
             help='Search model to import. If empty, it imports all',
         )
+        parser.add_argument(
+            '--foreground',
+            action='store_true',
+            help='If specified, the command runs in the foreground without needing Celery '
+                 'running. (By default, it runs asynchronously using Celery.)',
+        )
 
     def handle(self, *args, **options):
         """Handle."""
@@ -33,6 +39,11 @@ class Command(BaseCommand):
             )
 
         for app in apps:
-            sync_model.apply(args=(app.name,), throw=True)
+            task_args = (app.name,)
+
+            if options['foreground']:
+                sync_model.apply(args=task_args, throw=True)
+            else:
+                sync_model.apply_async(args=task_args)
 
         logger.info('Elasticsearch sync complete!')
