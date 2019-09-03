43c3ea73ccaadd827981c3ac9a140edf469deac5
Reupen Shah <reupen@users.noreply.github.com>
2019-09-03 16:18:50 +0100

    Update sync_es management command to call sync_model Celery task
    This changes the `sync_es` management command to call the `sync_model` Celery task synchronously.
    
    This is in preparation for it calling the Celery task asynchronously by default.
    
    The `--batch_size` argument was removed as it is rarely used and isn't currently supported by the Celery task.

--- datahub/search/management/commands/sync_es.py
+++ datahub/search/management/commands/sync_es.py
@@ -3,17 +3,9 @@ from logging import getLogger, WARNING
 from django.core.management.base import BaseCommand, CommandError
 
 from datahub.search.apps import are_apps_initialised, get_search_apps, get_search_apps_by_name
-from datahub.search.bulk_sync import sync_app
+from datahub.search.tasks import sync_model
 
 logger = getLogger(__name__)
-
-
-def sync_es(batch_size, search_apps):
-    """Sends data to Elasticsearch."""
-    for app in search_apps:
-        sync_app(app, batch_size=batch_size)
-
-    logger.info('Elasticsearch sync complete!')
 
 
 class Command(BaseCommand):
@@ -21,12 +13,6 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         """Handle arguments."""
-        parser.add_argument(
-            '--batch_size',
-            type=int,
-            help='Batch size - number of rows processed at a time (defaults to per-model '
-                 'defaults)',
-        )
         parser.add_argument(
             '--model',
             action='append',
@@ -46,7 +32,7 @@ class Command(BaseCommand):
                 f'Index and mapping not initialised, please run `init_es` first.',
             )
 
-        sync_es(
-            batch_size=options['batch_size'],
-            search_apps=apps,
-        )
+        for app in apps:
+            sync_model.apply(args=(app.name,), throw=True)
+
+        logger.info('Elasticsearch sync complete!')
