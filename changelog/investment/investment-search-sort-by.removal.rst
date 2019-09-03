b8f73773c575b8087a02ee074cdf6168db5389e8
Reupen Shah <reupen@users.noreply.github.com>
2019-09-03 15:12:47 +0100

    Remove deprecated investment project search sorting options
    This removes various deprecated sorting options from investment project search (affecting the`POST /v3/search/investment_project` and `POST /v3/search/investment_project/export` endpoints).
    
    These were previously deprecated in #2033.

---
+++ changelog/investment/investment-search-sort-by.removal.rst
@@ -0,0 +1,1 @@
+``POST /v3/search/investment_project``, ``POST /v3/search/investment_project/export`` various deprecated ``sortby`` values were removed. See the API section for more details.
--- datahub/search/investment/serializers.py
+++ datahub/search/investment/serializers.py
@@ -42,68 +42,9 @@ class SearchInvestmentProjectQuerySerializer(EntitySearchQuerySerializer):
     gross_value_added_start = serializers.IntegerField(required=False, min_value=0)
     gross_value_added_end = serializers.IntegerField(required=False, min_value=0)
 
-    # TODO: Remove these following deprecation period.
-    deprecated_sortby_fields = {
-        'actual_land_date',
-        'approved_commitment_to_invest',
-        'approved_fdi',
-        'approved_good_value',
-        'approved_high_value',
-        'approved_landed',
-        'approved_non_fdi',
-        'archived',
-        'archived_by.name',
-        'average_salary.name',
-        'business_activities.name',
-        'client_cannot_provide_total_investment',
-        'client_contacts.name',
-        'client_relationship_manager.name',
-        'export_revenue',
-        'fdi_type.name',
-        'foreign_equity_investment',
-        'government_assistance',
-        'id',
-        'intermediate_company.name',
-        'investment_type.name',
-        'investor_company.name',
-        'likelihood_to_land.name',
-        'modified_on',
-        'new_tech_to_uk',
-        'non_fdi_r_and_d_budget',
-        'number_new_jobs',
-        'project_assurance_adviser.name',
-        'project_code',
-        'project_manager.name',
-        'r_and_d_budget',
-        'referral_source_activity.name',
-        'referral_source_activity_event',
-        'referral_source_activity_marketing.name',
-        'referral_source_activity_website.name',
-        'sector.name',
-        'site_decided',
-        'total_investment',
-        'uk_company.name',
-    }
-
     SORT_BY_FIELDS = (
         'created_on',
         'estimated_land_date',
         'name',
         'stage.name',
-        *deprecated_sortby_fields,
     )
-
-    def validate(self, data):
-        """
-        Log all uses of deprecated sorting options as an extra check to make sure that they
-        aren't being used before we get rid of them.
-
-        TODO: Remove following deprecation period.
-        """
-        sortby = data.get('sortby')
-        if sortby and sortby.field in self.deprecated_sortby_fields:
-            logger.error(
-                f'The following deprecated investment project search sortby field was used:'
-                f' {sortby.field}',
-            )
-        return super().validate(data)
