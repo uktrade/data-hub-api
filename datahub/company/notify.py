0e4e4cf39911bb511fee8c095c6f78fbf6580adb
Brendan Smith <brendan.smith@gamer-network.net>
2019-09-09 14:16:12 +0100

    Notify DNB about new company investigation based on company created signal.

---
+++ datahub/company/notify.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+from django.conf import settings
+
+from datahub.notification.constants import NotifyServiceName
+from datahub.notification.notify import notify_by_email
+
+
+class Template(Enum):
+    """
+    GOV.UK notifications template ids.
+    """
+
+    request_new_business_record = '6b987540-82c3-47ea-86b8-dacdd26410c4'
+
+
+def notify_new_dnb_investigation(company):
+    """
+    Notify DNB of a new company investigation.
+    """
+    investigation_context = get_dnb_investigation_context(company)
+    recipients = settings.DNB_INVESTIGATION_NOTIFICATION_RECIPIENTS
+    for email_address in recipients:
+        notify_by_email(
+            email_address,
+            Template.request_new_business_record.value,
+            investigation_context,
+            notify_service_name=NotifyServiceName.dnb_investigation,
+        )
+
+
+def get_dnb_investigation_context(company):
+    """
+    Get a dict with values for fields that are to be sent
+    to DNB for investigation.
+    """
+    address_parts = [
+        company.address_1,
+        company.address_2,
+        company.address_town,
+        company.address_county,
+        company.address_country.name,
+        company.address_postcode,
+    ]
+    dnb_investigation_data = company.dnb_investigation_data or {}
+    return {
+        'business_name': company.name,
+        'business_address': ', '.join(
+            address_part for address_part in address_parts if address_part
+        ),
+        'website': company.website or '',
+        'contact_number': dnb_investigation_data.get('telephone_number', ''),
+    }
--- datahub/company/signals.py
+++ datahub/company/signals.py
@@ -1,9 +1,11 @@
 import logging
 
-from django.db.models.signals import post_migrate
+from django.db.models.signals import post_migrate, post_save
 from django.dispatch import receiver
 
 from datahub.company.constants import BusinessTypeConstant
+from datahub.company.models import Company
+from datahub.company.notify import notify_new_dnb_investigation
 from datahub.core.utils import load_constants_to_database
 from datahub.metadata.models import BusinessType
 
@@ -22,3 +24,18 @@ def company_business_type_post_migrate(sender, **kwargs):
     Any new business types are created, and any existing ones are updated if the label has changed.
     """
     load_constants_to_database(BusinessTypeConstant, BusinessType)
+
+
+@receiver(
+    post_save,
+    sender=Company,
+)
+def notify_dnb_investigation(sender, instance, created, raw, **kwargs):
+    """
+    Notify recipients of a new company that needs investigation by DNB. This will only be triggered
+    for a company which has `pending_dnb_investigation=True` and was not created
+    through a data fixture (or similar).
+    """
+    if created and not raw and instance.pending_dnb_investigation:
+        logger.info(f'Company with ID {instance.id} is pending DNB investigation.')
+        notify_new_dnb_investigation(instance)
