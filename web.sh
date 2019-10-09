cef517976eba305996aed787bfdc4f2a7df5dece
Reupen Shah <reupen@users.noreply.github.com>
2019-10-09 16:49:13 +0100

    Apply schema migrations for the mi database automatically on deployment
    Prior to this change, migrations for the `mi` database had to be applied manually in deployed environments, and we would often forget to do this.
    
    This change modifies the script used for the Procfile web process so that migrations for that database are applied automatically.
    
    Because we don't have an MI database in some environments, this step can be skipped by setting the `SKIP_MI_DATABASE_MIGRATIONS` environment variable to a non-empty value.

--- web.sh
+++ web.sh
@@ -5,5 +5,10 @@
 set  -xe
 
 ./manage.py distributed_migrate --noinput
+
+if [ -z "$SKIP_MI_DATABASE_MIGRATIONS" ]; then
+  ./manage.py distributed_migrate --noinput --database mi
+fi
+
 ./manage.py init_es
 gunicorn config.wsgi --config config/gunicorn.py
