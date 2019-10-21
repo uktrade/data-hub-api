5580150f9da9e9241adbd5ef6ed6aeb7f1e8cc6b
Alexey Bezhan <al@fueledbylemons.com>
2019-10-21 16:18:36 +0100

    Add an API endpoint to export Teams
    This is similar to Advisors #2187 and other dataset endpoints,
    but since teams don't have a creation date we're ordering the
    table by ID only.
    
    This has a small chance of potentially breaking pagination, but
    (based on a discussion with @reupen) adding a creation date to the
    model is out of scope for this and the table data doesn't change
    that often so the risk is low.
    
    Since Teams are pre-populated by a session-wide fixture the test
    endpoint returns a lot of existing results. So instead of creating
    new ones we check serialization for one of the existing teams and
    test that items are sorted by ID.

---
+++ changelog/adviser/teams-dataset-api-endpoint.api.md
@@ -0,0 +1,1 @@
+A new teams dataset endpoint (`GET /v4/dataset/teams-dataset`) was added to be consumed by data-flow and used in data-workspace.
--- datahub/dataset/urls.py
+++ datahub/dataset/urls.py
@@ -5,6 +5,7 @@ from datahub.dataset.company.views import CompaniesDatasetView
 from datahub.dataset.contact.views import ContactsDatasetView
 from datahub.dataset.interaction.views import InteractionsDatasetView
 from datahub.dataset.order.views import OMISDatasetView
+from datahub.dataset.team.views import TeamsDatasetView
 
 
 urlpatterns = [
@@ -13,4 +14,5 @@ urlpatterns = [
     path('contacts-dataset', ContactsDatasetView.as_view(), name='contacts-dataset'),
     path('companies-dataset', CompaniesDatasetView.as_view(), name='companies-dataset'),
     path('interactions-dataset', InteractionsDatasetView.as_view(), name='interactions-dataset'),
+    path('teams-dataset', TeamsDatasetView.as_view(), name='teams-dataset'),
 ]
