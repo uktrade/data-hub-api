4aa73a036a138aa166f4c9b9319162fe38954bbc
Reupen Shah <reupen@users.noreply.github.com>
2019-10-30 17:04:28 +0000

    Remove squashed interaction migrations
    This transitions `0001_squashed_0068_remove_interaction_location_from_database` to a normal migration and removes the migrations it replaced.

--- datahub/interaction/migrations/0069_add_composite_index_to_interaction.py
+++ datahub/interaction/migrations/0069_add_composite_index_to_interaction.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('interaction', '0068_remove_interaction_location_from_database'),
+        ('interaction', '0001_squashed_0068_remove_interaction_location_from_database'),
     ]
 
     operations = [
