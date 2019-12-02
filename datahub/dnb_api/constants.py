3c785a702fc0209d0a009085d429e6a5f6fd6e1e
Brendan Smith <brendan.smith@gamer-network.net>
2019-12-02 16:31:25 +0000

    Make a DRY list of all DNB updated company fields

--- datahub/dnb_api/constants.py
+++ datahub/dnb_api/constants.py
@@ -1,1 +1,16 @@
 FEATURE_FLAG_DNB_COMPANY_SEARCH = 'dnb-company-search'
+
+ALL_DNB_UPDATED_FIELDS = (
+    'name',
+    'trading_names',
+    'address',
+    'registered_address',
+    'website',
+    'number_of_employees',
+    'is_number_of_employees_estimated',
+    'turnover',
+    'is_turnover_estimated',
+    'website',
+    'global_ultimate_duns_number',
+    'company_number',
+)
