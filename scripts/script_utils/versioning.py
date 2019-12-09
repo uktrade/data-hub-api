db2aefe02d3018bc0244c0111fbe804b02b0fb39
Reupen Shah <reupen@users.noreply.github.com>
2019-12-09 16:46:14 +0000

    Write and source version number from the package
    This updates the release scripts to:
    
    - write the version number to the package
    - source the version number from the package instead of the changelog
    
    The `create_changelog.py` script has also been renamed `prepare_release.py` as it now also writes the updated version number to the package.
    
    The main reason for making this change is to:
    
    - make it easier to access from version number at run-time (e.g. for an endpoint that returns the version or to display it in the admin site)
    - avoid sourcing the version number from the changelog

--- scripts/script_utils/versioning.py
+++ scripts/script_utils/versioning.py
@@ -1,11 +1,11 @@
-import re
 from enum import Enum
 from pathlib import PurePath
 
 from semantic_version import Version
 
 
-CHANGELOG_PATH = PurePath(__file__).parents[2] / 'CHANGELOG.md'
+DATA_HUB_PACKAGE_PATH = PurePath(__file__).parents[2]
+VERSION_FILE_PATH = DATA_HUB_PACKAGE_PATH / 'datahub' / 'VERSION'
 
 
 class ReleaseType(Enum):
@@ -17,34 +17,28 @@ class ReleaseType(Enum):
 
 
 def get_current_version():
-    """
-    Get the current version number from the changelog.
-
-    Note: In future we may want to write and obtain the version number to and from a more
-    authoritative source e.g. a module in the datahub package.
-    """
-    with open(CHANGELOG_PATH, 'r') as file:
-        changelog = file.read(10_000)
-
-    match = re.search(r' (?P<version>\d+\.\d+\.\d+) ', changelog)
-
-    if match:
-        return match.group('version')
-
-    return None
+    """Get the current version number from the package."""
+    with open(VERSION_FILE_PATH, 'r') as version_file:
+        return version_file.read().strip()
 
 
-def get_next_version(current_version, release_type):
+def set_current_version(new_version):
+    """Write the current version number to the package."""
+    with open(VERSION_FILE_PATH, 'w') as version_file:
+        version_file.write(f'{new_version}\n')
+
+
+def get_next_version(release_type):
     """Increment a version for a particular release type."""
     if not isinstance(release_type, ReleaseType):
         raise TypeError()
 
-    parsed_version = Version(current_version)
+    version = Version(get_current_version())
 
     if release_type is ReleaseType.major:
-        return str(parsed_version.next_major())
+        return str(version.next_major())
 
     if release_type is ReleaseType.minor:
-        return str(parsed_version.next_minor())
+        return str(version.next_minor())
 
-    return str(parsed_version.next_patch())
+    return str(version.next_patch())
