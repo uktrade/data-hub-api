e2c9664e6414b59f977ed6f4f8ce59623ebe3b44
Ali Zaidi <alixedi@gmail.com>
2019-12-17 15:32:10 +0000

    Add utility function to rollback D&B updates

--- datahub/dnb_api/constants.py
+++ datahub/dnb_api/constants.py
@@ -15,3 +15,28 @@ ALL_DNB_UPDATED_FIELDS = (
     'global_ultimate_duns_number',
     'company_number',
 )
+
+ALL_DNB_UPDATED_MODEL_FIELDS = (
+    'name',
+    'trading_names',
+    'address_1',
+    'address_2',
+    'address_town',
+    'address_county',
+    'address_country',
+    'address_postcode',
+    'registered_address_1',
+    'registered_address_2',
+    'registered_address_town',
+    'registered_address_county',
+    'registered_address_country',
+    'registered_address_postcode',
+    'website',
+    'number_of_employees',
+    'is_number_of_employees_estimated',
+    'turnover',
+    'is_turnover_estimated',
+    'website',
+    'global_ultimate_duns_number',
+    'company_number',
+)
