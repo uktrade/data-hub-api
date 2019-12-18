eeebb13936ac0a8f7bd7fccc3594856e2c9c4243
Ali Zaidi <alixedi@gmail.com>
2019-12-18 09:29:53 +0000

    Add allow_join_result to avoid RuntimeError

--- datahub/dnb_api/test/test_tasks.py
+++ datahub/dnb_api/test/test_tasks.py
@@ -521,7 +521,7 @@ class TestGetCompanyUpdates:
             'datahub.dnb_api.tasks.get_company_update_page',
             mock_get_company_update_page,
         )
-        task_result = get_company_updates.apply()
+        task_result = get_company_updates.apply_async()
 
         company.refresh_from_db()
         dnb_company = dnb_company_updates_response_uk['results'][0]
