6a8dec4c97d6bdbcdfb4a5d8081f4d40f8132fba
Reupen Shah <reupen@users.noreply.github.com>
2020-01-02 14:59:51 +0000

    Add interaction DIT participants to company list item response
    This adds a list of DIT participants to the latest interaction of each company returned by `GET /v4/company-list/<pk>/item`.
    
    As a reminder, the retrieval of this data is done in the main query to avoid doing individual queries for each company list item. This was tricky in this case, as each interaction can have multiple participants. I came up with a somewhat creative solution of combining `ArrayAgg` and `JSONBBuildObject` to do this.
    
    From checking one of the current worst cases in production, the execution time of the query according to `EXPLAIN ANALYSE` goes up from around 4ms to 9ms (which is still fast).

--- datahub/user/company_list/queryset.py
+++ datahub/user/company_list/queryset.py
@@ -1,8 +1,13 @@
-from django.db.models import DurationField, ExpressionWrapper, F
+from django.db.models import Case, DurationField, ExpressionWrapper, F, Q, When
 from django.db.models.functions import Now
 
-from datahub.core.query_utils import get_top_related_expression_subquery
-from datahub.interaction.models import Interaction
+from datahub.core.query_utils import (
+    get_array_agg_subquery,
+    get_full_name_expression,
+    get_top_related_expression_subquery,
+    JSONBBuildObject,
+)
+from datahub.interaction.models import Interaction, InteractionDITParticipant
 from datahub.user.company_list.models import CompanyListItem
 
 
@@ -20,6 +25,29 @@ def get_company_list_item_queryset():
         latest_interaction_created_on=_get_field_of_latest_interaction('created_on'),
         latest_interaction_date=_get_field_of_latest_interaction('date'),
         latest_interaction_subject=_get_field_of_latest_interaction('subject'),
+        latest_interaction_dit_participants=_get_field_of_latest_interaction(
+            get_array_agg_subquery(
+                InteractionDITParticipant,
+                'interaction',
+                JSONBBuildObject(
+                    adviser=_get_null_when_expression(
+                        JSONBBuildObject(
+                            id='adviser__id',
+                            name=get_full_name_expression('adviser'),
+                        ),
+                        Q(adviser__isnull=True),
+                    ),
+                    team=_get_null_when_expression(
+                        JSONBBuildObject(
+                            id='team__id',
+                            name='team__name',
+                        ),
+                        Q(team__isnull=True),
+                    ),
+                ),
+                ordering=('pk',),
+            ),
+        ),
         latest_interaction_time_ago=ExpressionWrapper(
             Now() - F('latest_interaction_date'),
             output_field=DurationField(),
@@ -32,7 +60,17 @@ def get_company_list_item_queryset():
 def _get_field_of_latest_interaction(field):
     return get_top_related_expression_subquery(
         Interaction.company.field,
-        F(field),
+        field,
         ('-date', '-created_on', 'pk'),
         outer_field='company_id',
+    )
+
+
+def _get_null_when_expression(expression, null_when_condition):
+    return Case(
+        When(
+            null_when_condition,
+            then=None,
+        ),
+        default=expression,
     )
