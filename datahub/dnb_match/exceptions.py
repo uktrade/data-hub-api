cdbd00e4f25a01a11dddd5f841c0d9bc4f64c30b
Ali Zaidi <alixedi@gmail.com>
2020-01-06 09:35:24 +0000

    Remove dnb_match serializer & utilities

--- datahub/dnb_match/exceptions.py
+++
@@ -1,6 +0,0 @@
-class MismatchedRecordsException(Exception):
-    """
-    To indicate that 2 entities were mistakenly thought to be the same
-    company. E.g. a Data Hub company record and a Worldbase record with
-    different DUNS numbers.
-    """
