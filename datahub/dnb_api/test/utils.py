4bcf6dbc3196275e17fcd138b9643d9666a841a2
Ali Zaidi <alixedi@gmail.com>
2020-01-08 13:33:39 +0000

    Add defailt fields for updates_company_from_dnb

---
+++ datahub/dnb_api/test/utils.py
@@ -0,0 +1,22 @@
+from django.forms import model_to_dict
+
+
+def model_to_dict_company(obj):
+    """
+    Returns the dict represetnation of the given model object
+    minus website and registered_address fields
+    """
+    field_to_filter = (
+        'registered_address_1',
+        'registered_address_2',
+        'registered_address_town',
+        'registered_address_county',
+        'registered_address_country',
+        'registered_address_postcode',
+        'website',
+    )
+    return {
+        field: value
+        for field, value in model_to_dict(obj).items()
+        if field not in field_to_filter
+    }
