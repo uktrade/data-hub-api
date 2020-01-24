0c13113509485877bab1aac72b43b92b4d26f6db
Reupen Shah <reupen@users.noreply.github.com>
2020-01-24 16:55:01 +0000

    Add create company referral view
    This adds a view at `POST /v4/company-referral` for creating a company referral.
    
    Similar patterns to other apps have been used.

---
+++ changelog/company/add-referral-view.api.md
@@ -0,0 +1,1 @@
+A new endpoint, `POST /v4/company-referral`, was added. This creates a new company referral. Refer to the API documentation for the schema.
---
+++ datahub/company_referral/views.py
@@ -0,0 +1,17 @@
+from datahub.company_referral.models import CompanyReferral
+from datahub.company_referral.serializers import CompanyReferralSerializer
+from datahub.core.viewsets import CoreViewSet
+from datahub.oauth.scopes import Scope
+
+
+class CompanyReferralViewSet(CoreViewSet):
+    """Company referral view set."""
+
+    serializer_class = CompanyReferralSerializer
+    required_scopes = (Scope.internal_front_end,)
+    queryset = CompanyReferral.objects.select_related(
+        'company',
+        'contact',
+        'created_by__team',
+        'recipient__team',
+    )
