707dd6c9b5048b187dbe5c655925e30440876999
John Fieldman <hello@johnfieldman.com>
2020-01-27 10:29:37 +0000

    create ES app for CompanyExportCountryHistory to output history and related interaction data

---
+++ datahub/search/export_country_history/__init__.py
@@ -0,0 +1,3 @@
+from datahub.search.export_country_history.apps import ExportCountryHistoryApp
+
+__all__ = ['ExportCountryHistoryApp']
---
+++ datahub/search/export_country_history/signals.py
@@ -0,0 +1,19 @@
+from django.db import transaction
+from django.db.models.signals import post_save
+
+from datahub.company.models import CompanyExportCountryHistory as DBCompanyExportCountryHistory
+from datahub.search.export_country_history import ExportCountryHistoryApp
+from datahub.search.signals import SignalReceiver
+from datahub.search.sync_object import sync_object_async
+
+
+def export_country_history_sync_es(instance):
+    """Sync export country history to the Elasticsearch."""
+    transaction.on_commit(
+        lambda: sync_object_async(ExportCountryHistoryApp, instance.pk),
+    )
+
+
+receivers = (
+    SignalReceiver(post_save, DBCompanyExportCountryHistory, export_country_history_sync_es),
+)
