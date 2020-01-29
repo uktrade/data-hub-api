5eb1ee6abe1ac219746ce299e8103cee26b167ce
Reupen Shah <reupen@users.noreply.github.com>
2020-01-29 10:06:41 +0000

    Remove deprecated adviser filters
    This removes various filters in the `/adviser/` endpoint that were deprecated in version 10.0.0.
    
    The deprecation was some time ago, but I've checked logs etc. and they aren't in use.

--- datahub/company/test/test_adviser_views.py
+++ datahub/company/test/test_adviser_views.py
@@ -111,39 +111,24 @@ class TestAdviser(APITestMixin):
         response = self.api_client.get(url)
         assert response.status_code == status.HTTP_200_OK
 
-    def test_adviser_filtered_view(self):
-        """Test filtering."""
-        adviser = AdviserFactory(last_name='UNIQUE')
-        url = reverse('api-v1:advisor-list')
-        response = self.api_client.get(url, data=dict(last_name__icontains='uniq'))
-        assert response.status_code == status.HTTP_200_OK
-        response_data = response.json()
-        assert len(response_data['results']) == 1
-        result = response_data['results'][0]
-        assert result['last_name'] == adviser.last_name
-        assert result['telephone_number'] == adviser.telephone_number
-        assert result['contact_email'] == adviser.contact_email
-        assert result['is_active'] == adviser.is_active
-
     def test_adviser_list_view_default_sort_order(self):
         """Test default sorting."""
         AdviserFactory(first_name='a', last_name='sorted adviser')
         AdviserFactory(first_name='z', last_name='sorted adviser')
         AdviserFactory(first_name='f', last_name='sorted adviser')
+
         url = reverse('api-v1:advisor-list')
-        response = self.api_client.get(
-            url,
-            data={
-                'last_name__icontains': 'sorted',
-            },
-        )
+        response = self.api_client.get(url)
+
         assert response.status_code == status.HTTP_200_OK
         result = response.json()
-        assert len(result['results']) == 3
+        assert len(result['results']) == 4
         results = result['results']
         assert [res['name'] for res in results] == [
             'a sorted adviser',
             'f sorted adviser',
+            # This is the test user making the request
+            'Testo Useri',
             'z sorted adviser',
         ]
 
