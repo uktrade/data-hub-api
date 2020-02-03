1fe091e5bfea3b3830de40331c1700117664b222
Brendan Smith <brendan.smith@gamer-network.net>
2020-02-03 09:25:29 +0000

    Stub out DNB link admin views/URLs

---
+++ datahub/company/admin/dnb_link/select_ids.py
@@ -0,0 +1,5 @@
+def dnb_link_select_ids(model_admin, request):
+    """
+    View to select Data Hub company id and D&B duns number for linking records.
+    Valid POSTs redirect to the change review page.
+    """
