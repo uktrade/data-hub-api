b9b463f59543b9431903c7f959d3ad72a4d67ce2
Sekhar Panja <sekharpanja@gmail.com>
2020-03-06 17:17:56 +0000

    Include interactions in export country history search
    This updates `POST /v4/search/export-country-history` so that interactions with export countries are additionally included in results.
    
    The endpoint now requires both the `company.view_companyexportcountry` and `interaction.view_all_interaction` permissions.

--- datahub/search/export_country_history/views.py
+++ datahub/search/export_country_history/views.py
@@ -1,9 +1,12 @@
+from elasticsearch_dsl.query import Term
 from oauth2_provider.contrib.rest_framework import IsAuthenticatedOrTokenHasScope
 
+from datahub.core.permissions import HasPermissions
+from datahub.interaction.models import InteractionPermission
 from datahub.oauth.scopes import Scope
 from datahub.search.export_country_history import ExportCountryHistoryApp
 from datahub.search.export_country_history.serializers import SearchExportCountryHistorySerializer
-from datahub.search.permissions import SearchPermissions
+from datahub.search.interaction.models import Interaction
 from datahub.search.views import register_v4_view, SearchAPIView
 
 
@@ -14,7 +17,37 @@ class ExportCountryHistoryView(SearchAPIView):
     required_scopes = (Scope.internal_front_end,)
     search_app = ExportCountryHistoryApp
 
-    permission_classes = (IsAuthenticatedOrTokenHasScope, SearchPermissions)
+    permission_classes = (
+        IsAuthenticatedOrTokenHasScope,
+        # Note: This search view does not use SearchPermissions, as it requires multiple
+        # permissions
+        HasPermissions(
+            f'company.view_companyexportcountry',
+            f'interaction.{InteractionPermission.view_all}',
+        ),
+    )
+
+    fields_to_include = (
+        # Fields common to interactions and export country history objects
+        'company',
+        'date',
+        'id',
+
+        # Export country history object fields
+        'country',
+        'history_date',
+        'history_type',
+        'history_user',
+        'status',
+
+        # Interaction fields
+        'contacts',
+        'dit_participants',
+        'export_countries',
+        'service',
+        'subject',
+    )
+
     FILTER_FIELDS = [
         'country',
         'company',
@@ -22,7 +55,33 @@ class ExportCountryHistoryView(SearchAPIView):
 
     REMAP_FIELDS = {
         'company': 'company.id',
-        'country': 'country.id',
+    }
+
+    COMPOSITE_FILTERS = {
+        'country': [
+            'country.id',
+            'export_countries.country.id',
+        ],
     }
 
     serializer_class = SearchExportCountryHistorySerializer
+
+    def get_entities(self):
+        """
+        Overriding to provide multiple entities
+        """
+        return [self.search_app.es_model, Interaction]
+
+    def get_base_query(self, request, validated_data):
+        """
+        Get the base query.
+
+        This is overridden to exclude UPDATE history items and interactions
+        without export countries.
+        """
+        base_query = super().get_base_query(request, validated_data)
+
+        is_relevant_interaction = Term(were_countries_discussed=True)
+        is_relevant_history_entry = Term(_type=ExportCountryHistoryApp.name)
+
+        return base_query.filter(is_relevant_interaction | is_relevant_history_entry)
