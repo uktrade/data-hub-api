4b4e9b8c4c89a8bf797207405e751ee35c7b1dc8
Sekhar Panja <sekharpanja@gmail.com>
2020-03-06 17:17:56 +0000

    Include interactions in export country history search
    This updates `POST /v4/search/export-country-history` so that interactions with export countries are additionally included in results.
    
    The endpoint now requires both the `company.view_companyexportcountry` and `interaction.view_all_interaction` permissions.

---
+++ changelog/company/export-country-history.api.md
@@ -0,0 +1,3 @@
+`POST /v4/search/export-country-history`: Search results now additionally include interactions that have export countries associated with them.
+
+The endpoint now requires both the `company.view_companyexportcountry` and `interaction.view_all_interaction` permissions.
--- datahub/search/export_country_history/apps.py
+++ datahub/search/export_country_history/apps.py
@@ -1,6 +1,5 @@
 from datahub.company.models import (
     CompanyExportCountryHistory as DBCompanyExportCountryHistory,
-    CompanyPermission,
 )
 from datahub.search.apps import SearchApp
 from datahub.search.export_country_history.models import ExportCountryHistory
@@ -12,7 +11,6 @@ class ExportCountryHistoryApp(SearchApp):
     name = 'export-country-history'
     es_model = ExportCountryHistory
     exclude_from_global_search = True
-    view_permissions = (f'company.{CompanyPermission.view_company}',)
     queryset = DBCompanyExportCountryHistory.objects.select_related(
         'history_user',
         'country',
