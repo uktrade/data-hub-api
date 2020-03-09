37099b2c0faf364065b99f8d18f436dd51843db3
Reupen Shah <reupen@users.noreply.github.com>
2020-03-09 10:13:08 +0000

    Update django-oauth-toolkit to version 1.3.0
    This updates django-oauth-toolkit to version 1.3.0.
    
    (This wasn't picked up by dependabot as we'd pinned oauthlib, due to oauthlib 3.x being incompatible with django-oauth-toolkit 1.2.0.)
    
    Changelogs:
    
    - https://github.com/jazzband/django-oauth-toolkit/blob/master/CHANGELOG.md
    - https://github.com/oauthlib/oauthlib/blob/master/CHANGELOG.rst
    
    (The `invalid_scope` status code was changed from 401 to 400 in oauthlib 3.0.0.)

--- datahub/oauth/test/test_scopes.py
+++ datahub/oauth/test/test_scopes.py
@@ -86,7 +86,7 @@ class TestOAuthScopeBackend:
             data=urlencode(data),
             content_type='application/x-www-form-urlencoded',
         )
-        assert response.status_code == status.HTTP_401_UNAUTHORIZED
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.json() == {
             'error': 'invalid_scope',
         }
