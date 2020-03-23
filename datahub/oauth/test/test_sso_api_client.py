d50871a502d8c5122cea378bbe0f822c021e975e
Reupen Shah <reupen@users.noreply.github.com>
2020-03-23 13:35:01 +0000

    Add utility function to get a user from Staff SSO by email address
    This adds a utility function that gets details of a user from Staff SSO using any of the user's email addresses.
    
    This function will be used as part of enhanced admin site functionality.

--- datahub/oauth/test/test_sso_api_client.py
+++ datahub/oauth/test/test_sso_api_client.py
@@ -1,10 +1,38 @@
+from urllib.parse import urlencode
+
 import pytest
 from django.conf import settings
 from requests.exceptions import ConnectionError
 from rest_framework import status
 
 
-from datahub.oauth.sso_api_client import introspect_token, SSORequestError, SSOTokenDoesNotExist
+from datahub.oauth.sso_api_client import (
+    get_user_by_email,
+    introspect_token,
+    SSORequestError,
+    SSOTokenDoesNotExist,
+    SSOUserDoesNotExist,
+)
+
+
+FAKE_SSO_USER_DATA = {
+    'email': 'email@email.test',
+    'user_id': 'c2c1afce-e45e-4139-9913-88b350f7a546',
+    'email_user_id': 'test@id.test',
+    'first_name': 'Johnny',
+    'last_name': 'Cakeman',
+    'related_emails': ['related@email.test'],
+    'contact_email': 'contact@email.test',
+    'groups': [],
+    'permitted_applications': [
+        {
+            'key': 'an-app',
+            'url': 'https://app.invalid',
+            'name': 'An app',
+        },
+    ],
+    'access_profiles': ['an-access-profile'],
+}
 
 
 class TestIntrospectToken:
@@ -64,3 +92,47 @@ class TestIntrospectToken:
         )
         assert introspect_token('test-token') == mock_data
         assert requests_mock.last_request.text == 'token=test-token'
+
+
+class TestGetUserByEmail:
+    """Tests for get_user_by_email()."""
+
+    @pytest.mark.parametrize(
+        'mock_kwargs,expected_exception',
+        (
+            (
+                {'status_code': status.HTTP_400_BAD_REQUEST},
+                SSORequestError('SSO request failed'),
+            ),
+            (
+                {'status_code': status.HTTP_404_NOT_FOUND},
+                SSOUserDoesNotExist(),
+            ),
+            (
+                {'exc': ConnectionError},
+                SSORequestError('SSO request failed'),
+            ),
+            (
+                {'text': '{"invalid-json}'},
+                SSORequestError('SSO response parsing failed'),
+            ),
+        ),
+    )
+    def test_error_handling(self, requests_mock, mock_kwargs, expected_exception):
+        """Test that various errors are handled as expected."""
+        params = {'email': 'email@email.test'}
+        request_url = f'{settings.STAFF_SSO_BASE_URL}api/v1/user/introspect/?{urlencode(params)}'
+        requests_mock.get(request_url, **mock_kwargs)
+
+        with pytest.raises(expected_exception.__class__) as excinfo:
+            get_user_by_email('email@email.test')
+
+        assert str(excinfo.value) == str(expected_exception)
+
+    def test_returns_data_on_success(self, requests_mock):
+        """Test that user data is returned on success."""
+        params = {'email': 'email@email.test'}
+        request_url = f'{settings.STAFF_SSO_BASE_URL}api/v1/user/introspect/?{urlencode(params)}'
+        requests_mock.get(request_url, json=FAKE_SSO_USER_DATA)
+
+        assert get_user_by_email('email@email.test') == FAKE_SSO_USER_DATA
