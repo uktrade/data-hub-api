6aba2a0dee97177c49d5bbab4d0758dbc49ba9ec
Reupen Shah <reupen@users.noreply.github.com>
2020-03-30 11:51:13 +0100

    Simplify record_user_event() and reorganise its tests

--- datahub/user_event_log/utils.py
+++ datahub/user_event_log/utils.py
@@ -3,11 +3,9 @@ from datahub.user_event_log.models import UserEvent
 
 def record_user_event(request, type_, data=None):
     """Records a user event in the database."""
-    event = UserEvent(
+    return UserEvent.objects.create(
         adviser=request.user,
         type=type_,
         api_url_path=request.path,
         data=data,
     )
-    event.save()
-    return event
