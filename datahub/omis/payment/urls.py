d137f2870dafb7ae0bec11c847fd5e702298bc95
elcct <pawel@mikesz.com>
2020-04-16 19:37:00 +0100

    Remove deprecated OMIS public payment endpoints.

--- datahub/omis/payment/urls.py
+++ datahub/omis/payment/urls.py
@@ -1,9 +1,5 @@
 from django.urls import path, re_path
 
-from datahub.omis.payment.legacy_public_views import (
-    LegacyPublicPaymentGatewaySessionViewSet,
-    LegacyPublicPaymentViewSet,
-)
 from datahub.omis.payment.views import (
     PaymentViewSet,
     PublicPaymentGatewaySessionViewSet,
@@ -23,31 +19,7 @@ payment_internal_frontend_urls = [
 ]
 
 # public facing API
-legacy_payment_public_urls = [
-    re_path(
-        r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/payment$',
-        LegacyPublicPaymentViewSet.as_view({'get': 'list'}),
-        name='collection',
-    ),
-]
-
-legacy_payment_gateway_session_public_urls = [
-    re_path(
-        r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/payment-gateway-session$',
-        LegacyPublicPaymentGatewaySessionViewSet.as_view({'post': 'create'}),
-        name='collection',
-    ),
-    re_path(
-        (
-            r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/'
-            r'payment-gateway-session/(?P<pk>[0-9a-z-]{36})$'
-        ),
-        LegacyPublicPaymentGatewaySessionViewSet.as_view({'get': 'retrieve'}),
-        name='detail',
-    ),
-]
-
-hawk_payment_public_urls = [
+payment_public_urls = [
     re_path(
         r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/payment$',
         PublicPaymentViewSet.as_view({'get': 'list'}),
@@ -55,7 +27,7 @@ hawk_payment_public_urls = [
     ),
 ]
 
-hawk_payment_gateway_session_public_urls = [
+payment_gateway_session_public_urls = [
     re_path(
         r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/payment-gateway-session$',
         PublicPaymentGatewaySessionViewSet.as_view({'post': 'create'}),
