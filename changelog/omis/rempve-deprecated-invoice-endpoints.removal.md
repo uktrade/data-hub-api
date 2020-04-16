d654cf3a47aa667c9bac4f43504f0c589319177e
elcct <pawel@mikesz.com>
2020-04-16 16:12:22 +0100

    Remove deprecated OMIS public invoice endpoint.

---
+++ changelog/omis/rempve-deprecated-invoice-endpoints.removal.md
@@ -0,0 +1,1 @@
+The deprecated `GET /v3/omis/public/order/<public-token>/invoice` endpoint has been removed.
--- datahub/omis/invoice/urls.py
+++ datahub/omis/invoice/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path, re_path
 
-from datahub.omis.invoice.legacy_public_views import LegacyPublicInvoiceViewSet
 from datahub.omis.invoice.views import InvoiceViewSet, PublicInvoiceViewSet
 
 # internal frontend API
@@ -12,17 +11,8 @@ internal_frontend_urls = [
     ),
 ]
 
-# legacy public facing API
-legacy_public_urls = [
-    re_path(
-        r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/invoice$',
-        LegacyPublicInvoiceViewSet.as_view({'get': 'retrieve'}),
-        name='detail',
-    ),
-]
-
 # Hawk authenticated public facing API
-hawk_public_urls = [
+public_urls = [
     re_path(
         r'^order/(?P<public_token>[0-9A-Za-z_\-]{50})/invoice$',
         PublicInvoiceViewSet.as_view({'get': 'retrieve'}),
