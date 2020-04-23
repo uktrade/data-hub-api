db54603e5fb697be82382cd7d91f79d53a4a7f49
David Matthews <dwm503@gmail.com>
2020-04-23 12:33:26 +0100

    Remove old company export dataset api endpoints

--- datahub/dataset/company_future_interest_countries/pagination.py
+++
@@ -1,9 +0,0 @@
-from datahub.dataset.core.pagination import DatasetCursorPagination
-
-
-class CompanyFutureInterestCountriesDatasetViewCursorPagination(DatasetCursorPagination):
-    """
-    Cursor Pagination for CompanyFutureInterestCountries
-    """
-
-    ordering = ('id', )
