fe3bbbc1266ff10d90806aaa0fbe7d7e210dfc16
Adam L Edwards <adam@aeweb.io>
2020-04-24 14:00:40 +0100

    fixup! revert core csv and views

--- datahub/core/csv.py
+++ datahub/core/csv.py
@@ -2,7 +2,6 @@ from codecs import BOM_UTF8
 from csv import DictWriter
 from datetime import datetime
 from decimal import Decimal
-from itertools import chain
 
 from django.http import StreamingHttpResponse
 
@@ -16,82 +15,29 @@ INCOMPLETE_CSV_MESSAGE = (
 ).encode('utf-8')
 
 
-def _flatten(list_of_lists):
-    """Flatten one level of nesting"""
-    return chain.from_iterable(list_of_lists)
-
-
-def _default_page_transformer_func(data):
-    """
-    Default no-op hook for page transformation.
-    """
-    return data
-
-
-def _csv_list_iterator(
-        rows,
-        field_titles,
-        page_size=None,
-        page_transformer_func=None,
-):
-    """
-    Returns an iterator producing a list containing one or more rows on each
-    yield from provided input.
-
-    This allows paging through underlying rows and applying a bulk transformation
-    function if required.
-    """
-    if not page_transformer_func:
-        page_transformer_func = _default_page_transformer_func
-
+def csv_iterator(rows, field_titles):
+    """Returns an iterator producing CSV formatted data from provided input."""
     try:
-        yield [BOM_UTF8]
+        yield BOM_UTF8
         writer = DictWriter(EchoUTF8(), fieldnames=field_titles.keys())
-        yield [writer.writerow(field_titles)]
-        page = []
+
+        yield writer.writerow(field_titles)
         for row in rows:
-            page.append(_transform_csv_row(row))
-            if len(page) == page_size or not page_size:
-                yield [writer.writerow(x) for x in page_transformer_func(page)]
-                page = []
-        yield [writer.writerow(x) for x in page_transformer_func(page)]
+            yield writer.writerow(_transform_csv_row(row))
     except Exception:
         # Because CSV responses are normally streamed, a 200 response will already have been
         # returned if an error occurs at this point. Hence we append an error to the CSV
         # contents (while re-raising the exception).
-        yield [INCOMPLETE_CSV_MESSAGE]
+        yield INCOMPLETE_CSV_MESSAGE
         raise
 
 
-def csv_iterator(
-        rows,
-        field_titles,
-        page_size=None,
-        page_transformer_func=None,
-):
-    """Returns an iterator producing CSV formatted data from provided input."""
-    for line in _flatten(_csv_list_iterator(
-            rows,
-            field_titles,
-            page_size,
-            page_transformer_func,
-    )):
-        yield line
-
-
-def create_csv_response(
-        rows,
-        field_titles,
-        filename,
-        streaming_page_size=None,
-        page_transformer_func=None,
-):
+def create_csv_response(rows, field_titles, filename):
     """Creates a CSV HTTP response."""
     # Note: FileResponse is not used as it is designed for file-like objects, while we are using
     # a generator here.
     response = StreamingHttpResponse(
-        csv_iterator(rows, field_titles, page_size=streaming_page_size,
-                     page_transformer_func=page_transformer_func),
+        csv_iterator(rows, field_titles),
         content_type=CSV_CONTENT_TYPE,
     )
 
