c840a9c79e3a38133c8c45a3464d67e5b652409d
Sekhar Panja <sekharpanja@gmail.com>
2020-05-05 20:39:04 +0100

    Allowing merged company's export wins to surface on export wins api

--- datahub/company/export_wins_api.py
+++ datahub/company/export_wins_api.py
@@ -35,9 +35,11 @@ class ExportWinsAPIConnectionError(ExportWinsAPIException):
     """
 
 
-def fetch_export_wins(match_id):
+def fetch_export_wins(match_ids):
     """
-    Queries the Export Wins API with the given match id.
+    Queries the Export Wins API with the given list of match ids.
+    Export Wins API takes either a single match id or comma separated
+    list of match ids.
     """
     if not all([
         settings.EXPORT_WINS_SERVICE_BASE_URL,
@@ -46,22 +48,24 @@ def fetch_export_wins(match_id):
     ]):
         raise ImproperlyConfigured('The all EXPORT_WINS_SERVICE* setting must be set')
 
+    match_ids_str = ','.join(list(map(str, match_ids)))
     response = api_client.request(
         'GET',
-        f'wins/match/{match_id}/',
+        f'wins/match?match_id={match_ids_str}',
         timeout=3.0,
     )
     return response
 
 
-def get_export_wins(match_id):
+def get_export_wins(match_ids):
     """
-    Get all export wins for a given the company match_id.
+    Get all export wins for all given company match_ids.
 
+    `match_ids` is a list of match ids from Company matchin service.
     Raises exception an requests.exceptions.HTTPError for status, timeout and a connection error.
     """
     try:
-        response = fetch_export_wins(match_id)
+        response = fetch_export_wins(match_ids)
     except ConnectionError as exc:
         error_message = 'Encountered an error connecting to Export Wins API'
         raise ExportWinsAPIConnectionError(error_message) from exc
