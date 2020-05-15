b635e6f0e9181e5601fbb38714fbe76bbf9afcbe
Sekhar Panja <sekharpanja@gmail.com>
2020-05-15 10:07:34 +0100

    adding iteration 2 fields to the pipeline item model

--- datahub/user/company_list/test/factories.py
+++ datahub/user/company_list/test/factories.py
@@ -1,6 +1,7 @@
 import factory
 
-from datahub.company.test.factories import AdviserFactory, CompanyFactory
+from datahub.company.test.factories import AdviserFactory, CompanyFactory, ContactFactory
+from datahub.metadata.test.factories import SectorFactory
 from datahub.user.company_list.models import PipelineItem
 
 
@@ -31,6 +32,11 @@ class PipelineItemFactory(factory.django.DjangoModelFactory):
     company = factory.SubFactory(CompanyFactory)
     adviser = factory.SubFactory(AdviserFactory)
     status = PipelineItem.Status.LEADS
+    contact = factory.SubFactory(ContactFactory, company=factory.SelfAttribute('..company'))
+    sector = factory.SubFactory(SectorFactory)
+    potential_value = 1000000
+    likelihood_to_win = PipelineItem.LikelihoodToWin.MEDIUM
+    expected_win_date = factory.Faker('future_date', end_date='+3y')
 
     class Meta:
         model = 'company_list.PipelineItem'
