6712d3a1cca1831e1a9c25cefcd063ff007a6539
David Voong <voong.david@gmail.com>
2020-06-24 15:28:54 +0100

    add delete_investment_sector management command

---
+++ datahub/dbmaintenance/management/commands/delete_investment_sector.py
@@ -0,0 +1,37 @@
+from logging import getLogger
+
+import reversion
+
+from datahub.dbmaintenance.management.base import CSVBaseCommand
+from datahub.dbmaintenance.utils import parse_uuid
+from datahub.investment.project.models import InvestmentSector
+
+
+logger = getLogger(__name__)
+
+
+class Command(CSVBaseCommand):
+    """Command to delete a new InvestmentSector object."""
+
+    def _process_row(self, row, simulate=False, **options):
+        """Process one single row."""
+        sector_id = parse_uuid(row['sector_id'])
+        fdi_sic_grouping_id = parse_uuid(row['fdi_sic_grouping_id'])
+        matches = InvestmentSector.objects.filter(
+            sector_id=sector_id,
+            fdi_sic_grouping_id=fdi_sic_grouping_id,
+        )
+        if len(matches) == 0:
+            error_msg = (
+                'InvestmentSector does not exist\n'
+                'sector_id: {0}'
+                'fdi_sic_grouping_id: {1}'
+            ).format(sector_id, fdi_sic_grouping_id)
+            raise Exception(error_msg)
+
+        if simulate:
+            return
+
+        with reversion.create_revision():
+            matches.delete()
+            reversion.set_comment('InvestmentSector creation.')
