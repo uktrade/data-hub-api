83a782cb9f1f7c7ecb2d503b89d3970540b436d1
Adam L Edwards <adam@aeweb.io>
2020-08-11 16:30:44 +0100

    fixup! fix comment

--- datahub/search/contact/views.py
+++ datahub/search/contact/views.py
@@ -168,7 +168,13 @@ class SearchContactExportAPIView(SearchContactAPIViewMixin, SearchExportAPIView)
                 yield row
 
     def _get_rows(self, ids, search_ordering):
-        """Get row queryset for constent service if the feature flag is enabled."""
+        """
+        Get row queryset for constent service.
+
+        This populates accepts_dit_email_marketing field from the consent service and 
+        removes the accepts_dit_email_marketing from the field query because the field is not in 
+        the db. 
+        """
         db_ordering = self._translate_search_ordering_to_django_ordering(search_ordering)
         field_titles = self.field_titles.copy()
         del field_titles['accepts_dit_email_marketing']
