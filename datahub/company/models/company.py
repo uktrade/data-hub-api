ca83f8084d7dd7efbd106b2b4d291ea47e4a0f89
Vincent Farah <vincent.farah@madetech.com>
2021-03-03 15:16:39 +0000

    Added foreign key look up on administrative area Added docker-compose.yml ports for postgres Generated a migration

--- datahub/company/models/company.py
+++ datahub/company/models/company.py
@@ -168,14 +168,13 @@ class Company(ArchivableModel, BaseModel):
     address_2 = models.CharField(max_length=MAX_LENGTH, blank=True)
     address_town = models.CharField(max_length=MAX_LENGTH, blank=True)
     address_state = models.CharField(max_length=MAX_LENGTH, blank=True)
-    # TODO: Uncomment when feature/cds-64 is ready
-    # address_state = models.ForeignKey(
-    #     metadata_models.State,
-    #     related_name='companies_with_state_registered_address',
-    #     blank=True,
-    #     null=True,
-    #     on_delete=models.SET_NULL,
-    # )
+    address_state = models.ForeignKey(
+        metadata_models.AdministrativeArea,
+        related_name='companies_with_address_state',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
     address_county = models.CharField(max_length=MAX_LENGTH, blank=True)
     address_country = models.ForeignKey(
         metadata_models.Country,
@@ -190,14 +189,13 @@ class Company(ArchivableModel, BaseModel):
     registered_address_2 = models.CharField(max_length=MAX_LENGTH, blank=True)
     registered_address_town = models.CharField(max_length=MAX_LENGTH, blank=True)
     registered_address_state = models.CharField(max_length=MAX_LENGTH, blank=True)
-    # TODO: Uncomment when feature/cds-64 is ready
-    # registered_state = models.ForeignKey(
-    #     metadata_models.State,
-    #     related_name='companies_with_state_registered_address',
-    #     blank=True,
-    #     null=True,
-    #     on_delete=models.SET_NULL,
-    # )
+    registered_address_state = models.ForeignKey(
+        metadata_models.AdministrativeArea,
+        related_name='companies_with_registered_state',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+    )
 
     registered_address_county = models.CharField(max_length=MAX_LENGTH, blank=True)
     registered_address_country = models.ForeignKey(
