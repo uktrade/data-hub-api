bf76e3010e2ade328f7fdbe6171a8a9281f59203
Tom Kingston <tom@kingstonlabs.co.uk>
2021-04-15 17:30:32 +0100

    Add per user feature flag

--- datahub/feature_flag/models.py
+++ datahub/feature_flag/models.py
@@ -25,3 +25,26 @@ class FeatureFlag(BaseModel):
     def __str__(self):
         """Human readable representation."""
         return f'{self.code} - {self.is_active}'
+
+
+class UserFeatureFlag(BaseModel):
+    """
+    Per-user feature flag.
+
+    This is a feature flag that can be set on a user-by-user basis.
+
+    The is_active flag can be used to enable / disable the feature for all
+    users that have the feature.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    code = models.CharField(unique=True, max_length=MAX_LENGTH)
+    description = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ('code',)
+
+    def __str__(self):
+        """Human readable representation."""
+        return f'{self.code} ({"active" if self.is_active else "inactive"})'
