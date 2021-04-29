5ba8b919e8db6181f9d3c9e98bf56541ad6b9975
Vincent Farah <vincent.farah@madetech.com>
2021-04-29 21:51:55 +0100

    Created simple bedfactory

---
+++ datahub/bed_api/factories.py
@@ -0,0 +1,28 @@
+import os
+
+from simple_salesforce import Salesforce
+
+
+class BedFactory:
+    """
+    Bed factory for creating Salesforce instance
+    """
+
+    def create(self):
+        """
+        Create a Salesforce instance with configured settings
+        @return: Salesforce instance
+        """
+        if os.environ['BED_IS_SANDBOX'] == 'True':
+            return Salesforce(
+                username=os.environ['BED_USERNAME'],
+                password=os.environ['BED_PASSWORD'],
+                security_token=os.environ['BED_SECURITY_TOKEN'],
+                domain='test',
+            )
+        else:
+            return Salesforce(
+                username=os.environ['BED_USERNAME'],
+                password=os.environ['BED_PASSWORD'],
+                security_token=os.environ['BED_SECURITY_TOKEN'],
+            )
