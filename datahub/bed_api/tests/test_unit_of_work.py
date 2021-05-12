7ff493c413f52fc06f749589b282292aabe00de7
Vincent Farah <vincent.farah@madetech.com>
2021-05-12 11:37:46 +0100

    Create unit tests for BED unit of work

--- datahub/bed_api/tests/test_unit_of_work.py
+++ datahub/bed_api/tests/test_unit_of_work.py
@@ -1,17 +1,81 @@
 import os
+from unittest import mock
 
 import pytest
 
 from datahub.bed_api.models import EditAccount, EditContact
 from datahub.bed_api.unit_of_work import BedUnitOfWork
+from datahub.core.test_utils import mock_environ
 
 
-class TestBedUnitOfWork:
+class TestBedUnitOfWorkShould:
     """
     Unit Tests for BED Unit of Work maps all the exposed repositories
     within a single unit of work
     """
-#     TODO: Unit Tests
+
+    @mock_environ(
+        BED_USERNAME='test-user@digital.trade.gov.uk',
+        BED_PASSWORD='test-password',
+        BED_SECURITY_TOKEN='test-token',
+        BED_IS_SANDBOX='False',
+    )
+    @mock.patch('datahub.bed_api.factories.Salesforce')
+    def test_sales_force_session_gets_created_and_closed(
+            self,
+            mock_salesforce,
+    ):
+        """
+        Test BedUnitOfWork is built with Salesforce session
+        :param mock_salesforce: Monkeypatch for Salesforce
+        """
+        with BedUnitOfWork() as bed_context:
+            assert bed_context is not None
+            assert mock_salesforce.called
+            assert mock_salesforce.call_args_list == [
+                mock.call(
+                    username='test-user@digital.trade.gov.uk',
+                    password='test-password',
+                    security_token='test-token',
+                ),
+            ]
+
+    @mock_environ(
+        BED_USERNAME='test-user@digital.trade.gov.uk',
+        BED_PASSWORD='test-password',
+        BED_SECURITY_TOKEN='test-token',
+        BED_IS_SANDBOX='False',
+    )
+    @mock.patch('datahub.bed_api.factories.Salesforce')
+    def test_repositories_created(
+            self,
+            mock_salesforce,
+    ):
+        """
+         Test BedUnitOfWork is built with Salesforce session
+        :param mock_salesforce: Monkeypatch for Salesforce
+        """
+        with BedUnitOfWork() as bed_context:
+            assert mock_salesforce.called
+            assert bed_context.accounts is not None
+            assert bed_context.contacts is not None
+
+    @mock_environ(
+        BED_USERNAME='test-user@digital.trade.gov.uk',
+        BED_PASSWORD='test-password',
+        BED_SECURITY_TOKEN='test-token',
+        BED_IS_SANDBOX='False',
+    )
+    @mock.patch('datahub.bed_api.factories.Salesforce')
+    def test_session_automatically_closes_the_session(
+        self,
+        mock_salesforce,
+    ):
+        """Test BedUnitOfWork closes the session"""
+        with BedUnitOfWork() as bed_context:
+            assert bed_context is not None
+        mock_close = mock_salesforce.return_value.session.close
+        assert mock_close.calledonce
 
 
 @pytest.mark.salesforce_test
