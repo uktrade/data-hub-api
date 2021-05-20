c914a7aaa4f147a2015f577f79bc97e30acea309
Vincent Farah <vincent.farah@madetech.com>
2021-05-20 10:05:28 +0100

    CDS-139 refactored repositories into own folder

---
+++ datahub/bed_api/repositories/event_attendee_repository.py
@@ -0,0 +1,78 @@
+from simple_salesforce import format_soql, Salesforce
+
+from datahub.bed_api.constants import EventAttendeeQuery
+from datahub.bed_api.repositories.salesforce_repository import SalesforceRepository
+
+
+class EventAttendeeRepository(SalesforceRepository):
+    """
+    Repository pattern for Salesforce interactions with Event Attendee data
+    https://loginhub--november.lightning.force.com/lightning/setup/ObjectManager/01I58000001EcAX/FieldsAndRelationships/view
+    """
+
+    def __init__(self, salesforce: Salesforce):
+        """
+        Constructor
+        :param salesforce: Simple Salesforce representing session information
+        """
+        super().__init__(salesforce=salesforce)
+
+    def add(self, data):
+        """
+        Add a new Event Attendee using a POST
+        :param data: A dict of Event data
+        :return: Returns Event Attendee
+        """
+        return self.salesforce.Event_Attendee__c.create(data)
+
+    def delete(self, record_id):
+        """
+        Delete a single item by unique identifier
+        :param record_id: Unique identifier for deleting records
+        :return: Result of deletion
+        """
+        return self.salesforce.Event_Attendee__c.delete(record_id)
+
+    def exists(self, record_id):
+        """
+        Checks if the record exists using the most unique identifier
+        :param record_id: Unique identifier value, associated with Id value typically
+        :return: True if it exists and False if it does not
+        """
+        response = self.query(
+            format_soql(
+                EventAttendeeQuery.get_by_id.value.sql,
+                id=record_id,
+            ),
+        )
+        return self.exists_status(record_id, response)
+
+    def get(self, record_id):
+        """
+        Get single item by identifier
+        :param record_id: Unique identifier value
+        :return: Result of event attendee get by id
+        """
+        return self.salesforce.Event_Attendee__c.get(record_id)
+
+    def get_by(self, custom_id_field, custom_id_value):
+        """
+        Returns the result of a GET by field
+        :param custom_id_field: API name of a custom field that was defined
+                             as an External ID
+        :param custom_id_value: External ID value
+        :return: Event record or None
+        """
+        return self.salesforce.Event_Attendee__c.get_by_custom_id(
+            custom_id_field,
+            custom_id_value,
+        )
+
+    def update(self, record_id, data):
+        """
+        Update Event
+        :param record_id: Record identifier
+        :param data: Represents a dictionary of name values:
+        :return: Created or updated Event
+        """
+        return self.salesforce.Event_Attendee__c.update(record_id, data)
