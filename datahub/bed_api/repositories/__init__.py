a2d59f76f35450478608602e0c84d212aa5531b6
Vincent Farah <vincent.farah@madetech.com>
2021-05-20 10:05:28 +0100

    CDS-139 refactored repositories into own folder

---
+++ datahub/bed_api/repositories/__init__.py
@@ -0,0 +1,10 @@
+from datahub.bed_api.repositories.account_repository import AccountRepository  # noqa:F401
+from datahub.bed_api.repositories.contact_repository import ContactRepository  # noqa:F401
+from datahub.bed_api.repositories.event_attendee_repository import (  # noqa:F401
+    EventAttendeeRepository,
+)
+from datahub.bed_api.repositories.event_repository import EventRepository  # noqa:F401
+from datahub.bed_api.repositories.policy_issues_repository import (  # noqa:F401
+    PolicyIssuesRepository,
+)
+from datahub.bed_api.repositories.salesforce_repository import SalesforceRepository  # noqa:F401
