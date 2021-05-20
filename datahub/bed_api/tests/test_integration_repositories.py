d25829d905ac158745e66553dde896e639a1b37f
Vincent Farah <vincent.farah@madetech.com>
2021-05-20 07:47:14 +0100

    CDS-139 Refactored integration test names

--- datahub/bed_api/tests/test_integration_repositories.py
+++ datahub/bed_api/tests/test_integration_repositories.py
@@ -48,20 +48,31 @@ class TestIntegrationEventRepositoryShould:
         """
         event_id = None
         try:
-            # CREATE event checking data and that the record exists
-            event_id = self.create_and_assert_event(event_repository, generate_event)
+            # ADD event checking data and that the record exists
+            event_id = self.add_and_assert_event(
+                event_repository=event_repository,
+                event=generate_event,
+            )
             generate_event.Id = event_id
 
             # UPDATE event checking the patch
-            self.update_and_assert_event(event_id, event_repository, faker, generate_event)
+            self.update_and_assert_event(
+                event_id=event_id,
+                event_repository=event_repository,
+                faker=faker,
+                event=generate_event,
+            )
 
             # QUERY and QUERY NEXT testing PAGINATION concepts and COUNT Query constructs
-            self.assert_and_query_paginated_data(event_repository, generate_event)
+            self.assert_and_query_paginated_data(
+                event_repository=event_repository,
+                event=generate_event,
+            )
         finally:
             # Delete to clean up integration test
             delete_and_assert_deletion(
-                event_repository,
-                event_id,
+                repository=event_repository,
+                record_id=event_id,
             )
 
     def update_and_assert_event(
@@ -161,7 +172,7 @@ class TestIntegrationEventRepositoryShould:
         assert query_response['totalSize'] >= 1
         assert query_response['records'] is not None
 
-    def create_and_assert_event(
+    def add_and_assert_event(
         self,
         event_repository,
         event: EditEvent,
@@ -179,7 +190,11 @@ class TestIntegrationEventRepositoryShould:
         assert event_response['success'] is True
         event_id = event_response['id']
         assert event_id is not None
-        assert_all_data_exists_on_bed(event, event_id, event_repository)
+        assert_all_data_exists_on_bed(
+            bed_entity=event,
+            record_id=event_id,
+            repository=event_repository,
+        )
         return event_id
 
 
@@ -214,9 +229,9 @@ class TestIntegrationContactWithEventAndAccountShould:
         generate_policy_issues: EditPolicyIssues,
     ):
         """
-        Test BedFactory integration with the contact and account repositories
-        sampling all functions in an idempotent way, generating potential test
-        data for unit tests
+        Test BedFactory integration with the several related repositories
+        sampling all functions in an idempotent way, generating fuzz data tests to make sure
+        of the data integrity of sample data
         :param contact_repository: ContactRepository fixture
         :param account_repository: AccountRepository fixture
         :param event_repository: EventRepository fixture
@@ -233,74 +248,116 @@ class TestIntegrationContactWithEventAndAccountShould:
         new_account_id = None
         new_event_id = None
         new_event_attendee_id = None
-        # new_policy_issue_id = None
+        new_policy_issue_id = None
         try:
-            # Create a new account / organization / company
-            new_account_id = self.generate_and_assert_account(
-                account_repository,
-                generate_account,
+            # ADD new account / organization / company
+            new_account_id = self.add_and_assert_account(
+                account_repository=account_repository,
+                account=generate_account,
             )
 
-            # Create contact
+            # ADD contact
             generate_contact.AccountId = new_account_id
-            new_contact_id = self.generate_and_assert_contact(
-                contact_repository,
-                generate_contact,
+            new_contact_id = self.add_and_assert_contact(
+                contact_repository=contact_repository,
+                contact=generate_contact,
             )
 
-            #  Update Contact
+            #  UPDATE Contact
             self.update_and_assert_contact(
-                contact_repository,
-                new_contact_id,
-                faker,
+                contact_repository=contact_repository,
+                contact_id=new_contact_id,
+                faker=faker,
             )
 
-            #  Create event
-            new_event_id = self.generate_assert_event(event_repository, generate_event)
-
-            #  Link Contact as event attendee
-            new_event_attendee_id = self.generate_and_assert_event_attendee(
-                event_attendee_repository,
-                generate_event_attendee,
-                new_contact_id,
-                new_event_id,
+            #  ADD event
+            new_event_id = self.add_assert_event(
+                event_repository=event_repository,
+                event=generate_event,
             )
 
-            #  Generate Policy issues assigning the AccountId into the values
-            generate_policy_issues.Company__c = new_account_id
-            generate_policy_issues.Add_Interactions__c = new_event_id
-            # policy_issues_add_response = policy_issues_repository.add(
-            #     generate_policy_issues.as_values_only_dict()
-            # )
+            #  Link Contact with CREATE event attendee
+            new_event_attendee_id = self.add_and_assert_event_attendee(
+                event_attendee_repository=event_attendee_repository,
+                event_attendee=generate_event_attendee,
+                contact_id=new_contact_id,
+                event_id=new_event_id,
+            )
 
+            #  Generate Policy issues assigning the Account and Interaction
+            new_policy_issue_id = self.add_and_assert_policy_issues(
+                policy_issues=generate_policy_issues,
+                policy_issues_repository=policy_issues_repository,
+                account_id=new_account_id,
+                event_id=new_event_id,
+            )
         finally:
             #  Clean up generated data
             delete_and_assert_deletion(
-                contact_repository,
-                new_contact_id,
+                repository=policy_issues_repository,
+                record_id=new_policy_issue_id,
             )
             delete_and_assert_deletion(
-                account_repository,
-                new_account_id,
+                repository=contact_repository,
+                record_id=new_contact_id,
             )
             delete_and_assert_deletion(
-                event_attendee_repository,
-                new_event_attendee_id,
+                repository=account_repository,
+                record_id=new_account_id,
             )
             delete_and_assert_deletion(
-                event_repository,
-                new_event_id,
+                repository=event_attendee_repository,
+                record_id=new_event_attendee_id,
+            )
+            delete_and_assert_deletion(
+                repository=event_repository,
+                record_id=new_event_id,
             )
 
-    def generate_and_assert_event_attendee(
-            self,
-            event_attendee_repository,
-            event_attendee,
-            contact_id,
-            event_id,
+    def add_and_assert_policy_issues(
+        self,
+        policy_issues: EditPolicyIssues,
+        policy_issues_repository,
+        account_id,
+        event_id,
     ):
         """
-        Create event attendee assigning a contact and event to marry the two
+        Create event policy issue unifying the account and interaction
+        data
+        :param policy_issues: Policy issues record generated with faker data
+        :param policy_issues_repository: PolicyIssuesRepository fixture
+        :param account_id: Associated Account id
+        :param event_id: Associated Event or Interaction id
+        :return:
+        """
+        policy_issues.Company__c = account_id
+        policy_issues.Add_Interactions__c = event_id
+        policy_issue_add_response = policy_issues_repository.add(
+            policy_issues.as_values_only_dict(),
+        )
+        assert policy_issue_add_response is not None
+        assert policy_issue_add_response['success'] is True
+        policy_issue_id = policy_issue_add_response['id']
+        assert policy_issue_id is not None
+        policy_issues.Id = policy_issue_id
+        # REMARKS: Not guaranteed data order as Company data added to Multilists
+        assert_all_data_exists_on_bed(
+            bed_entity=policy_issues,
+            record_id=policy_issue_id,
+            repository=policy_issues_repository,
+            validate_data=False,
+        )
+        return policy_issue_id
+
+    def add_and_assert_event_attendee(
+        self,
+        event_attendee_repository,
+        event_attendee,
+        contact_id,
+        event_id,
+    ):
+        """
+        Create event attendee assigning a contact and event to link the two
         :param event_attendee_repository: EventAttendeeRepository fixture
         :param event_attendee: New event attendee record generated with faker data
         :param contact_id: New contact id
@@ -317,13 +374,13 @@ class TestIntegrationContactWithEventAndAccountShould:
         event_attendee_id = event_attendee_response['id']
         assert event_attendee_id is not None
         assert_all_data_exists_on_bed(
-            event_attendee,
-            event_attendee_id,
-            event_attendee_repository,
+            bed_entity=event_attendee,
+            record_id=event_attendee_id,
+            repository=event_attendee_repository,
         )
         return event_attendee_id
 
-    def generate_assert_event(self, event_repository, event):
+    def add_assert_event(self, event_repository, event):
         """
         Generate a new event and assert success
         :param event_repository: EventRepository fixture
@@ -338,49 +395,49 @@ class TestIntegrationContactWithEventAndAccountShould:
         event_id = event_response['id']
         assert event_id is not None
         assert_all_data_exists_on_bed(
-            event,
-            event_id,
-            event_repository,
+            bed_entity=event,
+            record_id=event_id,
+            repository=event_repository,
         )
         return event_id
 
     def update_and_assert_contact(
         self,
         contact_repository,
-        new_contact_id,
+        contact_id,
         faker,
     ):
         """
         Update the contact with basic notes testing update
         :param contact_repository: ContactRepository fixture
-        :param new_contact_id: Contact id to update
+        :param contact_id: Contact id to update
         :param faker: Faker library
         """
         # Example using original edit object sending all values
         # contact.Notes__c = 'Integration Test Notes - Update'
         # update_contact_response = repository.update(
-        #     f'Id/{contact_id}', contact.as_values_only_dict())
+        #     contact_id, contact.as_values_only_dict())
         notes_update = faker.text(max_nb_chars=100)
         update_contact_response = contact_repository.update(
-            f'Id/{new_contact_id}',
+            contact_id,
             dict(Notes__c=notes_update),
         )
         assert update_contact_response is not None
         assert update_contact_response == 204
 
         contact_check = contact_repository.query(
-            format_soql(
+            query=format_soql(
                 ContactQuery.get_notes_by_id.value.sql,
-                id=new_contact_id,
+                id=contact_id,
             ),
         )
         assert contact_check is not None
         assert contact_check['totalSize'] == 1
         assert contact_check['done'] is True
-        assert contact_check['records'][0]['Id'] == new_contact_id
+        assert contact_check['records'][0]['Id'] == contact_id
         assert contact_check['records'][0]['Notes__c'] == notes_update
 
-    def generate_and_assert_contact(
+    def add_and_assert_contact(
         self,
         contact_repository,
         contact: EditContact,
@@ -392,15 +449,21 @@ class TestIntegrationContactWithEventAndAccountShould:
         :param contact: Random Generated Contact
         :return: new contact id
         """
-        contact_add_response = contact_repository.add(contact.as_values_only_dict())
+        contact_add_response = contact_repository.add(
+            contact.as_values_only_dict(),
+        )
         assert contact_add_response is not None
         assert contact_add_response['success'] is True
         contact_id = contact_add_response['id']
         contact.Id = contact_id
-        assert_all_data_exists_on_bed(contact, contact_id, contact_repository)
+        assert_all_data_exists_on_bed(
+            bed_entity=contact,
+            record_id=contact_id,
+            repository=contact_repository,
+        )
         return contact_id
 
-    def generate_and_assert_account(
+    def add_and_assert_account(
         self,
         account_repository,
         account: EditAccount,
@@ -411,11 +474,17 @@ class TestIntegrationContactWithEventAndAccountShould:
         :param account: New account record generated with faker data
         :return: Account Id
         """
-        account_add_response = account_repository.add(account.as_values_only_dict())
+        account_add_response = account_repository.add(
+            account.as_values_only_dict(),
+        )
         assert account_add_response is not None
         assert account_add_response['success'] is True
         account_id = account_add_response['id']
         assert account_id is not None
         account.Id = account_id
-        assert_all_data_exists_on_bed(account, account_id, account_repository)
+        assert_all_data_exists_on_bed(
+            bed_entity=account,
+            record_id=account_id,
+            repository=account_repository,
+        )
         return account_id
