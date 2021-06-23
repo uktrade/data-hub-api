4cc8236c84f29a793d4b01c3523b9fe4d7f54a3f
Vincent Farah <vincent.farah@madetech.com>
2021-06-23 12:45:50 +0100

    Release 36.24.0 (#3578)
    * CDS-208 Move Area column beside company in company search CSV export
    
    * Add interaction companies to search.
    
    * Bump boto3 from 1.17.93 to 1.17.97
    
    Bumps [boto3](https://github.com/boto/boto3) from 1.17.93 to 1.17.97.
    - [Release notes](https://github.com/boto/boto3/releases)
    - [Changelog](https://github.com/boto/boto3/blob/develop/CHANGELOG.rst)
    - [Commits](https://github.com/boto/boto3/compare/1.17.93...1.17.97)
    
    ---
    updated-dependencies:
    - dependency-name: boto3
      dependency-type: direct:production
      update-type: version-update:semver-patch
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump django-axes from 5.18.0 to 5.19.0
    
    Bumps [django-axes](https://github.com/jazzband/django-axes) from 5.18.0 to 5.19.0.
    - [Release notes](https://github.com/jazzband/django-axes/releases)
    - [Changelog](https://github.com/jazzband/django-axes/blob/master/CHANGES.rst)
    - [Commits](https://github.com/jazzband/django-axes/compare/5.18.0...5.19.0)
    
    ---
    updated-dependencies:
    - dependency-name: django-axes
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump notifications-python-client from 6.1.0 to 6.2.0
    
    Bumps [notifications-python-client](https://github.com/alphagov/notifications-python-client) from 6.1.0 to 6.2.0.
    - [Release notes](https://github.com/alphagov/notifications-python-client/releases)
    - [Changelog](https://github.com/alphagov/notifications-python-client/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/alphagov/notifications-python-client/compare/6.1.0...6.2.0)
    
    ---
    updated-dependencies:
    - dependency-name: notifications-python-client
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump psycopg2 from 2.8.6 to 2.9.1
    
    Bumps [psycopg2](https://github.com/psycopg/psycopg2) from 2.8.6 to 2.9.1.
    - [Release notes](https://github.com/psycopg/psycopg2/releases)
    - [Changelog](https://github.com/psycopg/psycopg2/blob/master/NEWS)
    - [Commits](https://github.com/psycopg/psycopg2/commits)
    
    ---
    updated-dependencies:
    - dependency-name: psycopg2
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Bump pytest-xdist from 2.2.1 to 2.3.0
    
    Bumps [pytest-xdist](https://github.com/pytest-dev/pytest-xdist) from 2.2.1 to 2.3.0.
    - [Release notes](https://github.com/pytest-dev/pytest-xdist/releases)
    - [Changelog](https://github.com/pytest-dev/pytest-xdist/blob/master/CHANGELOG.rst)
    - [Commits](https://github.com/pytest-dev/pytest-xdist/compare/v2.2.1...v2.3.0)
    
    ---
    updated-dependencies:
    - dependency-name: pytest-xdist
      dependency-type: direct:production
      update-type: version-update:semver-minor
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * CPS-170 - Optimise company data on interactions (#3576)
    
    * CPS-170 - Added companies to the autocomplete list to optimise the many relationship
    
    * CPS-170 - added change log
    
    * Prepare for release 36.24.0 (#3577)
    
    * Prepare for release 36.24.0
    
    This bumps the version and adds the changelog for version 36.24.0.
    
    * Fixed type in change log
    
    Co-authored-by: Toby Petty <tobypetty1@gmail.com>
    Co-authored-by: elcct <pawel@mikesz.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Gregor Ambrozic <ambrozic@users.noreply.github.com>

--- datahub/interaction/admin.py
+++ datahub/interaction/admin.py
@@ -52,7 +52,7 @@ class InteractionDITParticipantInline(admin.TabularInline):
 class InteractionAdmin(BaseModelAdminMixin, VersionAdmin):
     """Interaction admin."""
 
-    autocomplete_fields = ('contacts',)
+    autocomplete_fields = ('contacts', 'companies')
     inlines = (
         InteractionDITParticipantInline,
     )
