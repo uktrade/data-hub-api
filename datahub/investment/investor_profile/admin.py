faab93ba625a19f27906b9df13d0e2b4e5831253
Vincent Farah <vincent.farah@madetech.com>
2021-06-24 11:32:08 +0100

    Investor profile and large capital gateway time  (#3580)
    CPS-171 - Optimised regions and countries
    CPS-172 - Optimised the region, promoters, dit contacts and investment projects lookup

--- datahub/investment/investor_profile/admin.py
+++ datahub/investment/investor_profile/admin.py
@@ -7,6 +7,10 @@ from datahub.investment.investor_profile import models
 class LargeCapitalInvestorProfileAdmin(admin.ModelAdmin):
     """Large capital investor profile admin."""
 
+    autocomplete_fields = (
+        'uk_region_locations',
+        'other_countries_being_considered',
+    )
     list_display = ('investor_company',)
     search_fields = ('investor_company__name', 'id')
     readonly_fields = ('id', 'created_by', 'modified_by')
