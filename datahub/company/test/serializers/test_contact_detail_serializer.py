33cf62aed4a3570ab89cf9453edc6bbe237c80a4
Vincent Farah <vincent.farah@madetech.com>
2021-08-06 16:45:14 +0100

    CDS-230-Fixed small code issues on WIP

--- datahub/company/test/serializers/test_contact_detail_serializer.py
+++ datahub/company/test/serializers/test_contact_detail_serializer.py
@@ -187,8 +187,11 @@ class TestContactSerializer:
 
 @freeze_time(FROZEN_TIME)
 class TestContactCV4SerializerShould:
-    """Tests for the ContactCV4Serializer for Area Changes specifically
-    consent service correctly."""
+    """
+    Tests for the ContactCV4Serializer for Area Changes specifically
+    consent service correctly.
+    """
+
     @pytest.mark.parametrize(
         'country_id, expected_response, is_valid',
         (
