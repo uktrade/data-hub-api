dd61bd466bb23f0ed6eba52a07f92d2984a4c79b
Toby Petty <tobypetty1@gmail.com>
2021-11-03 16:43:29 +0000

    CDS-335 Add turnover_gbp to company serializer and subclasses

---
+++ datahub/metadata/utils.py
@@ -0,0 +1,14 @@
+from datahub.metadata.models import ExchangeRate
+
+
+def convert_usd_to_gbp(usd):
+    """convert_usd_to_gbp.
+
+    :param usd: A numeric value in US Dollars
+    :returns: A numeric value in GBP
+    """
+    exchange_rate = ExchangeRate.objects.filter(
+        from_currency_code='USD',
+        to_currency_code='GBP',
+    ).order_by('-created_on').first()
+    return usd * exchange_rate.exchange_rate
