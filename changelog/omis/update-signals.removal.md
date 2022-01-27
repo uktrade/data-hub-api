867c4e70e2b798ab4783fdd0476a9cb3b87fc905
cgsunkel <christopher.sunkel@digital.trade.gov.uk>
2022-01-27 16:03:32 +0000

    Update OMIS order signals

---
+++ changelog/omis/update-signals.removal.md
@@ -0,0 +1,9 @@
+The deprecated `providing_args` argument has been removed from the following signals:
+- order_paid
+- order_completed
+- order_cancelled
+- quote_generated
+- quote_accepted
+- quote_cancelled 
+
+The content has been retained for reference purposes.
--- datahub/omis/order/signals.py
+++ datahub/omis/order/signals.py
@@ -1,10 +1,16 @@
 import django.dispatch
 
+"""
+The `providing_args` argument has been deprecated and is no longer supported by Django.
+All signals were using 'providing_args=['order']' unless mentioned otherwise.
+"""
 
-order_paid = django.dispatch.Signal(providing_args=['order'])
-order_completed = django.dispatch.Signal(providing_args=['order'])
-order_cancelled = django.dispatch.Signal(providing_args=['order'])
+order_paid = django.dispatch.Signal()
+order_completed = django.dispatch.Signal()
+order_cancelled = django.dispatch.Signal()
 
-quote_generated = django.dispatch.Signal(providing_args=['order'])
-quote_accepted = django.dispatch.Signal(providing_args=['order'])
-quote_cancelled = django.dispatch.Signal(providing_args=['order', 'by'])
+quote_generated = django.dispatch.Signal()
+quote_accepted = django.dispatch.Signal()
+
+"""providing_args=['order', 'by']"""
+quote_cancelled = django.dispatch.Signal()
