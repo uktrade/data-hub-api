124f0154caa79fe04d3b36a92d28d09bf4a90416
Chloe <chloe.wong@madetech.com>
2022-01-31 10:53:45 +0000

    CDS-294 formatting and adding doc strings

--- datahub/core/test_utils.py
+++ datahub/core/test_utils.py
@@ -278,6 +278,15 @@ def get_attr_or_none(obj, attr):
 
 
 def get_attr_or_default(obj, attr, default=None):
+    """
+    Gets an attribute of an object, or returns a default value if the attribute does not exist.
+
+    Dotted paths to attributes can be provided to specify nested attributes.
+
+    Usage example:
+        # Returns company.contact.name or [None] if contact is None
+        get_attr_or_default(company, 'contact.name', [None])
+    """
     try:
         return attrgetter(attr)(obj)
     except AttributeError:
