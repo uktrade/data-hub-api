81a36fe8f5b09b2ea433a6b27914e4ed40b89e64
Tom Kingston <tom@kingstonlabs.co.uk>
2022-03-21 13:23:42 +0000

    Use update in management command to speed it up

--- datahub/company/management/commands/populate_full_telephone_number.py
+++ datahub/company/management/commands/populate_full_telephone_number.py
@@ -1,6 +1,8 @@
 from logging import getLogger
 
 from django.core.management.base import BaseCommand
+from django.db.models import F, Q, Value
+from django.db.models.functions import Concat
 
 from datahub.company.models.contact import Contact
 
@@ -14,8 +16,12 @@ class Command(BaseCommand):
         """Populate full telephone number data."""
         logger.info('Populating full_telephone_number field...')
 
-        for contact in Contact.objects.all().iterator(chunk_size=2000):
-            contact.full_telephone_number = contact.get_full_telephone_number()
-            contact.save()
+        Contact.objects.filter(
+            Q(full_telephone_number='') | Q(full_telephone_number__isnull=True),
+        ).update(
+            full_telephone_number=Concat(
+                F('telephone_countrycode'), Value(' '), F('telephone_number'),
+            ),
+        )
 
         logger.info('Finished populating full_telephone_number')
