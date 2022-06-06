1bb569bad5583082204e7d6d936a90ea8de89d55
richmadetech <richard.byrne@madetech.com>
2022-06-06 13:17:47 +0100

    Fixed lint issues

--- datahub/activity_stream/event/serializers.py
+++ datahub/activity_stream/event/serializers.py
@@ -32,8 +32,8 @@ class EventActivitySerializer(ActivitySerializer):
                 'dit:address_postcode': instance.address_postcode,
                 'dit:address_country': {'name': instance.address_country.name},
                 'dit:disabledOn': instance.disabled_on,
+                'dit:service': {'name': instance.service.name},
                 'dit:archivedDocumentsUrlPath': instance.archived_documents_url_path,
-                'dit:eventType': {'name': instance.event_type.name},
             },
 
         }
