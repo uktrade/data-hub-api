304354287e9ba42a0719c778b830a2e473199d05
elcct <pawel@mikesz.com>
2022-06-09 11:25:29 +0100

    Change reminder and notification scheduling and add summary email.

--- datahub/investment/project/notification/models.py
+++ datahub/investment/project/notification/models.py
@@ -1,7 +1,11 @@
+import uuid
+
 from django.conf import settings
 from django.db import models
 
 from datahub.core.fields import MultipleChoiceField
+
+MAX_LENGTH = settings.CHAR_FIELD_MAX_LENGTH
 
 
 class InvestmentNotificationSubscription(models.Model):
@@ -37,3 +41,29 @@ class InvestmentNotificationSubscription(models.Model):
     def __str__(self):
         """Human-readable representation."""
         return f'{self.investment_project} – {self.adviser}'
+
+
+class NotificationInnerTemplate(models.Model):
+    """Notification inner template content."""
+
+    class NotificationType(models.TextChoices):
+        NOT_SET = ('not_set', 'Not set')
+        NO_RECENT_INTERACTION = (
+            'no_investment_recent_interaction',
+            'No investment recent interaction',
+        )
+        UPCOMING_ESTIMATED_LAND_DATE = (
+            'upcoming_estimated_land_date',
+            'Upcoming estimated land date',
+        )
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+
+    content = models.TextField()
+    notification_type = models.CharField(
+        max_length=MAX_LENGTH,
+        choices=NotificationType.choices,
+        default=NotificationType.NOT_SET,
+        unique=True,
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
--- datahub/investment/project/notification/tasks.py
+++ datahub/investment/project/notification/tasks.py
@@ -1,7 +1,10 @@
+import datetime
 from logging import getLogger
+from operator import attrgetter
 
 from celery import shared_task
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from django.core.cache import cache
 from django.db.models import F, Q
 from django.utils.timezone import now
@@ -13,16 +16,21 @@ from datahub.investment.project import (
     INVESTMENT_ESTIMATED_LAND_DATE_NOTIFICATION_FEATURE_FLAG_NAME,
 )
 from datahub.investment.project.models import InvestmentProject
-from datahub.investment.project.notification.emails import send_estimated_land_date_reminder
+from datahub.investment.project.notification.emails import (
+    send_estimated_land_date_reminder,
+    send_estimated_land_date_summary,
+)
 from datahub.investment.project.notification.models import InvestmentNotificationSubscription
 
 logger = getLogger(__name__)
 
 estimated_land_date_notification = InvestmentNotificationSubscription.EstimatedLandDateNotification
-TASK_TOKEN_TIMEOUT = 24 * 3600
+TASK_TOKEN_TIMEOUT = 72 * 3600
+
+NOTIFICATION_SUMMARY_THRESHOLD = settings.NOTIFICATION_SUMMARY_THRESHOLD
 
 
-def get_subscriptions_for_estimated_land_date(notification_type: str):
+def get_subscriptions_for_estimated_land_date(adviser_id, current_date: datetime.date):
     """
     Gets subscriptions for estimated land date.
 
@@ -31,30 +39,35 @@ def get_subscriptions_for_estimated_land_date(notification_type: str):
     The subscriptions are only returned for advisers who are project managers,
     client relationship managers, project assurance advisers and referral source advisers.
     """
-    days = int(notification_type)
-    future_estimated_land_date = now() + relativedelta(days=days)
+    notification_types = {
+        30: current_date + relativedelta(months=1),
+        60: current_date + relativedelta(months=2),
+    }
 
-    subscriptions = InvestmentNotificationSubscription.objects.select_related(
+    eld_filter = Q()
+    for key, value in notification_types.items():
+        eld_filter |= Q(
+            estimated_land_date__contains=[key],
+        ) & Q(investment_project__estimated_land_date=value)
+
+    return InvestmentNotificationSubscription.objects.select_related(
         'investment_project',
         'adviser',
     ).filter(
+        eld_filter,
         # the estimated land date subscriptions are only valid for project managers
         # client relationship managers, project assurance advisers and referral source advisers.
         Q(adviser_id=F('investment_project__project_manager_id'))
         | Q(adviser_id=F('investment_project__client_relationship_manager_id'))
         | Q(adviser_id=F('investment_project__project_assurance_adviser_id'))
         | Q(adviser_id=F('investment_project__referral_source_adviser_id')),
-        estimated_land_date__contains=[notification_type],
-        investment_project__estimated_land_date__year=future_estimated_land_date.year,
-        investment_project__estimated_land_date__month=future_estimated_land_date.month,
-        investment_project__estimated_land_date__day=future_estimated_land_date.day,
+        adviser_id=adviser_id,
         investment_project__status__in=[
             InvestmentProject.Status.ONGOING,
             InvestmentProject.Status.DELAYED,
         ],
         investment_project__stage_id=InvestmentProjectStage.active.value.id,
-    )
-    return subscriptions
+    ).order_by('pk')
 
 
 @shared_task(
@@ -63,13 +76,14 @@ def get_subscriptions_for_estimated_land_date(notification_type: str):
     max_retries=5,
     retry_backoff=30,
 )
-def send_estimated_land_date_task():
+def send_estimated_land_date_task(token_prefix=None):
     """
-    The task runs every day for each notification type. It collects the active subscriptions
-    and sends a notification if investment project's estimated land date is in 30 or 60 days
-    and project manager has active subscription for given days.
+    The task runs first day of the month for all notification types. It collects the active
+    subscriptions and sends a notification if investment project's estimated land date is in
+    one or two months (labelled as 30 and 60 days) and project manager has active subscription
+    for given days option.
 
-    For each notification a token is set with 24h expiration time, in an event when the task
+    For each notification a token is set with 72h expiration time, in an event when the task
     gets restarted so that the notifications won't be sent multiple times.
     """
     if not is_feature_flag_active(INVESTMENT_ESTIMATED_LAND_DATE_NOTIFICATION_FEATURE_FLAG_NAME):
@@ -88,26 +102,51 @@ def send_estimated_land_date_task():
             )
             return
 
-        notification_types = estimated_land_date_notification.values
+        current_date = now().date().replace(day=1)
 
-        for notification_type in notification_types:
-            subscriptions = get_subscriptions_for_estimated_land_date(notification_type)
-
-            for subscription in subscriptions.iterator():
-                token = _get_token_name(
-                    subscription.investment_project,
-                    subscription.adviser,
-                    notification_type,
-                )
-                if not cache.get(token):
-                    cache.set(token, True, TASK_TOKEN_TIMEOUT)
-                    send_estimated_land_date_reminder(
-                        project=subscription.investment_project,
-                        adviser=subscription.adviser,
-                        days_left=notification_type,
-                    )
+        subscriptions_by_adviser = InvestmentNotificationSubscription.objects.distinct(
+            'adviser',
+        ).iterator()
+        for subscription in subscriptions_by_adviser:
+            _send_notifications_for_adviser(
+                subscription.adviser,
+                current_date,
+            )
 
 
-def _get_token_name(project, adviser, notification_type):
-    token = f'notification:{project.id}-{adviser.id}-{notification_type}'
+def _send_notifications_for_adviser(
+    adviser,
+    current_date,
+):
+    token = _get_token_name(adviser)
+    if cache.get(token):
+        return
+
+    cache.set(token, True, TASK_TOKEN_TIMEOUT)
+
+    subscriptions = get_subscriptions_for_estimated_land_date(adviser.id, current_date)
+    if subscriptions.count() > NOTIFICATION_SUMMARY_THRESHOLD:
+        projects = [subscription.investment_project for subscription in subscriptions]
+        projects.sort(key=attrgetter('pk'))
+
+        send_estimated_land_date_summary(
+            projects=projects,
+            adviser=adviser,
+            current_date=current_date,
+        )
+        return
+
+    for subscription in subscriptions.iterator():
+        day_diff = (subscription.investment_project.estimated_land_date - current_date).days
+        days_left = 30 if day_diff < 32 else 60
+
+        send_estimated_land_date_reminder(
+            project=subscription.investment_project,
+            adviser=subscription.adviser,
+            days_left=days_left,
+        )
+
+
+def _get_token_name(adviser):
+    token = f'notification:{adviser.id}'
     return token
