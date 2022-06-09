94a44adde466b5411452019264cdf810eaa6a0b2
richmadetech <72502389+richmadetech@users.noreply.github.com>
2022-06-09 16:47:26 +0100

    Added None checks to catch possible empty relational data for the eve… (#4170)
    * Added None checks to catch possible empty relational data for the events information
    
    * Added tests to cover new None checks in the event serializer

--- datahub/activity_stream/event/serializers.py
+++ datahub/activity_stream/event/serializers.py
@@ -21,29 +21,41 @@ class EventActivitySerializer(ActivitySerializer):
                     'dit:dataHub:Event',
                 ],
                 'name': instance.name,
-                'dit:eventType': {'name': instance.event_type.name},
                 'content': instance.notes,
                 'startTime': instance.start_date,
                 'endTime': instance.end_date,
                 'url': instance.get_absolute_url(),
-                'dit:locationType': {'name': instance.location_type.name},
                 'dit:address_1': instance.address_1,
                 'dit:address_2': instance.address_2,
                 'dit:address_town': instance.address_town,
                 'dit:address_county': instance.address_county,
                 'dit:address_postcode': instance.address_postcode,
                 'dit:address_country': {'name': instance.address_country.name},
-                'dit:leadTeam': {'name': instance.lead_team.name},
-                'dit:organiser': {'name': instance.organiser.name},
                 'dit:disabledOn': instance.disabled_on,
-                'dit:service': {'name': instance.service.name},
                 'dit:archivedDocumentsUrlPath': instance.archived_documents_url_path,
+                'dit:eventType': {'name': instance.event_type.name},
             },
 
         }
         if instance.uk_region is not None:
             event['object']['dit:ukRegion'] = {
                 'name': instance.uk_region.name,
+            }
+        if instance.organiser is not None:
+            event['object']['dit:organiser'] = {
+                'name': instance.organiser.name,
+            }
+        if instance.lead_team is not None:
+            event['object']['dit:leadTeam'] = {
+                'name': instance.lead_team.name,
+            }
+        if instance.location_type is not None:
+            event['object']['dit:locationType'] = {
+                'name': instance.location_type.name,
+            }
+        if instance.service is not None:
+            event['object']['dit:service'] = {
+                'name': instance.service.name,
             }
 
         return event
