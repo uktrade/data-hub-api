77df34d7adc7b7c539ab4ffd55744ead8ec33940
Pawel Szymanski <pawel@mikesz.com>
2022-08-10 09:30:39 +0100

    Remove individual subscriptions model and endpoints.

--- datahub/investment/project/urls.py
+++ datahub/investment/project/urls.py
@@ -3,7 +3,6 @@
 from django.urls import include, path
 
 from datahub.investment.project.evidence.urls import urlpatterns as evidence_urlpatterns
-from datahub.investment.project.notification.urls import urlpatterns as notification_urlpatterns
 from datahub.investment.project.proposition.urls import urls_v3 as proposition_urlpatterns
 from datahub.investment.project.views import (
     IProjectAuditViewSet,
@@ -97,13 +96,6 @@ urlpatterns = [
         include(
             (evidence_urlpatterns, 'evidence-document'),
             namespace='evidence-document',
-        ),
-    ),
-    path(
-        'investment/<uuid:project_pk>/',
-        include(
-            (notification_urlpatterns, 'notification'),
-            namespace='notification',
         ),
     ),
 ]
