6cb8a3ac7a358528ae3925620046852c347b1629
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2022-09-15 10:36:29 +0100

    Fix flake8 and removed logging to tmp file.

--- datahub/core/queues/health_check.py
+++ datahub/core/queues/health_check.py
@@ -1,21 +1,10 @@
-import os
 from datetime import datetime
 from logging import getLogger
-
-from django.conf import settings
 
 logger = getLogger(__name__)
 
 
 def queue_health_check():
-    log = f'Running RQ health check on "{datetime.now().strftime("%c")}" succeeds'
     logger.info(
-        log,
+        f'Running RQ health check on "{datetime.now().strftime("%c")}" succeeds',
     )
-    if settings.DEBUG:
-        log_health(log)
-
-
-def log_health(log):
-    with open('/tmp/test_rq.log', 'a', encoding='utf_8') as health_log:
-        health_log.write(log + os.linesep)
