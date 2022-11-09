dfa836807dd2917e2f24b2dd07f71232d6eae0de
Pawel Szymanski <pawel@mikesz.com>
2022-11-09 12:29:07 +0000

    Add "active_feature_groups" to "whoami" endpoint. Improve feature flag labelling.

--- datahub/reminder/__init__.py
+++ datahub/reminder/__init__.py
@@ -1,4 +1,8 @@
-NO_RECENT_INTERACTION_REMINDERS_FEATURE_FLAG_NAME = 'no-recent-interaction-reminders'
-NO_RECENT_INTERACTION_REMINDERS_EMAIL_STATUS_FLAG_NAME = 'no-recent-interaction-email-status'
-ESTIMATED_LAND_DATE_REMINDERS_FEATURE_FLAG_NAME = 'estimated-land-date-reminders'
-ESTIMATED_LAND_DATE_EMAIL_STATUS_FEATURE_FLAG_NAME = 'estimated-land-date-email-status'
+INVESTMENT_NO_RECENT_INTERACTION_REMINDERS_FEATURE_FLAG_NAME = \
+    'investment-no-recent-interaction-reminders'
+INVESTMENT_NO_RECENT_INTERACTION_REMINDERS_EMAIL_STATUS_FLAG_NAME = \
+    'investment-no-recent-interaction-email-status'
+INVESTMENT_ESTIMATED_LAND_DATE_REMINDERS_FEATURE_FLAG_NAME = \
+    'investment-estimated-land-date-reminders'
+INVESTMENT_ESTIMATED_LAND_DATE_EMAIL_STATUS_FEATURE_FLAG_NAME = \
+    'investment-estimated-land-date-email-status'
