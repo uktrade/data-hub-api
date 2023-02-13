fff020c83c189a0f77424d86a688413cc2055c42
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-02-13 15:46:41 +0000

    RR-634-auto-migrate (#4480)
    * Added additional criteria to the migrate post users
    Added port for postgres to docker
    Added related_name for investment project model to allow reverse querying
    
    * fix broken cron scheduler
    
    * add additional logging to help debugging
    
    * logging refactor
    
    * fix broken test
    
    * remove email from logging
    change logging message to lowercase
    make use of create batch for django models
    
    * change default days for reminders

--- cron-scheduler.py
+++ cron-scheduler.py
@@ -36,7 +36,7 @@ from datahub.investment.project.tasks import (
     schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
 )
 from datahub.omis.payment.tasks import refresh_pending_payment_gateway_sessions
-from datahub.reminder.migration_tasks import migrate_ita_users, migrate_post_users
+from datahub.reminder.migration_tasks import run_ita_users_migration, run_post_users_migration
 from datahub.reminder.tasks import (
     generate_new_export_interaction_reminders,
     generate_no_recent_export_interaction_reminders,
@@ -243,7 +243,7 @@ def schedule_new_export_interaction_jobs():
 def schedule_user_reminder_migration():
 
     job_scheduler(
-        function=migrate_ita_users,
+        function=run_ita_users_migration,
         max_retries=5,
         queue_name=LONG_RUNNING_QUEUE,
         retry_backoff=True,
@@ -253,7 +253,7 @@ def schedule_user_reminder_migration():
     )
 
     job_scheduler(
-        function=migrate_post_users,
+        function=run_post_users_migration,
         max_retries=5,
         queue_name=LONG_RUNNING_QUEUE,
         retry_backoff=True,
