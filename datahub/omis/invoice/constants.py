95860eb631e188d334f2c7da83587403b8ea10b5
Christopher Sunkel <christopher.sunkel@digital.trade.gov.uk>
2023-03-31 11:00:31 +0100

    Update OMIS content

--- datahub/omis/invoice/constants.py
+++ datahub/omis/invoice/constants.py
@@ -1,11 +1,11 @@
 from datahub.core import constants
 
-DIT_COMPANY_NAME = 'Department for International Trade'
-DIT_ADDRESS_1 = '3 Whitehall Place'
-DIT_ADDRESS_2 = ''
+DIT_COMPANY_NAME = 'Department for Business and Trade'
+DIT_ADDRESS_1 = 'Old Admiralty Building'
+DIT_ADDRESS_2 = 'Admiralty Place'
 DIT_ADDRESS_TOWN = 'London'
 DIT_ADDRESS_COUNTY = ''
-DIT_ADDRESS_POSTCODE = 'SW1A 2AW'
+DIT_ADDRESS_POSTCODE = 'SW1A 2DY'
 DIT_ADDRESS_COUNTRY_ID = constants.Country.united_kingdom.value.id
 DIT_VAT_NUMBER = '888 850455'
 
