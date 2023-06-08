edbcf7f83c6aceb0361429dd098ae8665cb9ad84
santosh dasari <74972011+santoshdasa12345@users.noreply.github.com>
2023-06-08 15:24:00 +0100

    Management command for rolling back merge.
    This is to support later changes where companies will be bulk merged based on
    recommendations from DnB.
    
    Co-authored-by: Michal Charemza <michal@charemza.name>
    Co-authored-by: Santosh Dasari <santosh.dasari@digital.trade.gov.uk>
    Co-authored-by: Josef Smith <josef.smith@trade.gov.uk>

---
+++ datahub/dbmaintenance/management/commands/rollback_company_merge.py
@@ -0,0 +1,26 @@
+from datahub.company.merge import rollback_merge_companies
+from datahub.company.models import Company
+from datahub.dbmaintenance.management.base import CSVBaseCommand
+from datahub.dbmaintenance.utils import parse_uuid
+
+
+class Command(CSVBaseCommand):
+    """
+    Command to rollback company merges.
+    """
+
+    help = """
+    Rollback company merges.  This consumes a one-column CSV file from S3 which is a
+    list of company IDs to rollback. The command will go through each company, and
+    its related models, and reinstate them to how they were before the merge.
+    """
+
+    def _process_row(self, row, simulate=False, **options):
+        """Process one single row."""
+        pk = parse_uuid(row['id'])
+        former_source_company = Company.objects.get(pk=pk)
+
+        if simulate:
+            return
+
+        rollback_merge_companies(former_source_company)
---
+++ datahub/dbmaintenance/test/commands/test_rollback_company_merge.py
@@ -0,0 +1,104 @@
+from io import BytesIO
+
+import pytest
+import reversion
+from django.core.management import call_command
+
+from datahub.company.merge import merge_companies
+from datahub.company.test.factories import CompanyFactory
+from datahub.core.test_utils import create_test_user
+
+pytestmark = pytest.mark.django_db
+
+
+def test_run(s3_stubber, formatted_dnb_company):
+    """
+    Test that the command successfully rolls back the specified records.
+    """
+    with reversion.create_revision():
+        company_1 = CompanyFactory(duns_number='123456789')
+        company_2 = CompanyFactory(duns_number='223456789')
+        company_3 = CompanyFactory(transferred_to=None)
+        company_4 = CompanyFactory(transferred_to=None)
+
+    user = create_test_user()
+    merge_companies(company_3, company_1, user)
+    merge_companies(company_4, company_2, user)
+
+    company_3.refresh_from_db()
+    company_4.refresh_from_db()
+    assert company_3.transferred_to == company_1
+    assert company_4.transferred_to == company_2
+
+    bucket = 'test_bucket'
+    object_key = 'test_key'
+    csv_content = f'id\n{company_3.id}\n{company_4.id}'
+
+    s3_stubber.add_response(
+        'get_object',
+        {
+            'Body': BytesIO(bytes(csv_content, encoding='utf-8')),
+        },
+        expected_params={
+            'Bucket': bucket,
+            'Key': object_key,
+        },
+    )
+
+    call_command(
+        'rollback_company_merge',
+        bucket,
+        object_key,
+    )
+
+    company_3.refresh_from_db()
+    company_4.refresh_from_db()
+    assert company_3.transferred_to is None
+    assert company_4.transferred_to is None
+
+
+def test_simulate(s3_stubber):
+    """
+    Test that the command simulates rollbacks if --simulate is passed in.
+    """
+    with reversion.create_revision():
+        company_1 = CompanyFactory(duns_number='123456789')
+        company_2 = CompanyFactory(duns_number='223456789')
+        company_3 = CompanyFactory(transferred_to=None)
+        company_4 = CompanyFactory(transferred_to=None)
+
+    user = create_test_user()
+    merge_companies(company_3, company_1, user)
+    merge_companies(company_4, company_2, user)
+
+    company_3.refresh_from_db()
+    company_4.refresh_from_db()
+    assert company_3.transferred_to == company_1
+    assert company_4.transferred_to == company_2
+
+    bucket = 'test_bucket'
+    object_key = 'test_key'
+    csv_content = f'id\n{company_3.id}\n{company_4.id}'
+
+    s3_stubber.add_response(
+        'get_object',
+        {
+            'Body': BytesIO(bytes(csv_content, encoding='utf-8')),
+        },
+        expected_params={
+            'Bucket': bucket,
+            'Key': object_key,
+        },
+    )
+
+    call_command(
+        'rollback_company_merge',
+        bucket,
+        object_key,
+        simulate=True,
+    )
+
+    company_3.refresh_from_db()
+    company_4.refresh_from_db()
+    assert company_3.transferred_to == company_1
+    assert company_4.transferred_to == company_2
