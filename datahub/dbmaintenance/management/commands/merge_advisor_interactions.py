d686a50eb2db35068386551c9fed204fac282408
Pippo Raimondi <piporamaa@gmail.com>
2023-06-26 08:07:16 +0100

    Command to merge interactions from inactive to active duplicate advisor

---
+++ datahub/dbmaintenance/management/commands/merge_advisor_interactions.py
@@ -0,0 +1,37 @@
+from django.core.management.base import BaseCommand
+from django.db import transaction
+
+from datahub.company.models import Advisor
+from datahub.interaction.models import InteractionDITParticipant
+
+
+class Command(BaseCommand):
+    help = 'Merges inactive advisor with active one in interaction model'
+
+    def add_arguments(self, parser):
+        parser.add_argument('inactive_advisor_id', type=str, help='UUID of the inactive advisor')
+        parser.add_argument('active_advisor_id', type=str, help='UUID of the active advisor')
+
+    @transaction.atomic
+    def handle(self, *args, **options):
+        inactive_advisor_id = options['inactive_advisor_id']
+        active_advisor_id = options['active_advisor_id']
+
+        try:
+            inactive_advisor = Advisor.objects.get(id=inactive_advisor_id, is_active=False)
+            active_advisor = Advisor.objects.get(id=active_advisor_id, is_active=True)
+        except Advisor.DoesNotExist as e:
+            self.stderr.write(self.style.ERROR(str(e)))
+            return
+
+        interactions_to_update = InteractionDITParticipant.objects.filter(adviser=inactive_advisor)
+
+        self.stdout.write(self.style.SUCCESS(
+            f'{interactions_to_update.count()} interactions will be updated' # noqa
+        ))
+
+        interactions_to_update.update(adviser=active_advisor)
+
+        self.stdout.write(self.style.SUCCESS(
+            f'Successully merged interactions from {inactive_advisor} into {active_advisor}' # noqa
+        ))
