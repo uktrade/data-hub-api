7527fc358cc9af9a920f5c116d47d65561007b93
Pawel Szymanski <pawel@mikesz.com>
2023-07-18 09:27:20 +0100

    Remove IMAP email integration.

--- cron-scheduler.py
+++ cron-scheduler.py
@@ -34,7 +34,7 @@ from datahub.core.queues.job_scheduler import job_scheduler
 from datahub.core.queues.scheduler import DataHubScheduler, LONG_RUNNING_QUEUE
 from datahub.dnb_api.tasks.sync import schedule_sync_outdated_companies_with_dnb
 from datahub.dnb_api.tasks.update import schedule_get_company_updates
-from datahub.email_ingestion.tasks import ingest_emails, process_mailbox_emails
+from datahub.email_ingestion.tasks import process_mailbox_emails
 from datahub.investment.project.tasks import (
     schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
 )
@@ -213,12 +213,6 @@ def schedule_jobs():
 
 
 def schedule_email_ingestion_tasks():
-    if settings.ENABLE_EMAIL_INGESTION:
-        job_scheduler(
-            function=ingest_emails,
-            cron=EVERY_TEN_MINUTES,
-            description='DataHub Email ingestion tasks ingest emails',
-        )
     if settings.ENABLE_MAILBOX_PROCESSING:
         job_scheduler(
             function=process_mailbox_emails,
--- datahub/email_ingestion/test/test_tasks.py
+++ datahub/email_ingestion/test/test_tasks.py
@@ -1,88 +1,69 @@
 from unittest import mock
 
 import pytest
-from django.test.utils import override_settings
 
-from datahub.email_ingestion.mailbox import MailboxHandler
-from datahub.email_ingestion.tasks import ingest_emails
-from datahub.email_ingestion.test.utils import MAILBOXES_SETTING, mock_import_string
+from datahub.email_ingestion.tasks import process_mailbox_emails
 from datahub.feature_flag.models import FeatureFlag
 from datahub.feature_flag.test.factories import FeatureFlagFactory
-from datahub.interaction import INTERACTION_EMAIL_INGESTION_FEATURE_FLAG_NAME
+from datahub.interaction import MAILBOX_INGESTION_FEATURE_FLAG_NAME
 
 
 @pytest.fixture()
-def interaction_email_ingestion_feature_flag():
+def mailbox_ingestion_feature_flag():
     """
     Creates the email ingestion feature flag.
     """
-    yield FeatureFlagFactory(code=INTERACTION_EMAIL_INGESTION_FEATURE_FLAG_NAME)
+    yield FeatureFlagFactory(code=MAILBOX_INGESTION_FEATURE_FLAG_NAME)
 
 
 @pytest.mark.django_db
-@pytest.mark.usefixtures('interaction_email_ingestion_feature_flag')
+@pytest.mark.usefixtures('mailbox_ingestion_feature_flag')
 class TestIngestEmails:
     """
     Test ingest_emails RQ task.
     """
 
-    @override_settings(MAILBOXES=MAILBOXES_SETTING)
     def test_ingest_emails_lock_acquired(self, monkeypatch):
         """
-        Test that our mailboxes are processed when the lock is acquired.
+        Test that our emails are processed when the lock is acquired.
         """
-        # Mock import_string to avoid import errors for processor_class path strings
-        mock_import_string(monkeypatch)
-        process_new_mail_patch = mock.Mock()
-        # ensure that the process_new_mail method is a mock so we can interrogate later
+        process_emails_patch = mock.Mock()
         monkeypatch.setattr(
-            'datahub.email_ingestion.mailbox.Mailbox.process_new_mail',
-            process_new_mail_patch,
+            'datahub.email_ingestion.emails.process_ingestion_emails',
+            process_emails_patch,
         )
-        # Refresh the mailbox_handler singleton as we have overidden the MAILBOXES setting
-        mailbox_handler = MailboxHandler()
-        mailbox_handler.initialise_mailboxes()
-        monkeypatch.setattr(
-            'datahub.email_ingestion.tasks.mailbox_handler',
-            mailbox_handler,
-        )
-        ingest_emails()
-        assert process_new_mail_patch.call_count == 2
+        process_mailbox_emails()
+        assert process_emails_patch.call_count == 1
 
-    @override_settings(MAILBOXES=MAILBOXES_SETTING)
     def test_ingest_emails_lock_not_acquired(self, monkeypatch):
         """
-        Test that our mailboxes are not processed when the lock cannot be acquired successfully.
+        Test that our emails are not processed when the lock cannot be acquired successfully.
         """
-        process_new_mail_patch = mock.Mock()
-        # ensure that the process_new_mail method is a mock so we can interrogate later
+        process_emails_patch = mock.Mock()
         monkeypatch.setattr(
-            'datahub.email_ingestion.mailbox.Mailbox.process_new_mail',
-            process_new_mail_patch,
+            'datahub.email_ingestion.emails.process_ingestion_emails',
+            process_emails_patch,
         )
-        # Have to mock rather than acquire the lock as locks are per connection (if the lock is
-        # already held by the current connection, the current connection can still re-acquire it).
         advisory_lock_mock = mock.MagicMock()
         advisory_lock_mock.return_value.__enter__.return_value = False
         monkeypatch.setattr('datahub.email_ingestion.tasks.advisory_lock', advisory_lock_mock)
 
-        ingest_emails()
-        assert process_new_mail_patch.called is False
+        process_mailbox_emails()
+        assert process_emails_patch.called is False
 
-    @override_settings(MAILBOXES=MAILBOXES_SETTING)
     def test_ingest_feature_flag_inactive(self, monkeypatch):
         """
-        Test that our mailboxes are not processed when the feature flag is not active.
+        Test that our emails are not processed when the feature flag is not active.
         """
-        process_new_mail_patch = mock.Mock()
-        # ensure that the process_new_mail method is a mock so we can interrogate later
+        process_emails_patch = mock.Mock()
+        # ensure that the process_ingestion_emails method is a mock so we can interrogate later
         monkeypatch.setattr(
-            'datahub.email_ingestion.mailbox.Mailbox.process_new_mail',
-            process_new_mail_patch,
+            'datahub.email_ingestion.emails.process_ingestion_emails',
+            process_emails_patch,
         )
-        flag = FeatureFlag.objects.get(code=INTERACTION_EMAIL_INGESTION_FEATURE_FLAG_NAME)
+        flag = FeatureFlag.objects.get(code=MAILBOX_INGESTION_FEATURE_FLAG_NAME)
         flag.is_active = False
         flag.save()
 
-        ingest_emails()
-        assert process_new_mail_patch.called is False
+        process_mailbox_emails()
+        assert process_emails_patch.called is False
