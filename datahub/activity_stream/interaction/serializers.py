c0d73415cd938843e636079f55bada97a94c965c
Caitlin Barnard <54268863+CaitBarnard@users.noreply.github.com>
2023-07-18 10:27:07 +0100

    Add business intelligence to tests

--- datahub/activity_stream/interaction/serializers.py
+++ datahub/activity_stream/interaction/serializers.py
@@ -72,7 +72,7 @@ class InteractionActivitySerializer(ActivitySerializer):
                 'dit:status': instance.status,
                 'dit:archived': instance.archived,
                 'dit:subject': instance.subject,
-                'dit:business_intelligence': instance.business_intelligence,
+                'dit:business_intelligence': instance.was_policy_feedback_provided,
                 'attributedTo': [
                     *self._get_companies(instance.companies),
                     *self._get_dit_participants(instance.dit_participants),
