be34b65b4c350ecb8e258eaffbdeb857af5c9ff8
Bau Nguyen <bauvannguyen123@gmail.com>
2023-07-21 11:09:40 +0100

    add new event type

---
+++ datahub/event/migrations/0025_update_event_types.py
@@ -0,0 +1,20 @@
+from pathlib import PurePath
+from django.db import migrations
+
+from datahub.core.migration_utils import load_yaml_data_in_migration
+
+
+def update_event_types(apps, schema_editor):
+    load_yaml_data_in_migration(
+        apps, PurePath(__file__).parent / "0025_update_event_types.yaml"
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('event', '0024_update_event_programme'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_event_types, migrations.RunPython.noop),
+    ]
