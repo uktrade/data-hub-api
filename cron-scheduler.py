ec91fe373ca63112fe7fa3e9825ed43acd9263a6
Chris Hopkins <chris.hopkins@madetech.com>
2023-09-12 09:27:49 +0100

    remove the auto update overnight from DNB

--- cron-scheduler.py
+++ cron-scheduler.py
@@ -164,7 +164,7 @@ def schedule_jobs():
             function=schedule_sync_outdated_companies_with_dnb,
             function_kwargs={
                 'dnb_modified_on_before': dnb_modified_on_before,
-                'fields_to_update': ['global_ultimate_duns_number', 'is_out_of_business'],
+                'fields_to_update': ['global_ultimate_duns_number'],
                 'limit': settings.DAILY_HIERARCHY_ROLLOUT_LIMIT,
                 'simulate': False,
                 'max_requests': 5,
