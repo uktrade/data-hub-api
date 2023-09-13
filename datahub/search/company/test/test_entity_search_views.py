be84dbabd42889285622fdb321abf47bca8e47d0
Paul Wheatcroft <paul_wheatcroft@hotmail.com>
2023-09-13 11:36:35 +0100

    Additional code coverage test set

--- datahub/search/company/test/test_entity_search_views.py
+++ datahub/search/company/test/test_entity_search_views.py
@@ -661,6 +661,40 @@ class TestSearch(APITestMixin):
         for expected_result in expected_results:
             assert expected_result in search_results
 
+    def test_headquarter_type_not_set_response_returns_all_companies(
+        self,
+        opensearch_with_collector,
+    ):
+        ghq_company = CompanyFactory(headquarter_type_id=constants.HeadquarterType.ghq.value.id)
+        ukhq_company = CompanyFactory(headquarter_type_id=constants.HeadquarterType.ukhq.value.id)
+        ehq_company = CompanyFactory(headquarter_type_id=constants.HeadquarterType.ehq.value.id)
+        ultimate_company_1 = CompanyFactory(
+            duns_number='123456789',
+            global_ultimate_duns_number='123456789',
+        )
+
+        opensearch_with_collector.flush_and_refresh()
+
+        url = reverse('api-v4:search:company')
+        response = self.api_client.post(
+            url,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        search_results = {company['id'] for company in response.data['results']}
+        expected_results = {
+            str(company.id)
+            for company in [
+                ghq_company,
+                ehq_company,
+                ukhq_company,
+                ultimate_company_1,
+            ]
+        }
+        for expected_result in expected_results:
+            assert expected_result in search_results
+
     @pytest.mark.parametrize(
         'num_account_managers',
         (1, 2, 3),
