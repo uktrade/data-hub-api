bc0c165056040287f74492c904b143cc69fe7ab1
Chris Hopkins <chris.hopkins@madetech.com>
2023-09-13 14:49:49 +0100

    added additional signal check

--- datahub/task/signals.py
+++ datahub/task/signals.py
@@ -9,7 +9,6 @@ from datahub.task.models import InvestmentProjectTask, Task
     sender=InvestmentProjectTask,
 )
 def delete_investment_project_task_delete(sender, instance, **kwargs):
-    if instance.task.id:
-        associated_task = Task.objects.filter(pk=instance.task.id).first()
-        if associated_task:
-            associated_task.delete()
+    associated_task = Task.objects.filter(pk=instance.task.id).first()
+    if associated_task:
+        associated_task.delete()
