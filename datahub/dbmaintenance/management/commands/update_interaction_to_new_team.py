b2d0de43dce4ad7512c0f5a4a0379d87ae23c24b
Bau Nguyen <bauvannguyen123@gmail.com>
2023-10-10 16:04:14 +0100

    change print to logger
    Updated command
    
    Changed command to look at InteractionDITParticipant table instead of interaction table
    
    flake8 test
    
    flake8 test
    
    Update update_interaction_to_new_team.py
    
    Update update_interaction_to_new_team.py

--- datahub/dbmaintenance/management/commands/update_interaction_to_new_team.py
+++ datahub/dbmaintenance/management/commands/update_interaction_to_new_team.py
@@ -1,33 +1,67 @@
 from logging import getLogger
-from datahub.dbmaintenance.management.base import CSVBaseCommand
 
 import reversion
 
-from datahub.dbmaintenance.utils import parse_uuid, parse_uuid_list
-from datahub.interaction.models import Interaction
+from datahub.dbmaintenance.management.base import CSVBaseCommand
+from datahub.dbmaintenance.utils import parse_uuid
+from datahub.interaction.models import InteractionDITParticipant
 
 logger = getLogger(__name__)
 
 
 class Command(CSVBaseCommand):
-    """Command to update an interaction advisers to a new team"""
+    """
+    Command to update an interaction advisers to a new team.
+
+    Requires a CSV with a row of interaction ids to update.
+    Must specify a `team_id` for updating the team on the InteractionDITParticipant.
+    """
 
     help = 'Updates interaction advisers to a new team with the history up to a certain date'
 
-    def _process_row(self, row, simulate=False):
+    def _process_row(self, row, simulate=True, **options):
         """Processes a CSV file row."""
-        pk = parse_uuid(row['id'])
-        interaction = Interaction.objects.get(pk=pk)
-        team_id = parse_uuid(row['team_id'])
-        adviser_ids = parse_uuid_list(row['adviser_id'])
-        new_team_name = parse_uuid(row['team_name'])
+        interaction_pk = parse_uuid(row['id'])
 
+        # Team to move to.
+        team_id = options.get('team_id')
+
+        if not team_id:
+            logger.warning('You must specify a team id')
+            return
+
+        # All advisers for the current interaction which are not already in the
+        # correct team.
+        interaction_participants = InteractionDITParticipant.objects.filter(
+            interaction_id=interaction_pk,
+        ).exclude(
+            team_id=team_id,
+        ).select_related('team', 'adviser')
+
+        if not interaction_participants:
+            logger.warning(f'No interaction participants for interaction: {interaction_pk}')
+            return
+
+        logger.info(f'{interaction_participants.count()} participants to update.')
+
+        # Update the team for each adviser on the current interaction.
+        for interaction_participant in interaction_participants:
+            if simulate:
+                logger.info(
+                    f'{interaction_participant.adviser} - {interaction_participant.team_id}:'
+                    f'{interaction_participant.team}',
+                )
+            interaction_participant.team_id = team_id
+
+        # Prevent DB changes for simulation.
         if simulate:
             return
 
+        # Bulk update all adviser teams.
         with reversion.create_revision():
-            if interaction.adviser.id in set(adviser_ids):
-                interaction.update(team=team_id)
-                reversion.set_comment(
-                    'Set the advisers: ' + adviser_ids + ' to the team: ' + new_team_name
-                    + ' for the given interaction: ' + pk)
+            InteractionDITParticipant.objects.bulk_update(
+                interaction_participants, ['team'],
+            )
+            reversion.set_comment(
+                f'Updated interaction {interaction_pk} participants to have team id {team_id}',
+            )
