fd17b0899c36f3c9add880c79bf0b73bcbf11425
Paul Gain <paul@javexconsulting.com>
2023-10-13 07:57:35 +0100

    Add export wins team type metadata

--- datahub/metadata/models.py
+++ datahub/metadata/models.py
@@ -364,3 +364,7 @@ class FDIValue(BaseOrderedConstantModel):
 
 class ExportBarrierType(BaseOrderedConstantModel):
     """Export barrier type (used for company interactions)."""
+
+
+class TeamType(BaseOrderedConstantModel):
+    """Team type (for export wins)."""
