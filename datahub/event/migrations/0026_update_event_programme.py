664c357cde55248e54ee6c395b2abd12f5afcd37
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2023-10-13 11:57:39 +0100

    Add Northern Ireland Investment Summit 2023 metadata

---
+++ datahub/event/migrations/0026_update_event_programme.py
@@ -0,0 +1,20 @@
+from pathlib import PurePath
+from django.db import migrations
+
+from datahub.core.migration_utils import load_yaml_data_in_migration
+
+
+def update_event_programmes(apps, schema_editor):
+    load_yaml_data_in_migration(
+        apps, PurePath(__file__).parent / "0026_update_event_programme.yaml"
+    )
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('event', '0025_update_event_types'),
+    ]
+
+    operations = [
+        migrations.RunPython(update_event_programmes, migrations.RunPython.noop),
+    ]
