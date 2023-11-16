854a227b3cee574bf013fea491ba46a98bf0e90f
Chris Hopkins <chris.hopkins@madetech.com>
2023-11-16 15:26:17 +0000

    move patch to helper function

--- datahub/task/test/test_signals.py
+++ datahub/task/test/test_signals.py
@@ -1,3 +1,4 @@
+import functools
 from unittest.mock import call, patch
 
 import pytest
@@ -7,7 +8,23 @@ from factory.django import mute_signals
 
 from datahub.company.test.factories import AdviserFactory
 from datahub.task.models import Task
+from datahub.task.signals import set_task_subscriptions_and_schedule_notifications
 from datahub.task.test.factories import InvestmentProjectTaskFactory, TaskFactory
+
+
+def patch_all_task_subscription_functions(f):
+    @patch('datahub.task.signals.schedule_create_task_reminder_subscription_task')
+    @patch(
+        'datahub.task.signals.schedule_create_task_assigned_to_me_from_others_subscription_task',
+    )
+    @patch(
+        'datahub.task.signals.schedule_create_task_overdue_subscription_task',
+    )
+    @functools.wraps(f)
+    def functor(*args, **kwargs):
+        return f(*args, **kwargs)
+
+    return functor
 
 
 @pytest.mark.django_db
@@ -38,13 +55,7 @@ class TestDeleteInvestmentProjectTask:
 
 @pytest.mark.django_db
 class TestTaskAdviserChangedSubscriptions:
-    @patch('datahub.task.signals.schedule_create_task_reminder_subscription_task')
-    @patch(
-        'datahub.task.signals.schedule_create_task_assigned_to_me_from_others_subscription_task',
-    )
-    @patch(
-        'datahub.task.signals.schedule_create_task_overdue_subscription_task',
-    )
+    @patch_all_task_subscription_functions
     def test_schedule_functions_called_for_each_adviser(
         self,
         schedule_create_task_overdue_subscription_task,
@@ -67,14 +78,50 @@ class TestTaskAdviserChangedSubscriptions:
             any_order=True,
         )
 
-    def test_modifications_to_task_advisers(self):
-        pass
+    @patch_all_task_subscription_functions
+    def test_pk_set_to_none_does_not_trigger_scheduled_tasks(
+        self,
+        schedule_create_task_overdue_subscription_task,
+        schedule_create_task_assigned_to_me_from_others_subscription_task,
+        schedule_create_task_reminder_subscription_task,
+    ):
+        myargs = {'action': 'post_add', 'instance': 'a task'}
+        set_task_subscriptions_and_schedule_notifications(sender=None, **myargs)
 
-    def test_pk_set_none(self):
-        pass
+        schedule_create_task_reminder_subscription_task.assert_not_called()
+        schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
+        schedule_create_task_overdue_subscription_task.assert_not_called()
 
-    def test_task_none(self):
-        pass
+    @patch_all_task_subscription_functions
+    def test_instance_set_to_none_does_not_trigger_scheduled_tasks(
+        self,
+        schedule_create_task_overdue_subscription_task,
+        schedule_create_task_assigned_to_me_from_others_subscription_task,
+        schedule_create_task_reminder_subscription_task,
+    ):
+        myargs = {'action': 'post_add', 'pk_set': 'some advisers'}
+        set_task_subscriptions_and_schedule_notifications(sender=None, **myargs)
 
-    def test_removing_adviser_does_not_trigger_signals(self):
-        pass
+        schedule_create_task_reminder_subscription_task.assert_not_called()
+        schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
+        schedule_create_task_overdue_subscription_task.assert_not_called()
+
+    @patch_all_task_subscription_functions
+    def test_removing_adviser_does_not_trigger_scheduled_tasks(
+        self,
+        schedule_create_task_overdue_subscription_task,
+        schedule_create_task_assigned_to_me_from_others_subscription_task,
+        schedule_create_task_reminder_subscription_task,
+    ):
+        advisers = AdviserFactory.create_batch(2)
+        task = TaskFactory(advisers=advisers)
+
+        schedule_create_task_reminder_subscription_task.reset_mock()
+        schedule_create_task_assigned_to_me_from_others_subscription_task.reset_mock()
+        schedule_create_task_overdue_subscription_task.reset_mock()
+
+        task.advisers.remove(advisers[0])
+
+        schedule_create_task_reminder_subscription_task.assert_not_called()
+        schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
+        schedule_create_task_overdue_subscription_task.assert_not_called()
