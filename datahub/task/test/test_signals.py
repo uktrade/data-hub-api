e181c309ec999a45e9ba38a506498ae1a4851a12
Chris Hopkins <102232401+chopkinsmade@users.noreply.github.com>
2023-11-27 10:29:40 +0000

    feature/CLS2-558-task-amended-by-others-reminders (#5014)
    * removed investment prkect task model
    
    * fixed cleanup command tests
    
    * Added reminder generation signal for task amended by others
    
    * added logging for email sending
    
    * dont send notifications for amended to new advisers

--- datahub/task/test/test_signals.py
+++ datahub/task/test/test_signals.py
@@ -20,6 +20,7 @@ def patch_all_task_subscription_functions(f):
         'datahub.task.signals.schedule_create_task_overdue_subscription_task',
     )
     @patch('datahub.task.signals.schedule_create_task_completed_subscription_task')
+    @patch('datahub.task.signals.schedule_create_task_amended_by_others_subscription_task')
     @functools.wraps(f)
     def functor(*args, **kwargs):
         return f(*args, **kwargs)
@@ -32,6 +33,7 @@ class TestTaskAdviserChangedSubscriptions:
     @patch_all_task_subscription_functions
     def test_schedule_functions_called_for_each_adviser(
         self,
+        schedule_create_task_amended_by_others_subscription_task,
         schedule_create_task_completed_subscription_task,
         schedule_create_task_overdue_subscription_task,
         schedule_create_task_assigned_to_me_from_others_subscription_task,
@@ -56,10 +58,15 @@ class TestTaskAdviserChangedSubscriptions:
             [call(adviser.id) for adviser in advisers],
             any_order=True,
         )
+        schedule_create_task_amended_by_others_subscription_task.assert_has_calls(
+            [call(adviser.id) for adviser in advisers],
+            any_order=True,
+        )
 
     @patch_all_task_subscription_functions
     def test_pk_set_to_none_does_not_trigger_scheduled_tasks(
         self,
+        schedule_create_task_amended_by_others_subscription_task,
         schedule_create_task_completed_subscription_task,
         schedule_create_task_overdue_subscription_task,
         schedule_create_task_assigned_to_me_from_others_subscription_task,
@@ -72,10 +79,12 @@ class TestTaskAdviserChangedSubscriptions:
         schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
         schedule_create_task_overdue_subscription_task.assert_not_called()
         schedule_create_task_completed_subscription_task.assert_not_called()
+        schedule_create_task_amended_by_others_subscription_task.assert_not_called()
 
     @patch_all_task_subscription_functions
     def test_instance_set_to_none_does_not_trigger_scheduled_tasks(
         self,
+        schedule_create_task_amended_by_others_subscription_task,
         schedule_create_task_completed_subscription_task,
         schedule_create_task_overdue_subscription_task,
         schedule_create_task_assigned_to_me_from_others_subscription_task,
@@ -88,10 +97,12 @@ class TestTaskAdviserChangedSubscriptions:
         schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
         schedule_create_task_overdue_subscription_task.assert_not_called()
         schedule_create_task_completed_subscription_task.assert_not_called()
+        schedule_create_task_amended_by_others_subscription_task.assert_not_called()
 
     @patch_all_task_subscription_functions
     def test_removing_adviser_does_not_trigger_scheduled_tasks(
         self,
+        schedule_create_task_amended_by_others_subscription_task,
         schedule_create_task_completed_subscription_task,
         schedule_create_task_overdue_subscription_task,
         schedule_create_task_assigned_to_me_from_others_subscription_task,
@@ -104,6 +115,7 @@ class TestTaskAdviserChangedSubscriptions:
         schedule_create_task_assigned_to_me_from_others_subscription_task.reset_mock()
         schedule_create_task_overdue_subscription_task.reset_mock()
         schedule_create_task_completed_subscription_task.reset_mock()
+        schedule_create_task_amended_by_others_subscription_task.reset_mock()
 
         task.advisers.remove(advisers[0])
 
@@ -111,6 +123,7 @@ class TestTaskAdviserChangedSubscriptions:
         schedule_create_task_assigned_to_me_from_others_subscription_task.assert_not_called()
         schedule_create_task_overdue_subscription_task.assert_not_called()
         schedule_create_task_completed_subscription_task.assert_not_called()
+        schedule_create_task_amended_by_others_subscription_task.assert_not_called()
 
 
 @pytest.mark.django_db
@@ -145,5 +158,43 @@ class TestTaskAdviserCompletedSubscriptions:
                 call(task, True),
                 call(task, False),
                 call(task, False),
+            ],
+        )
+
+
+@pytest.mark.django_db
+class TestTaskAmededByOthersSubscriptions:
+    @mute_signals(m2m_changed)
+    @patch('datahub.task.signals.schedule_notify_advisers_task_amended_by_others')
+    def test_creating_task_triggers_notify_advisers_task_amended_by_others_scheduled_task(
+        self,
+        schedule_notify_advisers_task_amended_by_others,
+    ):
+        adviser = AdviserFactory()
+        task = TaskFactory(advisers=[adviser])
+
+        schedule_notify_advisers_task_amended_by_others.assert_has_calls(
+            [
+                call(task, True, []),
+                call(task, False, [adviser.id]),
+            ],
+        )
+
+    @mute_signals(m2m_changed)
+    @patch('datahub.task.signals.schedule_notify_advisers_task_amended_by_others')
+    def test_modifying_task_triggers_notify_advisers_task_amended_by_others_scheduled_task(
+        self,
+        schedule_notify_advisers_task_amended_by_others,
+    ):
+        adviser = AdviserFactory()
+        task = TaskFactory(archived=False, advisers=[adviser])
+        task.archived = True
+        task.save()
+
+        schedule_notify_advisers_task_amended_by_others.assert_has_calls(
+            [
+                call(task, True, []),
+                call(task, False, [adviser.id]),
+                call(task, False, [adviser.id]),
             ],
         )
