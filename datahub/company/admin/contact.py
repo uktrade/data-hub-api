81f07ad4ad8435c2d98d9fa3082dc4d1e048f793
Richard Pentecost <richardpentecost@DBT000444.local>
2023-12-01 14:51:40 +0000

    added admin urls to contact model, created button in admin for merging contacts, add first step for merging contacts

--- datahub/company/admin/contact.py
+++ datahub/company/admin/contact.py
@@ -1,7 +1,11 @@
+from functools import partial
+
 from django.contrib import admin
+from django.urls import path
 from reversion.admin import VersionAdmin
 
 from datahub.company.models import Contact
+from datahub.company.admin.merge_contact.step_1 import merge_select_other_contact
 from datahub.core.admin import BaseModelAdminMixin
 
 
@@ -39,3 +43,16 @@ class ContactAdmin(BaseModelAdminMixin, VersionAdmin):
         'modified_on',
         'modified_by',
     )
+
+    def get_urls(self):
+        model_meta = self.model._meta
+
+        return [
+            path(
+                'merge/step-1-select-other-contact',
+                self.admin_site.admin_view(partial(merge_select_other_contact, self)),
+                name=f'{model_meta.app_label}_'
+                f'{model_meta.model_name}_merge-select-other-contact',
+            ),
+            *super().get_urls(),
+        ]
