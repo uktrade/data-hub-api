dfc6dca7069ebedede51a72b3238560399d62d3a
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-12-06 10:46:07 +0000

    WIP: task not created by

--- datahub/search/task/views.py
+++ datahub/search/task/views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from datahub.search.task import TaskSearchApp
 from datahub.search.task.serializers import (
     SearchTaskQuerySerializer,
@@ -22,6 +24,7 @@ class SearchTaskAPIViewMixin:
         'title',
         'due_date',
         'created_by',
+        'not_created_by',
         'advisers',
         'company',
         'investment_project',
@@ -39,4 +42,95 @@ class SearchTaskAPIViewMixin:
 
 @register_v4_view()
 class SearchTaskAPIView(SearchTaskAPIViewMixin, SearchAPIView):
-    pass
+    """Filtered company search view."""
+
+    def deep_get(self, dictionary, keys, default=None):
+        """
+        Perform a deep search on a dictionary to find the item at the location provided in the keys
+        """
+        return reduce(
+            lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+            keys.split('|'),
+            dictionary,
+        )
+
+    def get_base_query(self, request, validated_data):
+        base_query = super().get_base_query(request, validated_data)
+
+        raw_query = base_query.to_dict()
+        filters = self.deep_get(raw_query, 'query|bool|filter')
+        if not filters:
+            return base_query
+        # from pprint import pprint
+
+        if request.data.get('not_created_by'):
+            filters = [
+                {
+                    'bool': {
+                        'must_not': [
+                            {
+                                'bool': {
+                                    'minimum_should_match': 1,
+                                    'should': [
+                                        {
+                                            'match': {
+                                                'created_by.id': {
+                                                    'operator': 'and',
+                                                    'query': request.data['not_created_by'],
+                                                },
+                                            },
+                                        },
+                                    ],
+                                },
+                            },
+                        ],
+                    },
+                },
+            ]
+            raw_query['query']['bool']['filter'] = filters
+            base_query.update_from_dict(raw_query)
+
+        # filter_index = None
+        # for index, filter in enumerate(filters):
+        #     if filter.get('bool'):
+        #         filter_index = index
+        #         break
+
+        # if filter_index is None:
+        #     return base_query
+
+        # must_filters = filters[filter_index]['bool']['must']
+        # for index, filter in enumerate(must_filters):
+        #     # By default the logic to generate an opensearch query inside get_base_query uses an
+        #     # and for each column passed to it. In this use case, when we detect a query for the
+        #     # ghq headquarter id we add an addiitonal should entry into the should array
+        #     should_queries = self.deep_get(filter, 'bool|should')
+
+        #     if should_queries:
+        #         for should_query in should_queries:
+        #             if (
+        #                 self.deep_get(should_query, 'match|headquarter_type.id|query')
+        #                 == HeadquarterType.ghq.value.id
+        #             ):
+        #                 should_queries.append(
+        #                     {
+        #                         'match': {
+        #                             'is_global_ultimate': {
+        #                                 'query': True,
+        #                             },
+        #                         },
+        #                     },
+        #                 )
+        #                 break
+        #         base_query.filter('terms', tags=['search', 'python'])
+        #         raw_query['query']['bool']['filter'][filter_index]['bool']['must'][index]['bool'][
+        #             'should'
+        #         ] = should_queries
+
+        # raw_query['query']['bool']['filter'] = filters
+        # pprint("post raw_query:")
+        # pprint(raw_query)
+        # base_query.update_from_dict(raw_query)
+
+        # base_query.filter('terms', tags=['search', 'python'])
+        return base_query
