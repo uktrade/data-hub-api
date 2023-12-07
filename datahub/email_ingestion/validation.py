247cb354db349a0710b0aa1617a2866aaeb4cefb
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2023-12-07 14:37:43 +0000

    Replace blacklist with blocklist

--- datahub/email_ingestion/validation.py
+++ datahub/email_ingestion/validation.py
@@ -81,7 +81,7 @@ def was_email_sent_by_dit(message):
     except IndexError:
         return False
 
-    if from_email.lower() in settings.DIT_EMAIL_INGEST_BLACKLIST:
+    if from_email.lower() in settings.DIT_EMAIL_INGEST_BLOCKLIST:
         return False
 
     try:
