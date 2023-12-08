9fd8b5ea81f88bc8248d4dc3ae55eb52dc13cded
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-12-08 09:14:48 +0000

    Feature/cls2 587 task filter created by (#5062)
    * WIP: task not created by
    
    * WIP: Initial open search query
    
    * WIP: Trying to access to Bool of the filter
    
    * WIP: Todo merge into single bool object
    
    * Not created by filter
    
    * Add tests
    
    * Fix test
    
    * Test code coverage
    
    * Optimise by only run deep_get when required

--- datahub/search/task/test/test_views.py
+++ datahub/search/task/test/test_views.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timedelta
+from unittest import mock
 
 import pytest
 
@@ -84,6 +85,131 @@ class TestTaskSearch(APITestMixin):
 
         assert response.data['count'] == 1
         assert response.data['results'][0]['id'] == str(task.id)
+
+    def test_search_task_not_created_by_id(self, opensearch_with_collector):
+        """Tests task search by not created by id."""
+        created_by_adviser = AdviserFactory()
+        not_created_by_adviser = AdviserFactory()
+
+        task = TaskFactory(
+            created_by=created_by_adviser,
+        )
+        TaskFactory(
+            created_by=not_created_by_adviser,
+        )
+
+        opensearch_with_collector.flush_and_refresh()
+
+        url = reverse('api-v4:search:task')
+
+        response = self.api_client.post(
+            url,
+            data={
+                'not_created_by': not_created_by_adviser.id,
+            },
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+        assert response.data['results'][0]['id'] == str(task.id)
+        assert response.data['results'][0]['created_by']['id'] == str(created_by_adviser.id)
+
+    def test_search_task_not_created_by_id_works_in_combination_with_other_parameters(
+        self,
+        opensearch_with_collector,
+    ):
+        """
+        Tests task not created by id filter works in combination with other parameters.
+        We're testing this with an adviser.
+        """
+        created_by_adviser = AdviserFactory()
+        not_created_by_adviser = AdviserFactory()
+        adviser1 = AdviserFactory()
+
+        task = TaskFactory(
+            created_by=created_by_adviser,
+            advisers=[
+                adviser1,
+            ],
+        )
+        TaskFactory(
+            created_by=not_created_by_adviser,
+            advisers=[
+                adviser1,
+            ],
+        )
+        TaskFactory(
+            created_by=not_created_by_adviser,
+        )
+        TaskFactory()
+
+        opensearch_with_collector.flush_and_refresh()
+
+        url = reverse('api-v4:search:task')
+
+        response = self.api_client.post(
+            url,
+            data={
+                'not_created_by': not_created_by_adviser.id,
+                'advisers': [adviser1.id],
+            },
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+        assert response.data['results'][0]['id'] == str(task.id)
+
+    @mock.patch('datahub.search.task.views.SearchTaskAPIView.deep_get')
+    def test_search_task_without_filters(
+        self,
+        deep_get,
+        opensearch_with_collector,
+    ):
+        """
+        Tests edge case where no filter returned from raw_query
+        """
+        deep_get.return_value = None
+        TaskFactory()
+
+        opensearch_with_collector.flush_and_refresh()
+
+        url = reverse('api-v4:search:task')
+
+        response = self.api_client.post(
+            url,
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        assert response.data['count'] == 1
+
+    @mock.patch('datahub.search.task.views.SearchTaskAPIView.deep_get')
+    def test_search_task_without_filter_index(
+        self,
+        deep_get,
+        opensearch_with_collector,
+    ):
+        """
+        Test edge case where no filter_index found in raw_query filter
+        """
+        not_created_by_adviser = AdviserFactory()
+        deep_get.return_value = [{'result': 'testing'}]
+        TaskFactory()
+
+        opensearch_with_collector.flush_and_refresh()
+
+        url = reverse('api-v4:search:task')
+
+        response = self.api_client.post(
+            url,
+            data={
+                'not_created_by': not_created_by_adviser.id,
+            },
+        )
+
+        assert response.status_code == status.HTTP_200_OK
+
+        assert response.data['count'] == 1
 
     def test_search_task_due_date_ordering(self, opensearch_with_collector):
         """Tests task search ordering on due date"""
