c10e71d1942db231c14bfcff1cf7cc3a7df1f8e1
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2023-12-08 09:14:48 +0000

    Feature/cls2 587 task filter created by (#5062)
    * WIP: task not created by
    
    * WIP: Initial open search query
    
    * WIP: Trying to access to Bool of the filter
    
    * WIP: Todo merge into single bool object
    
    * Not created by filter
    
    * Add tests
    
    * Fix test
    
    * Test code coverage
    
    * Optimise by only run deep_get when required

--- datahub/search/task/views.py
+++ datahub/search/task/views.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 from datahub.search.task import TaskSearchApp
 from datahub.search.task.serializers import (
     SearchTaskQuerySerializer,
@@ -14,7 +16,7 @@ class SearchTaskAPIViewMixin:
     search_app = TaskSearchApp
     serializer_class = SearchTaskQuerySerializer
     es_sort_by_remappings = {}
-    fields_to_exclude = ()
+    fields_to_exclude = ('not_created_by',)
 
     FILTER_FIELDS = (
         'archived',
@@ -34,9 +36,59 @@ class SearchTaskAPIViewMixin:
         'investment_project': 'investment_project.id',
     }
 
-    COMPOSITE_FILTERS = {}
-
 
 @register_v4_view()
 class SearchTaskAPIView(SearchTaskAPIViewMixin, SearchAPIView):
-    pass
+    """Filtered company search view."""
+
+    def deep_get(self, dictionary, keys, default=None):
+        """
+        Perform a deep search on a dictionary to find the item at the location provided in the keys
+        """
+        return reduce(
+            lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+            keys.split('|'),
+            dictionary,
+        )
+
+    def get_base_query(self, request, validated_data):
+        """
+        Check for filters that filter for not_* parameters and extend the open search base query
+        with the must_not filters.
+        """
+        must_not = []
+        base_query = super().get_base_query(request, validated_data)
+
+        if request.data.get('not_created_by'):
+            must_not.append(
+                {
+                    'match': {
+                        'created_by.id': {
+                            'operator': 'and',
+                            'query': request.data['not_created_by'],
+                        },
+                    },
+                },
+            )
+
+        if len(must_not) > 0:
+            raw_query = base_query.to_dict()
+            filters = self.deep_get(raw_query, 'query|bool|filter')
+            if not filters:
+                return base_query
+
+            filter_index = None
+            for index, filter in enumerate(filters):
+                if filter.get('bool') or filter.get('bool') == {}:
+                    filter_index = index
+                    break
+
+            if filter_index is None:
+                return base_query
+
+            filters[filter_index]['bool']['must_not'] = must_not
+            raw_query['query']['bool']['filter'] = filters
+
+            base_query.update_from_dict(raw_query)
+
+        return base_query
