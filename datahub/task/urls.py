7e0ed74e8a0eb4223610d7ca9c13248040c46ac8
Derek Baker <derek.baker89@gmail.com>
2023-12-20 12:36:23 +0000

    WIP: initial commit

--- datahub/task/urls.py
+++ datahub/task/urls.py
@@ -20,8 +20,18 @@ Task_v4_collection = TaskV4ViewSet.as_view(
 
 task_archive = TaskV4ViewSet.as_action_view('archive')
 
+task_v4_tasks_companies_and_projects = TaskV4ViewSet.as_view(
+    {
+        'get': 'list',
+    },
+)
+
 urls_v4 = [
     path('task', Task_v4_collection, name='collection'),
     path('task/<uuid:pk>', Task_v4_item, name='item'),
     path('task/<uuid:pk>/archive', task_archive, name='task_archive'),
+    path(
+        'task/companies-and-projects',
+        task_v4_tasks_companies_and_projects,
+        name='task_companies_and_projects',)
 ]
