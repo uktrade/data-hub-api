92c8ff98b79c64570eb59cfb54fd0e3b8ef8a2a1
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2023-12-21 15:06:04 +0000

    feature/rr-1262-function-for-token-generation (#5094)
    * Create function to generate token for customer response
    
    * Add a test for a case where more than a token already exist
    
    * Code refactor and remove has_unexpired_token_for_contact function
    
    * Summarise comments in the beginning part of each function

--- datahub/export_win/tasks.py
+++ datahub/export_win/tasks.py
@@ -1,4 +1,8 @@
+from datetime import datetime, timedelta
+
 from django.conf import settings
+
+from datahub.export_win.models import CustomerResponseToken
 
 
 def get_all_fields_for_client_email_receipt(token, customer_response):
@@ -14,3 +18,20 @@ def get_all_fields_for_client_email_receipt(token, customer_response):
     }
 
     return details
+
+
+def create_token_for_contact(contact, customer_response):
+    """
+    Generate new token and set all existing unexpired token to expire
+    """
+    CustomerResponseToken.objects.filter(
+        company_contact=contact,
+        customer_response=customer_response,
+        expires_on__gte=datetime.utcnow()).update(expires_on=datetime.utcnow())
+    expires_on = datetime.utcnow() + timedelta(days=7)
+    new_token = CustomerResponseToken.objects.create(
+        expires_on=expires_on,
+        company_contact=contact,
+        customer_response=customer_response,
+    )
+    return new_token
