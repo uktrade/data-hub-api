b570f776bf2704b9b4bedf0aaad806541a31ef7b
Bau Nguyen <bauvannguyen123@gmail.com>
2024-01-15 11:32:27 +0000

    update export owner
    Add command to update owner of export project
    
    Update update_company_export_adviser.py
    
    Update update_company_export_adviser.py
    
    Update update_company_export_adviser.py
    
    Update update_company_export_adviser.py

---
+++ datahub/dbmaintenance/management/commands/update_company_export_adviser.py
@@ -0,0 +1,27 @@
+import reversion
+
+from datahub.company.models.export import CompanyExport
+from datahub.dbmaintenance.management.base import CSVBaseCommand
+from datahub.dbmaintenance.utils import parse_limited_string, parse_uuid
+
+
+class Command(CSVBaseCommand):
+    """Process one single row."""
+
+    def _process_row(self, row, simulate=False, **options):
+        pk = parse_uuid(row['company_id'])
+        company_export = CompanyExport.objects.get(pk=pk)
+        old_adviser = parse_limited_string(row['old_adviser_id'])
+        new_adviser = parse_limited_string(row['new_adviser_id'])
+
+        if company_export.owner != old_adviser:
+            return
+
+        company_export.owner = new_adviser
+
+        if simulate:
+            return
+
+        with reversion.create_revision():
+            company_export.save(update_fields=('owner',))
+            reversion.set_comment('Company export owner updated.')
