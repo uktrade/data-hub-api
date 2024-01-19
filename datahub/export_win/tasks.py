1dac8db545e782d6217d93bca8b2e87a767eb720
E De Los Reyes <28296624+dredmonds@users.noreply.github.com>
2024-01-19 09:53:25 +0000

    Refactor export win task test (#5135)
    * Update client email receipt success test
    
    * Update lead officer  email receipt success test
    
    * Update lead officer email receipt rejected success test

--- datahub/export_win/tasks.py
+++ datahub/export_win/tasks.py
@@ -44,7 +44,7 @@ def get_all_fields_for_client_email_receipt(token, customer_response):
     win_token = token.company_contact
     details = {
         'customer_email': win_token.email,
-        'country_destination': win.country,
+        'country_destination': win.country.name,
         'client_firstname': win_token.first_name,
         'lead_officer_name': win.lead_officer.name,
         'goods_services': win.goods_vs_services.name,
@@ -59,7 +59,7 @@ def get_all_fields_for_lead_officer_email_receipt_no(token, customer_response):
     win_token = token.company_contact
     details = {
         'lead_officer_email': win.lead_officer.email,
-        'country_destination': win.country,
+        'country_destination': win.country.name,
         'client_fullname': win_token.first_name + ' ' + win_token.last_name,
         'lead_officer_first_name': win.lead_officer.first_name,
         'goods_services': win.goods_vs_services.name,
@@ -72,17 +72,17 @@ def get_all_fields_for_lead_officer_email_receipt_no(token, customer_response):
 
 def get_all_fields_for_lead_officer_email_receipt_yes(token, customer_response):
     win = customer_response.win
-    win_token = token.company_contact
+    company_contact = token.company_contact
     total_export_win_value = Breakdown.objects.filter(win=win).aggregate(
         Sum('value'))['value__sum'] or 0
     details = {
         'lead_officer_email': win.lead_officer.email,
-        'country_destination': win.country,
-        'client_fullname': win_token.first_name + ' ' + win_token.last_name,
+        'country_destination': win.country.name,
+        'client_fullname': company_contact.name,
         'lead_officer_first_name': win.lead_officer.first_name,
         'total_export_win_value': total_export_win_value,
         'goods_services': win.goods_vs_services.name,
-        'client_company_name': win_token.company.name,
+        'client_company_name': company_contact.company.name,
         'url': settings.EXPORT_WIN_LEAD_OFFICER_REVIEW_WIN_URL.format(uuid=win.id),
     }
 
