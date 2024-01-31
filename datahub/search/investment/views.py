53fdd996ebf5750f53645495f479f53326a49508
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-01-31 11:56:33 +0000

    Limit results to current adviser in team when adviser_in_team filter is set.
    Co-authored-by: Claudia Gonzalez-Casales <ClaudiaGC1339@users.noreply.github.com>

--- datahub/search/investment/views.py
+++ datahub/search/investment/views.py
@@ -1,4 +1,5 @@
 import datetime
+from functools import reduce
 
 from django.db.models import Case, Max, When
 from django.db.models import CharField
@@ -40,6 +41,7 @@ class SearchInvestmentProjectAPIViewMixin:
     es_sort_by_remappings = {
         'name': 'name.keyword',
     }
+    fields_to_exclude = ('adviser_in_team')
 
     FILTER_FIELDS = (
         'adviser',
@@ -207,7 +209,9 @@ class SearchInvestmentProjectAPIView(SearchInvestmentProjectAPIViewMixin, Search
     """Filtered investment project search view."""
 
     def get_base_query(self, request, validated_data):
-        """Add aggregations to show the number of projects at each stage."""
+        """
+        Add aggregations to show the number of projects at each stage.
+        """
         investor_company_ids = validated_data.get('investor_company')
         if investor_company_ids:
             related_company_ids = get_datahub_ids_for_dnb_service_company_hierarchy(
@@ -222,7 +226,89 @@ class SearchInvestmentProjectAPIView(SearchInvestmentProjectAPIViewMixin, Search
         base_query = super().get_base_query(request, validated_data)
         if validated_data.get('show_summary'):
             base_query.aggs.bucket('stage', 'terms', field='stage.id')
+
+        if request.data.get('adviser_in_team'):
+            must = self.must_limit_query_to_current_adviser_in_team(request)
+            if len(must) > 0:
+                base_query.update_from_dict(
+                    self.add_must_and_must_not_to_filters(
+                        base_query,
+                        must,
+                        [],
+                    ),
+                )
+
         return base_query
+
+    def must_limit_query_to_current_adviser_in_team(self, request):
+        """
+        Create filter that limits results to investment projects that have the current adviser in
+        its team.
+        """
+        must = []
+        if str(request.user.id):
+            must = {'bool': {'minimum_should_match': 1, 'should': []}}
+            relationships = [
+                'client_relationship_manager',
+                'one_list_group_global_account_manager',
+                'project_assurance_adviser',
+                'project_manager',
+                'team_members',
+            ]
+            for relationship in relationships:
+                must['bool']['should'].append(
+                    {
+                        'match': {
+                            f'{relationship}.id': {
+                                'operator': 'or',
+                                'query': str(request.user.id),
+                            },
+                        },
+                    },
+                )
+        return must
+
+    # TODO MK: If code below hasn't changed refactor to generic (==search/task/views.py)
+    def deep_get(self, dictionary, keys, default=None):
+        """
+        Perform a deep search on a dictionary to find the item at the location provided in the keys
+        """
+        return reduce(
+            lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+            keys.split('|'),
+            dictionary,
+        )
+
+    # TODO MK: If code below hasn't changed refactor to generic (==search/task/views.py)
+    def add_must_and_must_not_to_filters(self, base_query, must, must_not):
+        """
+        Merge the must and must not filters into single query.
+        """
+        raw_query = base_query.to_dict()
+        filters = self.deep_get(raw_query, 'query|bool|filter')
+        if not filters:
+            return raw_query
+
+        filter_index = None
+        for index, filter in enumerate(filters):
+            if filter.get('bool') or filter.get('bool') == {}:
+                filter_index = index
+                break
+
+        if filter_index is None:
+            return raw_query
+
+        if len(must_not) > 0:
+            filters[filter_index]['bool']['must_not'] = must_not
+        if len(must) > 0:
+            if 'must' not in filters[filter_index]['bool']:
+                filters[filter_index]['bool']['must'] = []
+
+            filters[filter_index]['bool']['must'].append(must)
+
+        raw_query['query']['bool']['filter'] = filters
+
+        return raw_query
 
     def get_sibling_company_ids(self, investor_companies):
         """Get a list of all sibling company id's"""
