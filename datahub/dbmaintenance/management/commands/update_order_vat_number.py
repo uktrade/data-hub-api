055c95dc1da1cf64192940ee0a1ef732c7f7fb9e
Bau Nguyen <bauvannguyen123@gmail.com>
2024-02-05 11:57:24 +0000

    Remove logger and amend typo

---
+++ datahub/dbmaintenance/management/commands/update_order_vat_number.py
@@ -0,0 +1,26 @@
+from logging import getLogger
+
+import reversion
+
+from datahub.dbmaintenance.management.base import CSVBaseCommand
+from datahub.dbmaintenance.utils import parse_limited_string, parse_uuid
+from datahub.omis.order.models import Order
+
+
+class Command(CSVBaseCommand):
+    """Command to update Order VAT number."""
+
+    def _process_row(self, row, simulate=False, **options):
+        """Process one single row."""
+        pk = parse_uuid(row['id'])
+        order = Order.objects.get(pk=pk)
+        newVatNumber = parse_limited_string(row['new_vat_number'])
+
+        order.vat_number = newVatNumber
+
+        if simulate:
+            return
+
+        with reversion.create_revision():
+            order.save(update_fields=('vat_number',))
+            reversion.set_comment('Order VAT Number updated.')
