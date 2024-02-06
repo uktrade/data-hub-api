fb948cc0c79b68ce763098ca57428cd5ce7457c4
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2024-02-06 12:17:48 +0000

    create end point for resending win with tests

--- datahub/export_win/urls.py
+++ datahub/export_win/urls.py
@@ -15,6 +15,10 @@ win_item = WinViewSet.as_view({
     'patch': 'partial_update',
 })
 
+resend_export_win = WinViewSet.as_action_view(
+    'resend_export_win',
+)
+
 customer_response = CustomerResponseViewSet.as_view({
     'get': 'retrieve',
     'patch': 'partial_update',
@@ -24,4 +28,5 @@ urls = [
     path('export-win', win_collection, name='collection'),
     path('export-win/<uuid:pk>', win_item, name='item'),
     path('export-win/review/<uuid:token_pk>', customer_response, name='customer-response'),
+    path('export-win/<uuid:pk>/resend-win', resend_export_win, name='win-resend'),
 ]
