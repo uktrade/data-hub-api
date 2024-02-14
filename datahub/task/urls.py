077aa8d33043ad5331f347665b8c46a48238df3c
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-02-14 16:31:13 +0000

    Create status_complete and status_active api calls

--- datahub/task/urls.py
+++ datahub/task/urls.py
@@ -22,11 +22,15 @@ Task_v4_collection = TaskV4ViewSet.as_view(
 )
 
 task_archive = TaskV4ViewSet.as_action_view('archive')
+task_status_complete = TaskV4ViewSet.as_action_view('status_complete')
+task_status_active = TaskV4ViewSet.as_action_view('status_active')
 
 urls_v4 = [
     path('task', Task_v4_collection, name='collection'),
     path('task/<uuid:pk>', Task_v4_item, name='item'),
     path('task/<uuid:pk>/archive', task_archive, name='task_archive'),
+    path('task/<uuid:pk>/status_complete', task_status_complete, name='task_status_complete'),
+    path('task/<uuid:pk>/status_active', task_status_complete, name='task_status_active'),
     path(
         'task/companies-and-projects',
         get_tasks_companies_and_projects,
