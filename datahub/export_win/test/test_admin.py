70ab3cab7e34cd2646e583b06f1116642df61394
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2024-03-13 19:50:27 +0000

    Add more unit tests

--- datahub/export_win/test/test_admin.py
+++ datahub/export_win/test/test_admin.py
@@ -1,10 +1,37 @@
 import pytest
+from django.contrib.admin.sites import site as admin_site
 from django.test import RequestFactory
 
 from datahub.company.test.factories import AdviserFactory
-from datahub.export_win.admin import DeletedWinAdmin
-from datahub.export_win.models import DeletedWin
+from datahub.export_win.admin import DeletedWinAdmin, WinAdmin
+from datahub.export_win.models import DeletedWin, Win
 from datahub.export_win.test.factories import DeletedWinFactory
+
+
+@pytest.mark.django_db
+def test_get_actions_removes_delete_selected():
+    user = AdviserFactory()
+    admin = WinAdmin(Win, admin_site)
+    request = RequestFactory().get('/')
+    request.user = user
+    actions = admin.get_actions(request)
+    assert 'delete_selected' not in actions
+
+
+@pytest.mark.django_db
+def test_soft_delete():
+    user = AdviserFactory()
+    win1 = DeletedWinFactory()
+    win2 = DeletedWinFactory()
+    admin = WinAdmin(Win, None)
+    request = RequestFactory().get('/')
+    request.user = user
+    queryset = Win.objects.filter(pk__in=[win1.pk, win2.pk])
+    admin.soft_delete(request, queryset)
+    win1.refresh_from_db()
+    win2.refresh_from_db()
+    assert win1.is_deleted
+    assert win2.is_deleted
 
 
 @pytest.mark.django_db
