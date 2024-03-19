3fb1e573e935d6de1afec60f571b0560365ef805
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2024-03-19 08:40:59 +0000

    Fix and extend test coverage

--- datahub/ping/test/test_ping_view.py
+++ datahub/ping/test/test_ping_view.py
@@ -1,15 +1,62 @@
 
+from unittest.mock import patch
+
 import pytest
+from django.db import DatabaseError
+
 from rest_framework import status
 from rest_framework.reverse import reverse
 
 pytestmark = pytest.mark.django_db
 
 
+class MockWorker:
+    """
+    Mock queue names for worker
+    """
+
+    queue_name = ''
+
+    def __init__(self, queue_name, *args, **kwargs):
+        self.queue_name = queue_name
+
+    def queue_names(self):
+        return self.queue_name
+
+
 def test_all_good(client):
     """Test all good."""
     url = reverse('ping')
+    with patch(
+        'datahub.core.queues.health_check.Worker.all',
+        return_value=[MockWorker(['short-running']), MockWorker(['long-running'])],
+    ):
+
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_200_OK
+        assert '<status>OK</status>' in str(response.content)
+        assert response.headers['content-type'] == 'text/xml'
+
+
+def test_check_database_fail(client):
+    url = reverse('ping')
+    with patch(
+        'datahub.ping.services.Company.objects.all',
+        side_effect=DatabaseError('No database'),
+    ):
+        response = client.get(url)
+
+        assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+        assert '<status>FALSE</status>' in str(response.content)
+        assert response.headers['content-type'] == 'text/xml'
+
+
+def test_check_rq_workers_fail(client):
+    url = reverse('ping')
     response = client.get(url)
-    assert response.status_code == status.HTTP_200_OK
-    assert '<status>OK</status>' in str(response.content)
+    assert response.status_code == status.HTTP_500_INTERNAL_SERVER_ERROR
+    assert '<status>FALSE</status>' in str(response.content)
+    assert "<!--RQ queue(s) not running: {\\\'long-running\\\', \\\'short-running\\\'}-->"\
+        in str(response.content)
     assert response.headers['content-type'] == 'text/xml'
