2a0c2a709c04f58a7b5f323a9dad2e8a911f802e
Chris Hopkins <chris.hopkins@madetech.com>
2024-03-27 14:49:47 +0000

    added more test assertions

--- datahub/dataset/export_wins/test/test_utils.py
+++ datahub/dataset/export_wins/test/test_utils.py
@@ -3,9 +3,10 @@ from datahub.dataset.export_wins.utils import use_nulls_on_empty_string_fields
 
 class TestUtils():
     def test_enrich_data_replaces_empty_strings_with_null_on_expected_column(self):
-        win = {'audit': '', 'ignore_empty_string': ''}
+        win = {'audit': '', 'ignore_empty_string': '', 'populated_value': 'A'}
 
         use_nulls_on_empty_string_fields(win)
 
         assert win['audit'] is None
         assert win['ignore_empty_string'] == ''
+        assert win['populated_value'] == 'A'
