73fc837d32a377c36257ce801b5ff802690275db
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2024-04-03 15:01:24 +0100

    Remove company and company contact from readonly as it is handled by Pawel's PR for different setting

--- datahub/export_win/admin.py
+++ datahub/export_win/admin.py
@@ -159,8 +159,6 @@ class WinAdmin(BaseModelAdminMixin, VersionAdmin):
         'id',
         'created_on',
         'modified_on',
-        'company',
-        'company_contacts',
     )
     search_fields = (
         'id',
