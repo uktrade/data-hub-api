accf68750a9d7bcc42e600df027bb7caa5ad3471
Chris Hopkins <chris.hopkins@madetech.com>
2024-04-05 11:45:19 +0100

    more flake8

--- datahub/export_win/models.py
+++ datahub/export_win/models.py
@@ -421,11 +421,11 @@ class Win(BaseModel):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return 'Export win {}: {} <{}> - {}'.format(
-            self.pk,
-            self.adviser,
-            self.adviser.email,
-            self.created_on.strftime('%Y-%m-%d %H:%M:%S') if self.created_on else '',
+        return 'Export win {win_id}: {adviser} <{adviser_email}> - {created}'.format(
+            win_id=self.pk,
+            adviser=self.adviser,
+            adviser_email=self.adviser.email,
+            created=self.created_on.strftime('%Y-%m-%d %H:%M:%S') if self.created_on else '',
         )
 
 
