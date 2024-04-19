7f5d97111b8678a86af0d3fed5f817a8d7928d87
Christopher <christopher.sunkel@digital.trade.gov.uk>
2024-04-19 17:12:07 +0100

    Add command to setup paid order

--- setup-uat.sh
+++ setup-uat.sh
@@ -58,6 +58,7 @@ python /app/manage.py add_access_token --skip-checks --hours 24 --token lepStaff
 python /app/manage.py loaddata --ignorenonexistent /app/fixtures/test_data.yaml
 python /app/manage.py add_quote_to_order
 python /app/manage.py add_invoice_to_order
+python /app/manage.py set_order_as_paid
 python /app/manage.py createinitialrevisions
 python /app/manage.py collectstatic --noinput
 DEBUG=False python app.py
