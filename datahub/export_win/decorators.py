6b8cdd1bcbf51d0d87f342246b6568952461fc33
ince-dbt <148454056+ince-dbt@users.noreply.github.com>
2024-05-21 16:51:40 +0100

    Improve the code tag check in the decorator

--- datahub/export_win/decorators.py
+++ datahub/export_win/decorators.py
@@ -6,35 +6,39 @@ from rest_framework import status
 from rest_framework.response import Response
 
 
+DISALLOWED_TAGS = ['script', 'style', 'iframe', 'embed', 'object', 'form']
+DISALLOWED_SYMBOLS = ['lt', 'gt', 'amp', 'quot', 'apos']
+DISALLOWED_CHARACTERS = ['<', '>']
+
+
 class TagChecker(HTMLParser):
     def __init__(self):
         super().__init__()
         self.contains_disallowed_content = False
-        self.disallowed_tags = ['script', 'style', 'iframe', 'embed', 'object', 'form']
-        self.disallowed_symbols = ['lt', 'gt', 'amp', 'quot', 'apos']
-        self.disallowed_characters = ['<', '>', '\\']
 
     def handle_starttag(self, tag, attrs):
-        if tag.lower() in self.disallowed_tags:
+        if tag.lower() in DISALLOWED_TAGS:
             self.contains_disallowed_content = True
 
     def handle_endtag(self, tag):
-        if tag.lower() in self.disallowed_tags:
+        if tag.lower() in DISALLOWED_TAGS:
             self.contains_disallowed_content = True
 
     def handle_data(self, data):
-        if any(char in data for char in self.disallowed_characters):
+        if any(char in data for char in DISALLOWED_CHARACTERS):
             self.contains_disallowed_content = True
 
     def handle_entityref(self, name):
-        if name.lower() in self.disallowed_symbols:
+        if name.lower() in DISALLOWED_SYMBOLS:
             self.contains_disallowed_content = True
 
     def handle_charref(self, name):
         self.contains_disallowed_content = True
 
 
-def contains_script_or_html_tags(data):
+def contains_disallowed_content(data):
+    if any(char in data for char in DISALLOWED_CHARACTERS):
+        return True
     tag_checker = TagChecker()
     tag_checker.feed(data)
     return tag_checker.contains_disallowed_content
@@ -43,8 +47,8 @@ def contains_script_or_html_tags(data):
 def validate_script_and_html_tags(view_method):
     @functools.wraps(view_method)
     def wrapper(self, request, *args, **kwargs):
-        data_str = json.dumps(request.data)  # Assuming request.data is already a dictionary
-        if contains_script_or_html_tags(data_str):
+        data_str = request.data if isinstance(request.data, str) else json.dumps(request.data)
+        if contains_disallowed_content(data_str):
             return Response({'error': 'Input contains disallowed HTML or script tags or symbols'},
                             status=status.HTTP_400_BAD_REQUEST)
         return view_method(self, request, *args, **kwargs)
