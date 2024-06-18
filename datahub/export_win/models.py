bf20d62c038e6ef4ff8720594694a648a8fe764e
Edmond De Los Reyes <edmond.delosreyes@digital.trade.gov.uk>
2024-06-18 13:51:40 +0100

    Added adviser field for customer response token
    Co-Authored-By: elcct <pawel.szymanski@digital.trade.gov.uk>

--- datahub/export_win/models.py
+++ datahub/export_win/models.py
@@ -757,6 +757,13 @@ class CustomerResponseToken(models.Model):
         on_delete=models.CASCADE)
     times_used = models.PositiveIntegerField(default=0)
     created_on = models.DateTimeField(db_index=True, null=True, blank=True, auto_now_add=True)
+    adviser = models.ForeignKey(
+        Advisor,
+        related_name='admin_tokens',
+        on_delete=models.PROTECT,
+        null=True,
+        blank=True,
+    )
 
     def __str__(self):
         return f'Token: {self.id} ({self.expires_on})'
