94af02bb714817a9ca2ee5221ae32d332aa5e826
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-07-01 16:16:12 +0100

    Sort name order, add hash and single string output

--- datahub/metadata/management/commands/verify_metadata.py
+++ datahub/metadata/management/commands/verify_metadata.py
@@ -1,4 +1,6 @@
+import hashlib
 from logging import getLogger
+from operator import itemgetter
 
 from django.apps import apps
 from django.core.management.base import BaseCommand
@@ -21,10 +23,26 @@ class Command(BaseCommand):
 
         metadata_models = list(apps.get_app_config('metadata').get_models())
 
-        metadata_model_counts = []
-        for model in metadata_models:
-            metadata_model_counts.append([model.__name__, model.objects.count()])
+        metadata_models_and_counts = [
+            [model.__name__, model.objects.count()] for model in metadata_models
+        ]
+
+        sorted_models_and_counts = sorted(metadata_models_and_counts, key=itemgetter(0))
 
         headers = ['Model', 'Count']
 
-        logger.info(f'\n{tabulate(metadata_model_counts, headers, tablefmt="github")}')
+        logger.info(
+            f'\n{tabulate(sorted_models_and_counts, headers, tablefmt="github")}'
+        )
+
+        sorted_models_and_counts = ','.join(
+            ':'.join(map(str, model_and_count))
+            for model_and_count in sorted_models_and_counts
+        )
+        logger.info(sorted_models_and_counts)
+
+        hash_sah256 = hashlib.sha256(
+            sorted_models_and_counts.encode('utf-8')
+        ).hexdigest()
+
+        logger.info(f"Hash value: {hash_sah256}")
