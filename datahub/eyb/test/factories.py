2ede04b7786444a366fdd9fa8f9a397eef86febb
Sophie Wenban <s.maclennan@hotmail.co.uk>
2024-08-16 16:45:44 +0100

    Make the EYB Lead Factory more dynamic

--- datahub/eyb/test/factories.py
+++ datahub/eyb/test/factories.py
@@ -1,11 +1,15 @@
+import random
 import factory
+
+from faker import Faker
+fake = Faker()
 
 
 class EYBLeadFactory(factory.django.DjangoModelFactory):
     """EYB Lead factory."""
 
     # EYB Triage data
-    triage_id = 96
+    triage_id = random.randint(1, 5000)
     triage_hashed_uuid = 'b85dabe2a4c46828424d61ec99f496d91952f9e53733898ff5f0fee89f08b635'
     triage_created = '2023-09-21 07:53:19.534794+00'
     triage_modified = '2023-09-27 11:32:28.853014+00'
@@ -26,20 +30,20 @@ class EYBLeadFactory(factory.django.DjangoModelFactory):
     is_high_value = True
 
     # EYB User data
-    user_id = 90
+    user_id = random.randint(1, 5000)
     user_hashed_uuid = 'b85dabe2a4c46828424d61ec99f496d91952f9e53733898ff5f0fee89f08b635'
     user_created = '2023-09-21 07:53:19.47271+00'
     user_modified = '2023-09-22 08:53:19.472723+00'
-    company_name = 'Stu co'
+    company_name = factory.Faker('company')
     company_location = 'FR'
-    full_name = 'John Doe'
-    role = 'Director'
-    email = 'foo@bar.com'
-    telephone_number = '447923454678'
+    full_name = fake.name()
+    role = factory.Faker('job')
+    email = factory.Sequence(lambda n: f'foo-{n}@bar.com')
+    telephone_number = factory.Faker('phone_number')
     agree_terms = True
     agree_info_email = False
     landing_timeframe = 'SIX_TO_TWELVE_MONTHS'
-    company_website = 'http://www.google.com'
+    company_website = factory.Sequence(lambda n: f'foo{n}.bar.com')
 
     class Meta:
         model = 'eyb.EYBLead'
