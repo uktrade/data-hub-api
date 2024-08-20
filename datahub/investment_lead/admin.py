cb9867510baf6c0e194d28b8bdf0cd75d923da14
Sophie Wenban <74198488+swenban@users.noreply.github.com>
2024-08-20 13:30:13 +0100

    CLS2-897 Create app and models for EYB Leads (#5597)
    * Create eyb app including EYBLead model
    
    * Fix flake8 errors
    
    * Create a factory that generates an EYB Lead
    
    * Fix flake8 errors
    
    * Fix integer and datetime fields in EYB model
    
    * Remove unused import
    
    * Test that the data for an instance created using the EYBLead factory is as intended
    
    Co-authored-by: Oliver Roberts <oliverjwroberts@gmail.com>
    
    * Fix flake8 errors
    
    * Fix additional flake8 errors
    
    * Display a human readable name for EYBLead objects in admin
    
    * WIP Refactor tests
    
    * Refactor and separate out the EYB model tests
    
    * Fix flake8 errors
    
    * Make the EYB Lead Factory more dynamic
    
    * Rename the eyb app to investment_lead
    
    * Create the InvestmentLead abstract model
    
    * Fix flake8 import formatting error
    
    * Update EYBLead factory
    
    * Improve consistency of data types as flagged by code-review-doctor
    
    * Fix flake8 double quotes error
    
    * Test that the triage_uuid and user_uuid are the same value
    
    ---------

---
+++ datahub/investment_lead/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from datahub.investment_lead.models import EYBLead
+
+
+class EYBLeadAdmin(admin.ModelAdmin):
+    list_display = (
+        # EYB Triage data
+        'triage_id',
+        'triage_hashed_uuid',
+        'triage_created',
+        'triage_modified',
+        'sector',
+        'sector_sub',
+        'intent',
+        'intent_other',
+        'location',
+        'location',
+        'location_none',
+        'hiring',
+        'spend',
+        'spend_other',
+        'is_high_value',
+
+        # EYB User data
+        'user_id',
+        'user_hashed_uuid',
+        'user_created',
+        'user_modified',
+        'company_name',
+        'company_location',
+        'full_name',
+        'role',
+        'email',
+        'telephone_number',
+        'agree_terms',
+        'agree_info_email',
+        'landing_timeframe',
+        'company_website',
+    )
+
+
+admin.site.register(EYBLead, EYBLeadAdmin)
