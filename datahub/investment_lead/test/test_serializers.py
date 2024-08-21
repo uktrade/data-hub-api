f5398bd8ebbf33bc02990e216754b9218889bf17
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-08-21 15:58:15 +0100

    EYBLead serializer tests

---
+++ datahub/investment_lead/test/test_serializers.py
@@ -0,0 +1,17 @@
+import pytest
+
+from datahub.investment_lead.models import EYBLead
+from datahub.investment_lead.serializers import EYBLeadSerializer
+from datahub.investment_lead.test.utils import verify_eyb_lead_data
+
+
+@pytest.mark.django_db
+@pytest.mark.usefixtures("eyb_lead_data", "eyb_lead_db")
+class TestEYBLeadSerializer:
+    """Tests for EYBLeadSerializer."""
+
+    def test_eyb_lead_serializer(self, eyb_lead_data, eyb_lead_db):
+        eyb_object = EYBLead.objects.first()
+        eyb_object_serialized = EYBLeadSerializer(eyb_object)
+
+        verify_eyb_lead_data(eyb_object, eyb_object_serialized.data)
