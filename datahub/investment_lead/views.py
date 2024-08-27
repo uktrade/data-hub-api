631fd475388473c98911ac56166c2ef509654bf8
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-08-27 14:42:03 +0100

    Improve EYB viewset to ensure no duplicates are created; tests

--- datahub/investment_lead/views.py
+++ datahub/investment_lead/views.py
@@ -1,5 +1,8 @@
 import logging
 
+from rest_framework import serializers, status
+from rest_framework.response import Response
+
 from config.settings.types import HawkScope
 from datahub.core.hawk_receiver import (
     HawkAuthentication,
@@ -26,3 +29,29 @@ class EYBLeadViewset(
     authentication_classes = (HawkAuthentication, )
     permission_classes = (HawkScopePermission, )
     required_hawk_scope = HawkScope.data_flow_api
+
+    def create(self, request):
+        eyb_lead_serializer = self.serializer_class(data=request.data)
+        try:
+            eyb_lead_serializer.is_valid(raise_exception=True)
+        except serializers.ValidationError:
+            message = 'EYB data failed DH serializer validation'
+            extra_data = {
+                'eyb_lead_serializer_errors': eyb_lead_serializer.errors,
+            }
+            logger.error(message, extra=extra_data)
+            raise
+
+        # Create or update to prevent duplicates
+        eyb_lead_data = eyb_lead_serializer.validated_data
+        eyb_lead, created = EYBLead.objects.update_or_create(
+            triage_hashed_uuid=eyb_lead_data.get('triage_hashed_uuid'),
+            user_hashed_uuid=eyb_lead_data.get('user_hashed_uuid'),
+            defaults=eyb_lead_data)
+
+        status_code = status.HTTP_201_CREATED if created else status.HTTP_200_OK
+
+        return Response(
+            eyb_lead_serializer.to_representation(eyb_lead),
+            status=status_code,
+        )
