8f4856ac8bd7d54e9fb12a413ebadc4a60c7ad0d
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-08-27 11:13:12 +0100

    Refactor to use Hawk for authentication

--- datahub/investment_lead/test/test_views.py
+++ datahub/investment_lead/test/test_views.py
@@ -1,3 +1,4 @@
+import pytest
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -10,25 +11,25 @@ class TestEYBLeadCreateAPI(APITestMixin):
     EYB Lead Create view test case.
     """
 
-    def test_get_not_allowed(self, eyb_lead_data):
+    @pytest.mark.parametrize('method', ('delete', 'patch', 'get', 'put'))
+    def test_methods_not_allowed(self, eyb_lead_data, data_flow_api_client, method):
         """
         Should return 405
         """
         post_url = reverse('api-v4:investment-lead:create')
-        response = self.api_client.get(post_url)
-
+        response = data_flow_api_client.request(method, post_url)
         assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
-    def test_post_with_no_payload(self):
+    def test_post_with_no_payload(self, data_flow_api_client):
         """
         Test that we get an Exception when no payload is sent
         """
         post_url = reverse('api-v4:investment-lead:create')
-        response = self.api_client.post(post_url, data={})
+        response = data_flow_api_client.post(post_url, json_={})
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
-    def test_post_with_incomplete_payload(self, eyb_lead_data):
+    def test_post_with_incomplete_payload(self, eyb_lead_data, data_flow_api_client):
         """
         Test that we get an Exception when incomplete payload is sent
         """
@@ -36,22 +37,22 @@ class TestEYBLeadCreateAPI(APITestMixin):
         incomplete_data['location'] = None
 
         post_url = reverse('api-v4:investment-lead:create')
-        response = self.api_client.post(
-            post_url, data=incomplete_data,
+        response = data_flow_api_client.post(
+            post_url, json_=incomplete_data,
         )
 
         assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data['location'][0] == 'This field may not be null.'
 
-    def test_post_success(self, eyb_lead_data):
+    def test_post_success(self, eyb_lead_data, data_flow_api_client):
         """
         Test successful POST to EYB
         """
         current_count = EYBLead.objects.count()
 
         post_url = reverse('api-v4:investment-lead:create')
-        response = self.api_client.post(
-            post_url, data=eyb_lead_data,
+        response = data_flow_api_client.post(
+            post_url, json_=eyb_lead_data,
         )
 
         assert response.status_code == status.HTTP_201_CREATED
--- datahub/investment_lead/views.py
+++ datahub/investment_lead/views.py
@@ -1,17 +1,28 @@
 import logging
 
-from rest_framework.permissions import IsAuthenticated
-
+from config.settings.types import HawkScope
+from datahub.core.hawk_receiver import (
+    HawkAuthentication,
+    HawkResponseSigningMixin,
+    HawkScopePermission,
+)
 from datahub.core.mixins import ArchivableViewSetMixin
 from datahub.core.viewsets import SoftDeleteCoreViewSet
 from datahub.investment_lead.models import EYBLead
 from datahub.investment_lead.serializers import EYBLeadSerializer
 
+
 logger = logging.getLogger(__name__)
 
 
-class EYBLeadViewset(ArchivableViewSetMixin, SoftDeleteCoreViewSet):
+class EYBLeadViewset(
+    ArchivableViewSetMixin,
+    SoftDeleteCoreViewSet,
+    HawkResponseSigningMixin,
+):
     serializer_class = EYBLeadSerializer
     queryset = EYBLead.objects.all()
 
-    permission_classes = (IsAuthenticated,)
+    authentication_classes = (HawkAuthentication, )
+    permission_classes = (HawkScopePermission, )
+    required_hawk_scope = HawkScope.data_flow_api
