8fa89364972f8c340b064727582ae37a927f89d3
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-08-27 11:13:12 +0100

    Refactor to use Hawk for authentication

--- datahub/investment_lead/test/conftest.py
+++ datahub/investment_lead/test/conftest.py
@@ -7,6 +7,16 @@ from datahub.investment_lead.test.factories import EYBLeadFactory
 
 
 @pytest.fixture
+def data_flow_api_client(hawk_api_client):
+    """Hawk API client fixture configured to use credentials with the data_flow_api scope."""
+    hawk_api_client.set_credentials(
+        'data-flow-api-id',
+        'data-flow-api-key',
+    )
+    yield hawk_api_client
+
+
+@pytest.fixture
 def eyb_lead_data():
     return {
         # EYB Triage data
