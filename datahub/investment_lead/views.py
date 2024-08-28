c36bee815caef29156b687efd26da3871055a51d
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-08-28 17:21:09 +0100

    Modify view to process a list of one or more leads, instead of a single instance

--- datahub/investment_lead/views.py
+++ datahub/investment_lead/views.py
@@ -1,6 +1,6 @@
 import logging
 
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.response import Response
 
 from config.settings.types import HawkScope
@@ -31,27 +31,68 @@ class EYBLeadViewset(
     required_hawk_scope = HawkScope.data_flow_api
 
     def create(self, request):
-        eyb_lead_serializer = self.serializer_class(data=request.data)
-        try:
-            eyb_lead_serializer.is_valid(raise_exception=True)
-        except serializers.ValidationError:
-            message = 'EYB data failed DH serializer validation'
-            extra_data = {
-                'eyb_lead_serializer_errors': eyb_lead_serializer.errors,
-            }
-            logger.error(message, extra=extra_data)
-            raise
+        """POST route definition.
 
-        # Create or update to prevent duplicates
-        eyb_lead_data = eyb_lead_serializer.validated_data
-        eyb_lead, created = EYBLead.objects.update_or_create(
-            triage_hashed_uuid=eyb_lead_data.get('triage_hashed_uuid'),
-            user_hashed_uuid=eyb_lead_data.get('user_hashed_uuid'),
-            defaults=eyb_lead_data)
+        This route needs to handle both creating new and updating existing instances
+        because data is received automatically from a Data Flow pipeline.
 
-        status_code = status.HTTP_201_CREATED if created else status.HTTP_200_OK
+        To reduce complexity of the pipeline, it was decided to have the separation
+        logic within the API endpoint.
+        """
+        if not isinstance(request.data, list):
+            return Response(
+                {'error': 'Expected a list of leads.'},
+                status.HTTP_400_BAD_REQUEST,
+            )
 
-        return Response(
-            eyb_lead_serializer.to_representation(eyb_lead),
-            status=status_code,
+        created_leads = []
+        updated_leads = []
+        errors = []
+
+        for index, lead_data in enumerate(request.data):
+            serializer = self.get_serializer(data=lead_data)
+            if serializer.is_valid():
+                _, created = self.perform_create(serializer)
+                if created:
+                    created_leads.append(serializer.validated_data)
+                else:
+                    updated_leads.append(serializer.validated_data)
+            else:
+                errors.append({'index': index, 'errors': serializer.errors})
+
+        response_data = {}
+        if created_leads:
+            response_data['created'] = created_leads
+        if updated_leads:
+            response_data['updated'] = updated_leads
+        if errors:
+            response_data['errors'] = errors
+
+        if not errors:
+            # All leads have been created
+            status_code = status.HTTP_201_CREATED
+        elif len(errors) == len(request.data):
+            # No leads have been created
+            status_code = status.HTTP_400_BAD_REQUEST
+        else:
+            # Some leads have been created, others failed
+            status_code = status.HTTP_207_MULTI_STATUS
+
+        return Response(response_data, status_code)
+
+    def perform_create(self, serializer):
+        """Processes a single lead.
+
+        The `triage_hashed_uuid` and `user_hashed_uuid` field should be unique for each EYBLead
+        instance. As a result, we use these to determine if a new instance is created, or an
+        existing one updated.
+
+        This overwrites the inherited BaseViewSet method.
+        """
+        validated_lead_data = serializer.validated_data
+        lead_instance, created = EYBLead.objects.update_or_create(
+            triage_hashed_uuid=validated_lead_data.get('triage_hashed_uuid'),
+            user_hashed_uuid=validated_lead_data.get('user_hashed_uuid'),
+            defaults=validated_lead_data,
         )
+        return lead_instance, created
