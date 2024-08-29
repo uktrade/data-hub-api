8498e2de5e056a1c763f98cbec930c6d7eb488b0
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-08-29 09:20:17 +0100

    Modify tests to accommodate new list-of-instances structure

--- datahub/investment_lead/test/test_serializers.py
+++ datahub/investment_lead/test/test_serializers.py
@@ -2,7 +2,6 @@ import pytest
 
 from rest_framework import serializers
 
-from datahub.investment_lead.models import EYBLead
 from datahub.investment_lead.serializers import (
     EYBLeadSerializer,
     UUIDS_ERROR_MESSAGE,
@@ -15,21 +14,18 @@ from datahub.investment_lead.test.utils import verify_eyb_lead_data
 class TestEYBLeadSerializer:
     """Tests for EYBLeadSerializer."""
 
-    def test_eyb_lead_serializer_raises_if_uuids_different(self, eyb_lead_data, eyb_lead_db):
+    def test_eyb_lead_serializer_raises_if_uuids_different(self, eyb_lead_data):
         invalid_eyb_data = eyb_lead_data.copy()
         invalid_eyb_data['triage_hashed_uuid'] = '123abc'
         invalid_eyb_data['user_hashed_uuid'] = '456def'
         eyb_serializer = EYBLeadSerializer(data=invalid_eyb_data)
 
-        with pytest.raises(serializers.ValidationError) as serial_error:
+        with pytest.raises(serializers.ValidationError):
             eyb_serializer.is_valid(raise_exception=True)
 
-        # Can this be done more elegantly?
-        error_message = str(serial_error.value.detail['non_field_errors'][0])
-        assert error_message == UUIDS_ERROR_MESSAGE
+        assert UUIDS_ERROR_MESSAGE in eyb_serializer.errors['non_field_errors']
 
-    def test_eyb_lead_serializer_as_expected(self, eyb_lead_data, eyb_lead_db):
-        eyb_object = EYBLead.objects.first()
-        eyb_object_serialized = EYBLeadSerializer(eyb_object)
-
-        verify_eyb_lead_data(eyb_object, eyb_object_serialized.data)
+    def test_eyb_lead_serializer_as_expected(self, eyb_lead_db):
+        db_instance = eyb_lead_db
+        serialized_instance = EYBLeadSerializer(eyb_lead_db)
+        verify_eyb_lead_data(db_instance, serialized_instance.data)
