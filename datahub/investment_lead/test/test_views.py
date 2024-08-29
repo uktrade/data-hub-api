f34642b125cacf634012e13a1438e3f9be3a8d65
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-08-29 09:20:17 +0100

    Modify tests to accommodate new list-of-instances structure

--- datahub/investment_lead/test/test_views.py
+++ datahub/investment_lead/test/test_views.py
@@ -4,89 +4,66 @@ from rest_framework.reverse import reverse
 
 from datahub.core.test_utils import APITestMixin
 from datahub.investment_lead.models import EYBLead
+from datahub.investment_lead.test.factories import EYBLeadFactory
+
+
+EYB_CREATE_URL = reverse('api-v4:investment-lead:eyb-create')
 
 
 class TestEYBLeadCreateAPI(APITestMixin):
-    """
-    EYB Lead Create view test case.
-    """
+    """EYB Lead Create view test case."""
 
     @pytest.mark.parametrize('method', ('delete', 'patch', 'get', 'put'))
-    def test_methods_not_allowed(self, eyb_lead_data, data_flow_api_client, method):
-        """
-        Should return 405
-        """
-        post_url = reverse('api-v4:investment-lead:create')
-        response = data_flow_api_client.request(method, post_url)
+    def test_methods_not_allowed(self, data_flow_api_client, method):
+        """Tests that requests using unsupported methods return status code 405."""
+        response = data_flow_api_client.request(method, EYB_CREATE_URL)
         assert response.status_code == status.HTTP_405_METHOD_NOT_ALLOWED
 
     def test_post_with_no_payload(self, data_flow_api_client):
-        """
-        Test that we get an Exception when no payload is sent
-        """
-        post_url = reverse('api-v4:investment-lead:create')
-        response = data_flow_api_client.post(post_url, json_={})
-
+        """Tests if an error is raised when no payload is sent."""
+        response = data_flow_api_client.post(EYB_CREATE_URL, json_={})
         assert response.status_code == status.HTTP_400_BAD_REQUEST
 
     def test_post_with_incomplete_payload(self, eyb_lead_data, data_flow_api_client):
-        """
-        Test that we get an Exception when incomplete payload is sent
-        """
+        """Tests if an error is raised when an incomplete payload is sent."""
         incomplete_data = eyb_lead_data.copy()
-        incomplete_data['location'] = None
-
-        post_url = reverse('api-v4:investment-lead:create')
+        incomplete_data.pop('location', None)
         response = data_flow_api_client.post(
-            post_url, json_=incomplete_data,
+            EYB_CREATE_URL, json_=[incomplete_data],
         )
-
         assert response.status_code == status.HTTP_400_BAD_REQUEST
-        assert response.data['location'][0] == 'This field may not be null.'
+
+    def test_post_with_invalid_values_in_payload(self, eyb_lead_data, data_flow_api_client):
+        """Tests if an error is raised when a payload with invalid values is sent."""
+        invalid_data = eyb_lead_data.copy()
+        invalid_data['location'] = None
+        response = data_flow_api_client.post(
+            EYB_CREATE_URL, json_=[invalid_data],
+        )
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert 'location' in response.data['errors'][0]['errors'].keys()
 
     def test_post_success(self, eyb_lead_data, data_flow_api_client):
-        """
-        Test successful POST to EYB
-        """
+        """Tests successful POST creates an EYB lead."""
         current_count = EYBLead.objects.count()
-
-        post_url = reverse('api-v4:investment-lead:create')
         response = data_flow_api_client.post(
-            post_url, json_=eyb_lead_data,
+            EYB_CREATE_URL, json_=[eyb_lead_data],
         )
-
         assert response.status_code == status.HTTP_201_CREATED
         assert EYBLead.objects.count() == current_count + 1
 
     def test_post_does_not_create_duplicates(self, eyb_lead_data, data_flow_api_client):
-        """
-        Test successful POST to EYB does not create duplicates
-        """
-        current_count = EYBLead.objects.count()
-        current_data = eyb_lead_data.copy()
-        post_url = reverse('api-v4:investment-lead:create')
+        """Tests successful POST does not create duplicate EYB leads."""
+        eyb_lead = EYBLeadFactory(**eyb_lead_data)
+        assert EYBLead.objects.count() == 1
 
-        # Create the first object
+        modified_eyb_lead_data = eyb_lead_data.copy()
+        modified_eyb_lead_data['location'] = 'different_location'
         response = data_flow_api_client.post(
-            post_url, json_=current_data,
+            EYB_CREATE_URL, json_=[modified_eyb_lead_data],
         )
 
         assert response.status_code == status.HTTP_201_CREATED
-        assert EYBLead.objects.count() == current_count + 1
-
-        current_obj = EYBLead.objects.first()
-        assert current_obj.location == current_data['location']
-
-        # Change the data, create the "second object"
-        current_data['location'] = 'different_location'
-        response = data_flow_api_client.post(
-            post_url, json_=current_data,
-        )
-
-        # Assert that no new object has been created but the data
-        # has succesfully been updated
-        assert response.status_code == status.HTTP_200_OK
-        assert EYBLead.objects.count() == current_count + 1
-
-        current_obj = EYBLead.objects.first()
-        assert current_obj.location == current_data['location']
+        assert EYBLead.objects.count() == 1
+        eyb_lead.refresh_from_db()
+        assert eyb_lead.location == modified_eyb_lead_data['location']
