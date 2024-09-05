094b7e8e0d6de84bb60fe47ec1d379cec8f77f0c
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-09-05 15:27:22 +0100

    Add fields for MVP to EYBLead model, factory, and admin page

--- datahub/investment_lead/test/factories.py
+++ datahub/investment_lead/test/factories.py
@@ -1,50 +1,70 @@
+
+import hashlib
 import random
+import uuid
 
 import factory
+from django.utils import timezone
 
-from faker import Faker
-fake = Faker()
+from datahub.company.test.factories import CompanyFactory
+from datahub.core import constants
+from datahub.investment_lead.models import EYBLead
+from datahub.metadata.test.factories import SectorFactory
+
+
+factory.Faker.override_default_locale('en_GB')
+
+
+def generate_hashed_uuid():
+    new_uuid = uuid.uuid4()
+    hashed_uuid = hashlib.sha256(new_uuid.bytes).hexdigest()
+    return hashed_uuid
 
 
 class EYBLeadFactory(factory.django.DjangoModelFactory):
     """EYB Lead factory."""
 
-    # EYB Triage data
-    triage_id = random.randint(1, 5000)
-    triage_hashed_uuid = 'b85dabe2a4c46828424d61ec99f496d91952f9e53733898ff5f0fee89f08b635'
-    triage_created = '2023-09-21 07:53:19.534794+00'
-    triage_modified = '2023-09-27 11:32:28.853014+00'
-    sector = 'FOOD_AND_DRINK'
-    sector_sub = 'PROCESSING_AND_PRESERVING_OF_MEAT'
-    intent = [
-        'SET_UP_NEW_PREMISES',
-        'SET_UP_A_NEW_DISTRIBUTION_CENTRE',
-        'ONWARD_SALES_AND_EXPORTS_FROM_THE_UK',
-    ]
-    intent_other = ''
-    location = 'NORTHERN_IRELAND'
-    location_city = 'ARMAGH_CITY'
-    location_none = False
-    hiring = '51-100'
-    spend = '1000000-2000000'
-    spend_other = '1234565432'
-    is_high_value = True
-
-    # EYB User data
-    user_id = random.randint(1, 5000)
-    user_hashed_uuid = 'b85dabe2a4c46828424d61ec99f496d91952f9e53733898ff5f0fee89f08b635'
-    user_created = '2023-09-21 07:53:19.47271+00'
-    user_modified = '2023-09-22 08:53:19.472723+00'
-    company_name = factory.Faker('company')
-    company_location = 'FR'
-    full_name = fake.name()
-    role = factory.Faker('job')
-    email = 'foo@bar.com'
-    telephone_number = factory.Faker('phone_number')
-    agree_terms = True
-    agree_info_email = False
-    landing_timeframe = 'SIX_TO_TWELVE_MONTHS'
-    company_website = f'{company_name}.com'
-
     class Meta:
         model = 'investment_lead.EYBLead'
+
+    # EYB triage fields
+    triage_hashed_uuid = factory.LazyFunction(generate_hashed_uuid)
+    triage_created = factory.LazyFunction(timezone.now)
+    triage_modified = factory.LazyFunction(timezone.now)
+    sector = factory.SubFactory(SectorFactory)
+    sector_sub = factory.LazyAttribute(lambda o: f'{o.sector.segment}')
+    intent = random.choices(EYBLead.IntentChoices.values, k=random.randint(1, 6))
+    intent_other = ''
+    location_id = constants.UKRegion.wales.value.id
+    location_city = 'Cardiff'
+    location_none = False
+    hiring = random.choice(EYBLead.HiringChoices.values)
+    spend = random.choice(EYBLead.SpendChoices.values)
+    spend_other = ''
+    is_high_value = factory.Faker('pybool')
+
+    # EYB user fields
+    user_hashed_uuid = factory.LazyFunction(generate_hashed_uuid)
+    user_created = factory.LazyFunction(timezone.now)
+    user_modified = factory.LazyFunction(timezone.now)
+    company_name = factory.Faker('company')
+    company_location_id = constants.Country.canada.value.id
+    full_name = factory.Faker('name')
+    role = factory.Faker('job')
+    email = factory.Faker('email')
+    telephone_number = factory.Faker('phone_number')
+    agree_terms = factory.Faker('pybool')
+    agree_info_email = factory.Faker('pybool')
+    landing_timeframe = random.choice(EYBLead.LandingTimeframeChoices.values)
+    company_website = factory.Faker('url')
+
+    # Company fields
+    duns_number = factory.Faker('numerify', text='00#######')
+    address_1 = factory.Faker('street_address')
+    address_2 = factory.Faker('secondary_address')
+    address_town = factory.Faker('city')
+    address_county = factory.Faker('county')
+    address_area_id = constants.AdministrativeArea.alberta.value.id
+    address_country_id = constants.Country.canada.value.id
+    address_postcode = factory.Faker('postcode')
+    company = factory.SubFactory(CompanyFactory)
--- datahub/investment_lead/test/utils.py
+++ datahub/investment_lead/test/utils.py
@@ -14,18 +14,33 @@ def assert_datetimes(first, second):
     assert _to_iso(first) == _to_iso(second)
 
 
-def verify_eyb_lead_data(instance: EYBLead, data: dict):
-    """Method to verify the EYBLead data against the instance created."""
-    # EYB Triage data
-    assert instance.triage_id == data['triage_id']
+def verify_eyb_lead_data(instance: EYBLead, data: dict, is_factory_data: bool = False):
+    """Method to verify the EYBLead data against the instance created.
+
+    Use:
+    - `is_factory_data=True` if passing in factory data which contains actual model
+    instances for related fields
+    - `is_factory_data=False` if passing in POST data that contains strings for
+    these fields.
+
+    Related fields are:
+    - Sector
+    - Location
+    - Company location
+    - Address area
+    - Address country
+    """
+    # EYB triage fields
     assert instance.triage_hashed_uuid == data['triage_hashed_uuid']
     assert_datetimes(instance.triage_created, data['triage_created'])
     assert_datetimes(instance.triage_modified, data['triage_modified'])
-    assert instance.sector == data['sector']
+    assert data['sector'] == instance.sector \
+        if is_factory_data else instance.sector.segment
     assert instance.sector_sub == data['sector_sub']
     assert instance.intent == data['intent']
     assert instance.intent_other == data['intent_other']
-    assert instance.location == data['location']
+    assert data['location'] == instance.location \
+        if is_factory_data else instance.location.name
     assert instance.location_city == data['location_city']
     assert instance.location_none == data['location_none']
     assert instance.hiring == data['hiring']
@@ -33,13 +48,13 @@ def verify_eyb_lead_data(instance: EYBLead, data: dict):
     assert instance.spend_other == data['spend_other']
     assert instance.is_high_value == data['is_high_value']
 
-    # EYB User data
-    assert instance.user_id == data['user_id']
+    # EYB user fields
     assert instance.user_hashed_uuid == data['user_hashed_uuid']
     assert_datetimes(instance.user_created, data['user_created'])
     assert_datetimes(instance.user_modified, data['user_modified'])
     assert instance.company_name == data['company_name']
-    assert instance.company_location == data['company_location']
+    assert data['company_location'] == instance.company_location \
+        if is_factory_data else instance.company_location.iso_alpha2_code
     assert instance.full_name == data['full_name']
     assert instance.role == data['role']
     assert instance.email == data['email']
@@ -47,4 +62,15 @@ def verify_eyb_lead_data(instance: EYBLead, data: dict):
     assert instance.agree_terms == data['agree_terms']
     assert instance.agree_info_email == data['agree_info_email']
     assert instance.landing_timeframe == data['landing_timeframe']
-    assert instance.company_website == data['company_website']
+
+    # Company fields
+    assert instance.duns_number == data['duns_number']
+    assert instance.address_1 == data['address_1']
+    assert instance.address_2 == data['address_2']
+    assert instance.address_town == data['address_town']
+    assert instance.address_county == data['address_county']
+    assert data['address_area'] == instance.address_area \
+        if is_factory_data else instance.address_area.name
+    assert data['address_country'] == instance.address_country \
+        if is_factory_data else instance.address_country.iso_alpha2_code
+    assert instance.address_postcode == data['address_postcode']
