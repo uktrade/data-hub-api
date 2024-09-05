fcf0b3c92ab34b0de10997bc2db8058a4b958e30
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-09-05 15:27:22 +0100

    Add fields for MVP to EYBLead model, factory, and admin page

--- datahub/investment_lead/admin.py
+++ datahub/investment_lead/admin.py
@@ -4,40 +4,85 @@ from datahub.investment_lead.models import EYBLead
 
 
 class EYBLeadAdmin(admin.ModelAdmin):
-    list_display = (
-        # EYB Triage data
-        'triage_id',
-        'triage_hashed_uuid',
-        'triage_created',
-        'triage_modified',
-        'sector',
-        'sector_sub',
-        'intent',
-        'intent_other',
-        'location',
-        'location',
-        'location_none',
-        'hiring',
-        'spend',
-        'spend_other',
-        'is_high_value',
-
-        # EYB User data
-        'user_id',
-        'user_hashed_uuid',
-        'user_created',
-        'user_modified',
-        'company_name',
-        'company_location',
-        'full_name',
-        'role',
-        'email',
-        'telephone_number',
-        'agree_terms',
-        'agree_info_email',
-        'landing_timeframe',
-        'company_website',
-    )
+    list_display = [
+        'id',
+        'created_on',
+        'modified_on',
+        'company',
+    ]
+    readonly_fields = [
+        'id',
+        'created_on',
+        'modified_on',
+    ]
+    fieldsets = [
+        (
+            None,
+            {
+                'fields': [
+                    'id',
+                    'created_on',
+                    'modified_on',
+                ],
+            },
+        ),
+        (
+            'Triage Information',
+            {
+                'fields': [
+                    'triage_hashed_uuid',
+                    'triage_created',
+                    'triage_modified',
+                    'sector',
+                    'sector_sub',
+                    'intent',
+                    'intent_other',
+                    'location',
+                    'location_none',
+                    'hiring',
+                    'spend',
+                    'spend_other',
+                    'is_high_value',
+                ],
+            },
+        ),
+        (
+            'User Information',
+            {
+                'fields': [
+                    'user_hashed_uuid',
+                    'user_created',
+                    'user_modified',
+                    'company_name',
+                    'company_location',
+                    'full_name',
+                    'role',
+                    'email',
+                    'telephone_number',
+                    'agree_terms',
+                    'agree_info_email',
+                    'landing_timeframe',
+                    'company_website',
+                ],
+            },
+        ),
+        (
+            'Company Information',
+            {
+                'fields': [
+                    'company',
+                    'duns_number',
+                    'address_1',
+                    'address_2',
+                    'address_town',
+                    'address_county',
+                    'address_area',
+                    'address_country',
+                    'address_postcode',
+                ],
+            },
+        ),
+    ]
 
 
 admin.site.register(EYBLead, EYBLeadAdmin)
