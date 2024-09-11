03d3229f2b2d37564d5c2503c99847ebfab760d4
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-09-11 03:52:35 +0100

    Add new model for companies and their activities and add tasks to populate the model

---
+++ datahub/company_activity/tasks.py
@@ -0,0 +1,99 @@
+import logging
+
+from datahub.interaction.models import Interaction
+from datahub.company_activity.models import CompanyActivity
+from datahub.company_referral.models import CompanyReferral
+from datahub.core.queues.constants import HALF_DAY_IN_SECONDS
+from datahub.core.queues.job_scheduler import job_scheduler
+from datahub.core.queues.scheduler import LONG_RUNNING_QUEUE
+
+
+logger = logging.getLogger(__name__)
+
+
+def schedule_sync_interactions_to_company_activity():
+    """
+    Schedules a task to relate all `Interaction`s to `CompanyActivity`s
+    """
+    job = job_scheduler(
+        queue_name=LONG_RUNNING_QUEUE,
+        function=relate_company_activity_to_interactions,
+        job_timeout=HALF_DAY_IN_SECONDS,
+        max_retries=5,
+        retry_backoff=True,
+    )
+    logger.info(
+        f'Task {job.id} schedule_sync_interactions_to_company_activity ' f'scheduled.',
+    )
+    return job
+
+
+def relate_company_activity_to_interactions():
+    """
+    Grabs all interactions so they can be related to in the
+    `CompanyActivity` model with bulk_create. Excludes any
+    interactions already associated in the CompanyActivity model.
+    """
+    activity_interactions = CompanyActivity.objects.filter(
+        interaction__isnull=False
+    ).values_list('interaction_id', flat=True)
+
+    interactions = Interaction.objects.exclude(
+        id__in=activity_interactions
+    ).values('id', 'date', 'company_id')
+
+    objs = [
+        CompanyActivity(
+            interaction_id=interaction['id'],
+            date=interaction['date'],
+            company_id=interaction['company_id'],
+            activity_source=CompanyActivity.ActivitySource.interaction,
+        )
+        for interaction in interactions
+    ]
+    logger.info('bulk_creating all interactions into CompanyActivity.')
+    CompanyActivity.objects.bulk_create(objs=objs, batch_size=500)
+
+
+def schedule_sync_referrals_to_company_activity():
+    """
+    Schedules a task to relate all `CompanyReferral`s to `CompanyActivity`s
+    """
+    job = job_scheduler(
+        queue_name=LONG_RUNNING_QUEUE,
+        function=relate_company_activity_to_referrals,
+        job_timeout=HALF_DAY_IN_SECONDS,
+        max_retries=5,
+        retry_backoff=True,
+    )
+    logger.info(
+        f'Task {job.id} schedule_sync_referrals_to_company_activity ' f'scheduled.',
+    )
+    return job
+
+
+def relate_company_activity_to_referrals():
+    """
+    Grabs all referrals so they can be related to in the
+    `CompanyActivity` model with a bulk_create. Excludes any
+    referrals already associated in the CompanyActivity model.
+    """
+    activity_referral = CompanyActivity.objects.filter(
+        referral__isnull=False
+    ).values_list('referral_id', flat=True)
+
+    referrals = CompanyReferral.objects.exclude(
+        id__in=activity_referral
+    ).values('id', 'created_on', 'company_id')
+
+    objs = [
+        CompanyActivity(
+            referral_id=referral['id'],
+            date=referral['created_on'],
+            company_id=referral['company_id'],
+            activity_source=CompanyActivity.ActivitySource.referral,
+        )
+        for referral in referrals
+    ]
+    logger.info('bulk_creating all referrals into CompanyActivity.')
+    CompanyActivity.objects.bulk_create(objs=objs, batch_size=500)
