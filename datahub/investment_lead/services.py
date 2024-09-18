3fb9d9cb7ad5b4621bd8845e36ee23218c94d2f6
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-09-18 15:32:56 +0100

    Add service that matches an eyb_lead.duns_number to a company.duns_number

--- datahub/investment_lead/services.py
+++ datahub/investment_lead/services.py
@@ -1,6 +1,25 @@
 import logging
 
+from datahub.company.models.company import Company
+
 logger = logging.getLogger(__name__)
+
+
+def match_by_duns_number(duns_number):
+    """ Uses an EYB lead provided DnB number
+    to search Data Hub for existing Companies
+
+    Args:
+        duns_number (string): a DnB number
+    """
+    companies = Company.objects.filter(duns_number=duns_number)
+
+    if len(companies) == 1:
+        # match found
+        return True, companies[0]
+
+    # TODO: what to do if len > 1?
+    return False, None
 
 
 def process_eyb_lead(eyb_lead):
@@ -9,3 +28,4 @@ def process_eyb_lead(eyb_lead):
     Args:
         eyb_lead (_type_): _description_
     """
+    return
