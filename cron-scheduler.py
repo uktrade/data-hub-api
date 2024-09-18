6bc645b2eedb152edc4622bc644e427ba47437cf
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-09-18 15:14:40 +0100

    Unschedule job to refresh GVA for FDI investment projects (#5646)
    The reason for this is because an issue has been identified where the job creates an infinite loop, flooding the RQ, blocking other jobs, and in some cases, causing the app to crash.
    
    This is a temporary action and will be reverted once a fix has been implemented.

--- cron-scheduler.py
+++ cron-scheduler.py
@@ -27,7 +27,7 @@ from datahub.core.queues.constants import (
     EVERY_TEN_MINUTES,
     EVERY_TEN_PM,
     EVERY_THREE_AM,
-    EVERY_THREE_AM_ON_TWENTY_EIGHTH_EACH_MONTH,
+    # EVERY_THREE_AM_ON_TWENTY_EIGHTH_EACH_MONTH,
     EVERY_TWO_AM,
     HALF_DAY_IN_SECONDS,
 )
@@ -42,9 +42,9 @@ from datahub.export_win.tasks import (
     update_notify_email_delivery_status_for_customer_response,
     update_notify_email_delivery_status_for_customer_response_token,
 )
-from datahub.investment.project.tasks import (
-    schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
-)
+# from datahub.investment.project.tasks import (
+#     schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
+# )
 from datahub.omis.payment.tasks import refresh_pending_payment_gateway_sessions
 from datahub.reminder.migration_tasks import run_ita_users_migration, run_post_users_migration
 from datahub.reminder.tasks import (
@@ -130,11 +130,12 @@ def schedule_jobs():
             description='schedule_generate_estimated_land_date_reminders',
         )
 
-    job_scheduler(
-        function=schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
-        cron=EVERY_THREE_AM_ON_TWENTY_EIGHTH_EACH_MONTH,
-        description='schedule_refresh_gross_value_added_value_for_fdi_investment_projects',
-    )
+    # TODO: uncomment this once the infinite loop issue (when refreshing GVA) has been fixed
+    # job_scheduler(
+    #     function=schedule_refresh_gross_value_added_value_for_fdi_investment_projects,
+    #     cron=EVERY_THREE_AM_ON_TWENTY_EIGHTH_EACH_MONTH,
+    #     description='schedule_refresh_gross_value_added_value_for_fdi_investment_projects',
+    # )
 
     if settings.ENABLE_ESTIMATED_LAND_DATE_REMINDERS_EMAIL_DELIVERY_STATUS:
         job_scheduler(
