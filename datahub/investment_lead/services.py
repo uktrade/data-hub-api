c9bd60bc25b2029e6dd8152b8073aa70e7182930
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-09-19 12:20:20 +0100

    CLS2-903: Link EYB record to existing company based on DUNs number (#5647)
    * Add service that matches an eyb_lead.duns_number to a company.duns_number
    
    * Attach company to EYBLead if found and correctly matching DnB number
    
    * Flake8
    
    * Remove useless comment
    
    * Update datahub/investment_lead/services.py
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- datahub/investment_lead/services.py
+++ datahub/investment_lead/services.py
@@ -1,11 +1,38 @@
 import logging
 
+from datahub.company.models.company import Company
+
 logger = logging.getLogger(__name__)
 
 
-def process_eyb_lead(eyb_lead):
-    """_summary_
+def match_by_duns_number(duns_number):
+    """Uses an EYB lead provided DnB number
+    to search Data Hub for existing Companies
 
     Args:
-        eyb_lead (_type_): _description_
+        duns_number (string): a DnB number
+    Returns:
+        found (boolean): true/false based on success of search
+        company (object): a Company object or None
     """
+    companies = Company.objects.filter(duns_number=duns_number)
+
+    if companies.count() == 1:
+        return True, companies[0]
+
+    # no match found
+    return False, None
+
+
+def process_eyb_lead(eyb_lead):
+    """Matches an EYB lead with an existing Company via DnB number
+
+    Args:
+        eyb_lead (object): an EYB lead object
+    """
+    if eyb_lead.duns_number is not None:
+        found, company = match_by_duns_number(eyb_lead.duns_number)
+
+        if found:
+            eyb_lead.company = company
+            eyb_lead.save()
