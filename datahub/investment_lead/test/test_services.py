b131baa5f9dd9865b6946dbb5c2f47e575505db9
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-09-19 12:20:20 +0100

    CLS2-903: Link EYB record to existing company based on DUNs number (#5647)
    * Add service that matches an eyb_lead.duns_number to a company.duns_number
    
    * Attach company to EYBLead if found and correctly matching DnB number
    
    * Flake8
    
    * Remove useless comment
    
    * Update datahub/investment_lead/services.py
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: code-review-doctor[bot] <72320148+code-review-doctor[bot]@users.noreply.github.com>

--- datahub/investment_lead/test/test_services.py
+++ datahub/investment_lead/test/test_services.py
@@ -1,7 +1,8 @@
 import pytest
 
 from datahub.company.models.company import Company
-from datahub.investment_lead.services import process_eyb_lead
+from datahub.company.test.factories import CompanyFactory
+from datahub.investment_lead.services import match_by_duns_number, process_eyb_lead
 from datahub.investment_lead.test.factories import EYBLeadFactory
 from datahub.investment_lead.test.utils import assert_company_matches_eyb_lead
 
@@ -9,6 +10,33 @@ from datahub.investment_lead.test.utils import assert_company_matches_eyb_lead
 @pytest.mark.django_db
 class TestEYBLeadServices:
     """Tests EYB Lead services"""
+
+    def test_duns_number_matches_existing_company(self):
+        company = CompanyFactory(duns_number='123456789')
+        eyb_lead = EYBLeadFactory(duns_number='123456789')
+
+        found, found_company = match_by_duns_number(eyb_lead.duns_number)
+
+        assert found is True
+        assert found_company.id == company.id
+
+    def test_duns_number_does_not_match_company(self):
+        CompanyFactory(duns_number='123456789')
+        eyb_lead = EYBLeadFactory(duns_number='123456788')
+
+        found, found_company = match_by_duns_number(eyb_lead.duns_number)
+
+        assert found is False
+        assert found_company is None
+
+    def test_attach_existing_company_from_eyb_lead(self):
+        company = CompanyFactory(duns_number='123456789')
+        eyb_lead = EYBLeadFactory(duns_number='123456789')
+
+        process_eyb_lead(eyb_lead)
+
+        assert eyb_lead.company is not None
+        assert eyb_lead.company == company
 
     def test_add_new_company_from_eyb_lead(self):
         eyb_lead = EYBLeadFactory(duns_number=None)
