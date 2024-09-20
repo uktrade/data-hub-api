b0687103312062e1c7ea5813fb7eda87706592bb
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-09-20 10:52:06 +0100

    Improve query for opensearch

--- datahub/search/company_activity/apps.py
+++ datahub/search/company_activity/apps.py
@@ -1,4 +1,7 @@
+from django.db.models import Prefetch
+
 from datahub.company_activity.models import CompanyActivity as DBCompanyActivity
+from datahub.interaction.models import InteractionDITParticipant
 from datahub.search.apps import SearchApp
 from datahub.search.company_activity.models import CompanyActivity
 
@@ -11,5 +14,16 @@ class CompanyActivitySearchApp(SearchApp):
     view_permissions = ('company_activity.view_companyactivity',)
     queryset = DBCompanyActivity.objects.select_related(
         'interaction',
+        'interaction__communication_channel',
+        'interaction__service',
         'referral',
+        'referral__contact',
+        'referral__created_by',
+        'referral__recipient',
+    ).prefetch_related(
+        'interaction__contacts',
+        Prefetch(
+            'interaction__dit_participants',
+            queryset=InteractionDITParticipant.objects.select_related('adviser', 'team'),
+        ),
     )
