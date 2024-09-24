b1951742619767eb03605a051919a30376297ad9
baarkerlounger <daniel.baark@digital.trade.gov.uk>
2024-09-24 15:38:48 +0100

    List S3 objects

--- datahub/company_activity/tests/test_tasks/test_ingestion_tasks.py
+++ datahub/company_activity/tests/test_tasks/test_ingestion_tasks.py
@@ -3,15 +3,39 @@ import sys
 
 from unittest.mock import patch
 
+import boto3
+import pytest
+
 from django.conf import settings
+from moto import mock_aws
+
 from redis import Redis
 from rq_scheduler import Scheduler
 
+from datahub.company_activity.tasks.ingest_company_activity import (
+    CompanyActivityIngestionTask, GREAT_PREFIX, REGION,
+)
 from datahub.core.queues.constants import EVERY_TEN_MINUTES
 
 
-@patch('os.system')
+@pytest.fixture
+def bucket_name():
+    return 'mock-bucket'
+
+
+@mock_aws
+def setup_s3_bucket(bucket_name):
+    mock_s3_client = boto3.client('s3', REGION)
+    mock_s3_client.create_bucket(
+        Bucket=bucket_name,
+        CreateBucketConfiguration={'LocationConstraint': REGION},
+    )
+    filename = GREAT_PREFIX + 'full_ingestion.jsonl.gz'
+    mock_s3_client.put_object(Bucket=bucket_name, Key=filename, Body='Test contents')
+
+
 class TestCompanyActivityIngestionTasks:
+    @patch('os.system')
     def test_company_activity_ingestion_task_schedule(self, mock_system):
         """
         Test that a task is scheduled to check for new Company Activity data
@@ -24,7 +48,8 @@ class TestCompanyActivityIngestionTasks:
         scheduler = Scheduler(queue, connection=Redis.from_url(settings.REDIS_BASE_URL))
         scheduled_jobs = scheduler.get_jobs()
         ingestion_task = (
-            'datahub.company_activity.tasks.ingest_company_activity.ingest_activity_data'
+            'datahub.company_activity.tasks.ingest_company_activity.'
+            'CompanyActivityIngestionTask.ingest_activity_data'
         )
         scheduled_job = [job for job in scheduled_jobs if job.func_name == ingestion_task][0]
         assert scheduled_job.func_name == ingestion_task
@@ -32,3 +57,14 @@ class TestCompanyActivityIngestionTasks:
 
         # Prevents the scheduler loop from running after tests finish by unloading the module again
         sys.modules.pop('cron-scheduler')
+
+    @mock_aws
+    def test_listing_great_data_files(self, bucket_name):
+        """
+        Test retrieval of the latest Great data file from S3
+        """
+        setup_s3_bucket(bucket_name)
+        task = CompanyActivityIngestionTask()
+        objects = task.list_objects(bucket_name, GREAT_PREFIX)
+        expected_file = GREAT_PREFIX + 'full_ingestion.jsonl.gz'
+        assert objects == [expected_file]
