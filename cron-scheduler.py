eb4d798c8810f1230ecb8fbf0e19bbb092a584a7
baarkerlounger <daniel.baark@digital.trade.gov.uk>
2024-09-24 15:38:48 +0100

    List S3 objects

--- cron-scheduler.py
+++ cron-scheduler.py
@@ -14,7 +14,7 @@ from datahub.company.tasks.adviser import schedule_automatic_adviser_deactivate
 from datahub.company.tasks.company import schedule_automatic_company_archive
 from datahub.company.tasks.contact import schedule_automatic_contact_archive
 from datahub.company.tasks.export_potential import update_company_export_potential_from_csv
-from datahub.company_activity.tasks.ingest_company_activity import ingest_activity_data
+from datahub.company_activity.tasks.ingest_company_activity import CompanyActivityIngestionTask
 from datahub.core.queues.constants import (
     EVERY_EIGHT_AM,
     EVERY_EIGHT_THIRTY_AM_ON_FIRST_EACH_MONTH,
@@ -124,7 +124,7 @@ def schedule_jobs():
         description='Update companies from dnb service',
     )
     job_scheduler(
-        function=ingest_activity_data,
+        function=CompanyActivityIngestionTask.ingest_activity_data,
         cron=EVERY_TEN_MINUTES,
         description='Check S3 for new Company Activity data files and schedule ingestion',
     )
--- datahub/company_activity/tasks/ingest_company_activity.py
+++ datahub/company_activity/tasks/ingest_company_activity.py
@@ -1,2 +1,25 @@
-def ingest_activity_data():
-    pass
+import boto3
+import environ
+
+env = environ.Env()
+REGION = env('AWS_DEFAULT_REGION')
+PREFIX = 'data-flow/exports/'
+GREAT_PREFIX = PREFIX + 'GreatGovUKFormsPipeline/'
+
+
+class CompanyActivityIngestionTask:
+    def __init__(self):
+        self.s3_client = boto3.client("s3", REGION)
+
+    def list_objects(self, bucket_name, prefix):
+        """Returns a list all objects with specified prefix."""
+        response = self.s3_client.list_objects(
+            Bucket=bucket_name,
+            Prefix=prefix,
+        )
+        return [object["Key"] for object in response["Contents"]]
+
+    def ingest_activity_data(self):
+        pass
+
+
