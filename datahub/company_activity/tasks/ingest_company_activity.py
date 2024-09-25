a7de57eaf8f6223b1671f7555f339c94bc6fa6ba
baarkerlounger <daniel.baark@digital.trade.gov.uk>
2024-09-25 13:10:26 +0100

    Enqueue new job to process new files

--- datahub/company_activity/tasks/ingest_company_activity.py
+++ datahub/company_activity/tasks/ingest_company_activity.py
@@ -1,6 +1,11 @@
 import boto3
 import environ
 
+from django.conf import settings
+
+from redis import Redis
+from rq import Queue
+
 from datahub.company_activity.models import IngestedFile
 
 env = environ.Env()
@@ -22,16 +27,28 @@ class CompanyActivityIngestionTask:
         )
         return [object['Key'] for object in response['Contents']]
 
-    def get_most_recent_obj(self, bucket_name, prefix):
+    def _get_most_recent_obj(self, bucket_name, prefix):
         """Returns the most recent file object in the given bucket/prefix"""
         files = self._list_objects(bucket_name, prefix)
         files.sort(reverse=True)
         return files[0]
 
-    def has_file_been_ingested(self, file):
+    def _has_file_been_ingested(self, file):
         previously_ingested = IngestedFile.objects.filter(filepath=file)
         return previously_ingested.count() > 0
 
     def ingest_activity_data(self):
-        # latest_great_file = self.get_most_recent_obj(BUCKET, GREAT_PREFIX)
-        pass
+        """
+        Gets the most recent file in the data-flow S3 bucekt for each
+        data source (prefix) and enqueues a job to process each file
+        that hasn't already been ingested
+        """
+        redis = Redis.from_url(settings.REDIS_BASE_URL)
+        rq_queue = Queue('long-running', connection=redis)
+        latest_file = self._get_most_recent_obj(BUCKET, GREAT_PREFIX)
+        if not self._has_file_been_ingested(latest_file):
+            rq_queue.enqueue(ingest_great_data, latest_file)
+
+
+def ingest_great_data(file):
+    pass
