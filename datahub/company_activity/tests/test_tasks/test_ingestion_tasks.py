5c520e4df0d9d84d5a6ef2df0a597d04c0630bac
baarkerlounger <daniel.baark@digital.trade.gov.uk>
2024-09-25 13:10:26 +0100

    Enqueue new job to process new files

--- datahub/company_activity/tests/test_tasks/test_ingestion_tasks.py
+++ datahub/company_activity/tests/test_tasks/test_ingestion_tasks.py
@@ -10,6 +10,7 @@ from django.conf import settings
 from moto import mock_aws
 
 from redis import Redis
+from rq import Queue
 from rq_scheduler import Scheduler
 
 from datahub.company_activity.models import IngestedFile
@@ -66,26 +67,27 @@ class TestCompanyActivityIngestionTasks:
         # Prevents the scheduler loop from running after tests finish by unloading the module again
         sys.modules.pop('cron-scheduler')
 
-    @mock_aws
-    def test_get_most_recent_obj(self, test_files):
-        """
-        Test retrieval of the latest Great data file from S3
-        """
-        setup_s3_bucket(BUCKET, test_files)
-        task = CompanyActivityIngestionTask()
-        most_recent = task.get_most_recent_obj(BUCKET, GREAT_PREFIX)
-        assert most_recent == GREAT_PREFIX + '20240920T000000/full_ingestion.jsonl.gz'
-
     @pytest.mark.django_db
-    def test_file_already_ingested(self):
+    @mock_aws
+    def test_ingestion_job_is_queued_for_new_files(self, test_files):
         """
-        Test checking whether the latest file has already been ingested
+        Test that when a new file is found a job is queued to ingest it
+        and no jobs are created for files that have already been ingested
         """
+        new_file = GREAT_PREFIX + '20240920T000000/full_ingestion.jsonl.gz'
+        setup_s3_bucket(BUCKET, test_files)
+        for file in test_files:
+            if not file == new_file:
+                IngestedFile.objects.create(filepath=file)
+
+        redis = Redis.from_url(settings.REDIS_BASE_URL)
+        rq_queue = Queue('long-running', connection=redis)
+        initial_job_count = len(rq_queue.jobs)
+
         task = CompanyActivityIngestionTask()
-        ingested_file = GREAT_PREFIX + '20240920T000000/full_ingestion.jsonl.gz'
-        not_ingested_file = GREAT_PREFIX + '20240921T000000/full_ingestion.jsonl.gz'
-        IngestedFile.objects.create(filepath=ingested_file)
-        result = task.has_file_been_ingested(ingested_file)
-        assert result
-        result = task.has_file_been_ingested(not_ingested_file)
-        assert not result
+        task.ingest_activity_data()
+        assert len(rq_queue.jobs) == initial_job_count + 1
+        job = rq_queue.jobs[-1]
+        fn = 'datahub.company_activity.tasks.ingest_company_activity.ingest_great_data'
+        assert job.func_name == fn
+        assert job.args[0] == new_file
