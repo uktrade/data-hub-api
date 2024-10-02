b1da66f9c9c0b94ce0604cbbf7a007465a803667
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-10-02 15:45:03 +0100

    Validate the ACTIVE stage (field becomes mandatory after ASSIGN PM, very confusing due to poorly named method and poorly commented method

--- datahub/investment/project/test/test_validate.py
+++ datahub/investment/project/test/test_validate.py
@@ -16,7 +16,7 @@ from datahub.investment.project.serializers import (
     VALUE_FIELDS,
 )
 from datahub.investment.project.test.factories import (
-    AssignPMInvestmentProjectFactory,
+    ActiveInvestmentProjectFactory,
     InvestmentProjectFactory,
 )
 from datahub.investment.project.validate import (
@@ -375,7 +375,7 @@ def test_validate_verify_win_instance_with_cond_fields():
 
 def test_likelihood_to_land_assign_pm_stage_missing_error():
     """Tests missing likelihood to land missing at assign pm stage."""
-    project = AssignPMInvestmentProjectFactory(
+    project = ActiveInvestmentProjectFactory(
         likelihood_to_land_id=None,
     )
     errors = validate(instance=project)
