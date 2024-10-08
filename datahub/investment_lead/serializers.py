96e71d58824b90e9889926e97ad2925965e43e9f
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-10-08 08:05:30 +0100

    Separate EYB triage, user, and marketing data into own serializers

--- datahub/investment_lead/serializers.py
+++ datahub/investment_lead/serializers.py
@@ -80,7 +80,7 @@ ADDRESS_FIELDS = [
     'address_postcode',
 ]
 
-UTM_FIELDS = [
+MARKETING_FIELDS = [
     'utm_name',
     'utm_source',
     'utm_medium',
@@ -88,42 +88,46 @@ UTM_FIELDS = [
 ]
 
 ALL_FIELDS = ARCHIVABLE_FIELDS + INVESTMENT_LEAD_BASE_FIELDS + \
-    TRIAGE_FIELDS + USER_FIELDS + UTM_FIELDS
-
-UUIDS_ERROR_MESSAGE = 'Invalid serializer data: UUIDs must match.'
+    TRIAGE_FIELDS + USER_FIELDS + MARKETING_FIELDS
 
 
 class BaseEYBLeadSerializer(serializers.ModelSerializer):
-    """Base serializer for an EYB lead object."""
+    """Base serializer for an EYB lead object.
+
+    EYB serves data from 3 endpoints: triage, user, and marketing.
+    However, in Data Hub, we combine them into one EYB lead model instance.
+    """
 
     class Meta:
         model = EYBLead
-        fields = ALL_FIELDS
-
-    def validate(self, data):
-        if data['triage_hashed_uuid'] != data['user_hashed_uuid']:
-            raise serializers.ValidationError(UUIDS_ERROR_MESSAGE)
-        return data
+        fields = []
 
 
-class CreateEYBLeadSerializer(BaseEYBLeadSerializer):
-    """Serializer for creating an EYB lead.
-
-    This serializer has been tailored to the data we expect to receive from EYB/DataFlow.
-    Most notably, this includes string representations of related model instances.
-    """
+class CreateEYBLeadTriageSerializer(BaseEYBLeadSerializer):
+    """Serializer for creating an EYB lead from triage data."""
 
     class Meta(BaseEYBLeadSerializer.Meta):
-        fields = [
-            f for f in ALL_FIELDS
-            if f not in ARCHIVABLE_FIELDS + INVESTMENT_LEAD_BASE_FIELDS + ['company']
-        ]
+        fields = TRIAGE_FIELDS
+        extra_kwargs = {
+            'triage_hashed_uuid': {'required': True},
+            'triage_created': {'required': True},
+            'triage_modified': {'required': True},
+            'sector': {'required': True},
+            'intent': {'required': True},
+            'intent_other': {'required': True},
+            'location': {'required': True},
+            'location_city': {'required': True},
+            'location_none': {'required': True},
+            'hiring': {'required': True},
+            'spend': {'required': True},
+            'spend_other': {'required': True},
+            'is_high_value': {'required': True},
+        }
 
     sector = serializers.CharField()
     location = serializers.CharField()
-    address_country = serializers.CharField()
 
-    # TODO: Refactor these field validation functions as they involve 3 db hits per lead
+    # TODO: Refactor these field validation functions as they involve 2 db hits per record
     def validate_sector(self, value):
         if not Sector.objects.filter(segment=value.capitalize()).exists():
             raise serializers.ValidationError(f'Sector "{value}" does not exist.')
@@ -134,20 +138,11 @@ class CreateEYBLeadSerializer(BaseEYBLeadSerializer):
             raise serializers.ValidationError(f'Location "{value}" does not exist.')
         return value
 
-    def validate_address_country(self, value):
-        if not Country.objects.filter(iso_alpha2_code=value.upper()).exists():
-            raise serializers.ValidationError(
-                f'Address country ISO code "{value}" does not exist.',
-            )
-        return value
-
     def to_representation(self, instance):
         """Convert model instance to built-in Python (JSON friendly) data types."""
         related_fields = {
             'sector': instance.sector.segment if instance.sector else None,
             'location': instance.location.name if instance.location else None,
-            'address_country': instance.address_country.iso_alpha2_code
-            if instance.address_country else None,
         }
         rep = super().to_representation(instance)
         rep.update(related_fields)
@@ -158,12 +153,12 @@ class CreateEYBLeadSerializer(BaseEYBLeadSerializer):
         # Extract related field strings
         sector_segment = data.get('sector', None)
         location_name = data.get('location', None)
-        address_country_iso_code = data.get('address_country', None)
 
         validated_data = super().to_internal_value(data)
 
         # Convert strings to model instances for related fields
-        # TODO: Investigate refactoring this logic to involve less db hits (currently 3 per lead)
+        # TODO: Investigate refactoring this logic to involve less db hits (currently 2 per record)
+        # TODO: Handle case where segment returns multiple sectors (e.g. Sensors)
         if sector_segment:
             validated_data['sector'] = Sector.objects.get(
                 segment=sector_segment.capitalize(),
@@ -172,12 +167,82 @@ class CreateEYBLeadSerializer(BaseEYBLeadSerializer):
             validated_data['location'] = UKRegion.objects.get(
                 name=location_name.title(),
             )
+        return validated_data
+
+
+class CreateEYBLeadUserSerializer(BaseEYBLeadSerializer):
+    """Serializer for creating an EYB lead from user data."""
+
+    class Meta(BaseEYBLeadSerializer.Meta):
+        fields = USER_FIELDS
+        extra_kwargs = {
+            'user_hashed_uuid': {'required': True},
+            'user_created': {'required': True},
+            'user_modified': {'required': True},
+            'company_name': {'required': True},
+            'duns_number': {'required': True},
+            'address_1': {'required': True},
+            'address_2': {'required': True},
+            'address_town': {'required': True},
+            'address_country': {'required': True},
+            'address_postcode': {'required': True},
+            'company_website': {'required': True},
+            'full_name': {'required': True},
+            'role': {'required': True},
+            'email': {'required': True},
+            'telephone_number': {'required': True},
+            'agree_terms': {'required': True},
+            'agree_info_email': {'required': True},
+            'landing_timeframe': {'required': True},
+        }
+
+    address_country = serializers.CharField()
+
+    # TODO: Refactor these field validation functions as they involve 1 db hit per record
+    def validate_address_country(self, value):
+        if not Country.objects.filter(iso_alpha2_code=value.upper()).exists():
+            raise serializers.ValidationError(
+                f'Address country ISO code "{value}" does not exist.',
+            )
+        return value
+
+    def to_representation(self, instance):
+        """Convert model instance to built-in Python (JSON friendly) data types."""
+        related_fields = {
+            'address_country': instance.address_country.iso_alpha2_code
+            if instance.address_country else None,
+        }
+        rep = super().to_representation(instance)
+        rep.update(related_fields)
+        return rep
+
+    def to_internal_value(self, data):
+        """Convert unvalidated JSON data to validated data."""
+        # Extract related field strings
+        address_country_iso_code = data.get('address_country', None)
+
+        validated_data = super().to_internal_value(data)
+
+        # Convert strings to model instances for related fields
+        # TODO: Investigate refactoring this logic to involve less db hits (currently 1 per record)
         if address_country_iso_code:
             validated_data['address_country'] = Country.objects.get(
                 iso_alpha2_code=address_country_iso_code.upper(),
             )
-
         return validated_data
+
+
+class CreateEYBLeadMarketingSerializer(BaseEYBLeadSerializer):
+    """Serializer for creating an EYB lead from marketing data."""
+
+    class Meta(BaseEYBLeadSerializer.Meta):
+        fields = MARKETING_FIELDS
+        extra_kwargs = {
+            'utm_name': {'required': True},
+            'utm_source': {'required': True},
+            'utm_medium': {'required': True},
+            'utm_content': {'required': True},
+        }
 
 
 class RetrieveEYBLeadSerializer(BaseEYBLeadSerializer):
