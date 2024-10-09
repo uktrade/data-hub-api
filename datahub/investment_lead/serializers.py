9e359b49afafa80593a7558ce78f5a9a150a31d1
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2024-10-09 12:20:57 +0100

    Amend EYBLead serializers according to new schema

--- datahub/investment_lead/serializers.py
+++ datahub/investment_lead/serializers.py
@@ -6,6 +6,7 @@ from datahub.core.serializers import (
     NestedRelatedField,
 )
 from datahub.investment_lead.models import EYBLead
+from datahub.investment_lead.test.conftest import get_segments_from_sector_instance
 from datahub.metadata.models import (
     Country,
     Sector,
@@ -107,41 +108,94 @@ class CreateEYBLeadTriageSerializer(BaseEYBLeadSerializer):
     """Serializer for creating an EYB lead from triage data."""
 
     class Meta(BaseEYBLeadSerializer.Meta):
-        fields = TRIAGE_FIELDS
-        extra_kwargs = {
-            'triage_hashed_uuid': {'required': True},
-            'triage_created': {'required': True},
-            'triage_modified': {'required': True},
-            'sector': {'required': True},
-            'intent': {'required': True},
-            'intent_other': {'required': True},
-            'location': {'required': True},
-            'location_city': {'required': True},
-            'location_none': {'required': True},
-            'hiring': {'required': True},
-            'spend': {'required': True},
-            'spend_other': {'required': True},
-            'is_high_value': {'required': True},
-        }
+        fields = [
+            'hashedUuid',
+            'created',
+            'modified',
+            'sector',
+            'sectorSub',
+            'sectorSubSub',
+            'intent',
+            'intentOther',
+            'location',
+            'locationCity',
+            'locationNone',
+            'hiring',
+            'spend',
+            'spendOther',
+            'isHighValue',
+        ]
 
-    sector = serializers.CharField()
-    location = serializers.CharField()
-
-    # TODO: Refactor these field validation functions as they involve 2 db hits per record
-    def validate_sector(self, value):
-        if not Sector.objects.filter(segment=value.capitalize()).exists():
-            raise serializers.ValidationError(f'Sector "{value}" does not exist.')
-        return value
+    hashedUuid = serializers.CharField(source='triage_hashed_uuid', required=True)  # noqa: N815
+    created = serializers.DateTimeField(source='triage_created', required=True)
+    modified = serializers.DateTimeField(source='triage_modified', required=True)
+    sector = serializers.CharField(required=True)
+    sectorSub = serializers.CharField(required=False, allow_null=True)  # noqa: N815
+    sectorSubSub = serializers.CharField(required=False, allow_null=True)  # noqa: N815
+    # Can't use MultipleChoiceField here as it returns a set rather than a list and raises db error
+    intent = serializers.ListField(required=True)
+    intentOther = serializers.CharField(  # noqa: N815
+        source='intent_other', required=True, allow_blank=True,
+    )
+    location = serializers.CharField(required=True)
+    locationCity = serializers.CharField(source='location_city', required=True)  # noqa: N815
+    locationNone = serializers.BooleanField(source='location_none', required=True)  # noqa: N815
+    hiring = serializers.ChoiceField(
+        required=True, choices=EYBLead.HiringChoices.choices,
+    )
+    spend = serializers.ChoiceField(
+        required=True, choices=EYBLead.SpendChoices.choices,
+    )
+    spendOther = serializers.CharField(  # noqa: N815
+        source='spend_other', required=True, allow_blank=True,
+    )
+    isHighValue = serializers.BooleanField(source='is_high_value', required=True)  # noqa: N815
 
     def validate_location(self, value):
         if not UKRegion.objects.filter(name=value.title()).exists():
             raise serializers.ValidationError(f'Location "{value}" does not exist.')
         return value
 
+    def _get_sector_instance_from_segments(
+        self,
+        level_zero_segment,
+        level_one_segment,
+        level_two_segment,
+    ):
+        """Looks up and returns a sector instance given the individual level segments."""
+        # Determine selected sector and parent name
+        if level_two_segment:
+            selected_sector_segment = level_two_segment
+            parent_sector_segment = level_one_segment
+        elif level_one_segment:
+            selected_sector_segment = level_one_segment
+            parent_sector_segment = level_zero_segment
+        elif level_zero_segment:
+            selected_sector_segment = level_zero_segment
+            parent_sector_segment = None
+
+        # Filter based on found names
+        queryset = Sector.objects.filter(segment=selected_sector_segment)
+        if parent_sector_segment is not None:
+            queryset.filter(parent__segment=parent_sector_segment)
+
+        # Raise validation error if no sectors matched
+        if not queryset.exists():
+            full_name = ' : '.join([
+                segment for segment in [level_zero_segment, level_one_segment, level_two_segment]
+                if segment is not None
+            ])
+            raise serializers.ValidationError(f'Sector "{full_name}" does not exist.')
+        return queryset.first()
+
     def to_representation(self, instance):
         """Convert model instance to built-in Python (JSON friendly) data types."""
+        level_zero_segment, level_one_segment, level_two_segment = \
+            get_segments_from_sector_instance(instance.sector)
         related_fields = {
-            'sector': instance.sector.segment if instance.sector else None,
+            'sector': level_zero_segment,
+            'sectorSub': level_one_segment,
+            'sectorSubSub': level_two_segment,
             'location': instance.location.name if instance.location else None,
         }
         rep = super().to_representation(instance)
@@ -150,19 +204,24 @@ class CreateEYBLeadTriageSerializer(BaseEYBLeadSerializer):
 
     def to_internal_value(self, data):
         """Convert unvalidated JSON data to validated data."""
-        # Extract related field strings
-        sector_segment = data.get('sector', None)
+        # Extract strings from incoming JSON for related fields
+        # Using .get() here as field is required by serializer/model
+        level_zero_segment = data.get('sector', None)
+        # Using .pop() here otherwise error is raised when calling serializer.save()
+        level_one_segment = data.pop('sectorSub', None)
+        level_two_segment = data.pop('sectorSubSub', None)
+
         location_name = data.get('location', None)
 
+        # Convert and validate rest of the data
         validated_data = super().to_internal_value(data)
 
-        # Convert strings to model instances for related fields
-        # TODO: Investigate refactoring this logic to involve less db hits (currently 2 per record)
-        # TODO: Handle case where segment returns multiple sectors (e.g. Sensors)
-        if sector_segment:
-            validated_data['sector'] = Sector.objects.get(
-                segment=sector_segment.capitalize(),
-            )
+        # Convert strings to model instances for related fields and overwrite validated data
+        validated_data['sector'] = self._get_sector_instance_from_segments(
+            level_zero_segment,
+            level_one_segment,
+            level_two_segment,
+        )
         if location_name:
             validated_data['location'] = UKRegion.objects.get(
                 name=location_name.title(),
@@ -174,31 +233,52 @@ class CreateEYBLeadUserSerializer(BaseEYBLeadSerializer):
     """Serializer for creating an EYB lead from user data."""
 
     class Meta(BaseEYBLeadSerializer.Meta):
-        fields = USER_FIELDS
-        extra_kwargs = {
-            'user_hashed_uuid': {'required': True},
-            'user_created': {'required': True},
-            'user_modified': {'required': True},
-            'company_name': {'required': True},
-            'duns_number': {'required': True},
-            'address_1': {'required': True},
-            'address_2': {'required': True},
-            'address_town': {'required': True},
-            'address_country': {'required': True},
-            'address_postcode': {'required': True},
-            'company_website': {'required': True},
-            'full_name': {'required': True},
-            'role': {'required': True},
-            'email': {'required': True},
-            'telephone_number': {'required': True},
-            'agree_terms': {'required': True},
-            'agree_info_email': {'required': True},
-            'landing_timeframe': {'required': True},
-        }
+        fields = [
+            'hashedUuid',
+            'created',
+            'modified',
+            'companyName',
+            'dunsNumber',
+            'addressLine1',
+            'addressLine2',
+            'town',
+            'companyLocation',
+            'postcode',
+            'companyWebsite',
+            'fullName',
+            'role',
+            'email',
+            'telephoneNumber',
+            'agreeTerms',
+            'agreeInfoEmail',
+            'landingTimeframe',
+        ]
 
-    address_country = serializers.CharField()
+    hashedUuid = serializers.CharField(source='user_hashed_uuid', required=True)  # noqa: N815
+    created = serializers.DateTimeField(source='user_created', required=True)
+    modified = serializers.DateTimeField(source='user_modified', required=True)
+    companyName = serializers.CharField(source='company_name', required=True)  # noqa: N815
+    dunsNumber = serializers.CharField(source='duns_number', required=True)  # noqa: N815
+    addressLine1 = serializers.CharField(source='address_1', required=True)  # noqa: N815
+    addressLine2 = serializers.CharField(source='address_2', required=False)  # noqa: N815
+    town = serializers.CharField(source='address_town', required=True)
+    companyLocation = serializers.CharField(source='address_country', required=True)  # noqa: N815
+    postcode = serializers.CharField(source='address_postcode', required=True)
+    companyWebsite = serializers.CharField(source='company_website', required=True)  # noqa: N815
+    fullName = serializers.CharField(source='full_name', required=True)  # noqa: N815
+    role = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    telephoneNumber = serializers.CharField(source='telephone_number', required=True)  # noqa: N815
+    agreeTerms = serializers.BooleanField(source='agree_terms', required=True)  # noqa: N815
+    agreeInfoEmail = serializers.BooleanField(  # noqa: N815
+        source='agree_info_email', required=True,
+    )
+    landingTimeframe = serializers.ChoiceField(  # noqa: N815
+        source='landing_timeframe',
+        required=True,
+        choices=EYBLead.LandingTimeframeChoices.choices,
+    )
 
-    # TODO: Refactor these field validation functions as they involve 1 db hit per record
     def validate_address_country(self, value):
         if not Country.objects.filter(iso_alpha2_code=value.upper()).exists():
             raise serializers.ValidationError(
@@ -209,7 +289,7 @@ class CreateEYBLeadUserSerializer(BaseEYBLeadSerializer):
     def to_representation(self, instance):
         """Convert model instance to built-in Python (JSON friendly) data types."""
         related_fields = {
-            'address_country': instance.address_country.iso_alpha2_code
+            'companyLocation': instance.address_country.iso_alpha2_code
             if instance.address_country else None,
         }
         rep = super().to_representation(instance)
@@ -218,13 +298,13 @@ class CreateEYBLeadUserSerializer(BaseEYBLeadSerializer):
 
     def to_internal_value(self, data):
         """Convert unvalidated JSON data to validated data."""
-        # Extract related field strings
-        address_country_iso_code = data.get('address_country', None)
+        # Extract strings from incoming JSON for related fields
+        address_country_iso_code = data.get('companyLocation', None)
 
+        # Convert and validate rest of the data
         validated_data = super().to_internal_value(data)
 
-        # Convert strings to model instances for related fields
-        # TODO: Investigate refactoring this logic to involve less db hits (currently 1 per record)
+        # Convert strings to model instances for related fields and overwrite validated data
         if address_country_iso_code:
             validated_data['address_country'] = Country.objects.get(
                 iso_alpha2_code=address_country_iso_code.upper(),
