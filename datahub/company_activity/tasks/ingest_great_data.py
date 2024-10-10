94a87c34bf8c6302ea1f935b39a2c7a597180b32
baarkerlounger <daniel.baark@digital.trade.gov.uk>
2024-10-10 10:38:52 +0100

    Fix date format

--- datahub/company_activity/tasks/ingest_great_data.py
+++ datahub/company_activity/tasks/ingest_great_data.py
@@ -13,7 +13,7 @@ from datahub.metadata.models import Country
 logger = logging.getLogger(__name__)
 env = environ.Env()
 REGION = env('AWS_DEFAULT_REGION', default='eu-west-2')
-DATE_FORMAT = '%Y-%m-%d %H:%M:%S.%f'
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 
 def ingest_great_data(bucket, file):
