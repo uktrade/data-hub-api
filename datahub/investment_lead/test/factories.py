a437ebac086c4faf4f9ed76647763a94df71a30e
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-10-10 16:34:53 +0100

    Linting

--- datahub/investment_lead/test/factories.py
+++ datahub/investment_lead/test/factories.py
@@ -10,7 +10,6 @@ from datahub.company.test.factories import CompanyFactory
 from datahub.core import constants
 from datahub.core.test.factories import to_many_field
 from datahub.investment_lead.models import EYBLead
-from datahub.investment.project.test.factories import InvestmentProjectFactory
 from datahub.metadata.models import Sector
 
 
