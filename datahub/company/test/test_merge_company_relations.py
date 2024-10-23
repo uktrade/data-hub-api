e495b809c0d790582c0060cd4908875aecc79cad
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-10-23 09:44:24 +0100

    Add test to ensuring all models related to a company are considered in the company merge

---
+++ datahub/company/test/test_merge_company_relations.py
@@ -0,0 +1,57 @@
+import pytest
+
+from datahub.company.merge_company import (
+    ALLOWED_RELATIONS_FOR_MERGING,
+)
+from datahub.company.models import Company
+from datahub.core.model_helpers import get_related_fields
+
+
+@pytest.mark.django_db
+class TestDuplicateCompanyMerger:
+    """Tests DuplicateCompanyMerger."""
+
+    def test_company_fields_are_setup_for_merging(self):
+        """
+        Test all the models related to a company are accounted for when merging
+        companies. This is used by support to merge duplicate companies and
+        breaks when relations are not accounted for.
+
+        If your test fails here, you may have related something to a company and this relation
+        needs to be accounted for.
+
+        If this relations is to be merged, add it to both:
+        ```
+            ALLOWED_RELATIONS_FOR_MERGING
+            MERGE_CONFIGURATION
+        ```
+
+        If the relation is NOT to be merged, it can ignored by adding it ONLY to:
+        ```
+            ALLOWED_RELATIONS_FOR_MERGING
+        ```
+
+        You will also need to add your relation to the `base_expected_result` inside
+        `TestDuplicateCompanyMerger` and then add any of your own tests.
+
+        Models for company merge also require the reversion decorator to undo the changes
+        if the merge fails:
+        ```
+            from datahub.core import reversion
+
+            @reversion.register_base_model()
+            class ModelRelatedToACompany(models.Model):
+        ```
+
+        To get the full error message, you may be better to run this test with
+        the -s flag.
+        """
+        relations = get_related_fields(Company)
+        for relation in relations:
+            assert relation.remote_field in ALLOWED_RELATIONS_FOR_MERGING, [(
+                'Required relationship missing from company merge. \n'
+                f'Field: {relation.field.name} \n'
+                f'Model: {relation.model} \n'
+                f'Related Name: {relation.related_name} \n'
+                f'Related Model: {relation.related_model} \n'
+            )]
