f4531bc07e29c7cd9d37b1a49e99cc361f5e424f
Sophie Wenban <74198488+swenban@users.noreply.github.com>
2024-10-31 12:06:39 +0000

    CLS2-976 Add queryset to enable EYB leads to be filtered by country (#5691)
    * Add queryset to enable EYB leads to be filtered  by country
    
    * Test filtering eyb leads by one country
    
    * Fix flake8 issues
    
    * Fix flake8 trailing comma issue
    
    * Finalise with tests
    
    * Flake8
    
    ---------
    
    Co-authored-by: Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>

--- datahub/investment_lead/test/test_views.py
+++ datahub/investment_lead/test/test_views.py
@@ -9,7 +9,7 @@ from datahub.core.test_utils import APITestMixin
 from datahub.investment_lead.models import EYBLead
 from datahub.investment_lead.test.factories import EYBLeadFactory
 from datahub.investment_lead.test.utils import assert_retrieved_eyb_lead_data
-from datahub.metadata.models import Sector
+from datahub.metadata.models import Country, Sector
 
 
 EYB_LEAD_COLLECTION_URL = reverse('api-v4:investment-lead:eyb-lead-collection')
@@ -228,5 +228,74 @@ class TestEYBLeadListAPI(APITestMixin):
         assert response.data['count'] == 3
 
         response = api_client.get(EYB_LEAD_COLLECTION_URL, data={'value': 'invalid_value'})
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 0
+
+    def test_filter_by_country(self, test_user_with_view_permissions):
+        """Test filtering EYB leads by one country."""
+        default_country = Country.objects.get(pk=constants.Country.france.value.id)
+        unrelated_country = Country.objects.get(pk=constants.Country.greece.value.id)
+        EYBLeadFactory(address_country_id=default_country.id)
+        EYBLeadFactory(address_country_id=unrelated_country.id)
+
+        api_client = self.create_api_client(user=test_user_with_view_permissions)
+        response = api_client.get(EYB_LEAD_COLLECTION_URL)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 2
+
+        response = api_client.get(EYB_LEAD_COLLECTION_URL, data={'country': default_country.pk})
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+        country_ids_in_results = set(
+            [lead['address']['country']['id'] for lead in response.data['results']],
+        )
+        assert {str(default_country.pk)} == country_ids_in_results
+
+    def test_filter_by_multiple_countries(self, test_user_with_view_permissions):
+        """Test filtering EYB leads by multiple countries."""
+        france_country = Country.objects.get(pk=constants.Country.france.value.id)
+        greece_country = Country.objects.get(pk=constants.Country.greece.value.id)
+        canada_country = Country.objects.get(pk=constants.Country.canada.value.id)
+        italy_country = Country.objects.get(pk=constants.Country.italy.value.id)
+        japan_country = Country.objects.get(pk=constants.Country.japan.value.id)
+
+        EYBLeadFactory(address_country_id=france_country.id)
+        EYBLeadFactory(address_country_id=greece_country.id)
+        EYBLeadFactory(address_country_id=canada_country.id)
+        EYBLeadFactory(address_country_id=italy_country.id)
+        EYBLeadFactory(address_country_id=japan_country.id)
+
+        api_client = self.create_api_client(user=test_user_with_view_permissions)
+        response = api_client.get(EYB_LEAD_COLLECTION_URL)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 5
+
+        response = api_client.get(EYB_LEAD_COLLECTION_URL, data={
+            'country': [france_country.pk, greece_country.pk],
+        })
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 2
+        country_ids_in_results = set(
+            [lead['address']['country']['id'] for lead in response.data['results']],
+        )
+        assert {
+            str(france_country.pk),
+            str(greece_country.pk),
+        } == country_ids_in_results
+
+    def test_filter_by_non_existing_country(self, test_user_with_view_permissions):
+        """Test filtering EYB leads by non existent country is handled without error."""
+        non_existing_country_uuid = uuid.uuid4()
+        country = Country.objects.get(pk=constants.Country.france.value.id)
+        EYBLeadFactory(address_country_id=country.id)
+
+        api_client = self.create_api_client(user=test_user_with_view_permissions)
+        response = api_client.get(EYB_LEAD_COLLECTION_URL)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['count'] == 1
+
+        response = api_client.get(
+            EYB_LEAD_COLLECTION_URL, data={'country': str(non_existing_country_uuid)},
+        )
         assert response.status_code == status.HTTP_200_OK
         assert response.data['count'] == 0
