981623f138f1b8bdb72c34a8df183094eed2f011
Samuele Mattiuzzo <1381563+samuele-mattiuzzo@users.noreply.github.com>
2024-11-29 10:03:33 +0000

    CLS2-1069 Fix marketing data ingestion bug (#5830)
    * Fixes a bug that affects the marketing data ingestion task
    
    * Test

--- datahub/investment_lead/tasks/ingest_eyb_marketing.py
+++ datahub/investment_lead/tasks/ingest_eyb_marketing.py
@@ -51,10 +51,10 @@ class EYBMarketingDataIngestionTask(BaseEYBDataIngestionTask):
     """Long running job to read the marketing file contents and ingest the records."""
 
     def _get_hashed_uuid(self, obj):
-        return obj.get('hashed_uuid', None)
+        return None if obj is None else obj.get('hashed_uuid', None)
 
     def _record_has_no_changes(self, record):
-        hashed_uuid = self._get_hashed_uuid(record['object'])
+        hashed_uuid = self._get_hashed_uuid(record.get('object'))
         if hashed_uuid and EYBLead.objects.filter(marketing_hashed_uuid=hashed_uuid).exists():
             return True
         return False
