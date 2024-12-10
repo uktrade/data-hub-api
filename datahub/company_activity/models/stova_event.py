9876725a49daf7ae58fbd886ec6d4c66525c8c60
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2024-12-10 15:02:59 +0000

    Rename files to match singular StovaEvent model

--- datahub/company_activity/models/stova_event.py
+++ datahub/company_activity/models/stova_event.py
@@ -1,7 +1,10 @@
 import uuid
 
 from django.conf import settings
-from django.db import models
+from django.db import models, transaction
+
+from datahub.event.models import Event, EventType
+from datahub.metadata.utils import get_country_by_country_name
 
 MAX_LENGTH = settings.CHAR_FIELD_MAX_LENGTH
 
@@ -54,3 +57,41 @@ class StovaEvent(models.Model):
     folder_id = models.IntegerField(null=True, blank=True)
     default_language = models.CharField(max_length=MAX_LENGTH)
     standard_currency = models.CharField(max_length=MAX_LENGTH)
+
+    def save(self, *args, **kwargs) -> None:
+        """Overwritten to create/update a DataHub event from data in a Stova Event."""
+        with transaction.atomic():
+            super().save(*args, **kwargs)
+            self.create_or_update_datahub_event()
+
+    def create_or_update_datahub_event(self) -> Event:
+        return Event.objects.update_or_create(
+            stova_event_id=self.id,
+            defaults={
+                'name': self.name,
+                'event_type_id': self.get_or_create_stova_event_type().id,
+                'start_date': self.start_date.date() if self.start_date else '',
+                'end_date': self.end_date.date() if self.end_date else '',
+                'address_1': self.location_address1,
+                'address_2': self.location_address2,
+                'address_town': self.location_city,
+                'address_county': self.location_state,
+                'address_postcode': self.location_postcode,
+                'address_country': get_country_by_country_name(self.name, 'GB'),
+                'notes': self.description,
+            }
+        )
+
+    @staticmethod
+    def get_or_create_stova_event_type() -> EventType:
+        """
+        Returns or creates an `EventType`.
+
+        DataHub events require an event type which is not provided by Stova. Therefore, all events
+        ingested from Stova will be given the below event type by default.
+        """
+
+        event_type, _ = EventType.objects.get_or_create(
+            name='Stova - unknown event type',
+        )
+        return event_type
