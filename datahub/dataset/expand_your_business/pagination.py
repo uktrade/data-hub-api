6c9a6a5e4fd1db389bc06c8593578931ad076a15
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2025-01-07 10:07:19 +0000

    CLS2-1113 Make EYB Data Hub data available in Data Workspace (#5881)
    Make Expand Your Business Data Hub records available in Data Workspace

---
+++ datahub/dataset/expand_your_business/pagination.py
@@ -0,0 +1,9 @@
+from datahub.dataset.core.pagination import DatasetCursorPagination
+
+
+class EYBDatasetViewCursorPagination(DatasetCursorPagination):
+    """
+    Cursor Pagination for AdvisersDatasetView
+    """
+
+    ordering = ('modified_on', 'created_on')
