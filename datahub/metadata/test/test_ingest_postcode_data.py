341a596015a9f6593bf306c60a445ddcee0aa68c
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2025-02-18 16:08:40 +0000

    Add test for the processing of a postcode data instance

--- datahub/metadata/test/test_ingest_postcode_data.py
+++ datahub/metadata/test/test_ingest_postcode_data.py
@@ -6,10 +6,17 @@ import pytest
 from moto import mock_aws
 
 from datahub.ingest.boto3 import S3ObjectProcessor
+from datahub.metadata.models import PostcodeData
+from datahub.metadata.serializers import PostcodeDataSerializer
 from datahub.metadata.tasks import (
     postcode_data_identification_task,
     postcode_data_ingestion_task,
     POSTCODE_DATA_PREFIX,
+    PostcodeDataIngestionTask,
+)
+from datahub.metadata.test.factories import (
+    generate_hashed_uuid,
+    postcode_data_record_faker,
 )
 
 pytestmark = pytest.mark.django_db
@@ -21,7 +28,7 @@ def postcode_object_key():
 
 
 @mock_aws
-def test_identification_task_schedules_ingestion_task(self, postcode_object_key, caplog):
+def test_identification_task_schedules_ingestion_task(postcode_object_key, caplog):
     with (
         mock.patch('datahub.ingest.tasks.job_scheduler') as mock_scheduler,
         mock.patch.object(
@@ -34,7 +41,7 @@ def test_identification_task_schedules_ingestion_task(self, postcode_object_key,
 
         assert 'Postcode data identification task started...' in caplog.text
         assert f'Scheduled ingestion of {postcode_object_key}' in caplog.text
-        assert 'Postcode identification task finished.' in caplog.text
+        assert 'Postcode data identification task finished.' in caplog.text
 
     mock_scheduler.assert_called_once_with(
         function=postcode_data_ingestion_task,
@@ -48,4 +55,25 @@ def test_identification_task_schedules_ingestion_task(self, postcode_object_key,
 
 @mock_aws
 class TestPostcodeDataIngestionTask:
-    pass
+
+    @pytest.fixture
+    def ingestion_task(self, postcode_object_key):
+        return PostcodeDataIngestionTask(
+            object_key=postcode_object_key,
+            s3_processor=S3ObjectProcessor(prefix=POSTCODE_DATA_PREFIX),
+            serializer_class=PostcodeDataSerializer,
+        )
+
+    def test_get_hashed_uuid(self, ingestion_task):
+        record = postcode_data_record_faker()
+        assert ingestion_task._get_hashed_uuid(record) == record['hashedUuid']
+
+    def test_process_record_creates_postcode_data_instance(self, ingestion_task):
+        hashed_uuid = generate_hashed_uuid()
+        record = postcode_data_record_faker({'hashed_uuid': hashed_uuid})
+        ingestion_task._process_record(record)
+
+        assert len(ingestion_task.created_ids) == 1
+        assert len(ingestion_task.updated_ids) == 0
+        assert len(ingestion_task.errors) == 0
+        assert PostcodeData.objects.filter(hashed_uuid=hashed_uuid).exists()
