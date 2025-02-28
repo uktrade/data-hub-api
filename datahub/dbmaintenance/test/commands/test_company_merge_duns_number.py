d08dc672e0793b99b68dfb40a4506595abe4f93c
Bau Nguyen <bauvannguyen123@gmail.com>
2025-02-28 16:11:25 +0000

    add logging for company merge command

--- datahub/dbmaintenance/test/commands/test_company_merge_duns_number.py
+++ datahub/dbmaintenance/test/commands/test_company_merge_duns_number.py
@@ -5,7 +5,7 @@ import reversion
 from django.core.management import call_command
 
 from datahub.company.test.factories import CompanyFactory
-
+from datahub.company.test.factories import SubsidiaryFactory
 pytestmark = pytest.mark.django_db
 
 
@@ -49,3 +49,66 @@ def test_merge_id_duns_number(s3_stubber):
     company_4.refresh_from_db()
     assert company_3.transferred_to == company_1
     assert company_4.transferred_to == company_2
+
+
+def test_logs_contain_errors(s3_stubber, caplog):
+    """Tests errors are captured in the logs"""
+    caplog.set_level('INFO')
+    global_company = CompanyFactory()
+    company_source = CompanyFactory(
+        global_headquarters=global_company
+    )
+    company_with_duns = CompanyFactory(duns_number='12345678', archived=True)
+
+    bucket = 'test_bucket'
+    object_key = 'test_key'
+    csv_content = f"""id,duns
+{company_source.id},{company_with_duns.duns_number}
+"""
+
+    s3_stubber.add_response(
+        'get_object',
+        {
+            'Body': BytesIO(csv_content.encode(encoding='utf-8')),
+        },
+        expected_params={
+            'Bucket': bucket,
+            'Key': object_key,
+        },
+    )
+
+    call_command('company_merge_duns_number', bucket, object_key)
+
+    assert 'List of Target Companies Archived: ' in caplog.text
+    assert str(company_with_duns.id) in caplog.text
+    assert 'List of Source Compnies with Global Headqaurters: ' in caplog.text
+    assert str(company_source.id)
+
+
+def test_subsidiary_logs(s3_stubber, caplog):
+    """Tests subsidiary errors are captured in the logs"""
+    caplog.set_level('INFO')
+    subsidiary_company = SubsidiaryFactory()
+    company_with_duns = CompanyFactory(duns_number='12345678')
+
+    bucket = 'test_bucket'
+    object_key = 'test_key'
+    csv_content = f"""id,duns
+{subsidiary_company.id},{company_with_duns.duns_number}
+"""
+
+    s3_stubber.add_response(
+        'get_object',
+        {
+            'Body': BytesIO(csv_content.encode(encoding='utf-8')),
+        },
+        expected_params={
+            'Bucket': bucket,
+            'Key': object_key,
+        },
+    )
+
+    call_command('company_merge_duns_number', bucket, object_key)
+
+    assert 'List of Source Companies with Subsidiaries: ' in caplog.text
+    assert str(subsidiary_company.id) in caplog.text
