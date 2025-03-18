984170f64eb0b249424cfa7ce71a97ef2d0d34a8
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2025-03-18 08:19:50 +0000

    Test schema and doc views

--- datahub/core/test/test_docs.py
+++ datahub/core/test/test_docs.py
@@ -8,17 +8,19 @@ from datahub.core.test_utils import AdminTestMixin, create_test_user, get_admin_
 class TestDocsSwaggerUIView(AdminTestMixin):
     """Test the Swagger UI view."""
 
-    def test_redirects_to_login_page_if_not_logged_in(self, client):
+    @pytest.mark.parametrize('version', ['v1', 'v3', 'v4'])
+    def test_redirects_to_login_page_if_not_logged_in(self, client, version):
         """Test that the view redirects to the login page if the user isn't authenticated."""
-        url = reverse('api-docs:swagger-ui')
+        url = reverse(f'api-{version}:swagger-ui-{version}')
         response = client.get(url)
 
         assert response.status_code == status.HTTP_302_FOUND
         assert response['Location'] == self.login_url_with_redirect(url)
 
-    def test_returns_200_if_authenticated(self, client):
+    @pytest.mark.parametrize('version', ['v1', 'v3', 'v4'])
+    def test_returns_200_if_authenticated(self, client, version):
         """Test that a 200 is returned if the user is authenticated via the admin site."""
-        url = reverse('api-docs:swagger-ui')
+        url = reverse(f'api-{version}:swagger-ui-{version}')
         user = create_test_user(is_staff=True, password=self.PASSWORD)
 
         client = self.create_client(user=user)
@@ -26,11 +28,11 @@ class TestDocsSwaggerUIView(AdminTestMixin):
         assert response.status_code == status.HTTP_200_OK
 
 
-@pytest.mark.django_db
-class TestDocsSchemaView:
+class TestDocsSchemaView(AdminTestMixin):
     """Test the OpenAPI schema view."""
 
-    def test_returns_200_if_logged_in(self, client):
+    @pytest.mark.parametrize('version', ['v1', 'v3', 'v4'])
+    def test_returns_200_if_logged_in(self, client, version):
         """
         Test that a 200 is returned if a user is logged in using session authentication.
 
@@ -40,14 +42,16 @@ class TestDocsSchemaView:
         user = get_admin_user(password=password)
         client.login(username=user.email, password=password)
 
-        url = reverse('api-docs:openapi-schema')
+        url = reverse(f'api-{version}:openapi-schema-{version}')
         response = client.get(url)
 
         assert response.status_code == status.HTTP_200_OK
 
-    def test_returns_403_if_not_logged_in(self, client):
-        """Test that a 403 error is returned if the user is not logged in."""
-        url = reverse('api-docs:openapi-schema')
+    @pytest.mark.parametrize('version', ['v1', 'v3', 'v4'])
+    def test_redirects_to_login_page_if_not_logged_in(self, client, version):
+        """Test that the view redirects to the login page if the user isn't authenticated."""
+        url = reverse(f'api-{version}:openapi-schema-{version}')
         response = client.get(url)
 
-        assert response.status_code == status.HTTP_403_FORBIDDEN
+        assert response.status_code == status.HTTP_302_FOUND
+        assert response['Location'] == self.login_url_with_redirect(url)
