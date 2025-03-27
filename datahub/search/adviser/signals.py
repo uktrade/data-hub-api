e88088ecc8c211c892e50942a8f039506547700a
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-03-27 16:54:43 +0000

    Delete Adviser from Opensearch when deleting from database

--- datahub/search/adviser/signals.py
+++ datahub/search/adviser/signals.py
@@ -1,8 +1,12 @@
 from django.db import transaction
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 
 from datahub.company.models import Advisor as DBAdviser
 from datahub.search.adviser import AdviserSearchApp
+from datahub.search.adviser.models import (
+    Adviser as SearchAdviser,
+)
+from datahub.search.deletion import delete_document
 from datahub.search.signals import SignalReceiver
 from datahub.search.sync_object import sync_object_async
 
@@ -14,4 +18,14 @@ def sync_adviser_to_opensearch(instance):
     )
 
 
-receivers = (SignalReceiver(post_save, DBAdviser, sync_adviser_to_opensearch),)
+def remove_adviser_from_opensearch(instance):
+    """Remove adviser from opensearch."""
+    transaction.on_commit(
+        lambda pk=instance.pk: delete_document(SearchAdviser, pk),
+    )
+
+
+receivers = (
+    SignalReceiver(post_save, DBAdviser, sync_adviser_to_opensearch),
+    SignalReceiver(post_delete, DBAdviser, remove_adviser_from_opensearch),
+)
