8b60465cf1d9af2885022fb8a54ede2a9ccdc2ce
Dean Elliott <dean.elliott@digital.trade.gov.uk>
2025-03-27 16:22:39 +0000

    Delete Contact from Opensearch when deleting from database

--- datahub/search/contact/signals.py
+++ datahub/search/contact/signals.py
@@ -1,8 +1,10 @@
 from django.db import transaction
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 
 from datahub.company.models import Company as DBCompany, Contact as DBContact
 from datahub.search.contact import ContactSearchApp
+from datahub.search.contact.models import Contact as SearchContact
+from datahub.search.deletion import delete_document
 from datahub.search.signals import SignalReceiver
 from datahub.search.sync_object import sync_object_async, sync_related_objects_async
 
@@ -21,7 +23,15 @@ def related_contact_sync_search(instance):
     )
 
 
+def remove_contact_from_opensearch(instance):
+    """Remove contact from opensearch."""
+    transaction.on_commit(
+        lambda pk=instance.pk: delete_document(SearchContact, pk),
+    )
+
+
 receivers = (
     SignalReceiver(post_save, DBContact, contact_sync_search),
     SignalReceiver(post_save, DBCompany, related_contact_sync_search),
+    SignalReceiver(post_delete, DBContact, remove_contact_from_opensearch),
 )
