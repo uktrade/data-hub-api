1141423f7a82c196c9942c29027dd6972fe9657d
Claudia Gonzalez-Casales <claudia.gonzalescasales@digital.trade.gov.uk>
2025-04-02 15:13:50 +0100

    Add advisers to lead model

--- datahub/investment_lead/serializers.py
+++ datahub/investment_lead/serializers.py
@@ -2,6 +2,7 @@ from django.db.models import Q
 from rest_framework import serializers
 
 from datahub.company.models import Company
+from datahub.company.serializers import NestedAdviserField
 from datahub.core.audit import AuditLogField
 from datahub.core.serializers import (
     AddressSerializer,
@@ -585,6 +586,10 @@ class RetrieveEYBLeadSerializer(BaseEYBLeadSerializer):
             'audit_log',
         ]
 
+    advisers = NestedAdviserField(
+        many=True,
+        allow_empty=False,
+    )
     sector = NestedRelatedField(Sector)
     proposed_investment_region = NestedRelatedField(UKRegion)
     address = AddressSerializer(
