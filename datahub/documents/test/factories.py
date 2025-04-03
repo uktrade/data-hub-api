81a07a13ff1dfca773d4e067916fd05ffc91e621
Oliver Roberts <oliver.roberts@digital.trade.gov.uk>
2025-04-03 14:33:54 +0100

    Add UploadableDocument model and factories

--- datahub/documents/test/factories.py
+++ datahub/documents/test/factories.py
@@ -37,10 +37,42 @@ class SharePointDocumentFactory(factory.django.DjangoModelFactory):
         model = 'documents.SharePointDocument'
 
 
+class UploadableDocumentFactory(factory.django.DjangoModelFactory):
+    title = factory.Faker('text', max_nb_chars=20)
+    original_filename = factory.Faker('file_path')
+    document = factory.SubFactory(DocumentFactory)
+    created_by = factory.SubFactory(AdviserFactory)
+    modified_by = factory.SubFactory(AdviserFactory)
+
+    class Meta:
+        model = 'documents.UploadableDocument'
+
+
 class CompanySharePointDocumentFactory(factory.django.DjangoModelFactory):
     """Generates a GenericDocument instance linking a Company to a SharePointDocument."""
 
     document = factory.SubFactory(SharePointDocumentFactory)
+    related_object = factory.SubFactory(CompanyFactory)
+    created_by = factory.SubFactory(AdviserFactory)
+    modified_by = factory.SubFactory(AdviserFactory)
+    archived = False
+
+    class Meta:
+        model = 'documents.GenericDocument'
+
+    @factory.post_generation
+    def sync_created_and_modified_on_document_instance(obj, create, extracted, **kwargs):  # noqa
+        obj.document.created_by = obj.created_by
+        obj.document.modified_by = obj.modified_by
+        obj.document.created_on = obj.created_on
+        obj.document.modified_on = obj.modified_on
+        obj.document.save()
+
+
+class CompanyUploadableDocumentFactory(factory.django.DjangoModelFactory):
+    """Generates a GenericDocument instance linking a Company to an UploadableDocument."""
+
+    document = factory.SubFactory(UploadableDocumentFactory)
     related_object = factory.SubFactory(CompanyFactory)
     created_by = factory.SubFactory(AdviserFactory)
     modified_by = factory.SubFactory(AdviserFactory)
