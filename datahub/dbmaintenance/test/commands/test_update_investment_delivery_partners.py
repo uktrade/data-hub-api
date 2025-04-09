d6278af1e7b0b3d8555e0eae24e89c06025fc246
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2025-04-09 08:45:54 +0100

    linting

--- datahub/dbmaintenance/test/commands/test_update_investment_delivery_partners.py
+++ datahub/dbmaintenance/test/commands/test_update_investment_delivery_partners.py
@@ -1,21 +1,13 @@
 from datetime import datetime, timezone
-from unittest import mock
-from unittest.mock import patch
 
 import pytest
 from django.core.management import call_command
-from django.db import DatabaseError
-from reversion.models import Version
 
-from datahub.company.models import Company, Contact
-from datahub.company.test.factories import (
-    CompanyFactory,
-    ContactFactory,
-)
-from datahub.interaction.models import Interaction
-from datahub.interaction.test.factories import CompanyInteractionFactory
 from datahub.investment.project.models import InvestmentDeliveryPartner, InvestmentProject
-from datahub.investment.project.test.factories import InvestmentProjectFactory, InvestmentDeliveryPartnerFactory
+from datahub.investment.project.test.factories import (
+    InvestmentDeliveryPartnerFactory,
+    InvestmentProjectFactory,
+)
 
 
 @pytest.fixture
@@ -32,7 +24,7 @@ def dpi():
 
 
 def replace_with_fixtures(source, lep, dpi):
-    """Simple replacement as fixtures can't be used in @pytest.mark.parametrize
+    """Simple replacement as fixtures can't be used in @pytest.mark.parametrize.
     """
     result = []
     for value in source:
@@ -61,46 +53,17 @@ class TestUpdateInvestmentDeliveryPartnersCommand:
         ],
     )
     def test_actual_land_date(self, caplog, lep, dpi, actual_land_date, delivery_partners, expected_partners):
-        from pprint import pprint
         delivery_partners = replace_with_fixtures(
             delivery_partners, lep, dpi)
         expected_partners = replace_with_fixtures(
             expected_partners, lep, dpi)
-        pprint(actual_land_date)
-        pprint(delivery_partners)
-        pprint(expected_partners)
 
         investment_project = InvestmentProjectFactory(
             actual_land_date=actual_land_date, delivery_partners=delivery_partners)
 
         call_command('update_investment_delivery_partners')
 
-        pprint(investment_project.__dict__)
         stored_investment_project = InvestmentProject.objects.get(
             pk=investment_project.id)
         assert set([str(value.id) for value in stored_investment_project.delivery_partners.all(
         )]) == set([str(expected_partner.id) for expected_partner in expected_partners])
-
-    def test_idp_added(self, test_base_stova_attendee, simulate, caplog):
-        """Test interactions created by Stova Attendees are removed and interactions not created by
-        stova are not removed.
-        """
-        s3_processor_mock = mock.Mock()
-        task = StovaAttendeeIngestionTask('dummy-prefix', s3_processor_mock)
-        data = test_base_stova_attendee
-        task._process_record(data)
-        data['id'] = 9876
-        task._process_record(data)
-        data['id'] = 8907
-        data['company_name'] = 'a new company'
-        task._process_record(data)
-        CompanyInteractionFactory.create_batch(5)
-
-        assert Interaction.objects.count() == 8
-
-        caplog.set_level('INFO')
-        call_command('remove_stova_relations', simulate=simulate)
-
-        log_text = caplog.text
-        assert 'There were 3 interactions deleted out of 3' in log_text
-        assert Interaction.objects.count() == 5
