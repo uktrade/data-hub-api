835367eaec5c3bd01b59697df00b004b1b073526
Marijn Kampf <marijn.kampf@digital.trade.gov.uk>
2025-04-22 11:15:03 +0100

    Add log and add lep to idp mappings

--- datahub/dbmaintenance/test/commands/test_update_investment_delivery_partners.py
+++ datahub/dbmaintenance/test/commands/test_update_investment_delivery_partners.py
@@ -14,7 +14,6 @@ from datahub.investment.project.test.factories import (
 def lep():
     lep_id = 'e96192bb-09f1-e511-8ffa-e4115bead28a'
     return InvestmentDeliveryPartner.objects.get(pk=lep_id)
-    return InvestmentDeliveryPartnerFactory(id=lep_id)
 
 
 @pytest.fixture
@@ -24,14 +23,14 @@ def dpi():
 
 
 def replace_with_fixtures(source, lep, dpi):
-    """Simple replacement as fixtures can't be used in @pytest.mark.parametrize.
-    """
+    """Simple replacement as fixtures can't be used in @pytest.mark.parametrize."""
     result = []
     for value in source:
-        if value == 'lep':
-            result.append(lep)
-        if value == 'dpi':
-            result.append(dpi)
+        match value:
+            case 'lep':
+                result.append(lep)
+            case 'dpi':
+                result.append(dpi)
     return result
 
 
@@ -45,25 +44,113 @@ class TestUpdateInvestmentDeliveryPartnersCommand:
             # Before 1st April 2024
             (datetime(2024, 3, 1, tzinfo=timezone.utc), ['lep'], ['lep']),
             # On 1st April 2024
-            (datetime(
-                2024, 4, 1, tzinfo=timezone.utc), ['lep'], ['dpi']),
+            (datetime(2024, 4, 1, tzinfo=timezone.utc), ['lep'], ['dpi']),
             # After 1st April 2024
-            (datetime(
-                2025, 4, 1, tzinfo=timezone.utc), ['lep'], ['dpi']),
+            (datetime(2025, 4, 1, tzinfo=timezone.utc), ['lep'], ['dpi']),
         ],
     )
-    def test_actual_land_date(self, caplog, lep, dpi, actual_land_date, delivery_partners, expected_partners):
-        delivery_partners = replace_with_fixtures(
-            delivery_partners, lep, dpi)
-        expected_partners = replace_with_fixtures(
-            expected_partners, lep, dpi)
+    def test_actual_land_date(
+        self,
+        caplog,
+        lep,
+        dpi,
+        actual_land_date,
+        delivery_partners,
+        expected_partners,
+    ):
+        caplog.set_level('INFO')
+        delivery_partners = replace_with_fixtures(delivery_partners, lep, dpi)
+        expected_partners = replace_with_fixtures(expected_partners, lep, dpi)
 
         investment_project = InvestmentProjectFactory(
-            actual_land_date=actual_land_date, delivery_partners=delivery_partners)
+            actual_land_date=actual_land_date,
+            delivery_partners=delivery_partners,
+        )
 
-        call_command('update_investment_delivery_partners')
+        call_command('update_investment_delivery_partners', delete=True)
 
-        stored_investment_project = InvestmentProject.objects.get(
-            pk=investment_project.id)
-        assert set([str(value.id) for value in stored_investment_project.delivery_partners.all(
-        )]) == set([str(expected_partner.id) for expected_partner in expected_partners])
+        stored_investment_project = InvestmentProject.objects.get(pk=investment_project.id)
+        assert set(
+            [str(value.id) for value in stored_investment_project.delivery_partners.all()],
+        ) == set([str(expected_partner.id) for expected_partner in expected_partners])
+
+    @pytest.mark.django_db
+    def test_multiple_delivery_partners(self, caplog, lep, dpi):
+        """Test Investment Project with multiple delivery partners."""
+        caplog.set_level('INFO')
+
+        leps = [
+            lep,
+            InvestmentDeliveryPartner.objects.get(pk='9abd575e-0af1-e511-8ffa-e4115bead28a'),
+            InvestmentDeliveryPartner.objects.get(pk='87b87bf6-9f1a-e511-8e8f-441ea13961e2'),
+        ]
+
+        dpis = [
+            dpi,
+            InvestmentDeliveryPartnerFactory(id='182e76ca-868d-4ca4-a336-17a26719f786'),
+            InvestmentDeliveryPartnerFactory(id='dedd7553-63fe-41cc-874f-740d4cec8f97'),
+        ]
+
+        investment_project = InvestmentProjectFactory(
+            actual_land_date=datetime(2025, 4, 1, tzinfo=timezone.utc),
+            delivery_partners=leps,
+        )
+
+        call_command('update_investment_delivery_partners', simulate=False, delete=True)
+
+        stored_investment_project = InvestmentProject.objects.get(pk=investment_project.id)
+        assert set(
+            [str(value.id) for value in stored_investment_project.delivery_partners.all()],
+        ) == set([str(idp.id) for idp in dpis])
+
+    @pytest.mark.django_db
+    @pytest.mark.parametrize(
+        ('simulate', 'delete', 'caplog_text'),
+        [
+            (
+                False,
+                False,
+                "{'projects': {'count': 0, 'errors': []}, 'leps': {'investment_project_count': 3, 'to_delete': 0, 'deleted': 0, 'errors': []}, 'idps': {'investment_project_count': 0, 'to_add': 3, 'added': 3, 'errors': []}",
+            ),
+            (
+                True,
+                False,
+                "{'projects': {'count': 0, 'errors': []}, 'leps': {'investment_project_count': 3, 'to_delete': 0, 'deleted': 0, 'errors': []}, 'idps': {'investment_project_count': 0, 'to_add': 3, 'added': 0, 'errors': []}",
+            ),
+            (
+                False,
+                True,
+                "{'projects': {'count': 0, 'errors': []}, 'leps': {'investment_project_count': 3, 'to_delete': 3, 'deleted': 3, 'errors': []}, 'idps': {'investment_project_count': 0, 'to_add': 3, 'added': 3, 'errors': []}",
+            ),
+            (
+                True,
+                True,
+                "{'projects': {'count': 0, 'errors': []}, 'leps': {'investment_project_count': 3, 'to_delete': 3, 'deleted': 0, 'errors': []}, 'idps': {'investment_project_count': 0, 'to_add': 3, 'added': 0, 'errors': []}",
+            ),
+        ],
+    )
+    def test_arguments(self, caplog, lep, dpi, simulate, delete, caplog_text):
+        """Test simulate and delete arguments."""
+        caplog.set_level('INFO')
+
+        leps = [
+            lep,
+            InvestmentDeliveryPartner.objects.get(pk='9abd575e-0af1-e511-8ffa-e4115bead28a'),
+            InvestmentDeliveryPartner.objects.get(pk='87b87bf6-9f1a-e511-8e8f-441ea13961e2'),
+            InvestmentDeliveryPartner.objects.get(pk='14ee950e-0bf1-e511-8ffa-e4115bead28a'),
+        ]
+
+        dpis = [
+            dpi,
+            InvestmentDeliveryPartnerFactory(id='182e76ca-868d-4ca4-a336-17a26719f786'),
+            InvestmentDeliveryPartnerFactory(id='dedd7553-63fe-41cc-874f-740d4cec8f97'),
+        ]
+
+        investment_project = InvestmentProjectFactory(
+            actual_land_date=datetime(2025, 4, 1, tzinfo=timezone.utc),
+            delivery_partners=leps,
+        )
+
+        call_command('update_investment_delivery_partners', simulate=simulate, delete=delete)
+
+        assert caplog_text in caplog.text
